1cf2e5b0eb6e9b0306eeb14f4075ed012e23aa69
Ilya Ryzhenkov <orangy@jetbrains.com>
2014-11-21 22:24:57 +0300

    Add tests

--- ktor-core/test/application/ApplicationRequestHeaderSpek.kt
+++ ktor-core/test/application/ApplicationRequestHeaderSpek.kt
@@ -21,14 +21,18 @@ class ApplicationRequestHeaderSpek : Spek() {{
                     it("should return empty string as queryString") {
                         shouldEqual("", queryString())
                     }
-
+                    response { status(HttpStatusCode.OK) }
                 }
             }
 
-            testHost.makeRequest {
+            val status = testHost.makeRequest {
                 uri = "/foo"
                 httpMethod = HttpMethod.Get
                 headers.add(Pair("Authorization", ""))
+            }.response?.status
+
+            it("should handle request") {
+                shouldEqual(HttpStatusCode.OK.value, status)
             }
         }
     }
@@ -61,15 +65,56 @@ class ApplicationRequestHeaderSpek : Spek() {{
                     it("should map port to 8888") {
                         shouldEqual(8888, port())
                     }
+                    response { status(HttpStatusCode.OK) }
                 }
             }
 
-            testHost.makeRequest {
+            val status = testHost.makeRequest {
                 uri = "/foo?key1=value1&key2=value2"
                 httpMethod = HttpMethod.Get
                 headers.add("Host" to "host.name.com:8888")
+            }.response?.status
+
+            it("should handle request") {
+                shouldEqual(HttpStatusCode.OK.value, status)
+            }
+        }
+    }
+
+    given("an application that handles requests to root with parameters") {
+        val testHost = createTestHost()
+        on("making a request to /?key1=value1&key2=value2") {
+            testHost.application.routing {
+                get("/") {
+                    it("shoud map uri to /?key1=value1&key2=value2") {
+                        shouldEqual("/?key1=value1&key2=value2", uri)
+                    }
+                    it("shoud map two parameters key1=value1 and key2=value2") {
+                        val params = queryParameters()
+                        shouldEqual("value1", params["key1"]?.single())
+                        shouldEqual("value2", params["key2"]?.single())
+                    }
+                    it("should map queryString to key1=value1&key2=value2") {
+                        shouldEqual("key1=value1&key2=value2", queryString())
+                    }
+                    it("should map document to empty") {
+                        shouldEqual("", document())
+                    }
+                    it("should map path to empty") {
+                        shouldEqual("/", path())
+                    }
+                    response { status(HttpStatusCode.OK) }
+                }
             }
 
+            val status = testHost.makeRequest {
+                uri = "/?key1=value1&key2=value2"
+                httpMethod = HttpMethod.Get
+            }.response?.status
+
+            it("should handle request") {
+                shouldEqual(HttpStatusCode.OK.value, status)
+            }
         }
     }
 
