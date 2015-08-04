cf921e6c85004d6fcb8daa55b8e9d868778b4d9f
Ilya Ryzhenkov <orangy@jetbrains.com>
2015-08-04 20:26:36 +0300

    Move ComponentApplication to ktor-components

---
+++ ktor-components/src/org/jetbrains/ktor/components/ComponentApplication.kt
@@ -0,0 +1,38 @@
+package org.jetbrains.ktor.components
+
+import org.jetbrains.ktor.application.*
+import org.jetbrains.ktor.components.*
+import org.jetbrains.ktor.routing.*
+import kotlin.util.*
+
+public class ComponentApplication(config: ApplicationConfig) : Application(config) {
+    val container = StorageComponentContainer("Application")
+
+    init {
+        container.registerInstance(this)
+        container.registerInstance(config)
+        container.registerInstance(config.log)
+        container.registerInstance(config.classLoader)
+
+        val routing = Routing()
+        container.registerInstance(routing)
+
+        val introspectionTime = measureTimeMillis {
+            config.classLoader
+                    .scanForClasses("")
+                    .filter { it.getAnnotation(javaClass<component>()) != null }
+                    .forEach { container.registerSingleton(it) }
+        }
+        config.log.info("Introspection took $introspectionTime ms")
+
+        val compositionTime = measureTimeMillis {
+            container.compose()
+        }
+        config.log.info("Composition took $compositionTime ms")
+
+        interceptRoute(routing)
+    }
+}
+
+annotation(AnnotationRetention.RUNTIME)
+public class component
