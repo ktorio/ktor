01af7e8e5083db7dfc9849198f719dd00fce22b0
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2015-09-08 20:48:17 +0300

    Initial OAuth implementation

--- ktor-core/src/org/jetbrains/ktor/http/HttpUrlEncoded.kt
+++ ktor-core/src/org/jetbrains/ktor/http/HttpUrlEncoded.kt
@@ -3,13 +3,35 @@ package org.jetbrains.ktor.http
 import org.jetbrains.ktor.application.*
 import org.jetbrains.ktor.util.*
 import java.net.*
+import java.nio.charset.*
 
-public fun ApplicationRequest.parseUrlEncodedParameters(): ValuesMap {
-    val parameters = content.get<String>().split("&").map { it.substringBefore("=") to it.substringAfter("=", "") }
-    val encoding = contentCharset?.name() ?: parameters.firstOrNull { it.first == "_charset_" }?.second ?: "UTF-8"
+public fun ApplicationRequest.parseUrlEncodedParameters(): ValuesMap =
+    content.get<String>().parseUrlEncodedParameters(contentCharset ?: Charsets.UTF_8)
+
+public fun String.parseUrlEncodedParameters(defaultEncoding: Charset = Charsets.UTF_8): ValuesMap {
+    val parameters = split("&").map { it.substringBefore("=") to it.substringAfter("=", "") }
+    val encoding = parameters.firstOrNull { it.first == "_charset_" }?.second ?: defaultEncoding.name()
 
     return parameters.fold(ValuesMap.Builder()) { builder, pair ->
         builder.append(URLDecoder.decode(pair.first, encoding), URLDecoder.decode(pair.second, encoding))
         builder
     }.build()
+}
+
+public fun List<Pair<String, *>>.formUrlEncode() =
+        StringBuilder {
+            formUrlEncodeTo(this)
+        }.toString()
+
+public fun List<Pair<String, *>>.formUrlEncodeTo(out: Appendable) {
+    joinTo(out, "&") { "${it.first.encodeURL()}=${it.second.toString().encodeURL()}" }
+}
+
+public fun ValuesMap.formUrlEncode() =
+        entries().flatMap { e -> e.value.map { e.key to e.value } }
+        .formUrlEncode()
+
+public fun ValuesMap.formUrlEncodeTo(out: Appendable) {
+    entries().flatMap { e -> e.value.map { e.key to e.value } }
+            .formUrlEncodeTo(out)
 }
---
+++ ktor-samples/ktor-samples-oauth/src/org/jetbrains/ktor/samples/locations/OAuthLoginApplication.kt
@@ -0,0 +1,199 @@
+package org.jetbrains.ktor.samples.locations
+
+import kotlinx.html.*
+import kotlinx.html.stream.*
+import org.jetbrains.ktor.application.*
+import org.jetbrains.ktor.http.*
+import org.jetbrains.ktor.http.auth.oauth.*
+import org.jetbrains.ktor.locations.*
+import org.jetbrains.ktor.routing.*
+import java.util.concurrent.*
+
+@location("/") class index()
+@location("/login/{type?}") class login(val type: String = "")
+
+/**
+ * DISCLAIMER
+ *
+ * The constants above for only demo purposes. You should NEVER keep secret keys in the source code
+ *  but keep them safe externally.
+ *  You also can keep them encrypted but in this case you have to keep encryption key safe
+ *
+ *  Also you SHOULD ALWAYS use HTTPS with OAuth2
+ */
+val loginProviders = listOf(
+        OAuthServerSettings.OAuth1aServerSettings(
+                name = "twitter",
+                requestTokenUrl = "https://api.twitter.com/oauth/request_token",
+                authorizeUrl = "https://api.twitter.com/oauth/authorize",
+                accessTokenUrl = "https://api.twitter.com/oauth/access_token",
+
+                consumerKey = "***",
+                consumerSecret = "***"
+        ),
+        OAuthServerSettings.OAuth2ServerSettings(
+                name = "vk",
+                authorizeUrl = "https://oauth.vk.com/authorize",
+                accessTokenUrl = "https://oauth.vk.com/access_token",
+                clientId = "***",
+                clientSecret = "***"
+        ),
+        OAuthServerSettings.OAuth2ServerSettings(
+                name = "github",
+                authorizeUrl = "https://github.com/login/oauth/authorize",
+                accessTokenUrl = "https://github.com/login/oauth/access_token",
+                clientId = "***",
+                clientSecret = "***"
+        ),
+        OAuthServerSettings.OAuth2ServerSettings(
+                name = "google",
+                authorizeUrl = "https://accounts.google.com/o/oauth2/auth",
+                accessTokenUrl = "https://www.googleapis.com/oauth2/v3/token",
+                requestMethod = HttpMethod.Post,
+
+                clientId = "***.apps.googleusercontent.com",
+                clientSecret = "***",
+                defaultScopes = listOf("https://www.googleapis.com/auth/plus.login")
+        ),
+        OAuthServerSettings.OAuth2ServerSettings(
+                name = "facebook",
+                authorizeUrl = "https://graph.facebook.com/oauth/authorize",
+                accessTokenUrl = "https://graph.facebook.com/oauth/access_token",
+                requestMethod = HttpMethod.Post,
+
+                clientId = "***",
+                clientSecret = "***",
+                defaultScopes = listOf("public_profile")
+        )
+).toMapBy { it.name }
+
+class OAuthLoginApplication(config: ApplicationConfig) : Application(config) {
+    val exec = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() * 4)
+
+    init {
+        locations {
+            get<index>() {
+                response.status(HttpStatusCode.OK)
+                response.contentType(ContentType.Text.Html)
+                response.write {
+                    appendHTML().html {
+                        head {
+                            title { +"index page" }
+                        }
+                        body {
+                            h1 {
+                                +"Try to login"
+                            }
+                            p {
+                                a(href = Locations.href(login())) {
+                                    +"Login"
+                                }
+                            }
+                        }
+                    }
+                }
+                ApplicationRequestStatus.Handled
+            }
+
+            oauthAtLocation<login>(exec,
+                    providerLookup = { loginProviders[it.type] },
+                    urlProvider = { l, p -> redirectUrl(login(p.name), false) },
+                    onSuccess = { l, accessToken ->
+                        loggedInSuccessResponse(accessToken)
+                    })
+
+            location<login>() {
+                param("error") {
+                    handle {
+                        loginFailedPage(resolveResult.values.getAll("error").orEmpty())
+                    }
+                }
+
+                handle {
+                    loginPage()
+                }
+            }
+        }
+    }
+
+    private fun <T : Any> ApplicationRequestContext.redirectUrl(t: T, secure: Boolean = true): String {
+        val hostPort = request.host()!! + request.port().let { port -> if (port == 80) "" else ":$port" }
+        val protocol = when {
+            secure -> "https"
+            else -> "http"
+        }
+        return "$protocol://$hostPort${Locations.href(t)}"
+    }
+
+    private fun ApplicationRequestContext.loginPage(): ApplicationRequestStatus {
+        response.status(HttpStatusCode.OK)
+        response.contentType(ContentType.Text.Html)
+        response.write {
+            appendHTML().html {
+                head {
+                    title { +"Login with" }
+                }
+                body {
+                    h1 {
+                        +"Login with:"
+                    }
+
+                    for (p in loginProviders) {
+                        p {
+                            a(href = Locations.href(login(p.key))) {
+                                +p.key
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return ApplicationRequestStatus.Handled
+    }
+
+    private fun ApplicationRequestContext.loginFailedPage(errors: List<String>): ApplicationRequestStatus {
+        response.status(HttpStatusCode.OK)
+        response.contentType(ContentType.Text.Html)
+        response.write {
+            appendHTML().html {
+                head {
+                    title { +"Login with" }
+                }
+                body {
+                    h1 {
+                        +"Login error"
+                    }
+
+                    for (e in errors) {
+                        p {
+                            +e
+                        }
+                    }
+                }
+            }
+        }
+
+        return ApplicationRequestStatus.Handled
+    }
+
+    private fun ApplicationRequestContext.loggedInSuccessResponse(callback: OAuthAccessTokenResponse) {
+        response.status(HttpStatusCode.OK)
+        response.contentType(ContentType.Text.Html)
+        response.write {
+            appendHTML().html {
+                head {
+                    title { +"Logged in" }
+                }
+                body {
+                    h1 {
+                        +"You are logged in"
+                    }
+                    p {
+                        +"Your token is $callback"
+                    }
+                }
+            }
+        }
+    }
+}
