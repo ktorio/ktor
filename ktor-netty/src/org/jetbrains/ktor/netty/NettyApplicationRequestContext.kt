f50b0140682c4bddf4a2c167fbecb2ed44fef510
Ilya Ryzhenkov <orangy@jetbrains.com>
2015-09-22 17:54:25 +0300

    Use chunked encoding if other kind of encoding or content-length was not specified.

--- ktor-netty/src/org/jetbrains/ktor/netty/NettyApplicationRequestContext.kt
+++ ktor-netty/src/org/jetbrains/ktor/netty/NettyApplicationRequestContext.kt
@@ -3,6 +3,7 @@ package org.jetbrains.ktor.netty
 import io.netty.channel.*
 import io.netty.handler.codec.http.*
 import org.jetbrains.ktor.application.*
+import org.jetbrains.ktor.http.HttpHeaders
 import org.jetbrains.ktor.interception.*
 
 class NettyApplicationRequestContext(override val application: Application,
@@ -11,6 +12,10 @@ class NettyApplicationRequestContext(override val application: Application,
     val httpResponse = DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK)
 
     override val close = Interceptable0<Unit> {
+        if (response.headers.get(HttpHeaders.TransferEncoding) == null
+                && response.headers.get(HttpHeaders.ContentLength) == null) {
+            response.headers.append(HttpHeaders.TransferEncoding, "chunked")
+        }
         context.writeAndFlush(httpResponse)
         context.close()
     }
