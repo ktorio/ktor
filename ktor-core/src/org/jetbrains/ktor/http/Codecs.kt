b3de47e7f52839d49d8d97aa88cccbc7d59f3a7a
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2015-09-24 15:50:12 +0300

    Support quoted and escaped values in headers

--- ktor-core/src/org/jetbrains/ktor/http/Codecs.kt
+++ ktor-core/src/org/jetbrains/ktor/http/Codecs.kt
@@ -4,3 +4,7 @@ import java.net.*
 
 fun String.decodeURL(): String = URLDecoder.decode(this, Charsets.UTF_8.name())
 fun String.encodeURL(): String = URLEncoder.encode(this, Charsets.UTF_8.name())
+fun String.unquoteAndUnescape() = when {
+    startsWith('"') && endsWith('"') -> removeSurrounding("\"").replace("\\\\.".toRegex()) { it.value.takeLast(1) }
+    else -> this
+}
