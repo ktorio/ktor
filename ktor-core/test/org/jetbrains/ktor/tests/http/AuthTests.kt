a1392848c8aee186d11f6203e4cdee57092662be
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2015-12-01 21:05:51 +0300

    Auth header parser test for random strings

--- ktor-core/test/org/jetbrains/ktor/tests/http/AuthTests.kt
+++ ktor-core/test/org/jetbrains/ktor/tests/http/AuthTests.kt
@@ -1,6 +1,7 @@
 package org.jetbrains.ktor.tests.http.auth.tests
 
 import org.jetbrains.ktor.auth.*
+import java.util.*
 import kotlin.test.*
 import org.junit.Test as test
 
@@ -47,6 +48,15 @@ class AuthorizeHeaderParserTest {
         testParserParameterized("Basic", mapOf("a" to "1 \" 2", "b" to "2"), "Basic a=\"1 \\\" 2\", b= 2")
     }
 
+    @test fun testGarbage() {
+        Random().let { rnd ->
+            repeat(10000) {
+                val random = rnd.nextString(3 + rnd.nextInt(7)) + " " + rnd.nextString(10, ('a'..'z') + ('A'..'Z') + ('0'..'9') + listOf(',', ' ', '/'))
+                parseAuthorizationHeader(random)
+            }
+        }
+    }
+
     private fun testParserSingle(scheme: String, value: String, headerValue: String) {
         val actual = parseAuthorizationHeader(headerValue)!!
 
@@ -70,4 +80,14 @@ class AuthorizeHeaderParserTest {
             fail("It should return parameterized-value credential")
         }
     }
+
+    private fun Random.nextString(length: Int, possible: Iterable<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')) = possible.toList().let { possibleElements ->
+        (0..length - 1).map { nextFrom(possibleElements) }.joinToString("")
+    }
+
+    private fun Random.nextString(length: Int, possible: String) = nextString(length, possible.toCharList())
+
+    private fun <T> Random.nextFrom(possibleElements: List<T>): T =
+            if (possibleElements.isEmpty()) throw NoSuchElementException()
+            else possibleElements[nextInt(possibleElements.size)]
 }
