c77275013aa97afe930a8d1b4044bd04143b265f
Ilya Ryzhenkov <orangy@jetbrains.com>
2016-01-18 18:10:54 +0300

    Unify interception using InterceptApplicationCall (former InterceptableWithContext)

--- ktor-core/src/org/jetbrains/ktor/content/Templating.kt
+++ ktor-core/src/org/jetbrains/ktor/content/Templating.kt
@@ -1,6 +1,7 @@
 package org.jetbrains.ktor.content
 
 import org.jetbrains.ktor.application.*
+import org.jetbrains.ktor.interception.*
 import org.jetbrains.ktor.routing.*
 import kotlin.reflect.*
 
@@ -9,21 +10,7 @@ interface TemplateEngine<C : Any, R> where R : StreamContent, R : HasContentType
     fun process(content: C): R
 }
 
-inline fun <reified C : Any> Application.templating(engine: TemplateEngine<C, *>) {
-    val javaType = engine.contentClass.java
-    intercept { next ->
-        response.interceptSend { obj, next ->
-            if (javaType.isInstance(obj)) {
-                response.send(engine.process(obj as C))
-            } else {
-                next(obj)
-            }
-        }
-        next()
-    }
-}
-
-inline fun <reified C : Any> RoutingEntry.templating(engine: TemplateEngine<C, *>) {
+inline fun <reified C : Any> InterceptApplicationCall<ApplicationCall>.templating(engine: TemplateEngine<C, *>) {
     val javaType = engine.contentClass.java
     intercept { next ->
         response.interceptSend { obj, next ->
