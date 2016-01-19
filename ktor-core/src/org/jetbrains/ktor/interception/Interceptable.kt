10c7886ec78a620fe7491831417acc064164c529
Ilya Ryzhenkov <orangy@jetbrains.com>
2016-01-19 21:18:31 +0300

    Do not create lambdas in each call to interceptors, optimise stack frames

--- ktor-core/src/org/jetbrains/ktor/interception/Interceptable.kt
+++ ktor-core/src/org/jetbrains/ktor/interception/Interceptable.kt
@@ -1,48 +1,45 @@
 package org.jetbrains.ktor.interception
 
-public class Interceptable0<TResult>(val function: () -> TResult) {
-    private val interceptors = arrayListOf<(() -> TResult) -> TResult>()
+class Interceptable0<TResult>(function: () -> TResult) {
+    private val interceptors = arrayListOf<() -> TResult>(function)
 
-    public fun intercept(handler: (next: () -> TResult) -> TResult) {
-        interceptors.add(handler)
+    fun intercept(handler: (next: () -> TResult) -> TResult) {
+        val index = interceptors.lastIndex
+        val nextHandler: () -> TResult = { interceptors[index + 1]() }
+        val function = interceptors[index]
+        interceptors[index] = { handler(nextHandler) }
+        interceptors.add(function)
     }
 
-    fun invokeAt(index: Int): TResult = when {
-        index < interceptors.size -> interceptors[index]({ invokeAt(index + 1) })
-        else -> function()
-    }
-
-    public fun execute(): TResult = invokeAt(0)
+    fun execute(): TResult = interceptors[0]()
 }
 
-public class Interceptable1<TParam0, TResult>(val function: (TParam0) -> TResult) {
-    private val interceptors = arrayListOf<(TParam0, (TParam0) -> TResult) -> TResult>()
+class Interceptable1<TParam0, TResult>(function: (TParam0) -> TResult) {
+    private val interceptors = arrayListOf<(TParam0) -> TResult>(function)
 
-    public fun intercept(handler: (param: TParam0, next: (TParam0) -> TResult) -> TResult) {
-        interceptors.add(handler)
+    fun intercept(handler: (param: TParam0, next: (TParam0) -> TResult) -> TResult) {
+        val index = interceptors.lastIndex
+        val nextHandler: (TParam0) -> TResult = { interceptors[index + 1](it) }
+        val function = interceptors[index]
+        interceptors[index] = { handler(it, nextHandler) }
+        interceptors.add(function)
     }
 
-    private fun invokeAt(param: TParam0, index: Int): TResult = when {
-        index < interceptors.size -> interceptors[index](param, { param -> invokeAt(param, index + 1) })
-        else -> function(param)
-    }
-
-    public fun execute(param: TParam0): TResult = invokeAt(param, 0)
+    fun execute(param: TParam0): TResult = interceptors[0](param)
 }
 
-public class Interceptable2<TParam0, TParam1, TResult>(val function: (TParam0, TParam1) -> TResult) {
-    private val interceptors = arrayListOf<(TParam0, TParam1, (TParam0, TParam1) -> TResult) -> TResult>()
+class Interceptable2<TParam0, TParam1, TResult>(val function: (TParam0, TParam1) -> TResult) {
+    private val interceptors = arrayListOf<(TParam0, TParam1) -> TResult>(function)
 
-    public fun intercept(handler: (TParam0, TParam1, (TParam0, TParam1) -> TResult) -> TResult) {
-        interceptors.add(handler)
+    fun intercept(handler: (param1: TParam0, param2: TParam1, next: (TParam0, TParam1) -> TResult) -> TResult) {
+        val index = interceptors.lastIndex
+        val nextHandler: (TParam0, TParam1) -> TResult = { p1, p2 -> interceptors[index + 1](p1, p2) }
+        val function = interceptors[index]
+        interceptors[index] = { p1, p2 -> handler(p1, p2, nextHandler) }
+        interceptors.add(function)
     }
 
-    fun invokeAt(param1: TParam0, param2: TParam1, index: Int): TResult = when {
-        index < interceptors.size -> interceptors[index](param1, param2, { param1, param2 -> invokeAt(param1, param2, index + 1) })
-        else -> function(param1, param2)
-    }
-
-    public fun execute(param1: TParam0, param2: TParam1): TResult = invokeAt(param1, param2, 0)
+    fun execute(param1: TParam0, param2: TParam1): TResult = interceptors[0](param1, param2)
 }
 
 public class InterceptableChain0<TResult> {
@@ -52,7 +49,7 @@ public class InterceptableChain0<TResult> {
         interceptors.add(handler)
     }
 
-    fun call(next: () -> TResult): TResult = invokeAt(next, 0)
+    fun execute(next: () -> TResult): TResult = invokeAt(next, 0)
 
     private fun invokeAt(last: () -> TResult, index: Int): TResult = when {
         index < interceptors.size -> interceptors[index]({ invokeAt(last, index + 1) })
