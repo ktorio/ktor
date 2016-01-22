6cdc95df723365291ffd3b16de0a4c61cb5e889c
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2016-01-23 01:26:15 +0300

    Last modified response header

--- ktor-core/src/org/jetbrains/ktor/http/ApplicationResponseHeaders.kt
+++ ktor-core/src/org/jetbrains/ktor/http/ApplicationResponseHeaders.kt
@@ -1,6 +1,7 @@
 package org.jetbrains.ktor.http
 
 import org.jetbrains.ktor.application.*
+import java.time.*
 import java.time.temporal.*
 
 fun ApplicationResponse.contentType(value: ContentType) = contentType(value.toString())
@@ -11,7 +12,7 @@ fun ApplicationResponse.header(name: String, value: Long) = headers.append(name,
 fun ApplicationResponse.header(name: String, date: Temporal) = headers.append(name, date.toHttpDateString())
 
 fun ApplicationResponse.etag(value: String) = header(HttpHeaders.ETag, value)
-fun ApplicationResponse.lastModified(value: Long) = header(HttpHeaders.LastModified, value)
+fun ApplicationResponse.lastModified(value: Long) = header(HttpHeaders.LastModified, LocalDateTime.ofInstant(Instant.ofEpochMilli(value), ZoneId.systemDefault()))
 fun ApplicationResponse.contentLength(value: Long) = header(HttpHeaders.ContentLength, value)
 
 
