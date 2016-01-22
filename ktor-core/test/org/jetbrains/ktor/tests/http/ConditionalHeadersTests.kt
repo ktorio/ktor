3484a24738bb1bbc9572a221d0c1b2c84000ea83
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2016-01-22 23:44:00 +0300

    If-Range conditional header

--- ktor-core/test/org/jetbrains/ktor/tests/http/ConditionalHeadersTests.kt
+++ ktor-core/test/org/jetbrains/ktor/tests/http/ConditionalHeadersTests.kt
@@ -387,7 +387,372 @@ class LastModifiedTest {
             }
         }
     }
-
-    protected fun Date.toLocalDateTime() = LocalDateTime.ofInstant(toInstant(), ZoneId.systemDefault())
-
 }
+
+class IfRangeTest {
+    @Test
+    fun testNoHeadersDate() {
+        val date = Date()
+
+        withTestApplication {
+            application.routing {
+                handle {
+                    withIfRange(date) { rangeOrNull ->
+                        assertNull(rangeOrNull)
+                        response.sendText("ok")
+                    }
+                }
+            }
+
+            handleRequest(HttpMethod.Get, "/").let { result ->
+                assertEquals(ApplicationCallResult.Handled, result.requestResult)
+                assertEquals(HttpStatusCode.OK, result.response.status())
+                assertEquals("ok", result.response.content)
+            }
+        }
+    }
+
+    @Test
+    fun testIfRangeOnly() {
+        val date = Date()
+
+        withTestApplication {
+            application.routing {
+                handle {
+                    withIfRange(date) { rangeOrNull ->
+                        assertNull(rangeOrNull)
+                        response.sendText("ok")
+                    }
+                }
+            }
+
+            handleRequest(HttpMethod.Get, "/", { addHeader(HttpHeaders.IfRange, date.toLocalDateTime().toHttpDateString()) }).let { result ->
+                assertEquals(ApplicationCallResult.Handled, result.requestResult)
+                assertEquals(HttpStatusCode.OK, result.response.status())
+                assertEquals("ok", result.response.content)
+            }
+
+            handleRequest(HttpMethod.Get, "/", { addHeader(HttpHeaders.IfRange, date.toLocalDateTime().plusDays(1).toHttpDateString()) }).let { result ->
+                assertEquals(ApplicationCallResult.Handled, result.requestResult)
+                assertEquals(HttpStatusCode.OK, result.response.status())
+                assertEquals("ok", result.response.content)
+            }
+
+            handleRequest(HttpMethod.Get, "/", { addHeader(HttpHeaders.IfRange, date.toLocalDateTime().minusDays(1).toHttpDateString()) }).let { result ->
+                assertEquals(ApplicationCallResult.Handled, result.requestResult)
+                assertEquals(HttpStatusCode.OK, result.response.status())
+                assertEquals("ok", result.response.content)
+            }
+        }
+    }
+
+    @Test
+    fun testRangeOnly() {
+        val date = Date()
+
+        withTestApplication {
+            application.routing {
+                handle {
+                    withIfRange(date) { rangeOrNull ->
+                        assertNotNull(rangeOrNull)
+                        response.sendText("ok")
+                    }
+                }
+            }
+
+            handleRequest(HttpMethod.Get, "/", { addHeader(HttpHeaders.Range, "bytes=0-1") }).let { result ->
+                assertEquals(ApplicationCallResult.Handled, result.requestResult)
+                assertEquals(HttpStatusCode.PartialContent, result.response.status())
+                assertEquals("ok", result.response.content)
+            }
+        }
+    }
+
+    @Test
+    fun testNoRangeOnlyWithIfModifiedSince() {
+        val date = Date()
+
+        withTestApplication {
+            application.routing {
+                handle {
+                    withIfRange(date) { rangeOrNull ->
+                        fail("should never reach here")
+                    }
+                }
+            }
+
+            handleRequest(HttpMethod.Get, "/", { addHeader(HttpHeaders.IfModifiedSince, date.toLocalDateTime().toHttpDateString()) }).let { result ->
+                assertEquals(ApplicationCallResult.Handled, result.requestResult)
+                assertEquals(HttpStatusCode.NotModified, result.response.status())
+                assertNull(result.response.content)
+            }
+        }
+    }
+
+    @Test
+    fun testNoRangeOnlyWithIfModifiedSinceLt() {
+        val date = Date()
+
+        withTestApplication {
+            application.routing {
+                handle {
+                    withIfRange(date) { rangeOrNull ->
+                        assertNull(rangeOrNull)
+                        response.sendText("ok")
+                    }
+                }
+            }
+
+            handleRequest(HttpMethod.Get, "/", { addHeader(HttpHeaders.IfModifiedSince, date.toLocalDateTime().minusDays(1).toHttpDateString()) }).let { result ->
+                assertEquals(ApplicationCallResult.Handled, result.requestResult)
+                assertEquals(HttpStatusCode.OK, result.response.status())
+                assertEquals(date.toLocalDateTime().toHttpDateString(), result.response.headers[HttpHeaders.LastModified])
+                assertEquals("ok", result.response.content)
+            }
+        }
+    }
+
+    @Test
+    fun testRangeOnlyWithIfModifiedSince() {
+        val date = Date()
+
+        withTestApplication {
+            application.routing {
+                handle {
+                    withIfRange(date) { rangeOrNull ->
+                        fail("should never reach here")
+                    }
+                }
+            }
+
+            handleRequest(HttpMethod.Get, "/", { addHeader(HttpHeaders.Range, "bytes=0-1"); addHeader(HttpHeaders.IfModifiedSince, date.toLocalDateTime().toHttpDateString()) }).let { result ->
+                assertEquals(ApplicationCallResult.Handled, result.requestResult)
+                assertEquals(HttpStatusCode.NotModified, result.response.status())
+                assertNull(result.response.content)
+            }
+        }
+    }
+
+    @Test
+    fun testRangeOnlyWithIfModifiedSinceLt() {
+        val date = Date()
+
+        withTestApplication {
+            application.routing {
+                handle {
+                    withIfRange(date) { rangeOrNull ->
+                        assertNotNull(rangeOrNull)
+                        response.sendText("ok")
+                    }
+                }
+            }
+
+            handleRequest(HttpMethod.Get, "/", { addHeader(HttpHeaders.Range, "bytes=0-1"); addHeader(HttpHeaders.IfModifiedSince, date.toLocalDateTime().minusDays(1).toHttpDateString()) }).let { result ->
+                assertEquals(ApplicationCallResult.Handled, result.requestResult)
+                assertEquals(HttpStatusCode.PartialContent, result.response.status())
+                assertEquals(date.toLocalDateTime().toHttpDateString(), result.response.headers[HttpHeaders.LastModified])
+                assertEquals("ok", result.response.content)
+            }
+        }
+    }
+
+    @Test
+    fun testRangeAndIfRangeEq() {
+        val date = Date()
+
+        withTestApplication {
+            application.routing {
+                handle {
+                    withIfRange(date) { rangeOrNull ->
+                        assertNotNull(rangeOrNull)
+                        response.sendText("ok")
+                    }
+                }
+            }
+
+            handleRequest(HttpMethod.Get, "/", { addHeader(HttpHeaders.Range, "bytes=0-1"); addHeader(HttpHeaders.IfRange, date.toLocalDateTime().toHttpDateString()) }).let { result ->
+                assertEquals(ApplicationCallResult.Handled, result.requestResult)
+                assertEquals(HttpStatusCode.PartialContent, result.response.status())
+                assertEquals("ok", result.response.content)
+            }
+        }
+    }
+
+    @Test
+    fun testRangeAndIfRangeLt() {
+        val date = Date()
+
+        withTestApplication {
+            application.routing {
+                handle {
+                    withIfRange(date) { rangeOrNull ->
+                        assertNull(rangeOrNull)
+                        response.sendText("ok")
+                    }
+                }
+            }
+
+            handleRequest(HttpMethod.Get, "/", { addHeader(HttpHeaders.Range, "bytes=0-1"); addHeader(HttpHeaders.IfRange, date.toLocalDateTime().minusDays(1).toHttpDateString()) }).let { result ->
+                assertEquals(ApplicationCallResult.Handled, result.requestResult)
+                assertEquals(HttpStatusCode.OK, result.response.status())
+                assertEquals("ok", result.response.content)
+            }
+        }
+    }
+
+    @Test
+    fun testRangeAndIfRangeGt() {
+        val date = Date()
+
+        withTestApplication {
+            application.routing {
+                handle {
+                    withIfRange(date) { rangeOrNull ->
+                        assertNotNull(rangeOrNull)
+                        response.sendText("ok")
+                    }
+                }
+            }
+
+            handleRequest(HttpMethod.Get, "/", { addHeader(HttpHeaders.Range, "bytes=0-1"); addHeader(HttpHeaders.IfRange, date.toLocalDateTime().plusDays(1).toHttpDateString()) }).let { result ->
+                assertEquals(ApplicationCallResult.Handled, result.requestResult)
+                assertEquals(HttpStatusCode.PartialContent, result.response.status())
+                assertEquals("ok", result.response.content)
+            }
+        }
+    }
+
+    @Test
+    fun testRangeAndIfRangeEtagOK() {
+        withTestApplication {
+            application.routing {
+                handle {
+                    withIfRange("etag1") { rangeOrNull ->
+                        assertNotNull(rangeOrNull)
+                        response.sendText("ok")
+                    }
+                }
+            }
+
+            handleRequest(HttpMethod.Get, "/", { addHeader(HttpHeaders.Range, "bytes=0-1"); addHeader(HttpHeaders.IfRange, "etag1") }).let { result ->
+                assertEquals(ApplicationCallResult.Handled, result.requestResult)
+                assertEquals(HttpStatusCode.PartialContent, result.response.status())
+                assertEquals("ok", result.response.content)
+            }
+        }
+    }
+
+    @Test
+    fun testRangeAndIfRangeEtagDiffer() {
+        withTestApplication {
+            application.routing {
+                handle {
+                    withIfRange("etag1") { rangeOrNull ->
+                        assertNull(rangeOrNull)
+                        response.sendText("ok")
+                    }
+                }
+            }
+
+            handleRequest(HttpMethod.Get, "/", { addHeader(HttpHeaders.Range, "bytes=0-1"); addHeader(HttpHeaders.IfRange, "etag2") }).let { result ->
+                assertEquals(ApplicationCallResult.Handled, result.requestResult)
+                assertEquals(HttpStatusCode.OK, result.response.status())
+                assertEquals("ok", result.response.content)
+            }
+        }
+    }
+
+    @Test
+    fun testRangeNoIfRangeEtag() {
+        withTestApplication {
+            application.routing {
+                handle {
+                    withIfRange("etag1") { rangeOrNull ->
+                        assertNotNull(rangeOrNull)
+                        response.sendText("ok")
+                    }
+                }
+            }
+
+            handleRequest(HttpMethod.Get, "/", { addHeader(HttpHeaders.Range, "bytes=0-1") }).let { result ->
+                assertEquals(ApplicationCallResult.Handled, result.requestResult)
+                assertEquals(HttpStatusCode.PartialContent, result.response.status())
+                assertEquals("ok", result.response.content)
+            }
+        }
+    }
+
+    @Test
+    fun testRangeNoIfRangeEtagIfNoneMatch() {
+        withTestApplication {
+            application.routing {
+                handle {
+                    withIfRange("etag1") { rangeOrNull ->
+                        assertNotNull(rangeOrNull)
+                        response.sendText("ok")
+                    }
+                }
+            }
+
+            handleRequest(HttpMethod.Get, "/", { addHeader(HttpHeaders.Range, "bytes=0-1"); addHeader(HttpHeaders.IfNoneMatch, "etag1") }).let { result ->
+                assertEquals(ApplicationCallResult.Handled, result.requestResult)
+                assertEquals(HttpStatusCode.NotModified, result.response.status())
+                assertNull(result.response.content)
+            }
+
+            handleRequest(HttpMethod.Get, "/", { addHeader(HttpHeaders.Range, "bytes=0-1"); addHeader(HttpHeaders.IfNoneMatch, "etag2") }).let { result ->
+                assertEquals(ApplicationCallResult.Handled, result.requestResult)
+                assertEquals(HttpStatusCode.PartialContent, result.response.status())
+                assertEquals("ok", result.response.content)
+            }
+        }
+    }
+
+    @Test
+    fun testRangeNoIfRangeEtagIfMatch() {
+        withTestApplication {
+            application.routing {
+                handle {
+                    withIfRange("etag1") { rangeOrNull ->
+                        assertNotNull(rangeOrNull)
+                        response.sendText("ok")
+                    }
+                }
+            }
+
+            handleRequest(HttpMethod.Get, "/", { addHeader(HttpHeaders.Range, "bytes=0-1"); addHeader(HttpHeaders.IfMatch, "etag2") }).let { result ->
+                assertEquals(ApplicationCallResult.Handled, result.requestResult)
+                assertEquals(HttpStatusCode.PreconditionFailed, result.response.status())
+                assertNull(result.response.content)
+            }
+
+            handleRequest(HttpMethod.Get, "/", { addHeader(HttpHeaders.Range, "bytes=0-1"); addHeader(HttpHeaders.IfMatch, "etag1") }).let { result ->
+                assertEquals(ApplicationCallResult.Handled, result.requestResult)
+                assertEquals(HttpStatusCode.PartialContent, result.response.status())
+                assertEquals("ok", result.response.content)
+            }
+        }
+    }
+
+    @Test
+    fun testNoRangeNoIfRangeEtag() {
+        withTestApplication {
+            application.routing {
+                handle {
+                    withIfRange("etag1") { rangeOrNull ->
+                        assertNull(rangeOrNull)
+                        response.sendText("ok")
+                    }
+                }
+            }
+
+            handleRequest(HttpMethod.Get, "/", {  }).let { result ->
+                assertEquals(ApplicationCallResult.Handled, result.requestResult)
+                assertEquals(HttpStatusCode.OK, result.response.status())
+                assertEquals("ok", result.response.content)
+            }
+        }
+    }
+}
+
+private fun Date.toLocalDateTime() = LocalDateTime.ofInstant(toInstant(), ZoneId.systemDefault())
