a9925a742104359f59fb837feb5a1dcec61bc043
Ilya Ryzhenkov <orangy@jetbrains.com>
2016-03-24 17:21:35 +0300

    Log failures

--- ktor-core/src/org/jetbrains/ktor/logging/DefaultLoggingInterceptor.kt
+++ ktor-core/src/org/jetbrains/ktor/logging/DefaultLoggingInterceptor.kt
@@ -3,8 +3,7 @@ package org.jetbrains.ktor.logging
 import org.jetbrains.ktor.application.*
 import org.jetbrains.ktor.http.*
 
-private fun Application.logCallResult(call: ApplicationCall) {
-
+private fun Application.logCallFinished(call: ApplicationCall) {
     val status = call.response.status()
     when (status) {
         HttpStatusCode.Found -> config.log.trace("$status: ${call.request.requestLine} -> ${call.response.headers[HttpHeaders.Location]}")
@@ -12,8 +11,14 @@ private fun Application.logCallResult(call: ApplicationCall) {
     }
 }
 
+private fun Application.logCallFailed(call: ApplicationCall, e: Throwable) {
+    val status = call.response.status()
+    config.log.error("$status: ${call.request.requestLine}", e)
+}
+
 fun Application.logApplicationCalls() {
     intercept { call ->
-        onFinish { logCallResult(call) }
+        onFinish { logCallFinished(call) }
+        onFail { logCallFailed(call, it) }
     }
 }
