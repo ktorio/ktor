868210315e3f412f047434bb1dc02e3a4e26e628
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2016-04-12 12:24:05 +0300

    Fix runAsync for PipelineContinue case

--- ktor-core/src/org/jetbrains/ktor/pipeline/Async.kt
+++ ktor-core/src/org/jetbrains/ktor/pipeline/Async.kt
@@ -3,14 +3,24 @@ package org.jetbrains.ktor.pipeline
 import java.util.concurrent.*
 
 fun <C : Any> PipelineContext<C>.runAsync(exec: ExecutorService, block: PipelineContext<C>.() -> Unit): Nothing {
-    val future = CompletableFuture.runAsync(Runnable {
-        block()
-    }, exec)
-    future.whenComplete { unit, throwable ->
-        if (throwable == null)
-            proceed()
-        else if (throwable !is PipelineControlFlow)
-            fail(throwable)
+    exec.submit {
+        asyncBlock(block)
     }
+
     pause()
+}
+
+private fun <C : Any> PipelineContext<C>.asyncBlock(block: PipelineContext<C>.() -> Unit): Nothing {
+    try {
+        try {
+            block()
+            proceed()
+        } catch (e: PipelineControlFlow) {
+            throw e
+        } catch (t: Throwable) {
+            fail(t)
+        }
+    } catch (e: PipelineContinue) {
+        proceed()
+    }
 }
