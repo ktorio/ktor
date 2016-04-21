f419159c4bb80eba43609cccdd4145289525a57a
Ilya Ryzhenkov <orangy@jetbrains.com>
2016-04-21 21:51:09 +0300

    Add Pure styles to YouKube pages.

--- ktor-samples/you-kube/src/you/kube/Styles.kt
+++ ktor-samples/you-kube/src/you/kube/Styles.kt
@@ -1,9 +1,7 @@
 package you.kube
 
-import kotlinx.css.port.*
 import org.jetbrains.ktor.application.*
 import org.jetbrains.ktor.content.*
-import org.jetbrains.ktor.http.*
 import org.jetbrains.ktor.locations.*
 import org.jetbrains.ktor.routing.*
 
@@ -13,15 +11,5 @@ class MainCss()
 fun RoutingEntry.styles() {
     get<MainCss> {
         call.respond(call.resolveClasspathWithPath("", "blog.css")!!)
-    }
-}
-
-fun Style.fontSize() {
-    font.size("14px")
-}
-
-fun mainCss() = renderCSS {
-    div.style("container") {
-        fontSize()
     }
 }
