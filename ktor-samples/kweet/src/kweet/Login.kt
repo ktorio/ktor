73834403a8299b7550ea5d1055771be67af530b2
Ilya Ryzhenkov <orangy@jetbrains.com>
2016-04-25 13:54:55 +0300

    Login in Kweet shouldn't look into previous login.

--- ktor-samples/kweet/src/kweet/Login.kt
+++ ktor-samples/kweet/src/kweet/Login.kt
@@ -4,7 +4,6 @@ import kweet.dao.*
 import org.jetbrains.ktor.application.*
 import org.jetbrains.ktor.freemarker.*
 import org.jetbrains.ktor.locations.*
-import org.jetbrains.ktor.pipeline.*
 import org.jetbrains.ktor.routing.*
 import org.jetbrains.ktor.sessions.*
 
@@ -19,24 +18,18 @@ fun RoutingEntry.login(dao: DAOFacade, hash: (String) -> String) {
         }
     }
     post<Login> {
-        val user = call.sessionOrNull<Session>()?.let { dao.user(it.userId) }
+        val login = when {
+            it.userId.length < 4 -> null
+            it.password.length < 6 -> null
+            !userNameValid(it.userId) -> null
+            else -> dao.user(it.userId, hash(it.password))
+        }
 
-        if (user != null) {
-            call.redirect(UserPage(user.userId))
+        if (login == null) {
+            call.redirect(it.copy(password = "", error = "Invalid username or password"))
         } else {
-            val login = when {
-                it.userId.length < 4 -> null
-                it.password.length < 6 -> null
-                !userNameValid(it.userId) -> null
-                else -> dao.user(it.userId, hash(it.password))
-            }
-
-            if (login == null) {
-                call.redirect(it.copy(password = "", error = "Invalid username or password"))
-            } else {
-                call.session(Session(login.userId))
-                call.redirect(UserPage(login.userId))
-            }
+            call.session(Session(login.userId))
+            call.redirect(UserPage(login.userId))
         }
     }
     get<Logout> {
