76d7436b823288aed9c8a88e5548ea63d82e66c2
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2016-06-22 21:05:58 +0300

    Application dispose hooks

--- ktor-core/src/org/jetbrains/ktor/application/Application.kt
+++ ktor-core/src/org/jetbrains/ktor/application/Application.kt
@@ -1,13 +1,14 @@
 package org.jetbrains.ktor.application
 
-import org.jetbrains.ktor.interception.*
-import org.jetbrains.ktor.pipeline.*
 import org.jetbrains.ktor.util.*
+import java.util.concurrent.*
 
 /**
  * Represents configured and running web application, capable of handling requests
  */
 open class Application(val environment: ApplicationEnvironment) : ApplicationCallPipeline() {
+    var closeHooks = CopyOnWriteArrayList<() -> Unit>()
+
     /**
      * Provides common place to store application-wide attributes
      */
@@ -17,5 +18,11 @@ open class Application(val environment: ApplicationEnvironment) : ApplicationCal
      * Called by host when [Application] is terminated
      */
     open fun dispose() {
+        closeHooks.forEach {
+            try {
+                it()
+            } catch (ignore: Throwable) {
+            }
+        }
     }
 }
