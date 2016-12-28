36973542992006b2f866bf8bbb2dc5c89fe2e103
Ilya Ryzhenkov <orangy@jetbrains.com>
2016-12-28 16:45:58 +0300

    Request Accept-Encoding:gzip for potential compression benchmarks

--- ktor-jmh/src/org/jetbrains/ktor/tests/IntegrationBenchmark.kt
+++ ktor-jmh/src/org/jetbrains/ktor/tests/IntegrationBenchmark.kt
@@ -45,38 +45,44 @@ open class IntegrationBenchmark {
         server.start()
     }
 
+    private fun load(url: String): ByteArray {
+        return (URL(url).openConnection() as HttpURLConnection).apply {
+            setRequestProperty("Accept-Encoding", "gzip")
+        }.inputStream.readBytes()
+    }
+
     @TearDown
     fun shutdownServer() {
         server.stop()
     }
 
     @Benchmark
-    fun sayOK(): String {
-        return URL("http://localhost:$port/sayOK").readText()
+    fun sayOK(): ByteArray {
+        return load("http://localhost:$port/sayOK")
     }
 
     @Benchmark
     fun jarfile(): ByteArray {
-        return URL("http://localhost:$port/jarfile").readBytes()
+        return load("http://localhost:$port/jarfile")
     }
 
     @Benchmark
     fun regularClasspathFile(): ByteArray {
-        return URL("http://localhost:$port/regularClasspathFile").readBytes()
+        return load("http://localhost:$port/regularClasspathFile")
     }
 
     @Benchmark
     fun regularFile(): ByteArray {
-        return URL("http://localhost:$port/regularFile").readBytes()
+        return load("http://localhost:$port/regularFile")
     }
 }
 
 /*
 Benchmark                                   Mode  Cnt   Score   Error   Units
-IntegrationBenchmark.jarfile               thrpt   10  10.041 ± 3.944  ops/ms
-IntegrationBenchmark.regularClasspathFile  thrpt   10   8.444 ± 2.189  ops/ms
-IntegrationBenchmark.regularFile           thrpt   10  15.393 ± 2.060  ops/ms
-IntegrationBenchmark.sayOK                 thrpt   10  21.573 ± 3.653  ops/ms
+IntegrationBenchmark.jarfile               thrpt   10  11.359 ± 2.814  ops/ms
+IntegrationBenchmark.regularClasspathFile  thrpt   10  14.951 ± 0.848  ops/ms
+IntegrationBenchmark.regularFile           thrpt   10  15.070 ± 0.607  ops/ms
+IntegrationBenchmark.sayOK                 thrpt   10  21.792 ± 3.005  ops/ms
 */
 
 fun main(args: Array<String>) {
