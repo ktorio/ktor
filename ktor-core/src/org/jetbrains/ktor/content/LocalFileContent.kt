0781b6659f16ed275bbd7fd833d7a62c1276539a
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2017-01-23 09:41:15 +0300

    Add extra parameters for better cache control as per Issue #63

--- ktor-core/src/org/jetbrains/ktor/content/LocalFileContent.kt
+++ ktor-core/src/org/jetbrains/ktor/content/LocalFileContent.kt
@@ -5,12 +5,30 @@ import org.jetbrains.ktor.nio.*
 import org.jetbrains.ktor.util.*
 import java.io.*
 import java.nio.file.*
+import java.time.*
 
-class LocalFileContent(val file: File, override val contentType: ContentType = defaultContentType(file.extension)) : FinalContent.ChannelContent(), Resource {
+class LocalFileContent(val file: File,
+                       override val contentType: ContentType = defaultContentType(file.extension),
+                       override val expires: LocalDateTime? = null,
+                       override val cacheControl: CacheControl? = null) : FinalContent.ChannelContent(), Resource {
 
-    constructor(baseDir: File, relativePath: String, contentType: ContentType = defaultContentType(relativePath.extension())) : this(baseDir.safeAppend(Paths.get(relativePath)), contentType)
-    constructor(baseDir: File, vararg relativePath: String, contentType: ContentType = defaultContentType(relativePath.last().extension())) : this(baseDir.safeAppend(Paths.get("", *relativePath)), contentType)
-    constructor(baseDir: Path, relativePath: Path, contentType: ContentType = defaultContentType(relativePath.fileName.extension())) : this(baseDir.safeAppend(relativePath).toFile(), contentType)
+    constructor(baseDir: File,
+                relativePath: String,
+                contentType: ContentType = defaultContentType(relativePath.extension()),
+                expires: LocalDateTime? = null,
+                cacheControl: CacheControl? = null) : this(baseDir.safeAppend(Paths.get(relativePath)), contentType, expires, cacheControl)
+
+    constructor(baseDir: File,
+                vararg relativePath: String,
+                contentType: ContentType = defaultContentType(relativePath.last().extension()),
+                expires: LocalDateTime? = null,
+                cacheControl: CacheControl? = null) : this(baseDir.safeAppend(Paths.get("", *relativePath)), contentType, expires, cacheControl)
+
+    constructor(baseDir: Path,
+                relativePath: Path,
+                contentType: ContentType = defaultContentType(relativePath.fileName.extension()),
+                expires: LocalDateTime? = null,
+                cacheControl: CacheControl? = null) : this(baseDir.safeAppend(relativePath).toFile(), contentType, expires, cacheControl)
 
     override val contentLength: Long
         get() = file.length()
@@ -21,8 +39,5 @@ class LocalFileContent(val file: File, override val contentType: ContentType = d
     override val headers by lazy { super.headers }
 
     override fun channel() = file.asyncReadOnlyFileChannel()
-
-    override val expires = null
-    override val cacheControl = null
 }
 
