ad4e912cdfdeb6486dd26bb30251838ffece3bd4
Ilya Ryzhenkov <orangy@jetbrains.com>
2017-01-27 23:05:51 +0300

    Uncomment integration benchmark and convert to Netty

--- ktor-jmh/src/org/jetbrains/ktor/tests/IntegrationBenchmark.kt
+++ ktor-jmh/src/org/jetbrains/ktor/tests/IntegrationBenchmark.kt
@@ -3,6 +3,7 @@ package org.jetbrains.ktor.tests
 import ch.qos.logback.classic.Level
 import org.jetbrains.ktor.application.*
 import org.jetbrains.ktor.content.*
+import org.jetbrains.ktor.netty.*
 import org.jetbrains.ktor.routing.*
 import org.openjdk.jmh.annotations.*
 import org.slf4j.*
@@ -12,12 +13,12 @@ import java.net.*
 
 
 @State(Scope.Benchmark)
-open class IntegrationBenchmark /*{
+open class IntegrationBenchmark {
     private val packageName = FullBenchmark::class.java.`package`.name
     private val classFileName = FullBenchmark::class.simpleName!! + ".class"
     private val pomFile = File("pom.xml")
 
-    lateinit private var server: JettyApplicationHost
+    lateinit private var server: NettyApplicationHost
 
     private val port = 5678
 
@@ -25,7 +26,7 @@ open class IntegrationBenchmark /*{
     fun configureServer() {
         val root = LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME) as ch.qos.logback.classic.Logger
         root.level = Level.ERROR
-        server = embeddedJettyServer(port) {
+        server = embeddedNettyServer(port) {
             routing {
                 get("/sayOK") {
                     call.respond("OK")
@@ -75,7 +76,6 @@ open class IntegrationBenchmark /*{
         return load("http://localhost:$port/regularFile")
     }
 }
-*/
 /*
 Benchmark                                   Mode  Cnt   Score   Error   Units
 IntegrationBenchmark.jarfile               thrpt   10  11.359 ± 2.814  ops/ms
