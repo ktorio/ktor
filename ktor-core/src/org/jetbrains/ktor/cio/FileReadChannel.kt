05f754850c8edf0408ada77cb6aa785181dbf321
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2017-02-02 13:04:47 +0300

    Replace async file implementation with sync one

--- ktor-core/src/org/jetbrains/ktor/cio/FileReadChannel.kt
+++ ktor-core/src/org/jetbrains/ktor/cio/FileReadChannel.kt
@@ -2,54 +2,41 @@ package org.jetbrains.ktor.cio
 
 import java.io.*
 import java.nio.*
-import java.nio.channels.*
 import java.nio.file.*
-import kotlin.coroutines.experimental.*
-import kotlin.coroutines.experimental.intrinsics.*
 
-class FileReadChannel(val source: AsynchronousFileChannel, val start: Long = 0, val endInclusive: Long = source.size() - 1) : RandomAccessReadChannel {
-    companion object {
-        val completionHandler = object : CompletionHandler<Int, FileReadChannel> {
-            override fun completed(result: Int, channel: FileReadChannel) {
-                channel.position += result
-                channel.currentContinuation!!.also { channel.currentContinuation = null }.resume(result)
-            }
+class FileReadChannel(val source: RandomAccessFile, val start: Long = 0, val endInclusive: Long = source.length() - 1) : RandomAccessReadChannel {
+    private var initialSeek = false
 
-            override fun failed(exc: Throwable, channel: FileReadChannel) {
-                channel.currentContinuation!!.also { channel.currentContinuation = null }.resumeWithException(exc)
-            }
-        }
-    }
+    override val position: Long
+        get() = source.filePointer
 
-    var currentContinuation: Continuation<Int>? = null
+    override val size: Long
+        get() = source.length()
 
     init {
         require(start >= 0L) { "start position shouldn't be negative but it is $start" }
-        require(endInclusive <= source.size() - 1) { "endInclusive points to the position out of the file: file size = ${source.size()}, endInclusive = $endInclusive" }
+        require(endInclusive <= source.length() - 1) { "endInclusive points to the position out of the file: file size = ${source.length()}, endInclusive = $endInclusive" }
     }
 
-    override var position: Long = start
-    override val size: Long
-        get() = source.size()
-
     suspend override fun read(dst: ByteBuffer): Int {
+        if (!initialSeek) {
+            seek(start)
+        }
+
         val limit = Math.min(dst.remaining().toLong(), endInclusive - position + 1).toInt()
         if (limit <= 0)
             return -1
         dst.limit(dst.position() + limit)
-        return suspendCoroutineOrReturn {
-            check(currentContinuation == null)
-            currentContinuation = it
-            source.read(dst, position, this, completionHandler)
-            COROUTINE_SUSPENDED
-        }
+
+        return source.channel.read(dst)
     }
 
     suspend override fun seek(position: Long) {
         require(position >= 0L) { "position should not be negative: $position" }
-        require(position < source.size()) { "position should not run out of the file range: $position !in [0, ${source.size()})" }
+        require(position <= source.length()) { "position should not run out of the file range: $position !in [0, ${source.length()}]" }
 
-        this.position = position
+        source.seek(position)
+        initialSeek = true
     }
 
     override fun close() {
@@ -58,10 +45,9 @@ class FileReadChannel(val source: AsynchronousFileChannel, val start: Long = 0, 
 }
 
 fun Path.readChannel(start: Long = 0, endInclusive: Long = Files.size(this) - 1): FileReadChannel {
-    val asyncFileChannel = AsynchronousFileChannel.open(this, StandardOpenOption.READ)
-    return FileReadChannel(asyncFileChannel, start, endInclusive)
+    return toFile().readChannel(start, endInclusive)
 }
 
 fun File.readChannel(start: Long = 0, endInclusive: Long = length() - 1): FileReadChannel {
-    return toPath().readChannel(start, endInclusive)
+    return FileReadChannel(RandomAccessFile(this, "r"), start, endInclusive)
 }
