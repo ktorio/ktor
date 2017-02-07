3625054243b97cfaeeb96ed4e479e9bbf098f3b1
Ilya Ryzhenkov <orangy@jetbrains.com>
2017-02-07 16:00:40 +0300

    Add FileWriteChannel for use in suspending stored sessions

---
+++ ktor-core/src/org/jetbrains/ktor/cio/FileWriteChannel.kt
@@ -0,0 +1,19 @@
+package org.jetbrains.ktor.cio
+
+import java.io.*
+import java.nio.*
+
+class FileWriteChannel(val target: RandomAccessFile) : WriteChannel {
+    suspend override fun write(src: ByteBuffer) {
+        target.write(src.array(), src.arrayOffset() + src.position(), src.limit())
+    }
+
+    override fun close() {
+        target.close()
+    }
+}
+
+fun File.writeChannel(): FileWriteChannel {
+    return FileWriteChannel(RandomAccessFile(this, "r"))
+}
+
