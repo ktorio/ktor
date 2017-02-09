49752d4a23228ababab82a37e045b3e659e60a4f
Ilya Ryzhenkov <orangy@jetbrains.com>
2017-02-09 23:24:25 +0300

    Avoid excess object allocations since ConcurrentHashMap is lazy itself

--- ktor-core/src/org/jetbrains/ktor/util/Attributes.kt
+++ ktor-core/src/org/jetbrains/ktor/util/Attributes.kt
@@ -10,35 +10,28 @@ open class AttributeKey<out T>(val name: String) {
 }
 
 class Attributes {
-    private var touched = false
-    private val map by lazy(LazyThreadSafetyMode.SYNCHRONIZED) {
-        ConcurrentHashMap<AttributeKey<*>, Any?>()
-    }
+    private val map = ConcurrentHashMap<AttributeKey<*>, Any?>()
 
     operator fun <T : Any> get(key: AttributeKey<T>): T = getOrNull(key) ?: throw IllegalStateException("No instance for key $key")
 
     @Suppress("UNCHECKED_CAST")
-    fun <T : Any> getOrNull(key: AttributeKey<T>): T? = if (touched) map[key] as T? else null
+    fun <T : Any> getOrNull(key: AttributeKey<T>): T? = map[key] as T?
 
-    operator fun contains(key: AttributeKey<*>) = if (touched) map.containsKey(key) else false
+    operator fun contains(key: AttributeKey<*>) = map.containsKey(key)
 
     fun <T : Any> put(key: AttributeKey<T>, value: T) {
-        touched = true
         map[key] = value
     }
 
     fun <T : Any> remove(key: AttributeKey<T>) {
-        if (touched) {
-            map.remove(key)
-        }
+        map.remove(key)
     }
 
     @Suppress("UNCHECKED_CAST")
     fun <T : Any> computeIfAbsent(key: AttributeKey<T>, block: () -> T): T {
-        touched = true
         return map.computeIfAbsent(key) { block() } as T
     }
 
     val allKeys: List<AttributeKey<Any?>>
-        get() = if (touched) map.keys.toList() else emptyList()
+        get() = map.keys.toList()
 }
