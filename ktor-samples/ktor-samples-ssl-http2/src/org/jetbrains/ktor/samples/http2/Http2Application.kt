a18d95d61ef2ff6e87df4e2c21762f47abfa442e
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2017-02-15 14:54:42 +0300

    Add HTTP2 with SSL example

---
+++ ktor-samples/ktor-samples-ssl-http2/src/org/jetbrains/ktor/samples/http2/Http2Application.kt
@@ -0,0 +1,33 @@
+package org.jetbrains.ktor.samples.http2
+
+import org.jetbrains.ktor.application.*
+import org.jetbrains.ktor.features.*
+import org.jetbrains.ktor.logging.*
+import org.jetbrains.ktor.response.*
+import org.jetbrains.ktor.routing.*
+import org.jetbrains.ktor.util.*
+import java.io.*
+
+fun Application.main() {
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(Routing) {
+        get("/") {
+            call.respondText("Hello, World!")
+        }
+    }
+}
+
+class CertificateGenerator {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val file = File("target/temporary.jks")
+
+            if (!file.exists()) {
+                file.parentFile.mkdirs()
+                generateCertificate(file)
+            }
+        }
+    }
+}
