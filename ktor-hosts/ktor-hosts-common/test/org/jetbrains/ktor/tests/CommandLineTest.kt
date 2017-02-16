d986b6843ea28c59368e3aa4642897ed5b0be911
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2017-02-16 16:57:45 +0300

    Command line test application.conf isolation (temporary solution, just to make tests work)

--- ktor-hosts/ktor-hosts-common/test/org/jetbrains/ktor/tests/CommandLineTest.kt
+++ ktor-hosts/ktor-hosts-common/test/org/jetbrains/ktor/tests/CommandLineTest.kt
@@ -2,11 +2,19 @@ package org.jetbrains.ktor.tests
 
 import org.jetbrains.ktor.host.*
 import org.junit.*
+import org.junit.rules.*
+import org.junit.runner.*
+import org.junit.runners.model.*
 import java.io.*
 import java.net.*
+import java.util.*
 import kotlin.test.*
 
 class CommandLineTest {
+
+    @get:Rule
+    var classLoader = IsolatedClassLoaderRule()
+
     @Test
     fun testEmpty() {
         commandLineConfig(emptyArray())
@@ -45,6 +53,50 @@ class CommandLineTest {
             return File(uri.path.substringBefore("!"))
         } else {
             return findContainingZipFile(URI(uri.rawSchemeSpecificPart))
+        }
+    }
+
+    class IsolatedClassLoaderRule : TestRule {
+        override fun apply(s: Statement, d: Description): Statement {
+            return object : Statement() {
+                override fun evaluate() {
+                    withIsolatedClassLoader {
+                        s.evaluate()
+                    }
+                }
+            }
+        }
+
+        private fun withIsolatedClassLoader(block: (ClassLoader) -> Unit) {
+            val classLoader = IsolatedResourcesClassLoader(
+                    File("ktor-hosts/ktor-hosts-common/test-resources").absoluteFile,
+                    block::class.java.classLoader)
+
+            val oldClassLoader = Thread.currentThread().contextClassLoader
+            Thread.currentThread().contextClassLoader = classLoader
+            try {
+                block(classLoader)
+            } finally {
+                Thread.currentThread().contextClassLoader = oldClassLoader
+            }
+        }
+    }
+
+    private class IsolatedResourcesClassLoader(val dir: File, parent: ClassLoader) : ClassLoader(parent) {
+        override fun getResources(name: String): Enumeration<URL> {
+            val lookup = File(dir, name)
+            if (lookup.isFile) return listOf(lookup.absoluteFile.toURI().toURL()).let { Collections.enumeration<URL>(it) }
+            return parent.getResources(name)
+        }
+
+        override fun getResource(name: String): URL? {
+            val lookup = File(dir, name)
+            if (lookup.isFile) return lookup.absoluteFile.toURI().toURL()
+            return parent.getResource(name)
+        }
+
+        override fun getResourceAsStream(name: String): InputStream? {
+            return getResource(name)?.openStream()
         }
     }
 }
