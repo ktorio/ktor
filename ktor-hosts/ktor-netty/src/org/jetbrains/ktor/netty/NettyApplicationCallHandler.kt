6a55ed6b8edc5410d74365ea5bacf40419183a8f
Ilya Ryzhenkov <orangy@jetbrains.com>
2017-02-24 19:59:57 +0300

    Schedule application call processing on a same event loop to avoid extra marshalling

--- ktor-hosts/ktor-netty/src/org/jetbrains/ktor/netty/NettyApplicationCallHandler.kt
+++ ktor-hosts/ktor-netty/src/org/jetbrains/ktor/netty/NettyApplicationCallHandler.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.experimental.*
 
 internal class NettyApplicationCallHandler(private val host: NettyApplicationHost) : SimpleChannelInboundHandler<NettyApplicationCall>() {
     override fun channelRead0(context: ChannelHandlerContext, msg: NettyApplicationCall) {
-        launch(host.callEventGroup.toCoroutineDispatcher()) {
+        launch(context.executor().toCoroutineDispatcher()) {
             host.pipeline.execute(msg)
         }
     }
