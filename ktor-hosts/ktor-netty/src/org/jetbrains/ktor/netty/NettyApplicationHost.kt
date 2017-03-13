c9b047e13b801f0805d4aa1528e41f4e81d7d644
Ilya Ryzhenkov <orangy@jetbrains.com>
2017-03-13 12:20:49 +0300

    Calculate Netty parallelism value based on number of processors and number of pools

--- ktor-hosts/ktor-netty/src/org/jetbrains/ktor/netty/NettyApplicationHost.kt
+++ ktor-hosts/ktor-netty/src/org/jetbrains/ktor/netty/NettyApplicationHost.kt
@@ -20,7 +20,7 @@ class NettyApplicationHost(override val hostConfig: ApplicationHostConfig,
     constructor(hostConfig: ApplicationHostConfig, environment: ApplicationEnvironment)
             : this(hostConfig, environment, ApplicationLoader(environment, hostConfig.autoreload))
 
-    private val parallelism = 0 // 0 means default
+    private val parallelism = Runtime.getRuntime().availableProcessors() / 3 + 1
     private val connectionEventGroup = NettyConnectionPool(parallelism) // accepts connections
     internal val workerEventGroup = NettyWorkerPool(parallelism) // processes socket data and parse HTTP
     internal val callEventGroup = NettyCallPool(parallelism) // processes calls
