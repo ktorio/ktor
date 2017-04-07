ccb038cf856ad1437d1dad709a1c92f476117769
Ilya Ryzhenkov <orangy@jetbrains.com>
2017-04-08 01:31:52 +0300

    Convert ByteArrayContent from function to a named class for better debugging

--- ktor-core/src/org/jetbrains/ktor/content/ByteArrayContent.kt
+++ ktor-core/src/org/jetbrains/ktor/content/ByteArrayContent.kt
@@ -3,7 +3,7 @@ package org.jetbrains.ktor.content
 import org.jetbrains.ktor.response.*
 import org.jetbrains.ktor.util.*
 
-fun ByteArrayContent(bytes: ByteArray) = object : FinalContent.ByteArrayContent() {
+class ByteArrayContent(private val bytes: ByteArray) : FinalContent.ByteArrayContent() {
     override val headers by lazy {
         ValuesMap.build(true) {
             contentLength(bytes.size.toLong())
