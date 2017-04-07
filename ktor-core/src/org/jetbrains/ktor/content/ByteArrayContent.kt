f93fd2d881113e89f8775ddb76bf412093f697e7
Ilya Ryzhenkov <orangy@jetbrains.com>
2017-04-08 01:29:34 +0300

    Fix ByteArrayContent to provide Content-Length header

--- ktor-core/src/org/jetbrains/ktor/content/ByteArrayContent.kt
+++ ktor-core/src/org/jetbrains/ktor/content/ByteArrayContent.kt
@@ -1,10 +1,14 @@
 package org.jetbrains.ktor.content
 
+import org.jetbrains.ktor.response.*
 import org.jetbrains.ktor.util.*
 
 fun ByteArrayContent(bytes: ByteArray) = object : FinalContent.ByteArrayContent() {
-    override val headers: ValuesMap
-        get() = ValuesMap.Empty
+    override val headers by lazy {
+        ValuesMap.build(true) {
+            contentLength(bytes.size.toLong())
+        }
+    }
 
     override fun bytes(): ByteArray = bytes
 }
