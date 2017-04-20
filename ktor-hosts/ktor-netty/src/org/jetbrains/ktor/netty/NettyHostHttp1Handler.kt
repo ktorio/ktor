74819e8c6a251fe8bb5fb06647acc55aae40b9fc
Ilya Ryzhenkov <orangy@jetbrains.com>
2017-04-20 18:24:06 +0300

    Simplify Netty processing of incoming HttpMessages

--- ktor-hosts/ktor-netty/src/org/jetbrains/ktor/netty/NettyHostHttp1Handler.kt
+++ ktor-hosts/ktor-netty/src/org/jetbrains/ktor/netty/NettyHostHttp1Handler.kt
@@ -7,29 +7,23 @@ import io.netty.handler.codec.http.HttpVersion.*
 import io.netty.util.*
 
 @ChannelHandler.Sharable
-internal class NettyHostHttp1Handler(private val host: NettyApplicationHost) : SimpleChannelInboundHandler<HttpObject>(false) {
-    override fun channelRead0(context: ChannelHandlerContext, message: HttpObject) {
-        when (message) {
-            is HttpRequest -> {
-                if (HttpUtil.is100ContinueExpected(message)) {
-                    context.write(DefaultFullHttpResponse(HTTP_1_1, CONTINUE));
-                }
-
-                context.channel().config().isAutoRead = false
-                val httpContentQueue = HttpContentQueue(context)
-                context.pipeline().addLast(httpContentQueue)
-                context.pipeline().addLast(host.callEventGroup, NettyApplicationCallHandler(host))
-
-                if (message is HttpContent) {
-                    httpContentQueue.queue.push(message, message is LastHttpContent)
-                }
-
-                ReferenceCountUtil.retain(message)
-                val call = NettyApplicationCall(host.application, context, message, httpContentQueue.queue)
-                context.fireChannelRead(call)
-            }
-            else -> context.fireChannelRead(message)
+internal class NettyHostHttp1Handler(private val host: NettyApplicationHost) : SimpleChannelInboundHandler<HttpRequest>(false) {
+    override fun channelRead0(context: ChannelHandlerContext, message: HttpRequest) {
+        if (HttpUtil.is100ContinueExpected(message)) {
+            context.write(DefaultFullHttpResponse(HTTP_1_1, CONTINUE));
         }
+
+        context.channel().config().isAutoRead = false
+        val httpContentQueue = HttpContentQueue(context)
+        context.pipeline().addLast(httpContentQueue)
+        context.pipeline().addLast(host.callEventGroup, NettyApplicationCallHandler(host))
+
+        if (message is HttpContent) {
+            httpContentQueue.queue.push(message, message is LastHttpContent)
+        }
+
+        val call = NettyApplicationCall(host.application, context, message, httpContentQueue.queue)
+        context.fireChannelRead(call)
     }
 }
 
