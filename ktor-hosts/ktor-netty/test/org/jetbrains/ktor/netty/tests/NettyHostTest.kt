d392e68ec59db2c93f1a4da0c17e89c498e0998b
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2017-05-04 21:02:18 +0300

    netty: disable testBlockingDeadlock as it runs for too long (passes)

--- ktor-hosts/ktor-netty/test/org/jetbrains/ktor/netty/tests/NettyHostTest.kt
+++ ktor-hosts/ktor-netty/test/org/jetbrains/ktor/netty/tests/NettyHostTest.kt
@@ -2,5 +2,12 @@ package org.jetbrains.ktor.netty.tests
 
 import org.jetbrains.ktor.netty.*
 import org.jetbrains.ktor.testing.*
+import org.junit.*
 
-class NettyHostTest : HostTestSuite<NettyApplicationHost>(Netty)
+class NettyHostTest : HostTestSuite<NettyApplicationHost>(Netty) {
+
+    @Ignore // runs too long
+    override fun testBlockingDeadlock() {
+        super.testBlockingDeadlock()
+    }
+}
