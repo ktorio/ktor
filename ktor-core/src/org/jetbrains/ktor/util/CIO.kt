fbda9ec28ca229452e7c2f9dc777fa939fd881e2
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2017-05-05 14:20:23 +0300

    Upgrade Kotlin and Coroutines

--- ktor-core/src/org/jetbrains/ktor/util/CIO.kt
+++ ktor-core/src/org/jetbrains/ktor/util/CIO.kt
@@ -1,11 +1,10 @@
 package org.jetbrains.ktor.util
 
-import kotlinx.coroutines.experimental.intrinsics.*
 import kotlin.coroutines.experimental.*
 import kotlin.coroutines.experimental.intrinsics.*
 
 fun <T> runSync(block: suspend () -> T): T {
-    val result = block.startCoroutineOrReturn(NoopContinuation)
+    val result = block.startCoroutineUninterceptedOrReturn(NoopContinuation)
     if (result == COROUTINE_SUSPENDED) {
         throw IllegalStateException("function passed to runSync suspended")
     }
