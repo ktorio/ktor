7f5d9c855d9f6a5a52ec0fe2d08f5cb5d6a059b0
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2017-06-02 19:10:45 +0300

    Netty: coroutine dispatcher

--- ktor-hosts/ktor-netty/src/org/jetbrains/ktor/netty/NettyApplicationCallHandler.kt
+++ ktor-hosts/ktor-netty/src/org/jetbrains/ktor/netty/NettyApplicationCallHandler.kt
@@ -1,12 +1,27 @@
 package org.jetbrains.ktor.netty
 
 import io.netty.channel.*
-import org.jetbrains.ktor.pipeline.*
+import kotlinx.coroutines.experimental.*
+import kotlin.coroutines.experimental.*
 
 internal class NettyApplicationCallHandler(private val host: NettyApplicationHost) : SimpleChannelInboundHandler<NettyApplicationCall>() {
     override fun channelRead0(context: ChannelHandlerContext, msg: NettyApplicationCall) {
-        launchAsync(context.executor()) {
+        launch(Dispatcher + CurrentContext(context)) {
             host.pipeline.execute(msg)
         }
     }
+
+    object Dispatcher : CoroutineDispatcher() {
+        override fun isDispatchNeeded(context: CoroutineContext): Boolean {
+            return !context[CurrentContextKey]!!.context.executor().inEventLoop()
+        }
+
+        override fun dispatch(context: CoroutineContext, block: Runnable) {
+            val nettyContext = context[CurrentContextKey]!!.context
+            nettyContext.executor().execute(block)
+        }
+    }
+
+    class CurrentContext(val context: ChannelHandlerContext) : AbstractCoroutineContextElement(CurrentContextKey)
+    object CurrentContextKey : CoroutineContext.Key<CurrentContext>
 }
