5fb680d54c67a9d6fc993a07d8a31ff5e7de8dcc
Ilya Ryzhenkov <orangy@jetbrains.com>
2017-06-26 12:57:40 +0300

    Move both default transforms into single file

--- ktor-hosts/ktor-hosts-common/src/org/jetbrains/ktor/host/BaseApplicationRequest.kt
+++ ktor-hosts/ktor-hosts-common/src/org/jetbrains/ktor/host/BaseApplicationRequest.kt
@@ -9,35 +9,3 @@ import java.io.*
 
 abstract class BaseApplicationRequest : ApplicationRequest
 
-fun ApplicationReceivePipeline.installDefaultTransformations() {
-    intercept(ApplicationReceivePipeline.Transform) { query ->
-        val value = query.value as? IncomingContent ?: return@intercept
-        val transformed: Any? = when (query.type) {
-            ReadChannel::class -> value.readChannel()
-            InputStream::class -> value.inputStream()
-            MultiPartData::class -> value.multiPartData()
-            String::class -> value.readText()
-            ValuesMap::class -> {
-                val contentType = value.request.contentType()
-                when {
-                    contentType.match(ContentType.Application.FormUrlEncoded) -> {
-                        val string = value.readText()
-                        parseQueryString(string)
-                    }
-                    contentType.match(ContentType.MultiPart.FormData) -> {
-                        val items = value.multiPartData().parts.filterIsInstance<PartData.FormItem>()
-                        ValuesMap.build {
-                            items.forEach {
-                                it.partName?.let { name -> append(name, it.value) }
-                            }
-                        }
-                    }
-                    else -> null // Respond UnsupportedMediaType? but what if someone else later would like to do it?
-                }
-            }
-            else -> null
-        }
-        if (transformed != null)
-            proceedWith(ApplicationReceiveRequest(query.type, transformed))
-    }
-}
