3fb030ae600779ca447af3282c85cb4af2fc2855
Ilya Ryzhenkov <orangy@jetbrains.com>
2017-06-26 13:38:04 +0300

    Add Gson feature with send/receive facilities

---
+++ ktor-features/ktor-gson/src/org/jetbrains/ktor/gson/GsonSupport.kt
@@ -0,0 +1,58 @@
+package org.jetbrains.ktor.gson
+
+import com.google.gson.*
+import org.jetbrains.ktor.application.*
+import org.jetbrains.ktor.content.*
+import org.jetbrains.ktor.http.*
+import org.jetbrains.ktor.request.*
+import org.jetbrains.ktor.response.*
+import org.jetbrains.ktor.util.*
+
+class GsonSupport(val gson: Gson) {
+    companion object Feature : ApplicationFeature<ApplicationCallPipeline, GsonBuilder, GsonSupport> {
+        override val key = AttributeKey<GsonSupport>("gson")
+
+        override fun install(pipeline: ApplicationCallPipeline, configure: GsonBuilder.() -> Unit): GsonSupport {
+            val gson = GsonBuilder().apply(configure).create()
+            val feature = GsonSupport(gson)
+            pipeline.sendPipeline.intercept(ApplicationSendPipeline.Transform) {
+                if (call.request.acceptItems().any { ContentType.Application.Json.match(it.value) }) {
+                    proceedWith(feature.transform(it))
+                }
+            }
+            pipeline.receivePipeline.intercept(ApplicationReceivePipeline.Transform) {
+                if (call.request.contentType().match(ContentType.Application.Json)) {
+                    val message = it.value as? IncomingContent ?: return@intercept
+                    val json = message.readText()
+                    val value = gson.fromJson(json, it.type.javaObjectType)
+                    proceedWith(ApplicationReceiveRequest(it.type, value))
+                }
+            }
+            return feature
+        }
+    }
+
+    private fun transform(model: Any): JsonContent {
+        val json = gson.toJson(model)
+        return JsonContent(json, HttpStatusCode.OK)
+    }
+}
+
+class JsonContent(val text: String, override val status: HttpStatusCode? = null) : FinalContent.ByteArrayContent() {
+    private val bytes by lazy { text.toByteArray(Charsets.UTF_8) }
+
+    override val headers by lazy {
+        ValuesMap.build(true) {
+            contentType(contentType)
+            contentLength(bytes.size.toLong())
+        }
+    }
+
+    override fun bytes(): ByteArray = bytes
+    override fun toString() = "JsonContent \"${text.take(30)}\""
+
+    companion object {
+        private val contentType = ContentType.Application.Json.withCharset(Charsets.UTF_8)
+    }
+}
+
--- ktor-samples/ktor-samples-json/src/org/jetbrains/ktor/samples/json/JsonApplication.kt
+++ ktor-samples/ktor-samples-json/src/org/jetbrains/ktor/samples/json/JsonApplication.kt
@@ -1,12 +1,10 @@
 package org.jetbrains.ktor.samples.json
 
-import com.google.gson.*
 import org.jetbrains.ktor.application.*
-import org.jetbrains.ktor.content.*
 import org.jetbrains.ktor.features.*
+import org.jetbrains.ktor.gson.*
 import org.jetbrains.ktor.http.*
 import org.jetbrains.ktor.logging.*
-import org.jetbrains.ktor.request.*
 import org.jetbrains.ktor.response.*
 import org.jetbrains.ktor.routing.*
 
@@ -25,14 +23,8 @@ fun Application.main() {
     install(DefaultHeaders)
     install(Compression)
     install(CallLogging)
-
-    val gson = GsonBuilder().create()
-    intercept(ApplicationCallPipeline.Infrastructure) {
-        if (call.request.acceptItems().any { it.value == "application/json" }) {
-            call.sendPipeline.intercept(ApplicationSendPipeline.Transform) { message ->
-                TextContent(gson.toJson(message), ContentType.Application.Json)
-            }
-        }
+    install(GsonSupport) {
+        setPrettyPrinting()
     }
 
     val model = Model("root", listOf(Item("A", "Apache"), Item("B", "Bing")))
@@ -41,7 +33,11 @@ fun Application.main() {
             call.respond(model)
         }
         get("/v1/item/{key}") {
-            call.respond(model.items.first { it.key == call.parameters["key"] })
+            val item = model.items.firstOrNull { it.key == call.parameters["key"] }
+            if (item == null)
+                call.respond(HttpStatusCode.NotFound)
+            else
+                call.respond(item)
         }
     }
 }
