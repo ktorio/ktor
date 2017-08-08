29638b650a531248cb2b74e15799e35f70b8cdb3
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2017-08-08 17:53:36 +0300

    Websocket: added route-functions with no path required

--- ktor-features/ktor-websockets/src/org/jetbrains/ktor/websocket/Routing.kt
+++ ktor-features/ktor-websockets/src/org/jetbrains/ktor/websocket/Routing.kt
@@ -6,35 +6,56 @@ import org.jetbrains.ktor.http.*
 import org.jetbrains.ktor.response.*
 import org.jetbrains.ktor.routing.*
 
-fun Route.webSocketRaw(path: String, protocol: String? = null, handler: suspend WebSocketSession.(WebSocketUpgrade.Dispatchers) -> Unit) {
+fun Route.webSocketRaw(protocol: String? = null, handler: suspend WebSocketSession.(WebSocketUpgrade.Dispatchers) -> Unit) {
     application.feature(WebSockets) // early require
 
-    route(HttpMethod.Get, path) {
-        header(HttpHeaders.Connection, "Upgrade") {
-            header(HttpHeaders.Upgrade, "websocket") {
-                webSocketProtocol(protocol) {
-                    handle {
-                        //val extensions = call.request.header(HttpHeaders.SecWebSocketExtensions)
-
-                        call.respond(WebSocketUpgrade(call, protocol, handler))
-                    }
+    header(HttpHeaders.Connection, "Upgrade") {
+        header(HttpHeaders.Upgrade, "websocket") {
+            webSocketProtocol(protocol) {
+                handle {
+                    call.respondWebSocketRaw(protocol, handler)
                 }
             }
         }
     }
 }
 
+fun Route.webSocketRaw(path: String, protocol: String? = null, handler: suspend WebSocketSession.(WebSocketUpgrade.Dispatchers) -> Unit) {
+    application.feature(WebSockets) // early require
+
+    route(HttpMethod.Get, path) {
+        webSocketRaw(protocol, handler)
+    }
+}
+
+fun Route.webSocket(protocol: String? = null, handler: suspend DefaultWebSocketSession.() -> Unit) {
+    webSocketRaw(protocol) { dispatchers ->
+        proceedWebSocket(dispatchers, handler)
+    }
+}
+
 fun Route.webSocket(path: String, protocol: String? = null, handler: suspend DefaultWebSocketSession.() -> Unit) {
     webSocketRaw(path, protocol) { dispatchers ->
-        val webSockets = application.feature(WebSockets)
-
-        val raw = this
-        val ws = DefaultWebSocketSessionImpl(raw, dispatchers.hostContext, dispatchers.userAppContext, NoPool)
-        ws.pingInterval = webSockets.pingInterval
-        ws.timeout = webSockets.timeout
-
-        ws.run(handler)
+        proceedWebSocket(dispatchers, handler)
     }
+}
+
+// these two functions could be potentially useful for users however it is not clear how to provide them better
+// so for now they are still private
+
+private suspend fun ApplicationCall.respondWebSocketRaw(protocol: String? = null, handler: suspend WebSocketSession.(WebSocketUpgrade.Dispatchers) -> Unit) {
+    respond(WebSocketUpgrade(this, protocol, handler))
+}
+
+private suspend fun WebSocketSession.proceedWebSocket(dispatchers: WebSocketUpgrade.Dispatchers, handler: suspend DefaultWebSocketSession.() -> Unit) {
+    val webSockets = application.feature(WebSockets)
+
+    val raw = this
+    val ws = DefaultWebSocketSessionImpl(raw, dispatchers.hostContext, dispatchers.userAppContext, NoPool)
+    ws.pingInterval = webSockets.pingInterval
+    ws.timeout = webSockets.timeout
+
+    ws.run(handler)
 }
 
 private fun Route.webSocketProtocol(protocol: String?, block: Route.() -> Unit) {
