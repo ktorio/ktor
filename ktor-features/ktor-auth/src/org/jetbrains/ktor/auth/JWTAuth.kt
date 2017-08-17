31be1090a284bb775596598d99ac3397dc6290cd
Bolot Kerimbaev <bolot@bignerdranch.com>
2017-08-17 12:47:55 -0400

    Ktor auth0 JWT/JWKS authentication implementation

---
+++ ktor-features/ktor-auth/src/org/jetbrains/ktor/auth/JWTAuth.kt
@@ -0,0 +1,91 @@
+package org.jetbrains.ktor.auth
+
+import com.auth0.jwk.Jwk
+import com.auth0.jwk.JwkProvider
+import com.auth0.jwt.JWT
+import com.auth0.jwt.JWTVerifier
+import com.auth0.jwt.algorithms.Algorithm
+import com.auth0.jwt.impl.JWTParser
+import com.auth0.jwt.interfaces.DecodedJWT
+import com.auth0.jwt.interfaces.Payload
+import org.jetbrains.ktor.application.ApplicationCall
+import org.jetbrains.ktor.http.HttpStatusCode
+import org.jetbrains.ktor.response.respond
+import java.security.interfaces.ECPublicKey
+import java.security.interfaces.RSAPublicKey
+import java.util.*
+
+val JWTAuthKey: Any = "JWTAuth"
+
+data class JWTCredential(val payload: Payload): Credential
+data class JWTPrincipal(val payload: Payload): Principal
+
+fun AuthenticationPipeline.jwtAuthentication(verifier: JWTVerifier, validate: (JWTCredential) -> Principal?) {
+    intercept(AuthenticationPipeline.RequestAuthentication) { context ->
+        try {
+            val token = call.getAuthToken()
+
+            val jwt = verifier.verify(token)
+            val payload = jwt.parsePayload()
+
+            val credentials = JWTCredential(payload)
+            val principal = requireNotNull(credentials.let(validate))
+            context.principal(principal)
+        } catch(e: Exception) {
+            context.challenge(JWTAuthKey, NotAuthenticatedCause.InvalidCredentials) {
+                it.success()
+                call.respond(HttpStatusCode.Unauthorized)
+            }
+        }
+    }
+}
+
+fun AuthenticationPipeline.jwtAuthentication(jwkProvider: JwkProvider, issuer: String, validate: (JWTCredential) -> Principal?) {
+    intercept(AuthenticationPipeline.RequestAuthentication) { context ->
+        try {
+            val token = call.getAuthToken()
+
+            val jwk = jwkProvider.get(JWT.decode(token).keyId)
+
+            val algorithm = jwk.makeAlgorithm()
+            val verifier = JWT.require(algorithm).withIssuer(issuer).build()
+
+            val jwt = verifier.verify(token)
+            val payload = jwt.parsePayload()
+
+            val credentials = JWTCredential(payload)
+            val principal = requireNotNull(credentials.let(validate))
+            context.principal(principal)
+        } catch(e: Exception) {
+            context.challenge(JWTAuthKey, NotAuthenticatedCause.InvalidCredentials) {
+                it.success()
+                call.respond(HttpStatusCode.Unauthorized)
+            }
+        }
+    }
+}
+
+fun ApplicationCall.getAuthToken(): String {
+    val headers = request.headers
+    val authHeader = requireNotNull(headers["Authorization"])
+    val token = authHeader.removePrefix("Bearer ")
+    return token
+}
+
+fun Jwk.makeAlgorithm(): Algorithm {
+    return when(algorithm) {
+        "RS256" -> Algorithm.RSA256(publicKey as RSAPublicKey, null)
+        "RS384" -> Algorithm.RSA384(publicKey as RSAPublicKey, null)
+        "RS512" -> Algorithm.RSA512(publicKey as RSAPublicKey, null)
+        "ES256" -> Algorithm.ECDSA256(publicKey as ECPublicKey, null)
+        "ES384" -> Algorithm.ECDSA384(publicKey as ECPublicKey, null)
+        "ES512" -> Algorithm.ECDSA512(publicKey as ECPublicKey, null)
+        else -> throw IllegalArgumentException("unsupported algorithm $algorithm")
+    }
+}
+
+fun DecodedJWT.parsePayload(): Payload {
+    val payloadString = String(Base64.getUrlDecoder().decode(payload))
+    val parsedPayload = JWTParser().parsePayload(payloadString)
+    return parsedPayload
+}
---
+++ ktor-samples/ktor-samples-auth/src/org/jetbrains/ktor/samples/auth/JwtAuthApplication.kt
@@ -0,0 +1,56 @@
+package org.jetbrains.ktor.samples.auth
+
+import com.auth0.jwk.JwkProvider
+import com.auth0.jwk.JwkProviderBuilder
+import org.jetbrains.ktor.application.Application
+import org.jetbrains.ktor.application.install
+import org.jetbrains.ktor.auth.JWTPrincipal
+import org.jetbrains.ktor.auth.authentication
+import org.jetbrains.ktor.auth.jwtAuthentication
+import org.jetbrains.ktor.features.CallLogging
+import org.jetbrains.ktor.features.DefaultHeaders
+import org.jetbrains.ktor.gson.GsonSupport
+import org.jetbrains.ktor.response.respond
+import org.jetbrains.ktor.routing.Routing
+import org.jetbrains.ktor.routing.route
+import java.util.concurrent.TimeUnit
+
+data class Who(val name: String, val planet: String)
+
+fun Application.JWTAuthApplication() {
+    val issuer = environment.config.property("jwt.domain").getString()
+    val audience = environment.config.property("jwt.audience").getString()
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(GsonSupport)
+    install(Routing) {
+        route("/who") {
+            val jwkProvider = makeJwkProvider(issuer)
+
+            authentication {
+                jwtAuthentication(jwkProvider, issuer) { credential ->
+                    when {
+                        credential.payload.audience.contains(audience)
+                            -> JWTPrincipal(credential.payload)
+                        else -> null
+                    }
+                }
+            }
+            handle {
+                val principal = call.authentication.principal<JWTPrincipal>()
+                val subjectString = principal!!.payload.subject.removePrefix("auth0|")
+                val doktor = Who(subjectString, "Gallifrey")
+                call.respond(doktor)
+            }
+        }
+    }
+}
+
+private fun makeJwkProvider(issuer: String): JwkProvider {
+    val jwkProvider = JwkProviderBuilder(issuer)
+            .cached(10, 24, TimeUnit.HOURS)
+            .rateLimited(10, 1, TimeUnit.MINUTES)
+            .build()
+    return jwkProvider
+}
+
