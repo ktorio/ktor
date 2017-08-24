c0e11b8be543c10d32410a43e01efd023d7150d9
Ilya Ryzhenkov <orangy@jetbrains.com>
2017-08-24 20:37:13 +0300

    Adds ContentNegotiation feature to support variable content on send and receive

---
+++ ktor-core/src/org/jetbrains/ktor/features/ContentNegotiation.kt
@@ -0,0 +1,105 @@
+package org.jetbrains.ktor.features
+
+import org.jetbrains.ktor.application.*
+import org.jetbrains.ktor.content.*
+import org.jetbrains.ktor.http.*
+import org.jetbrains.ktor.pipeline.*
+import org.jetbrains.ktor.request.*
+import org.jetbrains.ktor.response.*
+import org.jetbrains.ktor.util.*
+import kotlin.reflect.*
+import kotlin.reflect.full.*
+
+/**
+ * https://tools.ietf.org/html/rfc7231#section-5.3
+ * https://developer.mozilla.org/en-US/docs/Web/HTTP/Content_negotiation
+ */
+class ContentNegotiation(val converters: List<ContentConverterRegistration>) {
+    data class ContentConverterRegistration(val contentType: ContentType, val converter: ContentConverter)
+
+    class Configuration {
+        internal val converters = ArrayList<ContentConverterRegistration>()
+
+        fun <T : ContentConverter> register(contentType: ContentType, converter: T, configure: T.() -> Unit = {}) {
+            val registration = ContentConverterRegistration(contentType, converter.apply(configure))
+            converters.add(registration)
+        }
+    }
+
+    companion object Feature : ApplicationFeature<ApplicationCallPipeline, Configuration, ContentNegotiation> {
+        override val key = AttributeKey<ContentNegotiation>("gson")
+
+        private val contentTypeComparator = compareByDescending<Pair<ContentType, Double>> { it.second }.thenBy {
+            val contentType = it.first
+            var asterisks = 0
+            if (contentType.contentType == "*")
+                asterisks += 2
+            if (contentType.contentSubtype == "*")
+                asterisks++
+            asterisks
+        }.thenByDescending { it.first.parameters.size }
+
+        override fun install(pipeline: ApplicationCallPipeline, configure: Configuration.() -> Unit): ContentNegotiation {
+            val configuration = Configuration().apply(configure)
+            val feature = ContentNegotiation(configuration.converters)
+
+            // Respond with "415 Unsupported Media Type" if content cannot be transformed on receive
+            pipeline.intercept(ApplicationCallPipeline.Infrastructure) {
+                try {
+                    proceed()
+                } catch (e: UnsupportedMediaTypeException) {
+                    call.respond(HttpStatusCode.UnsupportedMediaType)
+                }
+            }
+
+            pipeline.sendPipeline.intercept(ApplicationSendPipeline.Render) {
+                if (subject is FinalContent) return@intercept
+
+                val suitableConverters = acceptedTypes().mapNotNull { (contentType, _) ->
+                    feature.converters.firstOrNull { it.contentType.match(contentType) }
+                }
+
+                val converted = suitableConverters.mapNotNull { it.converter.convertForSend(this, subject) }.firstOrNull()
+                val rendered = converted?.let { transformDefaultContent(it) } ?: HttpStatusCodeContent(HttpStatusCode.NotAcceptable)
+                proceedWith(rendered)
+            }
+
+            pipeline.receivePipeline.intercept(ApplicationReceivePipeline.Transform) {
+                val contentType = call.request.contentType()
+                val suitableConverter = feature.converters.firstOrNull { it.contentType.match(contentType) }
+                        ?: throw UnsupportedMediaTypeException(contentType)
+                val converted = suitableConverter.converter.convertForReceive(this)
+                        ?: throw UnsupportedMediaTypeException(contentType)
+                proceedWith(ApplicationReceiveRequest(it.type, converted))
+            }
+            return feature
+        }
+
+        private fun PipelineContext<Any, ApplicationCall>.acceptedTypes(): List<Pair<ContentType, Double>> {
+            val acceptHeader = call.request.header(HttpHeaders.Accept)
+            val acceptHeaderValues = parseHeaderValue(acceptHeader)
+            return acceptHeaderValues
+                    .map { ContentType.parse(it.value) to it.quality }
+                    .sortedWith(contentTypeComparator)
+        }
+    }
+}
+
+class UnsupportedMediaTypeException(contentType: ContentType) : Exception("Content type $contentType is not supported")
+
+interface ContentConverter {
+    suspend fun convertForSend(context: PipelineContext<Any, ApplicationCall>, value: Any): Any?
+    suspend fun convertForReceive(context: PipelineContext<ApplicationReceiveRequest, ApplicationCall>): Any?
+}
+
+class ConvertedContent(text: String, val contentType: ContentType) : FinalContent.ByteArrayContent() {
+    private val bytes = text.toByteArray(Charsets.UTF_8)
+    override fun bytes(): ByteArray = bytes
+
+    override val headers = ValuesMap.build(true) {
+        set(HttpHeaders.ContentType, contentType.toString())
+        set(HttpHeaders.ContentLength, bytes.size.toString())
+    }
+
+    override fun toString() = "ConvertedContent($contentType) \"${bytes.toString(Charsets.UTF_8).take(30)}\""
+}
--- ktor-features/ktor-gson/src/org/jetbrains/ktor/gson/GsonSupport.kt
+++ ktor-features/ktor-gson/src/org/jetbrains/ktor/gson/GsonSupport.kt
@@ -3,12 +3,19 @@ package org.jetbrains.ktor.gson
 import com.google.gson.*
 import org.jetbrains.ktor.application.*
 import org.jetbrains.ktor.content.*
+import org.jetbrains.ktor.features.*
 import org.jetbrains.ktor.http.*
 import org.jetbrains.ktor.pipeline.*
 import org.jetbrains.ktor.request.*
 import org.jetbrains.ktor.response.*
 import org.jetbrains.ktor.util.*
 
+/**
+ *    install(ContentNegotiation) {
+ *       register(ContentType.Application.Json, GsonConverter())
+ *    }
+ */
+@Deprecated("GsonSupport is deprecated in favor of generic ContentNegotiation Feature")
 class GsonSupport(val gson: Gson) {
     companion object Feature : ApplicationFeature<ApplicationCallPipeline, GsonBuilder, GsonSupport> {
         override val key = AttributeKey<GsonSupport>("gson")
@@ -33,26 +40,24 @@ class GsonSupport(val gson: Gson) {
         }
     }
 
-    private fun renderJsonContent(model: Any): JsonContent {
+    private fun renderJsonContent(model: Any): ConvertedContent {
         val json = gson.toJson(model)
-        return JsonContent(json, HttpStatusCode.OK)
+        return ConvertedContent(json, ContentType.Application.Json.withCharset(Charsets.UTF_8))
     }
 }
 
-private class JsonContent(text: String, override val status: HttpStatusCode? = null) : FinalContent.ByteArrayContent() {
-    private val bytes = text.toByteArray(Charsets.UTF_8)
 
-    override val headers = ValuesMap.build(true) {
-        set(HttpHeaders.ContentType, contentType)
-        set(HttpHeaders.ContentLength, bytes.size.toString())
+class GsonConverter(private val gson: Gson = Gson()) : ContentConverter {
+    private val contentType = ContentType.Application.Json.withCharset(Charsets.UTF_8)
+
+    override suspend fun convertForSend(context: PipelineContext<Any, ApplicationCall>, value: Any): Any? {
+        return ConvertedContent(gson.toJson(value), contentType)
     }
 
-
-    override fun bytes(): ByteArray = bytes
-    override fun toString() = "JsonContent \"${bytes.toString(Charsets.UTF_8).take(30)}\""
-
-    companion object {
-        private val contentType = ContentType.Application.Json.withCharset(Charsets.UTF_8).toString()
+    override suspend fun convertForReceive(context: PipelineContext<ApplicationReceiveRequest, ApplicationCall>): Any? {
+        val request = context.subject
+        val value = request.value as? IncomingContent ?: return null
+        val type = request.type
+        return gson.fromJson(value.readText(), type.javaObjectType)
     }
 }
-
