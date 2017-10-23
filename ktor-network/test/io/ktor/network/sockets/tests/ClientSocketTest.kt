3d18fb0950f7d813ff090460b6ecbefa6d7991e5
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2017-10-23 13:13:06 +0300

    Add ktor-network server and client TCP tests

---
+++ ktor-network/test/io/ktor/network/sockets/tests/ClientSocketTest.kt
@@ -0,0 +1,116 @@
+package io.ktor.network.sockets.tests
+
+import io.ktor.network.selector.*
+import io.ktor.network.sockets.*
+import io.ktor.network.sockets.Socket
+import kotlinx.coroutines.experimental.*
+import kotlinx.coroutines.experimental.io.*
+import org.junit.*
+import org.junit.rules.*
+import java.net.ServerSocket
+import java.util.concurrent.*
+import kotlin.concurrent.*
+import kotlin.test.*
+
+class ClientSocketTest {
+    private val exec = Executors.newCachedThreadPool()
+    private val selector = ActorSelectorManager(exec.asCoroutineDispatcher())
+    private var server: Pair<ServerSocket, Thread>? = null
+
+    @get:Rule
+    val timeout = Timeout(15L, TimeUnit.SECONDS)
+
+    @get:Rule
+    val errors = ErrorCollector()
+
+    @After
+    fun tearDown() {
+        server?.let { (server, thread) ->
+            server.close()
+            thread.interrupt()
+        }
+        selector.close()
+        exec.shutdown()
+    }
+
+    @Test
+    fun testConnect() {
+        server { it.close() }
+
+        client {
+        }
+    }
+
+    @Test
+    fun testRead() {
+        server { client ->
+            client.getOutputStream().use { o ->
+                o.write("123".toByteArray())
+                o.flush()
+            }
+        }
+
+        client { socket ->
+            val bb = ByteBuffer.allocate(3)
+            val channel = socket.openReadChannel()
+            channel.readFully(bb)
+            assertEquals("123", String(bb.array()))
+        }
+    }
+
+    @Test
+    fun testWrite() {
+        server { client ->
+            assertEquals("123", client.getInputStream().reader().readText())
+        }
+
+        client { socket ->
+            val channel = socket.openWriteChannel(true)
+            channel.writeStringUtf8("123")
+        }
+    }
+
+    @Test
+    fun testReadParts() {
+        server { client ->
+            client.getOutputStream().use { o ->
+                o.write("0123456789".toByteArray())
+                o.flush()
+            }
+        }
+
+        client { socket ->
+            assertEquals("0123456789", socket.openReadChannel().readUTF8Line())
+        }
+    }
+
+    private fun client(block: suspend (Socket) -> Unit) {
+        runBlocking {
+            aSocket(selector).tcp().connect(server!!.first.localSocketAddress).use {
+                block(it)
+            }
+        }
+    }
+
+    private fun server(block: (java.net.Socket) -> Unit) {
+        val server = java.net.ServerSocket(0)
+        val thread = thread(start = false) {
+            try {
+                while (true) {
+                    val client = try {
+                        server.accept()
+                    } catch (t: Throwable) {
+                        break
+                    }
+
+                    client.use(block)
+                }
+            } catch (t: Throwable) {
+                errors.addError(t)
+            }
+        }
+
+        this.server = Pair(server, thread)
+        thread.start()
+    }
+}
---
+++ ktor-network/test/io/ktor/network/sockets/tests/ServerSocketTest.kt
@@ -0,0 +1,120 @@
+package io.ktor.network.sockets.tests
+
+import io.ktor.network.selector.*
+import io.ktor.network.sockets.*
+import kotlinx.coroutines.experimental.*
+import kotlinx.coroutines.experimental.io.*
+import org.junit.*
+import org.junit.rules.*
+import java.nio.channels.*
+import java.util.concurrent.*
+import kotlin.concurrent.*
+import kotlin.test.*
+
+class ServerSocketTest {
+    private val exec = Executors.newCachedThreadPool()
+    private var tearDown = false
+    private val selector = ActorSelectorManager(exec.asCoroutineDispatcher())
+    private var client: Pair<java.net.Socket, Thread>? = null
+    private var server = CompletableDeferred<ServerSocket>()
+    private var failure: Throwable? = null
+    private val bound = CountDownLatch(1)
+
+    @get:Rule
+    val timeout = Timeout(15L, TimeUnit.SECONDS)
+
+    @After
+    fun tearDown() {
+        tearDown = true
+
+        client?.let { (s, t) ->
+            s.close()
+            t.interrupt()
+        }
+        server.cancel()
+
+        selector.close()
+        failure?.let { throw it }
+        exec.shutdown()
+    }
+
+    @Test
+    fun testBindAndAccept() {
+        server {  }
+        client {  }
+    }
+
+    @Test
+    fun testRead() {
+        server { client ->
+            assertEquals("123", client.openReadChannel().readUTF8Line())
+        }
+
+        client { socket ->
+            socket.getOutputStream().use { os ->
+                os.write("123".toByteArray())
+            }
+        }
+    }
+
+    @Test
+    fun testWrite() {
+        server { client ->
+            val channel = client.openWriteChannel(true)
+            channel.writeStringUtf8("123")
+        }
+
+        client { socket ->
+            assertEquals("123", socket.getInputStream().reader().use { it.readText() })
+        }
+    }
+
+    private fun server(block: suspend (Socket) -> Unit) {
+        launch(CommonPool) {
+            try {
+                val server = aSocket(selector).tcp().bind(null)
+                this@ServerSocketTest.server.complete(server)
+
+                bound.countDown()
+
+                loop@ while (failure == null) {
+                    server.accept().use {
+                        try {
+                            block(it)
+                        } catch (t: Throwable) {
+                            addFailure(t)
+                        }
+                    }
+                }
+            } catch (e: ClosedChannelException) {
+            } catch (e: CancelledKeyException) {
+            } catch (t: Throwable) {
+                this@ServerSocketTest.server.completeExceptionally(t)
+                addFailure(t)
+            }
+        }
+    }
+
+    private fun client(block: (java.net.Socket) -> Unit) {
+        val address = runBlocking { server.await().localAddress }
+        val client = java.net.Socket().apply { connect(address) }
+
+        val thread = thread(start = false) {
+            try {
+                client.use {
+                    block(it)
+                }
+            } catch (t: Throwable) {
+                addFailure(t)
+            }
+        }
+
+        this.client = Pair(client, thread)
+        thread.start()
+        thread.join()
+    }
+
+    private fun addFailure(t: Throwable) {
+        failure?.addSuppressed(t) ?: run { failure = t }
+    }
+}
