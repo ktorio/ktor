81120afb0e61501b1e6f6e93837b0b07466717ac
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2017-10-31 15:22:13 +0300

    Add multithreaded test

---
+++ ktor-client/ktor-client-tests/src/io/ktor/client/tests/MultithreadedTest.kt
@@ -0,0 +1,47 @@
+package io.ktor.client.tests
+
+import io.ktor.application.*
+import io.ktor.client.*
+import io.ktor.client.backend.*
+import io.ktor.client.tests.utils.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.jetty.*
+import kotlinx.coroutines.experimental.*
+import org.junit.*
+import org.junit.Assert.*
+import java.net.*
+import java.util.concurrent.atomic.*
+
+
+abstract class MultithreadedTest(factory: HttpClientBackendFactory) : TestWithKtor(factory) {
+    private val DEFAULT_SIZE = 100_000
+    private val counter: AtomicInteger = AtomicInteger()
+    private val port: Int = ServerSocket(0).use { it.localPort }
+
+    override val server: ApplicationEngine = embeddedServer(Jetty, port) {
+        routing {
+            get("/") {
+                call.respondText(counter.incrementAndGet().toString())
+            }
+        }
+    }
+
+    @Test
+    fun numberTest() = runBlocking {
+        val client = createClient()
+
+        val result = List(DEFAULT_SIZE) {
+            async {
+                return@async client.get<String>("http://127.0.0.1:$port").toInt()
+            }
+        }.map {
+            it.await()
+        }.toSet().size
+
+        assertEquals(DEFAULT_SIZE, result)
+        assertEquals(DEFAULT_SIZE, counter.get())
+        client.close()
+    }
+}
