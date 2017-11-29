0e720294ec47b628bc6dbd4ff7df0a2b28ccbbaf
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2017-11-29 17:13:30 +0300

    Ensure ShutDownUrl feature does termination after HTTP 410 Gone

--- ktor-server/ktor-server-host-common/src/io/ktor/server/engine/ShutDownUrl.kt
+++ ktor-server/ktor-server-host-common/src/io/ktor/server/engine/ShutDownUrl.kt
@@ -5,19 +5,32 @@ import io.ktor.http.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.util.*
+import java.util.concurrent.*
 import kotlin.concurrent.*
 import kotlin.system.*
 
 class ShutDownUrl(val url: String, val exitCode: ApplicationCall.() -> Int) {
-
     suspend fun doShutdown(call: ApplicationCall) {
         call.application.log.warn("Shutdown URL was called: server is going down")
+        val application = call.application
+        val environment = application.environment
+        val exitCode = exitCode(call)
+
+        val latch = CountDownLatch(1)
         thread {
-            call.application.dispose()
-            exitProcess(exitCode(call))
+            latch.await(10L, TimeUnit.SECONDS)
+
+            if (environment is ApplicationEngineEnvironment) {
+                environment.stop()
+            } else {
+                application.dispose()
+            }
+
+            exitProcess(exitCode)
         }
 
         call.respond(HttpStatusCode.Gone)
+        latch.countDown()
     }
 
     object EngineFeature : ApplicationFeature<EnginePipeline, Configuration, ShutDownUrl> {
