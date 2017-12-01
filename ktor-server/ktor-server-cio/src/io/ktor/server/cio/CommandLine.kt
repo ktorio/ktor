a567e532c62879b8bc4812773184ca7f2264e7fd
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2017-12-01 12:54:42 +0300

    Add shutdown hook in development launcher to handle Ctrl-C (SIGINT)

--- ktor-server/ktor-server-cio/src/io/ktor/server/cio/CommandLine.kt
+++ ktor-server/ktor-server-cio/src/io/ktor/server/cio/CommandLine.kt
@@ -3,8 +3,15 @@
 package io.ktor.server.cio
 
 import io.ktor.server.engine.*
+import java.util.concurrent.*
 
 fun main(args: Array<String>) {
     val applicationEnvironment = commandLineEnvironment(args)
-    CIOApplicationEngine(applicationEnvironment, {}).start(true)
+    val engine = CIOApplicationEngine(applicationEnvironment, {})
+    Runtime.getRuntime().addShutdownHook(object : Thread() {
+        override fun run() {
+            engine.stop(3, 5, TimeUnit.SECONDS)
+        }
+    })
+    engine.start(true)
 }
