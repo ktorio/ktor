7edc60b2bd489c8ab72f6e2151600bc4aeac8d8d
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2018-01-12 18:50:09 +0300

    Remove copyAndClose

--- ktor-utils/src/io/ktor/cio/Channels.kt
+++ ktor-utils/src/io/ktor/cio/Channels.kt
@@ -1,6 +1,5 @@
 package io.ktor.cio
 
-import kotlinx.coroutines.experimental.io.*
 import java.io.*
 
 
@@ -8,8 +7,3 @@ open class ChannelIOException(message: String, exception: Exception) : IOExcepti
 class ChannelWriteException(message: String = "Cannot write to a channel", exception: Exception) : ChannelIOException(message, exception)
 class ChannelReadException(message: String = "Cannot read from a channel", exception: Exception) : ChannelIOException(message, exception)
 
-suspend fun ByteReadChannel.copyAndClose(dst: ByteWriteChannel, limit: Long = Long.MAX_VALUE): Long {
-    val count = copyTo(dst, limit)
-    dst.close()
-    return count
-}
