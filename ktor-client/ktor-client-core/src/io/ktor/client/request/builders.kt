7bac26285135fc6e8df97e57fb95ada74854aaad
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2018-01-25 13:13:19 +0300

    Add missing client builders

--- ktor-client/ktor-client-core/src/io/ktor/client/request/builders.kt
+++ ktor-client/ktor-client-core/src/io/ktor/client/request/builders.kt
@@ -19,11 +19,16 @@ suspend inline fun <reified T> HttpClient.get(builder: HttpRequestBuilder): T {
     return request(builder)
 }
 
+suspend inline fun <reified T> HttpClient.post(builder: HttpRequestBuilder): T {
+    builder.method = HttpMethod.Get
+    return request(builder)
+}
+
 suspend inline fun <reified T> HttpClient.get(
         scheme: String = "http", host: String = "localhost", port: Int = 80,
         path: String = "/",
         body: Any = EmptyContent,
-        block: HttpRequestBuilder.() -> Unit
+        block: HttpRequestBuilder.() -> Unit = {}
 ): T = request {
     url(scheme, host, port, path)
     method = HttpMethod.Get
@@ -31,23 +36,11 @@ suspend inline fun <reified T> HttpClient.get(
     apply(block)
 }
 
-suspend inline fun <reified T> HttpClient.get(
-        scheme: String = "http", host: String = "localhost", port: Int = 80,
-        path: String = "/",
-        body: Any = EmptyContent
-): T = get(scheme, host, port, path, body, {})
-
-suspend inline fun <reified T> HttpClient.get(data: URL): T = get {
-    url.takeFrom(data)
-}
-
-suspend inline fun <reified T> HttpClient.get(url: String): T = get(URL(url))
-
 suspend inline fun <reified T> HttpClient.post(
         scheme: String = "http", host: String = "localhost", port: Int = 80,
         path: String = "/",
         body: Any = EmptyContent,
-        block: HttpRequestBuilder.() -> Unit
+        block: HttpRequestBuilder.() -> Unit = {}
 ): T = request {
     url(scheme, host, port, path)
     method = HttpMethod.Post
@@ -55,16 +48,29 @@ suspend inline fun <reified T> HttpClient.post(
     apply(block)
 }
 
-suspend inline fun <reified T> HttpClient.post(
-        scheme: String = "http", host: String = "localhost", port: Int = 80,
-        path: String = "/",
-        body: Any = EmptyContent
-): T = post(scheme, host, port, path, body, {})
+suspend inline fun <reified T> HttpClient.get(
+        data: URL,
+        block: HttpRequestBuilder.() -> Unit = {}
+): T = get {
+    url.takeFrom(data)
+    block()
+}
 
-suspend inline fun <reified T> HttpClient.post(data: URL): T = post {
+suspend inline fun <reified T> HttpClient.post(
+        data: URL,
+        block: HttpRequestBuilder.() -> Unit = {}
+): T = post {
     url.takeFrom(data)
 }
 
-suspend inline fun <reified T> HttpClient.post(url: String): T = post(URL(url))
+suspend inline fun <reified T> HttpClient.get(
+        url: String,
+        block: HttpRequestBuilder.() -> Unit = {}
+): T = get(URL(url), block = block)
+
+suspend inline fun <reified T> HttpClient.post(
+        url: String,
+        block: HttpRequestBuilder.() -> Unit = {}
+): T = post(URL(url), block = block)
 
 fun request(block: HttpRequestBuilder.() -> Unit) = HttpRequestBuilder().apply(block)
