c478cf723efb2e949ff7c4574aae84580e1f098d
Carlos Ballesteros Velasco <soywiz@gmail.com>
2018-02-17 21:33:28 +0100

    Convert DevelopmentEngine.main into an object to make it more obvious

--- ktor-server/ktor-server-jetty/src/io/ktor/server/jetty/DevelopmentEngine.kt
+++ ktor-server/ktor-server-jetty/src/io/ktor/server/jetty/DevelopmentEngine.kt
@@ -1,20 +1,21 @@
-@file:JvmName("DevelopmentEngine")
-
 package io.ktor.server.jetty
 
 import io.ktor.config.*
 import io.ktor.server.engine.*
 
-/**
- * Main function for starting DevelopmentEngine with Jetty
- * Creates an embedded Jetty application with an environment built from command line arguments.
- */
-fun main(args: Array<String>) {
-    val applicationEnvironment = commandLineEnvironment(args)
-    JettyApplicationEngine(applicationEnvironment, { loadConfiguration(applicationEnvironment.config) }).start()
-}
+object DevelopmentEngine {
+    /**
+     * Main function for starting DevelopmentEngine with Jetty
+     * Creates an embedded Jetty application with an environment built from command line arguments.
+     */
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val applicationEnvironment = commandLineEnvironment(args)
+        JettyApplicationEngine(applicationEnvironment, { loadConfiguration(applicationEnvironment.config) }).start()
+    }
 
-private fun JettyApplicationEngineBase.Configuration.loadConfiguration(config: ApplicationConfig) {
-    val deploymentConfig = config.config("ktor.deployment")
-    loadCommonConfiguration(deploymentConfig)
+    private fun JettyApplicationEngineBase.Configuration.loadConfiguration(config: ApplicationConfig) {
+        val deploymentConfig = config.config("ktor.deployment")
+        loadCommonConfiguration(deploymentConfig)
+    }
 }
--- ktor-server/ktor-server-tomcat/src/io/ktor/server/tomcat/DevelopmentEngine.kt
+++ ktor-server/ktor-server-tomcat/src/io/ktor/server/tomcat/DevelopmentEngine.kt
@@ -1,14 +1,15 @@
-@file:JvmName("DevelopmentEngine")
-
 package io.ktor.server.tomcat
 
 import io.ktor.server.engine.*
 
-/**
- * Main function for starting DevelopmentEngine with Tomcat
- * Creates an embedded Tomcat application with an environment built from command line arguments.
- */
-fun main(args: Array<String>) {
-    val applicationEnvironment = commandLineEnvironment(args)
-    TomcatApplicationEngine(applicationEnvironment, {}).start(true)
+object DevelopmentEngine {
+    /**
+     * Main function for starting DevelopmentEngine with Tomcat
+     * Creates an embedded Tomcat application with an environment built from command line arguments.
+     */
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val applicationEnvironment = commandLineEnvironment(args)
+        TomcatApplicationEngine(applicationEnvironment, {}).start(true)
+    }
 }
