80f8c7bf352ac8075b8922b7f1aa94d7dc2ffdce
Carlos Ballesteros Velasco <soywiz@gmail.com>
2018-02-17 21:33:28 +0100

    Convert DevelopmentEngine.main into an object to make it more obvious

--- ktor-server/ktor-server-cio/src/io/ktor/server/cio/DevelopmentEngine.kt
+++ ktor-server/ktor-server-cio/src/io/ktor/server/cio/DevelopmentEngine.kt
@@ -1,26 +1,27 @@
-@file:JvmName("DevelopmentEngine")
-
 package io.ktor.server.cio
 
 import io.ktor.config.*
 import io.ktor.server.engine.*
 import java.util.concurrent.*
 
-fun main(args: Array<String>) {
-    val applicationEnvironment = commandLineEnvironment(args)
-    val engine = CIOApplicationEngine(applicationEnvironment, { loadConfiguration(applicationEnvironment.config) })
-    Runtime.getRuntime().addShutdownHook(object : Thread() {
-        override fun run() {
-            engine.stop(3, 5, TimeUnit.SECONDS)
-        }
-    })
-    engine.start(true)
-}
+object DevelopmentEngine {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val applicationEnvironment = commandLineEnvironment(args)
+        val engine = CIOApplicationEngine(applicationEnvironment, { loadConfiguration(applicationEnvironment.config) })
+        Runtime.getRuntime().addShutdownHook(object : Thread() {
+            override fun run() {
+                engine.stop(3, 5, TimeUnit.SECONDS)
+            }
+        })
+        engine.start(true)
+    }
 
-private fun CIOApplicationEngine.Configuration.loadConfiguration(config: ApplicationConfig) {
-    val deploymentConfig = config.config("ktor.deployment")
-    loadCommonConfiguration(deploymentConfig)
-    deploymentConfig.propertyOrNull("connectionIdleTimeoutSeconds")?.getString()?.toInt()?.let {
-        connectionIdleTimeoutSeconds = it
+    private fun CIOApplicationEngine.Configuration.loadConfiguration(config: ApplicationConfig) {
+        val deploymentConfig = config.config("ktor.deployment")
+        loadCommonConfiguration(deploymentConfig)
+        deploymentConfig.propertyOrNull("connectionIdleTimeoutSeconds")?.getString()?.toInt()?.let {
+            connectionIdleTimeoutSeconds = it
+        }
     }
 }
--- ktor-server/ktor-server-netty/src/io/ktor/server/netty/DevelopmentEngine.kt
+++ ktor-server/ktor-server-netty/src/io/ktor/server/netty/DevelopmentEngine.kt
@@ -1,29 +1,30 @@
-@file:JvmName("DevelopmentEngine")
-
 package io.ktor.server.netty
 
 import io.ktor.config.*
 import io.ktor.server.engine.*
 
-/**
- * Main function for starting DevelopmentEngine with Netty
- * Creates an embedded Netty application with an environment built from command line arguments.
- */
-fun main(args: Array<String>) {
-    val applicationEnvironment = commandLineEnvironment(args)
-    NettyApplicationEngine(applicationEnvironment, { loadConfiguration(applicationEnvironment.config) }).start()
-}
+object DevelopmentEngine {
+    /**
+     * Main function for starting DevelopmentEngine with Netty
+     * Creates an embedded Netty application with an environment built from command line arguments.
+     */
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val applicationEnvironment = commandLineEnvironment(args)
+        NettyApplicationEngine(applicationEnvironment, { loadConfiguration(applicationEnvironment.config) }).start()
+    }
 
-private fun NettyApplicationEngine.Configuration.loadConfiguration(config: ApplicationConfig) {
-    val deploymentConfig = config.config("ktor.deployment")
-    loadCommonConfiguration(deploymentConfig)
-    deploymentConfig.propertyOrNull("requestQueueLimit")?.getString()?.toInt()?.let {
-        requestQueueLimit = it
-    }
-    deploymentConfig.propertyOrNull("shareWorkGroup")?.getString()?.toBoolean()?.let {
-        shareWorkGroup = it
-    }
-    deploymentConfig.propertyOrNull("responseWriteTimeoutSeconds")?.getString()?.toInt()?.let {
-        responseWriteTimeoutSeconds = it
+    private fun NettyApplicationEngine.Configuration.loadConfiguration(config: ApplicationConfig) {
+        val deploymentConfig = config.config("ktor.deployment")
+        loadCommonConfiguration(deploymentConfig)
+        deploymentConfig.propertyOrNull("requestQueueLimit")?.getString()?.toInt()?.let {
+            requestQueueLimit = it
+        }
+        deploymentConfig.propertyOrNull("shareWorkGroup")?.getString()?.toBoolean()?.let {
+            shareWorkGroup = it
+        }
+        deploymentConfig.propertyOrNull("responseWriteTimeoutSeconds")?.getString()?.toInt()?.let {
+            responseWriteTimeoutSeconds = it
+        }
     }
 }
