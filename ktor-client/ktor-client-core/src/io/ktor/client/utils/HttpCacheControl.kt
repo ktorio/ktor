eb90caa91e432725f0fcf105aa9ada64483e5631
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2018-02-21 12:45:13 +0300

    Remove old helper methods, fix argument names

--- ktor-client/ktor-client-core/src/io/ktor/client/utils/HttpCacheControl.kt
+++ ktor-client/ktor-client-core/src/io/ktor/client/utils/HttpCacheControl.kt
@@ -19,20 +19,3 @@ object CacheControl {
     val PROXY_REVALIDATE = "proxy-revalidate"
     val S_MAX_AGE = "s-maxage"
 }
-
-fun Headers.cacheControl(): List<String> = getAll(HttpHeaders.CacheControl) ?: listOf()
-fun Headers.maxAge(): Int? = cacheControl(CacheControl.MAX_AGE)
-fun Headers.onlyIfCached(): Boolean = cacheControl(CacheControl.ONLY_IF_CACHED) ?: false
-
-fun HeadersBuilder.maxAge(value: Int) = append(HttpHeaders.CacheControl, "${CacheControl.MAX_AGE}=$value")
-
-inline fun <reified T> Headers.cacheControl(key: String): T? = when (T::class) {
-    Int::class -> cacheControl().intProperty(key) as T?
-    Boolean::class -> cacheControl().booleanProperty(key) as T?
-    else -> null
-}
-
-fun List<String>.booleanProperty(key: String): Boolean = contains(key)
-
-fun List<String>.intProperty(key: String): Int? =
-        find { it.startsWith(key) }?.split("=")?.getOrNull(1)?.toInt()
