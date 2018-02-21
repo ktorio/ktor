5d9467a8cfab42aead33db4a1c47ce0a123b009b
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2018-02-21 13:22:28 +0300

    Add url request builders, cleanup

--- ktor-client/ktor-client-core/src/io/ktor/client/request/HttpRequest.kt
+++ ktor-client/ktor-client-core/src/io/ktor/client/request/HttpRequest.kt
@@ -25,19 +25,17 @@ interface HttpRequest : HttpMessage {
 
 class HttpRequestBuilder : HttpMessageBuilder {
     val url = URLBuilder()
-    var method = HttpMethod.Get
+    var method: HttpMethod = HttpMethod.Get
     override val headers = HeadersBuilder()
     var body: Any = EmptyContent
 
-    val executionContext = CompletableDeferred<Unit>()
-
-    fun headers(block: HeadersBuilder.() -> Unit) = headers.apply(block)
-
-    fun url(block: URLBuilder.(URLBuilder) -> Unit) = url.block(url)
+    val executionContext: CompletableDeferred<Unit> = CompletableDeferred<Unit>()
 
     fun build(): HttpRequestData = HttpRequestData(
             url.build(), method, headers.build(), body, executionContext
     )
+
+    companion object
 }
 
 class HttpRequestData(
@@ -48,6 +46,8 @@ class HttpRequestData(
         val executionContext: CompletableDeferred<Unit>
 )
 
+fun HttpRequestBuilder.headers(block: HeadersBuilder.() -> Unit): HeadersBuilder = headers.apply(block)
+
 fun HttpRequestBuilder.takeFrom(builder: HttpRequestBuilder): HttpRequestBuilder {
     method = builder.method
     body = builder.body
@@ -57,20 +57,30 @@ fun HttpRequestBuilder.takeFrom(builder: HttpRequestBuilder): HttpRequestBuilder
     return this
 }
 
+fun HttpRequestBuilder.url(block: URLBuilder.() -> Unit): Unit = block(url)
+
+operator fun HttpRequestBuilder.Companion.invoke(block: URLBuilder.() -> Unit): HttpRequestBuilder =
+        HttpRequestBuilder().apply { url(block) }
+
 fun HttpRequestBuilder.url(
-        scheme: String = "http",
-        host: String = "localhost",
-        port: Int = 80,
-        path: String = "/"
-) {
+        scheme: String = "http", host: String = "localhost", port: Int = 80, path: String = "/",
+        block: URLBuilder.() -> Unit = {}
+): Unit {
     url.apply {
         protocol = URLProtocol.createOrDefault(scheme, port)
         this.host = host
         this.port = port
         encodedPath = path
+        block(url)
     }
 }
 
-fun HttpRequestBuilder.url(data: java.net.URL) {
-    url.takeFrom(data)
-}
+operator fun HttpRequestBuilder.Companion.invoke(
+        scheme: String = "http", host: String = "localhost", port: Int = 80, path: String = "/",
+        block: URLBuilder.() -> Unit = {}
+): HttpRequestBuilder = HttpRequestBuilder().apply { url(scheme, host, port, path, block) }
+
+fun HttpRequestBuilder.url(url: java.net.URL): Unit = this.url.takeFrom(url)
+
+operator fun HttpRequestBuilder.Companion.invoke(url: java.net.URL): HttpRequestBuilder =
+        HttpRequestBuilder().apply { url(url) }
