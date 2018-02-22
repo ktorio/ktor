8f8c768cf9f44a96983eb1ab1e47fa33f15f4119
Carlos Ballesteros Velasco <soywiz@gmail.com>
2018-02-22 11:41:48 +0100

    Added Kdoc for experimental locations feature

--- ktor-features/ktor-locations/src/io/ktor/locations/Location.kt
+++ ktor-features/ktor-locations/src/io/ktor/locations/Location.kt
@@ -6,19 +6,46 @@ import io.ktor.pipeline.*
 import io.ktor.routing.*
 import kotlin.reflect.*
 
+/**
+ * Annotation for classes that will act as typed routes.
+ * [path] is the route path, including class property names wrapped with curly braces.
+ */
 annotation class Location(val path: String)
 
 @Deprecated("Use Location instead", replaceWith = ReplaceWith("Location"))
 typealias location = Location
 
+/**
+ * Gets the [Application.locations] feature
+ */
 val PipelineContext<Unit, ApplicationCall>.locations get() = call.application.locations
+
+/**
+ * Gets the [Application.locations] feature
+ */
 val ApplicationCall.locations get() = application.locations
+
+/**
+ * Gets the [Application.locations] feature
+ */
 val Application.locations get() = feature(Locations)
 
+/**
+ * Registers a route [body] for a location defined by class [T].
+ *
+ * Class [T] **must** be annotated with [Location].
+ */
 inline fun <reified T : Any> Route.location(noinline body: Route.() -> Unit): Route {
     return location(T::class, body)
 }
 
+/**
+ * Registers a typed handler [body] for a `GET` location defined by class [T].
+ *
+ * Class [T] **must** be annotated with [Location].
+ *
+ * @param body receives an instance of typed location [T] as first parameter.
+ */
 inline fun <reified T : Any> Route.get(noinline body: suspend PipelineContext<Unit, ApplicationCall>.(T) -> Unit): Route {
     return location(T::class) {
         method(HttpMethod.Get) {
@@ -27,6 +54,13 @@ inline fun <reified T : Any> Route.get(noinline body: suspend PipelineContext<Un
     }
 }
 
+/**
+ * Registers a typed handler [body] for a `OPTIONS` location defined by class [T].
+ *
+ * Class [T] **must** be annotated with [Location].
+ *
+ * @param body receives an instance of typed location [T] as first parameter.
+ */
 inline fun <reified T : Any> Route.options(noinline body: suspend PipelineContext<Unit, ApplicationCall>.(T) -> Unit): Route {
     return location(T::class) {
         method(HttpMethod.Options) {
@@ -35,6 +69,13 @@ inline fun <reified T : Any> Route.options(noinline body: suspend PipelineContex
     }
 }
 
+/**
+ * Registers a typed handler [body] for a `HEAD` location defined by class [T].
+ *
+ * Class [T] **must** be annotated with [Location].
+ *
+ * @param body receives an instance of typed location [T] as first parameter.
+ */
 inline fun <reified T : Any> Route.head(noinline body: suspend PipelineContext<Unit, ApplicationCall>.(T) -> Unit): Route {
     return location(T::class) {
         method(HttpMethod.Head) {
@@ -43,6 +84,13 @@ inline fun <reified T : Any> Route.head(noinline body: suspend PipelineContext<U
     }
 }
 
+/**
+ * Registers a typed handler [body] for a `POST` location defined by class [T].
+ *
+ * Class [T] **must** be annotated with [Location].
+ *
+ * @param body receives an instance of typed location [T] as first parameter.
+ */
 inline fun <reified T : Any> Route.post(noinline body: suspend PipelineContext<Unit, ApplicationCall>.(T) -> Unit): Route {
     return location(T::class) {
         method(HttpMethod.Post) {
@@ -51,6 +99,13 @@ inline fun <reified T : Any> Route.post(noinline body: suspend PipelineContext<U
     }
 }
 
+/**
+ * Registers a typed handler [body] for a `PUT` location defined by class [T].
+ *
+ * Class [T] **must** be annotated with [Location].
+ *
+ * @param body receives an instance of typed location [T] as first parameter.
+ */
 inline fun <reified T : Any> Route.put(noinline body: suspend PipelineContext<Unit, ApplicationCall>.(T) -> Unit): Route {
     return location(T::class) {
         method(HttpMethod.Put) {
@@ -59,6 +114,13 @@ inline fun <reified T : Any> Route.put(noinline body: suspend PipelineContext<Un
     }
 }
 
+/**
+ * Registers a typed handler [body] for a `DELETE` location defined by class [T].
+ *
+ * Class [T] **must** be annotated with [Location].
+ *
+ * @param body receives an instance of typed location [T] as first parameter.
+ */
 inline fun <reified T : Any> Route.delete(noinline body: suspend PipelineContext<Unit, ApplicationCall>.(T) -> Unit): Route {
     return location(T::class) {
         method(HttpMethod.Delete) {
@@ -67,6 +129,13 @@ inline fun <reified T : Any> Route.delete(noinline body: suspend PipelineContext
     }
 }
 
+/**
+ * Registers a typed handler [body] for a `PATCH` location defined by class [T].
+ *
+ * Class [T] **must** be annotated with [Location].
+ *
+ * @param body receives an instance of typed location [T] as first parameter.
+ */
 inline fun <reified T : Any> Route.patch(noinline body: suspend PipelineContext<Unit, ApplicationCall>.(T) -> Unit): Route {
     return location(T::class) {
         method(HttpMethod.Patch) {
@@ -75,15 +144,32 @@ inline fun <reified T : Any> Route.patch(noinline body: suspend PipelineContext<
     }
 }
 
+/**
+ * Registers a route [body] for a location defined by class [data].
+ *
+ * Class [data] **must** be annotated with [Location].
+ */
 fun <T : Any> Route.location(data: KClass<T>, body: Route.() -> Unit): Route {
     val entry = application.locations.createEntry(this, data)
     return entry.apply(body)
 }
 
+/**
+ * Registers a handler [body] for a location defined by class [T].
+ *
+ * Class [T] **must** be annotated with [Location].
+ */
 inline fun <reified T : Any> Route.handle(noinline body: suspend PipelineContext<Unit, ApplicationCall>.(T) -> Unit) {
     return handle(T::class, body)
 }
 
+/**
+ * Registers a handler [body] for a location defined by class [dataClass].
+ *
+ * Class [dataClass] **must** be annotated with [Location].
+ *
+ * @param body receives an instance of typed location [dataClass] as first parameter.
+ */
 fun <T : Any> Route.handle(dataClass: KClass<T>, body: suspend PipelineContext<Unit, ApplicationCall>.(T) -> Unit) {
     handle {
         val location = locations.resolve<T>(dataClass, call)
