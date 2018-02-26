384cf8d5df182ad190310513f061f56ea02bb5e1
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2018-02-26 13:21:53 +0300

    OAuth: add typed location support in urlProvider of oauthAtLocation

--- ktor-features/ktor-locations/src/io/ktor/locations/OAuth.kt
+++ ktor-features/ktor-locations/src/io/ktor/locations/OAuth.kt
@@ -3,6 +3,7 @@ package io.ktor.locations
 import io.ktor.application.*
 import io.ktor.auth.*
 import io.ktor.client.*
+import io.ktor.util.*
 import kotlinx.coroutines.experimental.*
 import kotlin.reflect.*
 
@@ -22,7 +23,7 @@ import kotlin.reflect.*
  */
 inline fun <reified T : Any> AuthenticationPipeline.oauthAtLocation(client: HttpClient, dispatcher: CoroutineDispatcher,
                                                                     noinline providerLookup: ApplicationCall.(T) -> OAuthServerSettings?,
-                                                                    noinline urlProvider: ApplicationCall.(T, OAuthServerSettings) -> String) {
+                                                                    noinline urlProvider: ApplicationCall.(T, OAuthServerSettings) -> Any) {
     oauthWithType(T::class, client, dispatcher, providerLookup, urlProvider)
 }
 
@@ -31,18 +32,25 @@ inline fun <reified T : Any> AuthenticationPipeline.oauthAtLocation(client: Http
  *
  * @see AuthenticationPipeline.oauthAtLocation
  */
-fun <T : Any> AuthenticationPipeline.oauthWithType(type: KClass<T>,
+fun <T : Any, R : Any> AuthenticationPipeline.oauthWithType(type: KClass<T>,
                                                    client: HttpClient,
                                                    dispatcher: CoroutineDispatcher,
                                                    providerLookup: ApplicationCall.(T) -> OAuthServerSettings?,
-                                                   urlProvider: ApplicationCall.(T, OAuthServerSettings) -> String) {
+                                                   urlProvider: ApplicationCall.(T, OAuthServerSettings) -> R) {
 
     fun ApplicationCall.resolve(): T {
         return application.locations.resolve<T>(type, this)
     }
 
     fun ApplicationCall.providerLookupLocal(): OAuthServerSettings? = providerLookup(resolve())
-    fun ApplicationCall.urlProviderLocal(s: OAuthServerSettings): String = urlProvider(resolve(), s)
+    fun ApplicationCall.urlProviderLocal(s: OAuthServerSettings): String {
+        val location = urlProvider(resolve(), s)
+        if (location is String) return location
+
+        return url {
+            encodedPath = locations.href(location)
+        }
+    }
 
     oauth(client, dispatcher,
             ApplicationCall::providerLookupLocal,
