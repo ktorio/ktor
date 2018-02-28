997030c86afda167c8a54c99d0371888baf4121d
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2018-02-28 12:04:45 +0300

    Fix ServerSocketTest: avoid false ClosedSelectorException and related

--- ktor-network/test/io/ktor/network/sockets/tests/ServerSocketTest.kt
+++ ktor-network/test/io/ktor/network/sockets/tests/ServerSocketTest.kt
@@ -7,8 +7,10 @@ import kotlinx.coroutines.experimental.io.*
 import org.junit.*
 import org.junit.Test
 import org.junit.rules.*
+import java.io.*
 import java.nio.channels.*
 import java.util.concurrent.*
+import java.util.concurrent.CancellationException
 import kotlin.concurrent.*
 import kotlin.test.*
 
@@ -17,7 +19,9 @@ class ServerSocketTest {
     private var tearDown = false
     private val selector = ActorSelectorManager(exec.asCoroutineDispatcher())
     private var client: Pair<java.net.Socket, Thread>? = null
-    private var server = CompletableDeferred<ServerSocket>()
+    private var serverSocket = CompletableDeferred<ServerSocket>()
+    @Volatile
+    private var server: Job? = null
     private var failure: Throwable? = null
     private val bound = CountDownLatch(1)
 
@@ -32,11 +36,17 @@ class ServerSocketTest {
             s.close()
             t.interrupt()
         }
-        server.cancel()
+        serverSocket.cancel()
+        server?.cancel()
+
+        runBlocking {
+            serverSocket.join()
+            server?.join()
+        }
 
         selector.close()
-        failure?.let { throw it }
         exec.shutdown()
+        failure?.let { throw it }
     }
 
     @Test
@@ -71,33 +81,38 @@ class ServerSocketTest {
     }
 
     private fun server(block: suspend (Socket) -> Unit) {
-        launch(CommonPool) {
+        val job = launch(CommonPool, start = CoroutineStart.LAZY) {
             try {
                 val server = aSocket(selector).tcp().bind(null)
-                this@ServerSocketTest.server.complete(server)
+                this@ServerSocketTest.serverSocket.complete(server)
 
                 bound.countDown()
 
-                loop@ while (failure == null) {
+                loop@ while (failure == null && isActive) {
                     server.accept().use {
                         try {
                             block(it)
+                        } catch (ignore: CancellationException) {
                         } catch (t: Throwable) {
-                            addFailure(t)
+                            addFailure(IOException("client handler failed", t))
                         }
                     }
                 }
+            } catch (ignore: CancellationException) {
             } catch (e: ClosedChannelException) {
             } catch (e: CancelledKeyException) {
             } catch (t: Throwable) {
-                this@ServerSocketTest.server.completeExceptionally(t)
-                addFailure(t)
+                val e = IOException("server failed", t)
+                this@ServerSocketTest.serverSocket.completeExceptionally(e)
+                addFailure(e)
             }
         }
+        server = job
+        job.start()
     }
 
     private fun client(block: (java.net.Socket) -> Unit) {
-        val address = runBlocking { server.await().localAddress }
+        val address = runBlocking { serverSocket.await().localAddress }
         val client = java.net.Socket().apply { connect(address) }
 
         val thread = thread(start = false) {
