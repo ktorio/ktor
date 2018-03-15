965c0a3177043bb81a731901438f79da945f3af9
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2018-03-15 23:46:27 +0300

    CIO: use MPSC queue in selector manager

--- ktor-network/src/io/ktor/network/selector/ActorSelectorManager.kt
+++ ktor-network/src/io/ktor/network/selector/ActorSelectorManager.kt
@@ -2,9 +2,12 @@ package io.ktor.network.selector
 
 import kotlinx.coroutines.experimental.*
 import kotlinx.coroutines.experimental.channels.*
+import kotlinx.coroutines.experimental.internal.*
 import java.io.*
 import java.nio.channels.*
 import java.util.concurrent.atomic.*
+import kotlin.coroutines.experimental.*
+import kotlin.coroutines.experimental.intrinsics.*
 
 class ActorSelectorManager(dispatcher: CoroutineDispatcher) : SelectorManagerSupport(), Closeable {
     @Volatile
@@ -15,35 +18,47 @@ class ActorSelectorManager(dispatcher: CoroutineDispatcher) : SelectorManagerSup
     @Volatile
     private var inSelect = false
 
-    private val mb = actor<Selectable>(dispatcher, capacity = kotlinx.coroutines.experimental.channels.Channel.UNLIMITED) {
-        provider.openSelector()!!.use { selector ->
-            selectorRef = selector
-            try {
-                process(channel, selector)
-            } catch (t: Throwable) {
-                channel.close()
-                cancelAllSuspensions(selector, t)
-            } finally {
-                channel.close()
-                cancelAllSuspensions(selector, null)
-                selectorRef = null
-            }
+    private val continuation = AtomicReference<Continuation<Selectable?>?>(null)
 
-            channel.consumeEach {
-                cancelAllSuspensions(it, ClosedSendChannelException("Failed to apply interest: selector closed"))
+    @Volatile
+    private var closed = false
+
+    private val mb = LockFreeMPSCQueue<Selectable>()
+
+    init {
+        launch(dispatcher) {
+            provider.openSelector()!!.use { selector ->
+                selectorRef = selector
+                try {
+                    process(mb, selector)
+                } catch (t: Throwable) {
+                    closed = true
+                    mb.close()
+                    cancelAllSuspensions(selector, t)
+                } finally {
+                    closed = true
+                    mb.close()
+                    selectorRef = null
+                    cancelAllSuspensions(selector, null)
+                }
+
+                while (true) {
+                    val m = mb.removeFirstOrNull() ?: break
+                    cancelAllSuspensions(m, ClosedSendChannelException("Failed to apply interest: selector closed"))
+                }
             }
         }
     }
 
-    private suspend fun process(mb: ReceiveChannel<Selectable>, selector: Selector) {
-        while (!mb.isClosedForReceive) {
+    private suspend fun process(mb: LockFreeMPSCQueue<Selectable>, selector: Selector) {
+        while (!closed) {
             processInterests(mb, selector)
 
             if (pending > 0) {
                 if (select(selector) > 0) {
                     handleSelectedKeys(selector.selectedKeys(), selector.keys())
                 } else {
-                    val received = mb.poll()
+                    val received = mb.removeFirstOrNull()
                     if (received != null) applyInterest(selector, received)
                     else yield()
                 }
@@ -78,9 +93,9 @@ class ActorSelectorManager(dispatcher: CoroutineDispatcher) : SelectorManagerSup
         }
     }
 
-    private fun processInterests(mb: ReceiveChannel<Selectable>, selector: Selector) {
+    private fun processInterests(mb: LockFreeMPSCQueue<Selectable>, selector: Selector) {
         while (true) {
-            val selectable = mb.poll() ?: break
+            val selectable = mb.removeFirstOrNull() ?: break
             applyInterest(selector, selectable)
         }
     }
@@ -97,15 +112,52 @@ class ActorSelectorManager(dispatcher: CoroutineDispatcher) : SelectorManagerSup
 
     override fun publishInterest(selectable: Selectable) {
         try {
-            mb.offer(selectable)
-            selectWakeup()
+            if (mb.addLast(selectable)) {
+                val cont = continuation.getAndSet(null)
+                if (cont != null) {
+                    cont.resume(null)
+                } else {
+                    selectWakeup()
+                }
+            }
+            else throw IOException("Failed to publish interest to the queue")
         } catch (t: Throwable) {
             cancelAllSuspensions(selectable, t)
         }
     }
 
+
+    private suspend fun LockFreeMPSCQueue<Selectable>.receiveOrNull(): Selectable? {
+        return removeFirstOrNull() ?: receiveOrNullSuspend()
+    }
+
+    private suspend fun LockFreeMPSCQueue<Selectable>.receiveOrNullSuspend(): Selectable? {
+        while (true) {
+            val m = removeFirstOrNull()
+            if (m != null) return m
+
+            if (closed) return null
+
+            val m3 = suspendCoroutineUninterceptedOrReturn<Selectable?> {
+                continuation.set(it)
+
+                val m2 = removeFirstOrNull()
+                if ((m2 != null || closed) && continuation.compareAndSet(it, null)) m2
+                else COROUTINE_SUSPENDED
+            }
+
+            if (m3 != null) return m3
+        }
+    }
+
     override fun close() {
+        closed = true
         mb.close()
-        selectWakeup()
+        val cont = continuation.getAndSet(null)
+        if (cont != null) {
+            cont.resume(null)
+        } else {
+            selectWakeup()
+        }
     }
 }
--- ktor-network/src/io/ktor/network/selector/Selectable.kt
+++ ktor-network/src/io/ktor/network/selector/Selectable.kt
@@ -1,21 +1,14 @@
 package io.ktor.network.selector
 
 import kotlinx.coroutines.experimental.*
-import kotlinx.coroutines.experimental.internal.*
 import java.io.*
 import java.nio.channels.*
 import java.util.concurrent.atomic.*
-
-abstract class SelectableNode : LockFreeLinkedListNode() {
-    abstract val selectable: Selectable
-}
 
 /**
  * A selectable entity with selectable NIO [channel], [interestedOps] subscriptions
  */
 interface Selectable : Closeable, DisposableHandle {
-
-    val node: SelectableNode
 
     val suspensions: InterestSuspensionsMap
 
@@ -35,10 +28,7 @@ interface Selectable : Closeable, DisposableHandle {
     fun interestOp(interest: SelectInterest, state: Boolean)
 }
 
-internal open class SelectableBase(override val channel: SelectableChannel) : Selectable, SelectableNode() {
-    override val selectable: Selectable get() = this
-    override val node get() = this
-
+internal open class SelectableBase(override val channel: SelectableChannel) : Selectable {
     override val suspensions = InterestSuspensionsMap()
 
     @Volatile
