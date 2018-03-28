eee6cb9798c9a0a69f5d1c8d0f7af134f0d46b40
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2018-03-28 11:50:05 +0300

    Add client-CIO missing chunked header

--- ktor-client/ktor-client-cio/src/io/ktor/client/engine/cio/utils.kt
+++ ktor-client/ktor-client-cio/src/io/ktor/client/engine/cio/utils.kt
@@ -11,6 +11,11 @@ import kotlinx.coroutines.experimental.io.*
 internal suspend fun CIOHttpRequest.write(output: ByteWriteChannel, content: OutgoingContent) {
     val builder = RequestResponseBuilder()
 
+    val contentLength = headers[HttpHeaders.ContentLength] ?: content.contentLength?.toString()
+    val contentEncoding = headers[HttpHeaders.TransferEncoding]
+    val responseEncoding = content.headers[HttpHeaders.TransferEncoding]
+    val chunked = contentLength == null || responseEncoding == "chunked" || contentEncoding == "chunked"
+
     try {
         builder.requestLine(method, url.fullPath, HttpProtocolVersion.HTTP_1_1.toString())
         builder.headerLine("Host", url.hostWithPort)
@@ -31,11 +36,14 @@ internal suspend fun CIOHttpRequest.write(output: ByteWriteChannel, content: Out
             builder.headerLine(name, value)
         }
 
-        val contentLength = headers[HttpHeaders.ContentLength] ?: content.contentLength?.toString()
         val contentType = headers[HttpHeaders.ContentType] ?: content.contentType?.toString()
 
         contentLength?.let { builder.headerLine(HttpHeaders.ContentLength, it) }
         contentType?.let { builder.headerLine(HttpHeaders.ContentType, it) }
+
+        if (chunked && contentEncoding == null && responseEncoding == null && content !is OutgoingContent.NoContent) {
+            builder.headerLine(HttpHeaders.TransferEncoding, "chunked")
+        }
 
         builder.emptyLine()
         output.writePacket(builder.build())
@@ -47,16 +55,15 @@ internal suspend fun CIOHttpRequest.write(output: ByteWriteChannel, content: Out
     if (content is OutgoingContent.NoContent)
         return
 
-    val contentLength = headers[HttpHeaders.ContentLength]?.toLong() ?: content.contentLength
-    val chunked = contentLength == null || content.headers[HttpHeaders.TransferEncoding] == "chunked" || headers[HttpHeaders.TransferEncoding] == "chunked"
-
     val chunkedJob: EncoderJob? = if (chunked) encodeChunked(output, Unconfined) else null
     val channel = chunkedJob?.channel ?: output
 
     try {
         when (content) {
             is OutgoingContent.NoContent -> return
-            is OutgoingContent.ByteArrayContent -> channel.writeFully(content.bytes())
+            is OutgoingContent.ByteArrayContent -> {
+                channel.writeFully(content.bytes())
+            }
             is OutgoingContent.ReadChannelContent -> content.readFrom().joinTo(channel, closeOnEnd = false)
             is OutgoingContent.WriteChannelContent -> content.writeTo(channel)
             is OutgoingContent.ProtocolUpgrade -> throw UnsupportedContentTypeException(content)
