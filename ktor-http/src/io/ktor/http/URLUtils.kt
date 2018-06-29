fef343bb88e4707373c9c36f2fc3eeed10c4e8bb
soywiz <soywiz@gmail.com>
2018-06-29 11:33:25 +0200

    Url and URLBuilder improvements - Url: Construction from full URL, Url.fullUrl, Url.toString - URLBuilder.takeFrom: Consder user and password too

--- ktor-http/src/io/ktor/http/URLUtils.kt
+++ ktor-http/src/io/ktor/http/URLUtils.kt
@@ -11,6 +11,12 @@ fun URLBuilder.takeFrom(uri: URI) {
     uri.scheme?.let { protocol = URLProtocol.createOrDefault(it) }
     uri.host?.let { host = it }
 
+    if (uri.userInfo != null && uri.userInfo.isNotEmpty()) {
+        val parts = uri.userInfo.split(":")
+        user = parts.first()
+        password = parts.getOrNull(1)
+    }
+
     port = if (uri.port > 0) {
         uri.port
     } else if (uri.scheme != null) {
@@ -75,3 +81,17 @@ val Url.fullPath: String
     }
 
 val Url.hostWithPort: String get() = "$host:$port"
+val Url.hostWithPortIfRequired: String get() = if (port == protocol.defaultPort) host else hostWithPort
+val Url.fullUrl: String get() {
+    var full = "${protocol.name}://"
+    if (user != null) {
+        full += "$user"
+        if (password != null) {
+            full += ":$password"
+        }
+        full += "@"
+    }
+    full += "$hostWithPortIfRequired$fullPath"
+    if (fragment.isNotEmpty()) full += "#$fragment"
+    return full
+}
---
+++ ktor-http/test/io/ktor/tests/http/UrlTest.kt
@@ -0,0 +1,47 @@
+package io.ktor.tests.http
+
+import io.ktor.http.*
+import org.junit.Test
+import kotlin.test.*
+
+class UrlTest {
+    @Test
+    fun testBasicProperties() {
+        val urlString = "https://ktor.io/quickstart/?query=string&param=value&param=value2#fragment"
+        val url = Url(urlString)
+        assertEquals("https", url.protocol.name)
+        assertEquals(443, url.port)
+        assertEquals(443, url.protocol.defaultPort)
+        assertEquals("ktor.io", url.host)
+        assertEquals("/quickstart/", url.encodedPath)
+        assertEquals(parametersOf("query" to listOf("string"), "param" to listOf("value", "value2")), url.parameters)
+        assertEquals("fragment", url.fragment)
+        assertEquals(null, url.user)
+        assertEquals(null, url.password)
+        assertEquals(false, url.trailingQuery)
+        assertEquals(urlString, url.toString())
+        assertEquals(urlString, url.fullUrl)
+    }
+
+    @Test
+    fun testOtherPort() {
+        val urlString = "http://user:password@ktor.io:8080/"
+        val url = Url(urlString)
+        assertEquals("http", url.protocol.name)
+        assertEquals(8080, url.port)
+        assertEquals("user", url.user)
+        assertEquals("password", url.password)
+        assertEquals(urlString, url.fullUrl)
+    }
+
+    @Test
+    fun testAuthInfoWithoutPassword() {
+        val urlString = "http://user@ktor.io:8080/"
+        val url = Url(urlString)
+        assertEquals("http", url.protocol.name)
+        assertEquals(8080, url.port)
+        assertEquals("user", url.user)
+        assertEquals(null, url.password)
+        assertEquals(urlString, url.fullUrl)
+    }
+}
