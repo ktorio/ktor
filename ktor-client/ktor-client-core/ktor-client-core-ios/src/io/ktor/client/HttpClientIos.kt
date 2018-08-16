063d5ab438a01612e52e05df846440351df0bdd9
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2018-08-16 10:47:07 +0300

    Fix ios HttpClient signature

--- ktor-client/ktor-client-core/ktor-client-core-ios/src/io/ktor/client/HttpClientIos.kt
+++ ktor-client/ktor-client-core/ktor-client-core-ios/src/io/ktor/client/HttpClientIos.kt
@@ -1,9 +1,8 @@
 package io.ktor.client
 
-import io.ktor.client.engine.*
 import io.ktor.client.engine.ios.*
 
 actual fun HttpClient(
     useDefaultTransformers: Boolean,
-    block: HttpClientConfig.() -> Unit
+    block: HttpClientConfig<*>.() -> Unit
 ): HttpClient = HttpClient(IosClient(), useDefaultTransformers, block)
