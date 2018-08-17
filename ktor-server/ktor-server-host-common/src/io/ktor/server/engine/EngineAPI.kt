8d2b1d5a4110c63766a1d999b43496e5e61b095d
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2018-08-17 16:13:41 +0300

    Add EngineAPI marker annotation
    So end users will never apply engine-related API accidentally
    however it is still possible to use this API in required cases

---
+++ ktor-server/ktor-server-host-common/src/io/ktor/server/engine/EngineAPI.kt
@@ -0,0 +1,8 @@
+package io.ktor.server.engine
+
+/**
+ * API marked with this annotation is not intended to be used by end users
+ * unless a custom server engine implementation is required
+ */
+@Experimental(Experimental.Level.WARNING)
+annotation class EngineAPI
