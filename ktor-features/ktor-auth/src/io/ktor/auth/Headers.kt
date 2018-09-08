87ddb20b78a079542be97127ac597f57000403af
soywiz <soywiz@gmail.com>
2018-09-08 13:54:42 +0200

    Adds API documentation to the auth Headers

--- ktor-features/ktor-auth/src/io/ktor/auth/Headers.kt
+++ ktor-features/ktor-auth/src/io/ktor/auth/Headers.kt
@@ -7,19 +7,74 @@ import java.nio.charset.*
 import java.util.*
 import kotlin.collections.LinkedHashMap
 
+/**
+ * Contains the standard auth schemes.
+ */
 object AuthScheme {
+    /**
+     * Basic Authentication described in the RFC-7617
+     *
+     * ```
+     * response = base64("$user:$password")
+     * ```
+     *
+     * see https://tools.ietf.org/html/rfc7617)
+     */
     val Basic = "Basic"
+
+    /**
+     * Digest Authentication described in the RFC-2069:
+     *
+     * ```
+     * HA1 = MD5("$username:$realm:$password") // What's usually stored
+     * HA2 = MD5("$method:$digestURI")
+     * response = MD5("$HA1:$nonce:$HA2") // The client and the server sends and checks this.
+     * ```
+     *
+     * see https://tools.ietf.org/html/rfc2069
+     */
     val Digest = "Digest"
+
+    /**
+     * Described in the RFC-4599:
+     *
+     * see https://www.ietf.org/rfc/rfc4559.txt
+     */
     val Negotiate = "Negotiate"
+
+    /**
+     * OAuth Authentication described in the RFC-6749:
+     *
+     * see https://tools.ietf.org/html/rfc6749
+     */
     val OAuth = "OAuth"
 }
 
+/**
+ * Describes how a header should be encoded.
+ */
 enum class HeaderValueEncoding {
+    /**
+     * The header will be quoted only when required.
+     */
     QUOTED_WHEN_REQUIRED,
+
+    /**
+     * The header will be quoted always.
+     */
     QUOTED_ALWAYS,
+
+    /**
+     * The header will be URI-encoded as described in the RFC-3986:
+     *
+     * see https://tools.ietf.org/html/rfc3986#page-12
+     */
     URI_ENCODE
 }
 
+/**
+ * Parses an authorization header from a [ApplicationRequest] returning a [HttpAuthHeader].
+ */
 fun ApplicationRequest.parseAuthorizationHeader(): HttpAuthHeader? = authorization()?.let {
     parseAuthorizationHeader(it)
 }
@@ -29,6 +84,9 @@ private val authSchemePattern = "\\S+".toRegex()
 private val valuePatternPart = """("((\\.)|[^\\"])*")|[^\s,]*"""
 private val parameterPattern = "\\s*,?\\s*($token68Pattern)\\s*=\\s*($valuePatternPart)\\s*,?\\s*".toRegex()
 
+/**
+ * Parses an authorization header [headerValue] into a [HttpAuthHeader].
+ */
 fun parseAuthorizationHeader(headerValue: String): HttpAuthHeader? {
     val schemeRegion = authSchemePattern.find(headerValue) ?: return null
     val authScheme = schemeRegion.value
@@ -45,11 +103,19 @@ fun parseAuthorizationHeader(headerValue: String): HttpAuthHeader? {
     return HttpAuthHeader.Parameterized(authScheme, parameters)
 }
 
+/**
+ * Describes an authentication header with a mandatory [authScheme] that usually is a standard [AuthScheme].
+ *
+ * This can be of type [HttpAuthHeader.Single] or [HttpAuthHeader.Parameterized].
+ */
 sealed class HttpAuthHeader(val authScheme: String) {
     init {
         require(authScheme.matches(token68Pattern)) { "invalid authScheme value: it should be token" }
     }
 
+    /**
+     * Describes an authentication header that is represented by a single [blob].
+     */
     class Single(authScheme: String, val blob: String) : HttpAuthHeader(authScheme) {
         init {
             require(blob.matches(token68Pattern)) { "invalid blob value: it should be token68 but it is $blob" }
@@ -69,6 +135,9 @@ sealed class HttpAuthHeader(val authScheme: String) {
         }
     }
 
+    /**
+     * Describes a parameterized authentication header that is represented by a set of [parameters] encoded with [encoding].
+     */
     class Parameterized(authScheme: String, val parameters: List<HeaderValueParam>, val encoding: HeaderValueEncoding = HeaderValueEncoding.QUOTED_WHEN_REQUIRED) : HttpAuthHeader(authScheme) {
         constructor(authScheme: String, parameters: Map<String, String>, encoding: HeaderValueEncoding = HeaderValueEncoding.QUOTED_WHEN_REQUIRED) : this(authScheme, parameters.entries.map { HeaderValueParam(it.key, it.value) }, encoding)
 
@@ -78,8 +147,16 @@ sealed class HttpAuthHeader(val authScheme: String) {
             }
         }
 
+        /**
+         * Copies this [Parameterized] appending a new parameter [name] [value].
+         */
         fun withParameter(name: String, value: String) = Parameterized(authScheme, this.parameters + HeaderValueParam(name, value))
+
         override fun render(encoding: HeaderValueEncoding) = parameters.joinToString(", ", prefix = "$authScheme ") { "${it.name}=${it.value.encode(encoding)}" }
+
+        /**
+         * Tries to extract the value a parameter [name]. Returns null when not found.
+         */
         fun parameter(name: String) = parameters.singleOrNull { it.name == name }?.value
 
         private fun String.encode(encoding: HeaderValueEncoding) = when (encoding) {
@@ -101,14 +178,27 @@ sealed class HttpAuthHeader(val authScheme: String) {
         }
     }
 
+    /**
+     * Encodes the header with a specified [encoding].
+     */
     abstract fun render(encoding: HeaderValueEncoding): String
+
+    /**
+     * Encodes the header with the default [HeaderValueEncoding] for this header.
+     */
     abstract fun render(): String
 
+    /**
+     * Encodes the header with the default [HeaderValueEncoding] for this header.
+     */
     override fun toString(): String {
         return render()
     }
 
     companion object {
+        /**
+         * Generates an [AuthScheme.Basic] challenge as a [HttpAuthHeader].
+         */
         fun basicAuthChallenge(realm: String, charset: Charset?) = Parameterized(
             AuthScheme.Basic, LinkedHashMap<String, String>().apply {
                 put(Parameters.Realm, realm)
@@ -118,6 +208,9 @@ sealed class HttpAuthHeader(val authScheme: String) {
             }
         )
 
+        /**
+         * Generates an [AuthScheme.Digest] challenge as a [HttpAuthHeader].
+         */
         fun digestAuthChallenge(realm: String, nonce: String = nextNonce(), domain: List<String> = emptyList(), opaque: String? = null, stale: Boolean? = null, algorithm: String = "MD5")
                 = Parameterized(AuthScheme.Digest, linkedMapOf<String, String>().apply {
             put("realm", realm)
@@ -135,6 +228,9 @@ sealed class HttpAuthHeader(val authScheme: String) {
         }, HeaderValueEncoding.QUOTED_ALWAYS)
     }
 
+    /**
+     * Standard parameters for [Parameterized] [HttpAuthHeader].
+     */
     object Parameters {
         val Realm = "realm"
         val Charset = "charset"
