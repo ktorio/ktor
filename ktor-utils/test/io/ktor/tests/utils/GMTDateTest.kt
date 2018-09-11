2bd17762d6908ddfba199e84173f72a5c10dd0e6
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2018-09-11 07:56:24 +0300

    Fix GMTDate construction in native

---
+++ ktor-utils/test/io/ktor/tests/utils/GMTDateTest.kt
@@ -0,0 +1,25 @@
+package io.ktor.tests.utils
+
+import io.ktor.util.date.*
+import kotlin.test.*
+
+class GMTDateTest {
+
+    @Test
+    fun constructionTest() {
+        val first = GMTDate()
+        val second = GMTDate(timestamp = first.timestamp)
+
+        val third = GMTDate(
+            first.seconds,
+            first.minutes,
+            first.hours,
+            first.dayOfMonth,
+            first.month,
+            first.year
+        )
+
+        assertEquals(first, second)
+        assertEquals(first, third)
+    }
+}
