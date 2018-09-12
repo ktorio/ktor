4483609f601bfb9da8ad70f7f38f5293b111abb1
Carlos Ballesteros Velasco <soywiz@gmail.com>
2018-09-12 17:54:11 +0200

    Simplify MockEngine response generation

--- ktor-client/ktor-client-mock/src/io/ktor/client/engine/mock/MockEngineConfig.kt
+++ ktor-client/ktor-client-mock/src/io/ktor/client/engine/mock/MockEngineConfig.kt
@@ -1,10 +1,8 @@
 package io.ktor.client.engine.mock
 
-import io.ktor.client.call.*
 import io.ktor.client.engine.*
-import io.ktor.client.request.*
 import io.ktor.client.response.*
 
 class MockEngineConfig : HttpClientEngineConfig() {
-    lateinit var check: suspend (HttpRequest, HttpClientCall) -> HttpResponse
+    lateinit var check: suspend MockHttpRequest.() -> HttpResponse
 }
