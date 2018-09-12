249a348fed01d0e0d184041c4bc41da6dcbaea1c
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2018-09-12 13:27:22 +0300

    Move mimes; fix osx tests

--- ktor-http/ktor-http-jvm/src/io/ktor/http/HttpStatusCodeJvm.kt
+++
@@ -1,8 +0,0 @@
-package io.ktor.http
-
-import kotlin.reflect.full.*
-
-internal actual fun allStatusCodes(): List<HttpStatusCode> =
-    HttpStatusCode.Companion::class.memberProperties
-        .filter { it.returnType.classifier == HttpStatusCode::class }
-        .map { it.get(HttpStatusCode.Companion) as HttpStatusCode }
