9b1ce0881545173ab4edc6802df0822407af6d72
Carlos Ballesteros Velasco <soywiz@gmail.com>
2018-09-19 12:51:33 +0200

    Add `AuthHeadersTest.testWithParameterParameterBehaviour`

--- ktor-features/ktor-auth/test/io/ktor/tests/auth/AuthHeadersTest.kt
+++ ktor-features/ktor-auth/test/io/ktor/tests/auth/AuthHeadersTest.kt
@@ -6,6 +6,15 @@ import kotlin.test.*
 
 class AuthHeadersTest {
     @Test
+    fun testWithParameterParameterBehaviour() {
+        val header = HttpAuthHeader.Parameterized("authScheme", mapOf("a" to "a"))
+        // It uses singleOrNull, and since withParameter appends, it would be twice and would return null.
+        // Verify if this is the expected behaviour.
+        assertEquals(null, header.withParameter("a", "A").parameter("a"))
+        assertEquals("B", header.withParameter("b", "B").parameter("b"))
+    }
+
+    @Test
     fun testWithParameterPreservesEncoding() {
         for (encoding in listOf(
             HeaderValueEncoding.QUOTED_ALWAYS, HeaderValueEncoding.URI_ENCODE
