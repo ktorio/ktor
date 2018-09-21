5862fa4700c50c2b65b49b0f020b58e35a3a9606
Johannes Jensen <spand@spand.org>
2018-09-21 13:16:14 +0200

    Support generic types in JsonContent.

--- ktor-client/ktor-client-features/ktor-client-json/ktor-client-json-jvm/src/io/ktor/client/features/json/DefaultJvm.kt
+++ ktor-client/ktor-client-features/ktor-client-json/ktor-client-json-jvm/src/io/ktor/client/features/json/DefaultJvm.kt
@@ -2,6 +2,7 @@ package io.ktor.client.features.json
 
 import io.ktor.client.call.*
 import java.lang.reflect.*
+import java.lang.reflect.Type
 import java.util.*
 
 actual fun defaultSerializer(): JsonSerializer {
@@ -19,8 +20,10 @@ internal actual fun safeUnwrapJsonContent(typeInfo: TypeInfo): TypeInfo? {
     val parameterizedType = typeInfo.reifiedType as? ParameterizedType
     // JSonContent is final so we can avoid subclass check
     return if (parameterizedType?.rawType == JsonContent::class.java){
-        val reifiedType = parameterizedType.actualTypeArguments.first()!!
-        val type = (reifiedType as Class<*>).kotlin
-        TypeInfo(type, reifiedType)
+        val wrappedType: Type = parameterizedType.actualTypeArguments.first()!!
+        val backingClass = (wrappedType as? ParameterizedType)?.rawType ?: wrappedType
+        val backingClassObj = backingClass as? Class<*> ?: error("Wrapped type was unexpectedly $backingClass")
+        val type = backingClassObj.kotlin
+        TypeInfo(type, wrappedType)
     } else null
 }
