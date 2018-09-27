dabf45f482501685f4b400da02f53afd47cb9cb9
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2018-09-27 10:00:07 +0300

    Improve code style

--- ktor-http/ktor-http-cio/src/io/ktor/http/cio/Pipeline.kt
+++ ktor-http/ktor-http-cio/src/io/ktor/http/cio/Pipeline.kt
@@ -18,10 +18,12 @@ fun lastHttpRequest(http11: Boolean, connectionOptions: ConnectionOptions?): Boo
 /**
  * HTTP request handler function
  */
-typealias HttpRequestHandler = suspend CoroutineScope.(request: Request,
-                                                       input: ByteReadChannel,
-                                                       output: ByteWriteChannel,
-                                                       upgraded: CompletableDeferred<Boolean>?) -> Unit
+typealias HttpRequestHandler = suspend CoroutineScope.(
+    request: Request,
+    input: ByteReadChannel,
+    output: ByteWriteChannel,
+    upgraded: CompletableDeferred<Boolean>?
+) -> Unit
 
 /**
  * HTTP pipeline coroutine name
@@ -40,23 +42,23 @@ val RequestHandlerCoroutine: CoroutineName = CoroutineName("request-handler")
 
 @Suppress("KDocMissingDocumentation")
 @Deprecated("Use startConnectionPipeline with CoroutineScope receiver")
-fun startConnectionPipeline(input: ByteReadChannel,
-                            output: ByteWriteChannel,
-                            parentJob: CoroutineContext?,
-                            ioContext: CoroutineContext,
-                            callContext: CoroutineContext,
-                            timeout: WeakTimeoutQueue,
-                            handler: HttpRequestHandler): Job {
-    return CoroutineScope(ioContext + (parentJob ?: EmptyCoroutineContext))
-            .startConnectionPipeline(input, output, timeout) { request: Request,
-                                                              _input: ByteReadChannel,
-                                                              _output: ByteWriteChannel,
-                                                              upgraded: CompletableDeferred<Boolean>?  ->
-                withContext(callContext) {
-                    handler(request, _input, _output, upgraded)
-                }
-            }
-}
+fun startConnectionPipeline(
+    input: ByteReadChannel,
+    output: ByteWriteChannel,
+    parentJob: CoroutineContext?,
+    ioContext: CoroutineContext,
+    callContext: CoroutineContext,
+    timeout: WeakTimeoutQueue,
+    handler: HttpRequestHandler
+): Job = CoroutineScope(ioContext + (parentJob ?: EmptyCoroutineContext))
+    .startConnectionPipeline(input, output, timeout) { request: Request,
+                                                       _input: ByteReadChannel,
+                                                       _output: ByteWriteChannel,
+                                                       upgraded: CompletableDeferred<Boolean>? ->
+        withContext(callContext) {
+            handler(request, _input, _output, upgraded)
+        }
+    }
 
 /**
  * Start connection HTTP pipeline invoking [handler] for every request.
@@ -69,151 +71,158 @@ fun startConnectionPipeline(input: ByteReadChannel,
  *
  * @return pipeline job
  */
-fun CoroutineScope.startConnectionPipeline(input: ByteReadChannel,
-                                           output: ByteWriteChannel,
-                                           timeout: WeakTimeoutQueue,
-                                           handler: HttpRequestHandler): Job {
-
-    return launch(HttpPipelineCoroutine) {
-        val outputsActor = actor<ByteReadChannel>(
-                context = HttpPipelineWriterCoroutine,
-                capacity = 3,
-                start = CoroutineStart.UNDISPATCHED) {
-            try {
-                val receiveChildOrNull = suspendLambda<CoroutineScope, ByteReadChannel?> { channel.receiveOrNull() }
-                while (true) {
-                    val child = timeout.withTimeout(receiveChildOrNull) ?: break
-                    try {
-                        child.joinTo(output, false)
-//                        child.copyTo(output)
-                        output.flush()
-                    } catch (t: Throwable) {
-                        if (child is ByteWriteChannel) {
-                            child.close(t)
-                        }
-                    }
-                }
-            } catch (t: Throwable) {
-                output.close(t)
-            } finally {
-                output.close()
-            }
-        }
-
-        val requestContext = RequestHandlerCoroutine + Dispatchers.Unconfined
-
+fun CoroutineScope.startConnectionPipeline(
+    input: ByteReadChannel,
+    output: ByteWriteChannel,
+    timeout: WeakTimeoutQueue,
+    handler: HttpRequestHandler
+): Job = launch(HttpPipelineCoroutine) {
+    val outputsActor = actor<ByteReadChannel>(
+        context = HttpPipelineWriterCoroutine,
+        capacity = 3,
+        start = CoroutineStart.UNDISPATCHED
+    ) {
         try {
-            while (true) {  // parse requests loop
-                val request = try {
-                    parseRequest(input) ?: break
-                } catch (io: IOException) {
-                    throw io
-                } catch (cancelled: CancellationException) {
-                    throw cancelled
-                } catch (parseFailed: Throwable) { // try to write 400 Bad Request
-                    // TODO log parseFailed?
-                    val bc = ByteChannel()
-                    if (outputsActor.offer(bc)) {
-                        bc.writePacket(BadRequestPacket.copy())
-                        bc.close()
-                    }
-                    outputsActor.close()
-                    break // end pipeline loop
-                }
-
-                val response = ByteChannel()
-
-                val transferEncoding = request.headers["Transfer-Encoding"]
-                val upgrade = request.headers["Upgrade"]
-                val contentType = request.headers["Content-Type"]
-                val http11 = request.version == "HTTP/1.1"
-
-                val connectionOptions: ConnectionOptions?
-                val contentLength: Long
-                val expectedHttpBody: Boolean
-                val expectedHttpUpgrade: Boolean
-
+            val receiveChildOrNull = suspendLambda<CoroutineScope, ByteReadChannel?> { channel.receiveOrNull() }
+            while (true) {
+                val child = timeout.withTimeout(receiveChildOrNull) ?: break
                 try {
-                    outputsActor.send(response)
+                    child.joinTo(output, false)
+//                        child.copyTo(output)
+                    output.flush()
                 } catch (t: Throwable) {
-                    request.release()
-                    throw t
-                }
-
-                try {
-                    val contentLengthIndex = request.headers.find("Content-Length")
-                    connectionOptions = ConnectionOptions.parse(request.headers["Connection"])
-                    if (contentLengthIndex != -1) {
-                        contentLength = request.headers.valueAt(contentLengthIndex).parseDecLong()
-                        if (request.headers.find("Content-Length", contentLengthIndex + 1) != -1) {
-                            throw ParserException("Duplicate Content-Length header")
-                        }
-                    } else {
-                        contentLength = -1
-                    }
-                    expectedHttpBody = expectHttpBody(request.method, contentLength, transferEncoding, connectionOptions, contentType)
-                    expectedHttpUpgrade = !expectedHttpBody && expectHttpUpgrade(request.method, upgrade, connectionOptions)
-                } catch (t: Throwable) {
-                    request.release()
-                    response.writePacket(BadRequestPacket.copy())
-                    response.close()
-                    throw t
-                }
-
-                val requestBody = if (expectedHttpBody || expectedHttpUpgrade) ByteChannel(true) else ByteReadChannel.Empty
-                val upgraded = if (expectedHttpUpgrade) CompletableDeferred<Boolean>() else null
-
-                launch(requestContext, start = CoroutineStart.UNDISPATCHED) {
-                    try {
-                        handler(request, requestBody, response, upgraded)
-                    } catch (t: Throwable) {
-                        response.close(t)
-                        upgraded?.completeExceptionally(t)
-                    } finally {
-                        response.close()
-                        upgraded?.complete(false)
+                    if (child is ByteWriteChannel) {
+                        child.close(t)
                     }
                 }
-
-                if (upgraded != null) {
-                    if (upgraded.await()) { // suspend pipeline until we know if upgrade performed?
-                        outputsActor.close()
-                        input.copyAndClose(requestBody as ByteChannel)
-                        break
-                    } else if (!expectedHttpBody && requestBody is ByteChannel) { // not upgraded, for example 404
-                        requestBody.close()
-                    }
-                }
-
-                if (expectedHttpBody && requestBody is ByteWriteChannel) {
-                    try {
-                        parseHttpBody(contentLength, transferEncoding, connectionOptions, input, requestBody)
-                    } catch (cause: Throwable) {
-                        requestBody.close(cause)
-                        throw cause
-                    } finally {
-                        requestBody.close()
-                    }
-                }
-
-                if (isLastHttpRequest(http11, connectionOptions)) break
             }
-        } catch (t: IOException) { // already handled
-            coroutineContext.cancel()
         } catch (t: Throwable) {
-            coroutineContext.cancel(t)
+            output.close(t)
         } finally {
-            outputsActor.close()
+            output.close()
         }
+    }
+
+    val requestContext = RequestHandlerCoroutine + Dispatchers.Unconfined
+
+    try {
+        while (true) {  // parse requests loop
+            val request = try {
+                parseRequest(input) ?: break
+            } catch (io: IOException) {
+                throw io
+            } catch (cancelled: CancellationException) {
+                throw cancelled
+            } catch (parseFailed: Throwable) { // try to write 400 Bad Request
+                // TODO log parseFailed?
+                val bc = ByteChannel()
+                if (outputsActor.offer(bc)) {
+                    bc.writePacket(BadRequestPacket.copy())
+                    bc.close()
+                }
+                outputsActor.close()
+                break // end pipeline loop
+            }
+
+            val response = ByteChannel()
+
+            val transferEncoding = request.headers["Transfer-Encoding"]
+            val upgrade = request.headers["Upgrade"]
+            val contentType = request.headers["Content-Type"]
+            val http11 = request.version == "HTTP/1.1"
+
+            val connectionOptions: ConnectionOptions?
+            val contentLength: Long
+            val expectedHttpBody: Boolean
+            val expectedHttpUpgrade: Boolean
+
+            try {
+                outputsActor.send(response)
+            } catch (cause: Throwable) {
+                request.release()
+                throw cause
+            }
+
+            try {
+                val contentLengthIndex = request.headers.find("Content-Length")
+                connectionOptions = ConnectionOptions.parse(request.headers["Connection"])
+                if (contentLengthIndex != -1) {
+                    contentLength = request.headers.valueAt(contentLengthIndex).parseDecLong()
+                    if (request.headers.find("Content-Length", contentLengthIndex + 1) != -1) {
+                        throw ParserException("Duplicate Content-Length header")
+                    }
+                } else {
+                    contentLength = -1
+                }
+                expectedHttpBody = expectHttpBody(
+                    request.method, contentLength, transferEncoding, connectionOptions, contentType
+                )
+                expectedHttpUpgrade = !expectedHttpBody &&
+                    expectHttpUpgrade(request.method, upgrade, connectionOptions)
+            } catch (cause: Throwable) {
+                request.release()
+                response.writePacket(BadRequestPacket.copy())
+                response.close()
+                throw cause
+            }
+
+            val requestBody = if (expectedHttpBody || expectedHttpUpgrade)
+                ByteChannel(true)
+            else
+                ByteReadChannel.Empty
+
+            val upgraded = if (expectedHttpUpgrade) CompletableDeferred<Boolean>() else null
+
+            launch(requestContext, start = CoroutineStart.UNDISPATCHED) {
+                try {
+                    handler(request, requestBody, response, upgraded)
+                } catch (cause: Throwable) {
+                    response.close(cause)
+                    upgraded?.completeExceptionally(cause)
+                } finally {
+                    response.close()
+                    upgraded?.complete(false)
+                }
+            }
+
+            if (upgraded != null) {
+                if (upgraded.await()) { // suspend pipeline until we know if upgrade performed?
+                    outputsActor.close()
+                    input.copyAndClose(requestBody as ByteChannel)
+                    break
+                } else if (!expectedHttpBody && requestBody is ByteChannel) { // not upgraded, for example 404
+                    requestBody.close()
+                }
+            }
+
+            if (expectedHttpBody && requestBody is ByteWriteChannel) {
+                try {
+                    parseHttpBody(contentLength, transferEncoding, connectionOptions, input, requestBody)
+                } catch (cause: Throwable) {
+                    requestBody.close(cause)
+                    throw cause
+                } finally {
+                    requestBody.close()
+                }
+            }
+
+            if (isLastHttpRequest(http11, connectionOptions)) break
+        }
+    } catch (cause: IOException) { // already handled
+        coroutineContext.cancel()
+    } catch (cause: Throwable) {
+        coroutineContext.cancel(cause)
+    } finally {
+        outputsActor.close()
     }
 }
 
 private val BadRequestPacket =
-        RequestResponseBuilder().apply {
-            responseLine("HTTP/1.0", HttpStatusCode.BadRequest.value, "Bad Request")
-            headerLine("Connection", "close")
-            emptyLine()
-        }.build()
+    RequestResponseBuilder().apply {
+        responseLine("HTTP/1.0", HttpStatusCode.BadRequest.value, "Bad Request")
+        headerLine("Connection", "close")
+        emptyLine()
+    }.build()
 
 private fun isLastHttpRequest(http11: Boolean, connectionOptions: ConnectionOptions?): Boolean {
     return when {
