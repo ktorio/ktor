c16857723e49231e56dae3928ab33a970e263e8d
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2018-10-10 11:27:53 +0300

    Use UTF-8 encoding as fallback in client response config

--- ktor-client/ktor-client-core/src/io/ktor/client/response/HttpResponseConfig.kt
+++ ktor-client/ktor-client-core/src/io/ktor/client/response/HttpResponseConfig.kt
@@ -1,10 +1,15 @@
 package io.ktor.client.response
 
-import kotlinx.io.charsets.Charset
+import kotlinx.io.charsets.*
 
 open class HttpResponseConfig {
     /**
      * Default [Charset] for response content if not specified, the initial value is ISO_8859_1.
+     * If ISO_8859_1 is not available, UTF-8 is used as a fallback.
      */
-    var defaultCharset: Charset = Charset.forName("ISO_8859_1")
+    var defaultCharset: Charset = try {
+        Charset.forName("ISO_8859_1")
+    } catch (_: Throwable) {
+        Charsets.UTF_8
+    }
 }
