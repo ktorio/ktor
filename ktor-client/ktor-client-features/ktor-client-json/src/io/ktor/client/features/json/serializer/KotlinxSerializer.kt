a6c3cd972fa27790ebdd043cbaad2a6a2935bfc2
sommd <d.j.s.1050@gmail.com>
2018-10-17 00:33:59 +1100

    Add default serializers to KotlinxSerializer

--- ktor-client/ktor-client-features/ktor-client-json/src/io/ktor/client/features/json/serializer/KotlinxSerializer.kt
+++ ktor-client/ktor-client-features/ktor-client-json/src/io/ktor/client/features/json/serializer/KotlinxSerializer.kt
@@ -6,11 +6,24 @@ import io.ktor.client.response.*
 import io.ktor.http.*
 import io.ktor.http.content.*
 import kotlinx.serialization.*
+import kotlinx.serialization.internal.*
 import kotlinx.serialization.json.*
 import kotlin.reflect.*
 
 class KotlinxSerializer : JsonSerializer {
-    private val mappers = mutableMapOf<KClass<Any>, KSerializer<Any>>()
+    @Suppress("UNCHECKED_CAST")
+    private val mappers = mutableMapOf(
+            Unit::class as KClass<Any> to UnitSerializer as KSerializer<Any>,
+            Boolean::class as KClass<Any> to BooleanSerializer as KSerializer<Any>,
+            Byte::class as KClass<Any> to ByteSerializer as KSerializer<Any>,
+            Short::class as KClass<Any> to ShortSerializer as KSerializer<Any>,
+            Int::class as KClass<Any> to IntSerializer as KSerializer<Any>,
+            Long::class as KClass<Any> to LongSerializer as KSerializer<Any>,
+            Float::class as KClass<Any> to FloatSerializer as KSerializer<Any>,
+            Double::class as KClass<Any> to DoubleSerializer as KSerializer<Any>,
+            Char::class as KClass<Any> to CharSerializer as KSerializer<Any>,
+            String::class as KClass<Any> to StringSerializer as KSerializer<Any>
+    )
 
     /**
      * Set mapping from [type] to generated [KSerializer].
