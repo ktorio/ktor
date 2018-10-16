8800f10ef1d83b58a57d72e991642f8c4746ab34
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2018-10-16 17:06:25 +0300

    Fix experimental annotation in benchmark

--- ktor-server/ktor-server-benchmarks/src-jmh/io/ktor/server/benchmarks/StringValuesBenchmark.kt
+++ ktor-server/ktor-server-benchmarks/src-jmh/io/ktor/server/benchmarks/StringValuesBenchmark.kt
@@ -5,6 +5,7 @@ import io.ktor.util.*
 import org.openjdk.jmh.annotations.*
 
 @State(Scope.Benchmark)
+@UseExperimental(InternalAPI::class)
 class StringValuesBenchmark {
     private val headers = valuesOf("A" to listOf("B"), "C" to listOf("D"))
 
