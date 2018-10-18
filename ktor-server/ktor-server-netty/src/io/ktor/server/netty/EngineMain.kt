e02a14bed1f02ae949428e5c2843a6c3260a71e7
soywiz <soywiz@gmail.com>
2018-10-18 19:00:06 +0200

    Rename `DevelopmentEngine` to `EngineMain`

--- ktor-server/ktor-server-netty/src/io/ktor/server/netty/EngineMain.kt
+++ ktor-server/ktor-server-netty/src/io/ktor/server/netty/EngineMain.kt
@@ -6,7 +6,7 @@ import io.ktor.server.engine.*
 /**
  * Netty development engine
  */
-object EmbeddedEngine {
+object EngineMain {
     /**
      * Main function for starting DevelopmentEngine with Netty
      * Creates an embedded Netty application with an environment built from command line arguments.
@@ -32,7 +32,7 @@ object EmbeddedEngine {
     }
 }
 
-@Deprecated("", replaceWith = ReplaceWith("EmbeddedEngine"), level = DeprecationLevel.ERROR)
+@Deprecated("", replaceWith = ReplaceWith("EngineMain"), level = DeprecationLevel.ERROR)
 object DevelopmentEngine {
-    @JvmStatic fun main(args: Array<String>) = EmbeddedEngine.main(args)
+    @JvmStatic fun main(args: Array<String>) = EngineMain.main(args)
 }
