44dfe139b9cb5b68dea6e3d58319e6b0620a5167
soywiz <soywiz@gmail.com>
2018-10-18 17:57:22 +0200

    Rename `DevelopmentEngine` to `EmbeddedEngine`. Fixes #425

--- ktor-server/ktor-server-jetty/src/io/ktor/server/jetty/EmbeddedEngine.kt
+++ ktor-server/ktor-server-jetty/src/io/ktor/server/jetty/EmbeddedEngine.kt
@@ -6,7 +6,7 @@ import io.ktor.server.engine.*
 /**
  * Jetty development engine
  */
-object DevelopmentEngine {
+object EmbeddedEngine {
     /**
      * Main function for starting DevelopmentEngine with Jetty
      * Creates an embedded Jetty application with an environment built from command line arguments.
@@ -21,4 +21,9 @@ object DevelopmentEngine {
         val deploymentConfig = config.config("ktor.deployment")
         loadCommonConfiguration(deploymentConfig)
     }
+}
+
+@Deprecated("", replaceWith = ReplaceWith("EmbeddedEngine"), level = DeprecationLevel.ERROR)
+object DevelopmentEngine {
+    @JvmStatic fun main(args: Array<String>) = EmbeddedEngine.main(args)
 }
--- ktor-server/ktor-server-tomcat/src/io/ktor/server/tomcat/EmbeddedEngine.kt
+++ ktor-server/ktor-server-tomcat/src/io/ktor/server/tomcat/EmbeddedEngine.kt
@@ -5,7 +5,7 @@ import io.ktor.server.engine.*
 /**
  * Tomcat development engine
  */
-object DevelopmentEngine {
+object EmbeddedEngine {
     /**
      * Main function for starting DevelopmentEngine with Tomcat
      * Creates an embedded Tomcat application with an environment built from command line arguments.
@@ -15,4 +15,9 @@ object DevelopmentEngine {
         val applicationEnvironment = commandLineEnvironment(args)
         TomcatApplicationEngine(applicationEnvironment, {}).start(true)
     }
+}
+
+@Deprecated("", replaceWith = ReplaceWith("EmbeddedEngine"), level = DeprecationLevel.ERROR)
+object DevelopmentEngine {
+    @JvmStatic fun main(args: Array<String>) = EmbeddedEngine.main(args)
 }
