584e5f20a962ebd58d5f8c3a7e1c3c272a54a0be
Alexander Gorshenev <alexander.gorshenev@jetbrains.com>
2018-10-22 19:14:55 +0300

    A couple of tweaks to make eap13-plugin branch buildable on macos.

--- ktor-client/ktor-client-ios/native/src/io/ktor/client/engine/ios/Ios.kt
+++ ktor-client/ktor-client-ios/native/src/io/ktor/client/engine/ios/Ios.kt
@@ -4,7 +4,7 @@ import io.ktor.client.engine.*
 
 object Ios : HttpClientEngineFactory<IosClientEngineConfig> {
     override fun create(block: IosClientEngineConfig.() -> Unit): HttpClientEngine =
-        IosClientEngine(HttpClientEngineConfig().apply(block))
+        IosClientEngine(IosClientEngineConfig().apply(block))
 }
 
 fun IosClient(): HttpClientEngineFactory<IosClientEngineConfig> = Ios
