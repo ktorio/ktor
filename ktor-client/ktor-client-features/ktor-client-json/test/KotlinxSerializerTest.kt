e8845c23a32c7eead7edc3308b8e9bff6ba14903
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2018-10-29 11:38:16 +0300

    Add js support in kotlinx.serialization feature
        Close #666

---
+++ ktor-client/ktor-client-features/ktor-client-json/test/KotlinxSerializerTest.kt
@@ -0,0 +1,53 @@
+package io.ktor.client.features.json
+
+import io.ktor.client.call.*
+import io.ktor.client.features.json.serializer.*
+import io.ktor.client.response.*
+import io.ktor.http.*
+import io.ktor.http.content.*
+import io.ktor.util.date.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.io.*
+import kotlinx.serialization.*
+import kotlin.coroutines.*
+import kotlin.jvm.*
+import kotlin.test.*
+
+@Serializable
+internal data class User(val id: Long, val login: String)
+
+class KotlinxSerializerTest {
+
+    @Test
+    fun registerCustomTest() {
+        val serializer = KotlinxSerializer().apply {
+            register(User::class.serializer())
+        }
+
+        val user = User(1, "vasya")
+        val actual = serializer.testWrite(user)
+        assertEquals("{\"id\":1,\"login\":\"vasya\"}", actual)
+//        assertEquals(user, serializer.testRead(actual))
+    }
+
+    private fun JsonSerializer.testWrite(data: Any): String =
+        (write(data) as? TextContent)?.text ?: error("Failed to get serialized $data")
+
+    private suspend inline fun <reified T : Any> JsonSerializer.testRead(data: String): T {
+        val info = typeInfo<T>()
+
+        val response = object : HttpResponse {
+            override val call: HttpClientCall get() = TODO()
+            override val status: HttpStatusCode get() = TODO()
+            override val version: HttpProtocolVersion get() = TODO()
+            override val requestTime: GMTDate get() = TODO()
+            override val responseTime: GMTDate get() = TODO()
+            override val headers: Headers get() = TODO()
+            override val coroutineContext: CoroutineContext get() = TODO()
+
+            override val content: ByteReadChannel = ByteReadChannel(data)
+        }
+
+        return read(info, response) as T
+    }
+}
