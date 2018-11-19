41db339d8bf2c6a4c1b0f1ff6fe1ef013409f686
Vitalii Dmitriev <dmitvitalii@gmail.com>
2018-11-19 20:35:55 +0300

    Add a secondary constructor with varargs to MockConfigValue

--- ktor-server/ktor-server-tests/test/io/ktor/tests/server/application/MockConfigValue.kt
+++ ktor-server/ktor-server-tests/test/io/ktor/tests/server/application/MockConfigValue.kt
@@ -3,6 +3,9 @@ package io.ktor.tests.server.application
 import io.ktor.config.ApplicationConfigValue
 
 class MockConfigValue(private val stringValue: String, private val listValue: List<String>) : ApplicationConfigValue {
+
+    constructor(stringValue: String, vararg listValues: String) : this(stringValue, listValues.toList())
+
     override fun getString() = stringValue
 
     override fun getList() = listValue
