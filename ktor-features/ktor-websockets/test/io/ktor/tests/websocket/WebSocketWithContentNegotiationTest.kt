3574a9b48fb02ab588cf18e5843294e840cf436c
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2018-11-22 15:40:48 +0300

    Prevent ContentNegotiation from handling byte channel pipeline (#745)

---
+++ ktor-features/ktor-websockets/test/io/ktor/tests/websocket/WebSocketWithContentNegotiationTest.kt
@@ -0,0 +1,45 @@
+package io.ktor.tests.websocket
+
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.http.cio.websocket.Frame
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.server.testing.*
+import io.ktor.util.pipeline.*
+import io.ktor.websocket.*
+import kotlin.test.*
+
+class WebSocketWithContentNegotiationTest {
+
+    @Test
+    fun test(): Unit = withTestApplication {
+        application.install(WebSockets)
+        application.install(ContentNegotiation) {
+            val converter = object : ContentConverter {
+                override suspend fun convertForSend(
+                    context: PipelineContext<Any, ApplicationCall>,
+                    contentType: ContentType,
+                    value: Any
+                ): Any? = fail("convertForSend shouldn't be invoked")
+
+                override suspend fun convertForReceive(context: PipelineContext<ApplicationReceiveRequest, ApplicationCall>): Any? {
+                    fail("convertForReceive shouldn't be invoked")
+                }
+            }
+
+            register(ContentType.Any, converter)
+        }
+
+        application.routing {
+            webSocket("/") {
+                outgoing.send(Frame.Text("OK"))
+            }
+        }
+
+        handleWebSocketConversation("/", {}) { incoming, outgoing ->
+            incoming.receive()
+        }
+    }
+}
