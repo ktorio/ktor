a36ed9fe231dade8c3e03916a56938267e115065
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2018-11-26 16:45:21 +0300

    Increase okhttp client engine timeout

--- ktor-client/ktor-client-okhttp/src/io/ktor/client/engine/okhttp/OkHttpConfig.kt
+++ ktor-client/ktor-client-okhttp/src/io/ktor/client/engine/okhttp/OkHttpConfig.kt
@@ -2,9 +2,13 @@ package io.ktor.client.engine.okhttp
 
 import io.ktor.client.engine.*
 import okhttp3.*
+import java.util.concurrent.*
 
 class OkHttpConfig : HttpClientEngineConfig() {
-    internal var config: OkHttpClient.Builder.() -> Unit = {}
+    internal var config: OkHttpClient.Builder.() -> Unit = {
+        readTimeout(60, TimeUnit.SECONDS)
+        writeTimeout(60, TimeUnit.SECONDS)
+    }
 
     fun config(block: OkHttpClient.Builder.() -> Unit) {
         val oldConfig = config
