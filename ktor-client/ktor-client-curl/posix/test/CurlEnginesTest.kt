2a561fdfef7a795602e725d84cd4355a8154eab1
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2018-12-17 13:16:18 +0300

    fixup! Setup native tests

--- ktor-client/ktor-client-curl/posix/test/CurlEnginesTest.kt
+++ ktor-client/ktor-client-curl/posix/test/CurlEnginesTest.kt
@@ -5,18 +5,19 @@ import io.ktor.client.engine.*
 import io.ktor.client.engine.curl.*
 import io.ktor.client.features.json.*
 import io.ktor.client.features.json.serializer.*
+import io.ktor.client.features.logging.*
 import io.ktor.client.request.*
 import kotlinx.io.core.*
 import kotlinx.serialization.*
 import kotlin.test.*
 import kotlinx.coroutines.*
+import kotlinx.serialization.json.*
 
 @Serializable
 data class HttpBinResponse(
     val url: String,
     val args: Map<String, String>,
-    val headers: Map<String, String>,
-    val origin: String
+    val headers: Map<String, String>
 )
 
 class CurlEnginesTest {
@@ -24,17 +25,44 @@ class CurlEnginesTest {
     @Test
     fun getTest(): Unit = clientTest { client ->
         val response = client.get<HttpBinResponse>("http://httpbin.org/get")
-        println(response)
+        val expected = HttpBinResponse(
+            "http://httpbin.org/get",
+            emptyMap(),
+            mapOf(
+                "Accept" to "application/json",
+                "Connection" to "close",
+                "Host" to "httpbin.org"
+            )
+        )
+
+        assertEquals(expected, response)
     }
 
     @Test
-    fun postTest() {
+    fun postTest() = clientTest { client ->
+        val response = client.post<HttpBinResponse>("http://httpbin.org/post") {
+            body = "Hello, bin!"
+        }
+
+        val expected = HttpBinResponse(
+            "http://httpbin.org/post",
+            emptyMap(),
+            mapOf(
+                "Content-Type" to "text/plain; charset=UTF-8",
+                "Accept" to "application/json",
+                "Content-Length" to "11",
+                "Connection" to "close",
+                "Host" to "httpbin.org"
+            )
+        )
+
+        assertEquals(expected, response)
     }
 
     private fun clientTest(block: suspend (HttpClient) -> Unit): Unit = runBlocking {
         HttpClient(Curl) {
             install(JsonFeature) {
-                serializer = KotlinxSerializer().apply {
+                serializer = KotlinxSerializer(JSON.nonstrict).apply {
                     register(HttpBinResponse.serializer())
                 }
             }
