774133b1c4ce7f35b20d5e54dd5e69a939cf1fcd
Luca Spinazzola <luca@talkshop.co>
2019-01-08 02:25:35 -0800

    On jvm, break up long logs that would be truncated by android's max length of 4068 characters
    https://stackoverflow.com/a/48284047/1363742

--- ktor-client/ktor-client-features/ktor-client-logging/jvm/src/io/ktor/client/features/logging/LoggerJvm.kt
+++ ktor-client/ktor-client-features/ktor-client-logging/jvm/src/io/ktor/client/features/logging/LoggerJvm.kt
@@ -7,6 +7,36 @@ actual val Logger.Companion.DEFAULT: Logger
     get() = object : Logger {
         private val delegate = LoggerFactory.getLogger(HttpClient::class.java)!!
         override fun log(message: String) {
-            delegate.info(message)
+            logLong(message)
+        }
+
+        /***
+         * Workaround to log long messages that would otherwise be truncated in Android.
+         */
+        fun logLong(message: String) {
+            val MAX_INDEX = 4000
+            val MIN_INDEX = 3000
+
+            // String to be logged is longer than the max...
+            if (message.length > MAX_INDEX) {
+                var theSubstring = message.substring(0, MAX_INDEX)
+                var theIndex = MAX_INDEX
+
+                // Try to find a substring break at a line end.
+                theIndex = theSubstring.lastIndexOf('\n')
+                if (theIndex >= MIN_INDEX) {
+                    theSubstring = theSubstring.substring(0, theIndex)
+                } else {
+                    theIndex = MAX_INDEX
+                }
+
+                // Log the substring.
+                delegate.info(theSubstring)
+
+                // Recursively log the remainder.
+                logLong(message.substring(theIndex))
+            } else {
+                delegate.info(message)
+            }// String to be logged is shorter than the max...
         }
     }
