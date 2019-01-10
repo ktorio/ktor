22213edd2a10626f265c408e02aab21455ed46e7
Luca Spinazzola <luca@talkshop.co>
2019-01-09 20:06:15 -0800

    revert changes to default logger on jvm. Add a workaround logger for Android that uses MaxLengthLogger

--- ktor-client/ktor-client-features/ktor-client-logging/jvm/src/io/ktor/client/features/logging/LoggerJvm.kt
+++ ktor-client/ktor-client-features/ktor-client-logging/jvm/src/io/ktor/client/features/logging/LoggerJvm.kt
@@ -7,36 +7,48 @@ actual val Logger.Companion.DEFAULT: Logger
     get() = object : Logger {
         private val delegate = LoggerFactory.getLogger(HttpClient::class.java)!!
         override fun log(message: String) {
-            logLong(message)
-        }
-
-        /***
-         * Workaround to log long messages that would otherwise be truncated in Android.
-         */
-        fun logLong(message: String) {
-            val MAX_INDEX = 4000
-            val MIN_INDEX = 3000
-
-            // String to be logged is longer than the max...
-            if (message.length > MAX_INDEX) {
-                var theSubstring = message.substring(0, MAX_INDEX)
-                var theIndex = MAX_INDEX
-
-                // Try to find a substring break at a line end.
-                theIndex = theSubstring.lastIndexOf('\n')
-                if (theIndex >= MIN_INDEX) {
-                    theSubstring = theSubstring.substring(0, theIndex)
-                } else {
-                    theIndex = MAX_INDEX
-                }
-
-                // Log the substring.
-                delegate.info(theSubstring)
-
-                // Recursively log the remainder.
-                logLong(message.substring(theIndex))
-            } else {
-                delegate.info(message)
-            }// String to be logged is shorter than the max...
+            delegate.info(message)
         }
     }
+
+
+/***
+ * Workaround Logger to log long messages that would otherwise be truncated in Android.
+ */
+val Logger.Companion.ANDROID: Logger
+    get() = MaxLengthLogger()
+
+
+class MaxLengthLogger(val MAX_INDEX : Int = 4000) : Logger {
+    private val delegate = LoggerFactory.getLogger(HttpClient::class.java)!!
+    override fun log(message: String) {
+        logLong(message)
+    }
+
+    fun logLong(message: String) {
+
+        val MIN_INDEX = 3000
+
+        // String to be logged is longer than the max...
+        if (message.length > MAX_INDEX) {
+            var theSubstring = message.substring(0, MAX_INDEX)
+            var theIndex = MAX_INDEX
+
+            // Try to find a substring break at a line end.
+            theIndex = theSubstring.lastIndexOf('\n')
+            if (theIndex >= MIN_INDEX) {
+                theSubstring = theSubstring.substring(0, theIndex)
+            } else {
+                theIndex = MAX_INDEX
+            }
+
+            // Log the substring.
+            delegate.info(theSubstring)
+
+            // Recursively log the remainder.
+            logLong(message.substring(theIndex))
+        } else {
+            delegate.info(message)
+        }// String to be logged is shorter than the max...
+    }
+}
