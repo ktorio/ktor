530eec2332abe53085351546da36acdc68263eee
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2019-02-06 18:07:13 +0300

    Rename http2 test class

--- ktor-server/ktor-server-jetty/ktor-server-jetty-test-http2/jvm/test/io/ktor/tests/server/jetty/JettyEngineTest.kt
+++ ktor-server/ktor-server-jetty/ktor-server-jetty-test-http2/jvm/test/io/ktor/tests/server/jetty/JettyEngineTest.kt
@@ -3,4 +3,4 @@ package io.ktor.tests.server.jetty
 import io.ktor.server.jetty.*
 import io.ktor.server.testing.*
 
-class JettyEngineTest : EngineTestSuite<JettyApplicationEngine, JettyApplicationEngineBase.Configuration>(Jetty)
+class JettyEngineHttp2Test : EngineTestSuite<JettyApplicationEngine, JettyApplicationEngineBase.Configuration>(Jetty)
