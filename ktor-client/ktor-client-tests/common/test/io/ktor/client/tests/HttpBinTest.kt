c2adcc0d92650c304a401dd27d51006543eee38b
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2019-02-20 14:42:30 +0300

    Update httpbin tests to https

--- ktor-client/ktor-client-tests/common/test/io/ktor/client/tests/HttpBinTest.kt
+++ ktor-client/ktor-client-tests/common/test/io/ktor/client/tests/HttpBinTest.kt
@@ -26,14 +26,13 @@ class HttpBinTest {
         }
 
         test { client ->
-            val response = client.get<HttpBinResponse>("http://httpbin.org/get")
+            val response = client.get<HttpBinResponse>("https://httpbin.org/get")
 
-            assertEquals("http://httpbin.org/get", response.url)
+            assertEquals("https://httpbin.org/get", response.url)
             assertEquals(emptyMap(), response.args)
 
             with(response.headers) {
                 assertEquals("application/json", get("Accept"))
-                assertEquals("close", get("Connection"))
                 assertEquals("httpbin.org", get("Host"))
             }
 
@@ -47,18 +46,17 @@ class HttpBinTest {
         }
 
         test { client ->
-            val response = client.post<HttpBinResponse>("http://httpbin.org/post") {
+            val response = client.post<HttpBinResponse>("https://httpbin.org/post") {
                 body = "Hello, bin!"
             }
 
-            assertEquals("http://httpbin.org/post", response.url)
+            assertEquals("https://httpbin.org/post", response.url)
             assertEquals(emptyMap(), response.args)
 
             with(response.headers) {
                 assertEquals("text/plain; charset=UTF-8", get("Content-Type"))
                 assertEquals("application/json", get("Accept"))
                 assertEquals("11", get("Content-Length"))
-                assertEquals("close", get("Connection"))
                 assertEquals("httpbin.org", get("Host"))
             }
         }
