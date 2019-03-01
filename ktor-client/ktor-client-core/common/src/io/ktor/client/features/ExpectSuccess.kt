6faf9239b6c4ad8a41f072d97dfc3c2b16b0453e
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2019-03-01 10:34:19 +0300

    Update public API

--- ktor-client/ktor-client-core/common/src/io/ktor/client/features/ExpectSuccess.kt
+++ ktor-client/ktor-client-core/common/src/io/ktor/client/features/ExpectSuccess.kt
@@ -1,5 +1,8 @@
 package io.ktor.client.features
 
+import io.ktor.client.*
+import io.ktor.util.*
+
 @Deprecated(
     "[BadResponseStatusException] is deprecated. Use [ResponseException] instead.",
     ReplaceWith("ResponseException"),
@@ -7,3 +10,26 @@ package io.ktor.client.features
 )
 @Suppress("KDocMissingDocumentation")
 typealias BadResponseStatusException = ResponseException
+
+@Deprecated(
+    "Use [HttpCallValidator] instead.",
+    ReplaceWith("HttpCallValidator"),
+    DeprecationLevel.ERROR
+)
+@Suppress("KDocMissingDocumentation")
+class ExpectSuccess {
+    @Suppress("DEPRECATION_ERROR")
+    companion object : HttpClientFeature<Unit, ExpectSuccess> {
+
+        override val key: AttributeKey<ExpectSuccess>
+            get() = error("Deprecated")
+
+        override fun prepare(block: Unit.() -> Unit): ExpectSuccess {
+            error("Deprecated")
+        }
+
+        override fun install(feature: ExpectSuccess, scope: HttpClient) {
+            error("Deprecated")
+        }
+    }
+}
