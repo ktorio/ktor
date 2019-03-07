d2ae05cfbea99630a07628630f7ed81144ab6452
Luca Spinazzola <mspinluca@gmail.com>
2019-03-06 21:41:56 -0800

    Revise MaxLengthLogger with requested changes: Document MaxLengthLogger Make logLong a private tailrec fun Make MIN_INDEX configurable like MAX_INDEX Rename variables with "the" as a suffix Discard leading newline characters

--- ktor-client/ktor-client-features/ktor-client-logging/jvm/src/io/ktor/client/features/logging/LoggerJvm.kt
+++ ktor-client/ktor-client-features/ktor-client-logging/jvm/src/io/ktor/client/features/logging/LoggerJvm.kt
@@ -12,41 +12,45 @@ actual val Logger.Companion.DEFAULT: Logger
     }
 
 
-/***
- * Workaround Logger to log long messages that would otherwise be truncated in Android.
+/**
+ * Android [Logger]: breaks up long log messages that would be truncated by Android's max log
+ * length of 4068 characters
  */
 val Logger.Companion.ANDROID: Logger
     get() = MaxLengthLogger()
 
-
-class MaxLengthLogger(val MAX_INDEX : Int = 4000) : Logger {
+/**
+ * A [Logger] that breaks up log messages into multiple logs no longer than [MAX_INDEX]
+ * @property MAX_INDEX max length allowed for a log message
+ * @property MIN_INDEX if log message is longer than [MAX_INDEX], attempt to break the log
+ * message at a new line between [MIN_INDEX] and [MAX_INDEX] if one exists
+ */
+class MaxLengthLogger(private val MAX_INDEX : Int = 4000, private val MIN_INDEX: Int = 3000) : Logger {
     private val delegate = LoggerFactory.getLogger(HttpClient::class.java)!!
     override fun log(message: String) {
         logLong(message)
     }
 
-    fun logLong(message: String) {
-
-        val MIN_INDEX = 3000
-
+    private tailrec fun logLong(message: String) {
         // String to be logged is longer than the max...
         if (message.length > MAX_INDEX) {
-            var theSubstring = message.substring(0, MAX_INDEX)
-            var theIndex = MAX_INDEX
+            var msgSubstring = message.substring(0, MAX_INDEX)
+            var msgSubstringEndIndex = MAX_INDEX
 
-            // Try to find a substring break at a line end.
-            theIndex = theSubstring.lastIndexOf('\n')
-            if (theIndex >= MIN_INDEX) {
-                theSubstring = theSubstring.substring(0, theIndex)
-            } else {
-                theIndex = MAX_INDEX
+            // Try to find a substring break at a newline char.
+            msgSubstring.lastIndexOf('\n').let { lastIndex->
+                if (lastIndex >= MIN_INDEX) {
+                    msgSubstring = msgSubstring.substring(0, lastIndex)
+                    //skip over new line char
+                    msgSubstringEndIndex = lastIndex + 1
+                }
             }
 
             // Log the substring.
-            delegate.info(theSubstring)
+            delegate.info(msgSubstring)
 
             // Recursively log the remainder.
-            logLong(message.substring(theIndex))
+            logLong(message.substring(msgSubstringEndIndex))
         } else {
             delegate.info(message)
         }// String to be logged is shorter than the max...
