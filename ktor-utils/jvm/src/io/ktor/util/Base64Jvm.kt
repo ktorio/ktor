2db52f6f88ea3099e4891090d60269d29f274d3a
Luca Spinazzola <mspinluca@gmail.com>
2019-03-07 16:24:53 -0800

    Remove use of java.util.Base64

--- ktor-utils/jvm/src/io/ktor/util/Base64Jvm.kt
+++ ktor-utils/jvm/src/io/ktor/util/Base64Jvm.kt
@@ -1,15 +1,16 @@
 package io.ktor.util
 
-import java.util.*
+import kotlinx.io.charsets.Charset
+
 
 /**
  * Decode bytes from a BASE64 string [s]
  */
 @InternalAPI
-fun decodeBase64(s: String): ByteArray = Base64.getDecoder().decode(s)
+fun decodeBase64(s: String): ByteArray = s.decodeBase64().toByteArray(Charset.forName("ISO-8859-1"))
 
 /**
  * Encode [bytes] as a BASE64 string
  */
 @InternalAPI
-fun encodeBase64(bytes: ByteArray): String = Base64.getEncoder().encodeToString(bytes)
+fun encodeBase64(bytes: ByteArray): String = bytes.encodeBase64()
