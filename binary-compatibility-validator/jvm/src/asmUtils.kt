631bc8508589b177c998b3a964f8d0d88eeeac72
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2019-04-02 14:00:06 +0300

    Update binary compatibility validator

--- binary-compatibility-validator/jvm/src/asmUtils.kt
+++ binary-compatibility-validator/jvm/src/asmUtils.kt
@@ -1,9 +1,11 @@
 /*
- * Copyright 2016-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2010-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
  */
 
-package kotlinx.coroutines.tools
+package io.ktor.validator
 
+import kotlinx.metadata.jvm.*
 import org.objectweb.asm.*
 import org.objectweb.asm.tree.*
 
@@ -29,32 +31,31 @@ data class ClassBinarySignature(
     val isEffectivelyPublic: Boolean,
     val isNotUsedWhenEmpty: Boolean
 ) {
-
     val signature: String
         get() = "${access.getModifierString()} class $name" + if (supertypes.isEmpty()) "" else " : ${supertypes.joinToString()}"
-
 }
 
 
 interface MemberBinarySignature {
-    val name: String
-    val desc: String
+    val jvmMember: JvmMemberSignature
+    val name: String get() = jvmMember.name
+    val desc: String get() = jvmMember.desc
     val access: AccessFlags
     val isPublishedApi: Boolean
 
     fun isEffectivelyPublic(classAccess: AccessFlags, classVisibility: ClassVisibility?) =
         access.isPublic && !(access.isProtected && classAccess.isFinal)
-            && (findMemberVisibility(classVisibility)?.isPublic(isPublishedApi) ?: true)
+                && (findMemberVisibility(classVisibility)?.isPublic(isPublishedApi) ?: true)
 
-    fun findMemberVisibility(classVisibility: ClassVisibility?) =
-        classVisibility?.members?.get(MemberSignature(name, desc))
+    fun findMemberVisibility(classVisibility: ClassVisibility?): MemberVisibility? {
+        return classVisibility?.findMember(jvmMember)
+    }
 
     val signature: String
 }
 
 data class MethodBinarySignature(
-    override val name: String,
-    override val desc: String,
+    override val jvmMember: JvmMethodSignature,
     override val isPublishedApi: Boolean,
     override val access: AccessFlags
 ) : MemberBinarySignature {
@@ -63,15 +64,41 @@ data class MethodBinarySignature(
 
     override fun isEffectivelyPublic(classAccess: AccessFlags, classVisibility: ClassVisibility?) =
         super.isEffectivelyPublic(classAccess, classVisibility)
-            && !isAccessOrAnnotationsMethod()
+                && !isAccessOrAnnotationsMethod()
+                && !isDummyDefaultConstructor()
 
-    private fun isAccessOrAnnotationsMethod() =
-        access.isSynthetic && (name.startsWith("access\$") || name.endsWith("\$annotations"))
+    override fun findMemberVisibility(classVisibility: ClassVisibility?): MemberVisibility? {
+        return super.findMemberVisibility(classVisibility) ?: classVisibility?.let { alternateDefaultSignature(it.name)?.let(it::findMember) }
+    }
+
+    private fun isAccessOrAnnotationsMethod() = access.isSynthetic && (name.startsWith("access\$") || name.endsWith("\$annotations"))
+
+    private fun isDummyDefaultConstructor() = access.isSynthetic && name == "<init>" && desc == "(Lkotlin/jvm/internal/DefaultConstructorMarker;)V"
+
+    /**
+     * Calculates the signature of this method without default parameters
+     *
+     * Returns `null` if this method isn't an entry point of a function
+     * or a constructor with default parameters.
+     * Returns an incorrect result, if there are more than 31 default parameters.
+     */
+    private fun alternateDefaultSignature(className: String): JvmMethodSignature? {
+        return when {
+            !access.isSynthetic -> null
+            name == "<init>" && "ILkotlin/jvm/internal/DefaultConstructorMarker;" in desc ->
+                JvmMethodSignature(name, desc.replace("ILkotlin/jvm/internal/DefaultConstructorMarker;", ""))
+            name.endsWith("\$default") && "ILjava/lang/Object;)" in desc ->
+                JvmMethodSignature(
+                    name.removeSuffix("\$default"),
+                    desc.replace("ILjava/lang/Object;)", ")").replace("(L$className;", "(")
+                )
+            else -> null
+        }
+    }
 }
 
 data class FieldBinarySignature(
-    override val name: String,
-    override val desc: String,
+    override val jvmMember: JvmFieldSignature,
     override val isPublishedApi: Boolean,
     override val access: AccessFlags
 ) : MemberBinarySignature {
@@ -79,17 +106,12 @@ data class FieldBinarySignature(
         get() = "${access.getModifierString()} field $name $desc"
 
     override fun findMemberVisibility(classVisibility: ClassVisibility?): MemberVisibility? {
-        val fieldVisibility = super.findMemberVisibility(classVisibility) ?: return null
-
-        // good case for 'satisfying': fieldVisibility.satisfying { it.isLateInit() }?.let { classVisibility?.findSetterForProperty(it) }
-        if (fieldVisibility.isLateInit()) {
-            classVisibility?.findSetterForProperty(fieldVisibility)?.let { return it }
-        }
-        return fieldVisibility
+        return super.findMemberVisibility(classVisibility)
+                ?: takeIf { access.isStatic }?.let { super.findMemberVisibility(classVisibility?.companionVisibilities) }
     }
 }
 
-val MemberBinarySignature.kind: Int
+private val MemberBinarySignature.kind: Int
     get() = when (this) {
         is FieldBinarySignature -> 1
         is MethodBinarySignature -> 2
@@ -110,9 +132,7 @@ data class AccessFlags(val access: Int) {
     val isFinal: Boolean get() = isFinal(access)
     val isSynthetic: Boolean get() = isSynthetic(access)
 
-    fun getModifiers(): List<String> =
-        ACCESS_NAMES.entries.mapNotNull { if (access and it.key != 0) it.value else null }
-
+    fun getModifiers(): List<String> = ACCESS_NAMES.entries.mapNotNull { if (access and it.key != 0) it.value else null }
     fun getModifierString(): String = getModifiers().joinToString(" ")
 }
 
@@ -125,13 +145,13 @@ fun isSynthetic(access: Int) = access and Opcodes.ACC_SYNTHETIC != 0
 
 fun ClassNode.isEffectivelyPublic(classVisibility: ClassVisibility?) =
     isPublic(access)
-        && !isLocal()
-        && !isWhenMappings()
-        && (classVisibility?.isPublic(isPublishedApi()) ?: true)
+            && !isLocal()
+            && !isWhenMappings()
+            && (classVisibility?.isPublic(isPublishedApi()) ?: true)
 
 
 val ClassNode.innerClassNode: InnerClassNode? get() = innerClasses.singleOrNull { it.name == name }
-fun ClassNode.isLocal() = innerClassNode?.run { innerName == null && outerName == null } ?: false
+fun ClassNode.isLocal() = innerClassNode?.run { innerName == null && outerName == null} ?: false
 fun ClassNode.isInner() = innerClassNode != null
 fun ClassNode.isWhenMappings() = isSynthetic(access) && name.endsWith("\$WhenMappings")
 
@@ -145,51 +165,25 @@ fun MethodNode.isPublishedApi() = findAnnotation(publishedApiAnnotationName, inc
 fun FieldNode.isPublishedApi() = findAnnotation(publishedApiAnnotationName, includeInvisible = true) != null
 
 
-private object KotlinClassKind {
-    const val FILE = 2
-    const val SYNTHETIC_CLASS = 3
-    const val MULTIPART_FACADE = 4
-
-    val FILE_OR_MULTIPART_FACADE_KINDS = listOf(FILE, MULTIPART_FACADE)
-}
-
-fun ClassNode.isFileOrMultipartFacade() =
-    kotlinClassKind.let { it != null && it in KotlinClassKind.FILE_OR_MULTIPART_FACADE_KINDS }
-
-fun ClassNode.isDefaultImpls() =
-    isInner() && name.endsWith("\$DefaultImpls") && kotlinClassKind == KotlinClassKind.SYNTHETIC_CLASS
+fun ClassNode.isDefaultImpls(metadata: KotlinClassMetadata?) = isInner() && name.endsWith("\$DefaultImpls") && metadata.isSyntheticClass()
 
 
-val ClassNode.kotlinClassKind: Int?
-    get() = findAnnotation("kotlin/Metadata", false)?.get("k") as Int?
-
-fun ClassNode.findAnnotation(annotationName: String, includeInvisible: Boolean = false) =
-    findAnnotation(annotationName, visibleAnnotations, invisibleAnnotations, includeInvisible)
-
-fun MethodNode.findAnnotation(annotationName: String, includeInvisible: Boolean = false) =
-    findAnnotation(annotationName, visibleAnnotations, invisibleAnnotations, includeInvisible)
-
-fun FieldNode.findAnnotation(annotationName: String, includeInvisible: Boolean = false) =
-    findAnnotation(annotationName, visibleAnnotations, invisibleAnnotations, includeInvisible)
+fun ClassNode.findAnnotation(annotationName: String, includeInvisible: Boolean = false) = findAnnotation(annotationName, visibleAnnotations, invisibleAnnotations, includeInvisible)
+fun MethodNode.findAnnotation(annotationName: String, includeInvisible: Boolean = false) = findAnnotation(annotationName, visibleAnnotations, invisibleAnnotations, includeInvisible)
+fun FieldNode.findAnnotation(annotationName: String, includeInvisible: Boolean = false) = findAnnotation(annotationName, visibleAnnotations, invisibleAnnotations, includeInvisible)
 
 operator fun AnnotationNode.get(key: String): Any? = values.annotationValue(key)
 
 private fun List<Any>.annotationValue(key: String): Any? {
-    for (index in (0..size / 2 - 1)) {
+    for (index in (0 until size / 2)) {
         if (this[index * 2] == key)
             return this[index * 2 + 1]
     }
     return null
 }
 
-private fun findAnnotation(
-    annotationName: String,
-    visibleAnnotations: List<AnnotationNode>?,
-    invisibleAnnotations: List<AnnotationNode>?,
-    includeInvisible: Boolean
-): AnnotationNode? =
+private fun findAnnotation(annotationName: String, visibleAnnotations: List<AnnotationNode>?, invisibleAnnotations: List<AnnotationNode>?, includeInvisible: Boolean): AnnotationNode? =
     visibleAnnotations?.firstOrNull { it.refersToName(annotationName) }
-        ?: if (includeInvisible) invisibleAnnotations?.firstOrNull { it.refersToName(annotationName) } else null
+            ?: if (includeInvisible) invisibleAnnotations?.firstOrNull { it.refersToName(annotationName) } else null
 
-fun AnnotationNode.refersToName(name: String) =
-    desc.startsWith('L') && desc.endsWith(';') && desc.regionMatches(1, name, 0, name.length)
+fun AnnotationNode.refersToName(name: String) = desc.startsWith('L') && desc.endsWith(';') && desc.regionMatches(1, name, 0, name.length)
