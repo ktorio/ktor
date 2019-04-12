11528d8ec2ffc6de60673f0a070005d00cd00f72
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2019-04-12 17:10:34 +0300

    Introduce ability to serve web resources

---
+++ ktor-server/ktor-server-jetty/jvm/test/io/ktor/tests/server/jetty/WebResourcesTest.kt
@@ -0,0 +1,77 @@
+package io.ktor.tests.server.jetty
+
+import io.ktor.http.*
+import io.ktor.http.content.*
+import io.ktor.routing.*
+import io.ktor.server.servlet.*
+import io.ktor.server.testing.*
+import org.eclipse.jetty.server.handler.*
+import org.junit.*
+import org.junit.rules.*
+import java.io.*
+import java.net.*
+import kotlin.test.*
+import kotlin.test.Test
+
+private const val PlainTextContent = "plain text"
+private const val HtmlContent = "<p>HTML</p>"
+
+class WebResourcesTest {
+    @get:Rule
+    val testDir: TemporaryFolder = TemporaryFolder()
+
+    lateinit var textFile: File
+    lateinit var htmlFile: File
+
+    @BeforeTest
+    fun createFiles() {
+        textFile = File(testDir.root, "1.txt").apply {
+            writeText(PlainTextContent)
+        }
+        htmlFile = File(testDir.root, "2.html").apply {
+            writeText(HtmlContent)
+        }
+    }
+
+    @AfterTest
+    fun cleanup() {
+        testDir.root.deleteRecursively()
+    }
+
+    @Test
+    fun testServeWebResources() {
+        withTestApplication {
+            application.attributes.put(ServletContextAttribute, TestContext())
+            application.routing {
+                static("webapp") {
+                    webResources("pages") {
+                        include { it.endsWith(".txt") }
+                        include { it.endsWith(".html") }
+                        exclude { it.endsWith("password.txt") }
+                    }
+                }
+            }
+
+            handleRequest(HttpMethod.Get, "/webapp/index.txt").apply {
+                assertEquals(PlainTextContent, response.content)
+            }
+            handleRequest(HttpMethod.Get, "/webapp/index.html").apply {
+                assertEquals(HtmlContent, response.content)
+            }
+            handleRequest(HttpMethod.Get, "/webapp/password.txt").apply {
+                assertFalse(requestHandled)
+            }
+        }
+    }
+
+    private inner class TestContext : ContextHandler.StaticContext() {
+        override fun getResource(path: String?): URL? {
+            return when (path) {
+                "/pages/index.txt" -> textFile
+                "/pages/password.txt" -> textFile
+                "/pages/index.html" -> htmlFile
+                else -> null
+            }?.toURI()?.toURL()
+        }
+    }
+}
---
+++ ktor-server/ktor-server-servlet/jvm/src/io/ktor/server/servlet/WebResources.kt
@@ -0,0 +1,82 @@
+package io.ktor.server.servlet
+
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.http.content.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.util.*
+import kotlin.random.*
+
+/**
+ * Web resources serve configuration
+ */
+@KtorExperimentalAPI
+class WebResourcesConfig {
+    /**
+     * Path predicates to be included. All files will be served if no include rules specified.
+     * A path provided to a predicate is always slash-separated (`/`).
+     */
+    val includes: MutableList<(String) -> Boolean> = mutableListOf()
+
+    /**
+     * Path predicates to be excluded. By default WEB-INF directory is excluded.
+     * A path provided to a predicate is always slash-separated (`/`).
+     */
+    val excludes: MutableList<(String) -> Boolean> = mutableListOf()
+
+    /**
+     * Content-type resolution, uses [defaultForFileExtension] by default
+     */
+    var mimeResolve: (String) -> ContentType = { ContentType.defaultForFileExtension(it) }
+
+    /**
+     * Add [predicate] to [includes]
+     * @see includes
+     */
+    fun include(predicate: (path: String) -> Boolean) {
+        includes.add(predicate)
+    }
+
+    /**
+     * Add [predicate] exclusion rule to [excludes]
+     * @see excludes
+     */
+    fun exclude(predicate: (path: String) -> Boolean) {
+        excludes.add(predicate)
+    }
+
+    init {
+        excludes.add { path -> path == "WEB-INF" || path.startsWith("WEB-INF/") }
+    }
+}
+
+/**
+ * Serve web resources (usually a directory named webapp containing WEB-INF/web.xml). Note that WEB-INF directory
+ * itself is not served by default.
+ * @param subPath slash-delimited web resources root path (relative to webapp directory)
+ */
+@KtorExperimentalAPI
+fun Route.webResources(subPath: String = "/", configure: WebResourcesConfig.() -> Unit = {}) {
+    val config = WebResourcesConfig().apply(configure)
+    val pathParameterName = pathParameterName + "_" + Random.nextInt(0, Int.MAX_VALUE)
+    val prefix = subPath.split('/', '\\').filter { it.isNotEmpty() }
+
+    get("{$pathParameterName...}") {
+        val filteredPath = call.parameters.getAll(pathParameterName)?.normalizePathComponents() ?: return@get
+        val path = (prefix + filteredPath).joinToString("/", prefix = "/")
+
+        if (config.excludes.any { it(path) }) {
+            return@get
+        }
+        if (config.includes.isNotEmpty() && config.includes.none { it(path) }) {
+            return@get
+        }
+
+        val url = application.attributes.getOrNull(ServletContextAttribute)?.getResource(path) ?: return@get
+        val content = resourceClasspathResource(url, path, config.mimeResolve) ?: return@get
+        call.respond(content)
+    }
+}
+
+private const val pathParameterName = "web_resources_path_parameter"
