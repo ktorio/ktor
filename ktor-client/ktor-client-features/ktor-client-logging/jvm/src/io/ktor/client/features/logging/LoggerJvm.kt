875cefc4d63f84905b4015f47e430933ce8ee731
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2019-05-07 12:51:05 +0300

    Rename android length limiting logger + add tests

--- ktor-client/ktor-client-features/ktor-client-logging/jvm/src/io/ktor/client/features/logging/LoggerJvm.kt
+++ ktor-client/ktor-client-features/ktor-client-logging/jvm/src/io/ktor/client/features/logging/LoggerJvm.kt
@@ -5,6 +5,7 @@
 package io.ktor.client.features.logging
 
 import io.ktor.client.*
+import io.ktor.util.*
 import org.slf4j.*
 
 actual val Logger.Companion.DEFAULT: Logger
@@ -15,35 +16,39 @@ actual val Logger.Companion.DEFAULT: Logger
         }
     }
 
-
 /**
  * Android [Logger]: breaks up long log messages that would be truncated by Android's max log
  * length of 4068 characters
  */
+@KtorExperimentalAPI
 val Logger.Companion.ANDROID: Logger
-    get() = MaxLengthLogger()
+    get() = MessageLengthLimitingLogger()
 
 /**
- * A [Logger] that breaks up log messages into multiple logs no longer than [MAX_INDEX]
- * @property MAX_INDEX max length allowed for a log message
- * @property MIN_INDEX if log message is longer than [MAX_INDEX], attempt to break the log
- * message at a new line between [MIN_INDEX] and [MAX_INDEX] if one exists
+ * A [Logger] that breaks up log messages into multiple logs no longer than [maxLength]
+ * @property maxLength max length allowed for a log message
+ * @property minLength if log message is longer than [maxLength], attempt to break the log
+ * message at a new line between [minLength] and [maxLength] if one exists
  */
-class MaxLengthLogger(private val MAX_INDEX : Int = 4000, private val MIN_INDEX: Int = 3000) : Logger {
-    private val delegate = LoggerFactory.getLogger(HttpClient::class.java)!!
+@KtorExperimentalAPI
+class MessageLengthLimitingLogger(
+    private val maxLength: Int = 4000,
+    private val minLength: Int = 3000,
+    private val delegate: Logger = Logger.DEFAULT
+) : Logger {
     override fun log(message: String) {
         logLong(message)
     }
 
     private tailrec fun logLong(message: String) {
         // String to be logged is longer than the max...
-        if (message.length > MAX_INDEX) {
-            var msgSubstring = message.substring(0, MAX_INDEX)
-            var msgSubstringEndIndex = MAX_INDEX
+        if (message.length > maxLength) {
+            var msgSubstring = message.substring(0, maxLength)
+            var msgSubstringEndIndex = maxLength
 
             // Try to find a substring break at a newline char.
-            msgSubstring.lastIndexOf('\n').let { lastIndex->
-                if (lastIndex >= MIN_INDEX) {
+            msgSubstring.lastIndexOf('\n').let { lastIndex ->
+                if (lastIndex >= minLength) {
                     msgSubstring = msgSubstring.substring(0, lastIndex)
                     //skip over new line char
                     msgSubstringEndIndex = lastIndex + 1
@@ -51,12 +56,12 @@ class MaxLengthLogger(private val MAX_INDEX : Int = 4000, private val MIN_INDEX:
             }
 
             // Log the substring.
-            delegate.info(msgSubstring)
+            delegate.log(msgSubstring)
 
             // Recursively log the remainder.
             logLong(message.substring(msgSubstringEndIndex))
         } else {
-            delegate.info(message)
+            delegate.log(message)
         }// String to be logged is shorter than the max...
     }
 }
