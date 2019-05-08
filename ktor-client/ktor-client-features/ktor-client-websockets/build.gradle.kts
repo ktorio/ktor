7059686fa4f1f4f1f8c61f8e4a57d1896d0a8f94
Leonid.Stashevsky <leonid.stashevsky@gmail.com>
2019-05-08 15:03:37 +0300

    Add backward compatible artifact

---
+++ ktor-client/ktor-client-features/ktor-client-websockets/build.gradle.kts
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2014-2019 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+kotlin.sourceSets {
+    commonMain {
+        dependencies {
+            api(project(":ktor-client:ktor-client-core"))
+        }
+    }
+}
