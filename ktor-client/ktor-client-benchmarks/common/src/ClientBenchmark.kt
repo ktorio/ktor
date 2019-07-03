554091fb2604819afe65434d9be73b3a6e7c1fb1
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2019-07-03 15:30:42 +0300

    Add client chaining benchmark; Simplify benchmarks

--- ktor-client/ktor-client-benchmarks/common/src/ClientBenchmark.kt
+++ ktor-client/ktor-client-benchmarks/common/src/ClientBenchmark.kt
@@ -8,9 +8,21 @@ import io.ktor.client.*
 import io.ktor.client.engine.*
 import io.ktor.client.request.*
 import kotlinx.benchmark.*
+import kotlinx.coroutines.io.*
+
+private val testData = mutableMapOf(
+    0 to ByteArray(0),
+    1 to ByteArray(1 * 1024),
+    16 to ByteArray(16 * 1024),
+    32 to ByteArray(32 * 1024),
+    32 to ByteArray(32 * 1024),
+    64 to ByteArray(64 * 1024),
+    256 to ByteArray(256 * 1024),
+    1024 to ByteArray(1024 * 1024)
+)
 
 @State(Scope.Benchmark)
-internal class ClientDownloadBenchmark {
+internal class ClientBenchmark {
     lateinit var client: HttpClient
 
 
@@ -27,52 +39,42 @@ internal class ClientDownloadBenchmark {
 
     @Benchmark
     fun download() = runBenchmark {
-        client.download(size)
+        val data = client.get<ByteArray>("$TEST_BENCHMARKS_SERVER/bytes?size=$size")
+        check(data.size == size * 1024)
     }
 
     @Benchmark
     fun upload() = runBenchmark {
-        client.upload(size)
+        val uploaded = client.post<String>("$TEST_BENCHMARKS_SERVER/bytes") {
+            body = testData[size]!!
+        }
+        check(uploaded.toInt() == size * 1024) { println("Expected ${size * 1024}, got $uploaded") }
     }
 
     @Benchmark
     fun echoStream() = runBenchmark {
-        client.echo(size)
+        val uploaded = client.post<ByteArray>("$TEST_BENCHMARKS_SERVER/echo") {
+            body = testData[size]!!
+        }
+
+        check(uploaded.size == size * 1024) { println("Expected ${size * 1024}, got ${uploaded.size}") }
+    }
+
+    @Benchmark
+    fun echoStreamChain() = runBenchmark {
+        val stream = client.post<ByteReadChannel>("$TEST_BENCHMARKS_SERVER/echo") {
+            body = testData[size]!!
+        }
+
+        val result = client.post<ByteArray>("$TEST_BENCHMARKS_SERVER/echo") {
+            body = stream
+        }
+
+        check(result.size == size * 1024) { println("Expected ${size * 1024}, got ${result.size}") }
     }
 
     @TearDown
     fun stop() {
         client.close()
     }
-}
-
-private val testData = mutableMapOf(
-    0 to ByteArray(0),
-    1 to ByteArray(1 * 1024),
-    16 to ByteArray(16 * 1024),
-    32 to ByteArray(32 * 1024),
-    32 to ByteArray(32 * 1024),
-    64 to ByteArray(64 * 1024),
-    256 to ByteArray(256 * 1024),
-    1024 to ByteArray(1024 * 1024)
-)
-
-internal suspend inline fun HttpClient.download(size: Int) {
-    val data = get<ByteArray>("$TEST_BENCHMARKS_SERVER/bytes?size=$size")
-    check(data.size == size * 1024)
-}
-
-internal suspend inline fun HttpClient.upload(size: Int) {
-    val uploaded = post<String>("$TEST_BENCHMARKS_SERVER/bytes") {
-        body = testData[size]!!
-    }
-    check(uploaded.toInt() == size * 1024) { println("Expected ${size * 1024}, got $uploaded")}
-}
-
-internal suspend inline fun HttpClient.echo(size: Int) {
-    val uploaded = post<ByteArray>("$TEST_BENCHMARKS_SERVER/echo?size=$size") {
-        body = testData[size]!!
-    }
-
-    check(uploaded.size == size * 1024) { println("Expected ${size * 1024}, got ${uploaded.size}")}
 }
