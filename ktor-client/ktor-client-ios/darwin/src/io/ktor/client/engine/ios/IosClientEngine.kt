69dc8799950ddb0c4884cd5e59cb3c171e15a633
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2019-07-19 16:13:31 +0300

    Fix follow redirect iOS
        Close #1000

--- ktor-client/ktor-client-ios/darwin/src/io/ktor/client/engine/ios/IosClientEngine.kt
+++ ktor-client/ktor-client-ios/darwin/src/io/ktor/client/engine/ios/IosClientEngine.kt
@@ -74,6 +74,16 @@ internal class IosClientEngine(override val config: IosClientEngineConfig) : Htt
 
                 continuation.resume(response)
             }
+
+            override fun URLSession(
+                session: NSURLSession,
+                task: NSURLSessionTask,
+                willPerformHTTPRedirection: NSHTTPURLResponse,
+                newRequest: NSURLRequest,
+                completionHandler: (NSURLRequest?) -> Unit
+            ) {
+                completionHandler(null)
+            }
         }
 
         val configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
