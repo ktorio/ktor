04a9f6e28517fb56d35944017a8326c27c3b77df
Leonid Stashevsky <leonid@MacBook-Pro-Leonid.local>
2019-07-23 13:29:47 +0300

    Add ios client proxy

--- ktor-client/ktor-client-core/posix/src/io/ktor/client/engine/ProxyConfigNative.kt
+++ ktor-client/ktor-client-core/posix/src/io/ktor/client/engine/ProxyConfigNative.kt
@@ -6,6 +6,8 @@ import io.ktor.http.*
  * Proxy configuration.
  *
  * See [ProxyBuilder] to create proxy.
+ *
+ * @param url: proxy url address.
  */
 actual class ProxyConfig(val url: Url) {
     override fun toString(): String = url.toString()
@@ -16,7 +18,21 @@ actual class ProxyConfig(val url: Url) {
  */
 actual object ProxyBuilder {
     /**
-     * Create http proxy from url.
+     * Create http proxy from [url].
      */
-    actual fun http(url: Url): ProxyConfig = ProxyConfig(url)
+    actual fun http(url: Url): ProxyConfig {
+        require(url.protocol.name.equals(URLProtocol.HTTP.name, ignoreCase = true))
+
+        return ProxyConfig(url)
+    }
+
+    /**
+     * Create socks proxy from [host] and [port].
+     */
+    actual fun socks(host: String, port: Int): ProxyConfig = ProxyConfig(URLBuilder().apply {
+        protocol = URLProtocol.SOCKS
+
+        this.host = host
+        this.port = port
+    }.build())
 }
