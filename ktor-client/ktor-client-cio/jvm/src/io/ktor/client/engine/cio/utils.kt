09e66a4f67886ca240a1915f8d67b7d1689df1ee
Evey-Edward Neely <evyymail@gmail.com>
2019-09-02 13:27:39 +0300

    CIO engine no longer sends port in "Host" header (#1295)
    * CIO engine no longer sends port in "Host" header
    
    Fixes #1294
    * only add port to Host header if it is non-standard for the protocol
    
    (cherry picked from commit beac785b9d04612df310594cfc9f6f4ce4350fd5)

--- ktor-client/ktor-client-cio/jvm/src/io/ktor/client/engine/cio/utils.kt
+++ ktor-client/ktor-client-cio/jvm/src/io/ktor/client/engine/cio/utils.kt
@@ -28,7 +28,8 @@ internal suspend fun HttpRequestData.write(output: ByteWriteChannel, callContext
 
     try {
         builder.requestLine(method, url.fullPath, HttpProtocolVersion.HTTP_1_1.toString())
-        builder.headerLine("Host", url.hostWithPort)
+        // this will only add the port to the host header if the port is non-standard for the protocol
+        builder.headerLine("Host", if (url.protocol.defaultPort == url.port) url.host else url.hostWithPort)
 
         mergeHeaders(headers, body) { key, value ->
             builder.headerLine(key, value)
