e6ae89ffeb2d4571fc31546ed98ef1a5cf537f33
6bangs <6bangs@gmail.com>
2019-09-17 01:21:38 -0700

    finishTasksAndInvalidate after executing a request (#1341). (#1342)

--- ktor-client/ktor-client-ios/darwin/src/io/ktor/client/engine/ios/IosClientEngine.kt
+++ ktor-client/ktor-client-ios/darwin/src/io/ktor/client/engine/ios/IosClientEngine.kt
@@ -11,10 +11,10 @@ import io.ktor.client.utils.*
 import io.ktor.http.*
 import io.ktor.http.content.*
 import io.ktor.util.date.*
-import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.*
 import io.ktor.utils.io.*
 import io.ktor.utils.io.core.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.*
 import platform.CFNetwork.*
 import platform.Foundation.*
 import platform.darwin.*
@@ -120,6 +120,10 @@ internal class IosClientEngine(override val config: IosClientEngineConfig) : Htt
                 config.requestConfig(nativeRequest)
                 session.dataTaskWithRequest(nativeRequest).resume()
             }
+        }
+
+        callContext[Job]?.invokeOnCompletion {
+            session.finishTasksAndInvalidate()
         }
     }
 
