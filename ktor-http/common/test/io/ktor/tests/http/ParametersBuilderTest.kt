4b1fc40991b670ec0d6b4e673a3182794399c17b
uharaqo <uharaqo+github@gmail.com>
2019-09-19 08:43:48 -0700

    Improve test code for ParametersBuilder

--- ktor-http/common/test/io/ktor/tests/http/ParametersBuilderTest.kt
+++ ktor-http/common/test/io/ktor/tests/http/ParametersBuilderTest.kt
@@ -9,34 +9,40 @@ import kotlin.test.*
 
 class ParametersBuilderTest {
 
-    private val original = "abc=def&efg=hij;klm=nop/qrs=tuv"
-    private val encoded = original.encodeURLParameter(false)
+    private val original = "abc=def&efg=hij;klm=nop/qrs=tu v"
+    private val encoded = original.encodeURLParameter(spaceToPlus = true)
 
     private fun assertEncodedQuery(option: UrlEncodingOption?, expectedKey: String, expectedValue: String) {
-        val builder = option?.let { ParametersBuilder(urlEncodingOption = it) } ?: ParametersBuilder()
+        // given that a builder is constructed with the UrlEncodingOption
+        val builder = option
+            ?.let { ParametersBuilder(urlEncodingOption = it) }
+            ?: ParametersBuilder()
+
+        // when the original string is added to the parameters as a key and value
         builder.append(original, original)
         val encoded = builder.build().formUrlEncode()
 
+        // then encoded string should be the expected key=value
         assertEquals("$expectedKey=$expectedValue", encoded)
     }
 
     @Test
-    fun encodeKeyValueDefaultConstructorTest() {
+    fun defaultConstructorTest() {
         assertEncodedQuery(null, encoded, encoded)
     }
 
     @Test
-    fun encodeKeyValueTest() {
+    fun defaultEncodingTest() {
         assertEncodedQuery(UrlEncodingOption.DEFAULT, encoded, encoded)
     }
 
     @Test
-    fun encodeKeyOnlyTest() {
+    fun keyOnlyEncodingTest() {
         assertEncodedQuery(UrlEncodingOption.KEY_ONLY, encoded, original)
     }
 
     @Test
-    fun encodeValueOnlyTest() {
+    fun valueOnlyEncodingTest() {
         assertEncodedQuery(UrlEncodingOption.VALUE_ONLY, original, encoded)
     }
 
