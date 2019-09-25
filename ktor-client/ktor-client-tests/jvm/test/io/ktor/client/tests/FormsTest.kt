eb5b6c2b5c75590777175d0cc433c6848e277418
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2019-09-26 02:40:18 +0300

    Fix cherry-picked import

--- ktor-client/ktor-client-tests/jvm/test/io/ktor/client/tests/FormsTest.kt
+++ ktor-client/ktor-client-tests/jvm/test/io/ktor/client/tests/FormsTest.kt
@@ -10,7 +10,7 @@ import io.ktor.client.request.*
 import io.ktor.client.request.forms.*
 import io.ktor.client.tests.utils.*
 import io.ktor.http.*
-import io.ktor.utils.io.streams.*
+import kotlinx.io.streams.*
 import java.io.*
 import kotlin.test.*
 
