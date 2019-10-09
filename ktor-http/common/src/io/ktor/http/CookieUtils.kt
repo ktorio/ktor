d7a84a72b1ab1d70379d213e2956761209fcf251
Thomas <thomasjamesfowler97@gmail.com>
2019-10-09 13:11:46 +1100

    Added the parsing of date-tokens as well as result conformation testing. Also added a few extension functions on Char

--- ktor-http/common/src/io/ktor/http/CookieUtils.kt
+++ ktor-http/common/src/io/ktor/http/CookieUtils.kt
@@ -15,6 +15,8 @@ import io.ktor.util.date.*
 internal class StringLexer(val source: String) {
     var index = 0
 
+    val hasRemaining: Boolean get() = index < source.length
+
     /**
      * Check if the current character satisfies the predicate
      *
@@ -57,7 +59,98 @@ internal class StringLexer(val source: String) {
     }
 }
 
+internal fun Char.isDelimiter(): Boolean =
+    this == '\u0009'
+        || this in ('\u0020'..'\u002f')
+        || this in ('\u003b'..'\u0040')
+        || this in ('\u005b'..'\u0060')
+        || this in ('\u007b'..'\u007e')
+
+internal fun Char.isNonDelimiter(): Boolean =
+    this in ('\u0000'..'\u0008')
+        || this in ('\u000a'..'\u001f')
+        || this in ('0'..'9')
+        || this == ':'
+        || this in ('a'..'z')
+        || this in ('A'..'Z')
+        || this in ('\u007f'..'\u00ff')
+
+internal fun Char.isOctet(): Boolean =
+    this in ('\u0000'..'\u00ff')
+
+internal fun Char.isNonDigit(): Boolean =
+    this in ('\u0000'..'\u002f') || this in ('\u004a'..'\u00ff')
+
+internal inline fun Boolean.otherwise(block: () -> Unit) {
+    if (!this) block()
+}
+
+internal fun CookieDateBuilder.handleToken(token: String) {
+
+}
+
 class CookieDateParser {
 
-    fun parse(source: String): GMTDate = TODO()
+    fun parse(source: String): GMTDate {
+        val lexer = StringLexer(source)
+        val builder = CookieDateBuilder()
+
+        lexer.acceptWhile { it.isDelimiter() }
+
+        while (lexer.hasRemaining) {
+            if (lexer.test { it.isNonDelimiter() }) {
+                val token = lexer.capture { acceptWhile { it.isNonDelimiter() } }
+
+                builder.handleToken(token)
+
+                lexer.acceptWhile { it.isDelimiter() }
+            }
+        }
+
+        when (builder.year) {
+            in 70..99 -> builder.year = builder.year!! + 1900
+            in 0..69 -> builder.year = builder.year!! + 2000
+        }
+
+        if (builder.dayOfMonth == null)
+            throw InvalidDateStringException(source, 0, "Could not find day-of-month")
+
+        if (builder.month == null)
+            throw InvalidDateStringException(source, 0, "Could not find month")
+
+        if (builder.year == null)
+            throw InvalidDateStringException(source, 0, "Could not find year")
+
+        if (builder.hours == null || builder.minutes == null || builder.seconds == null)
+            throw InvalidDateStringException(source, 0, "Could not find time")
+
+        if (builder.dayOfMonth!! !in 1..31)
+            throw InvalidDateStringException(source, 0, "Invalid day of month: ${builder.dayOfMonth} not in [1,31]")
+
+        if (builder.year!! < 1601)
+            throw InvalidDateStringException(source, 0, "Invalid year: ${builder.year} < 1601")
+
+        if (builder.hours!! > 23)
+            throw InvalidDateStringException(source, 0, "Invalid hours: ${builder.hours} > 23")
+
+        if (builder.minutes!! > 59)
+            throw InvalidDateStringException(source, 0, "Invalid minutes: ${builder.minutes} > 59")
+
+        if (builder.seconds!! > 59)
+            throw InvalidDateStringException(source, 0, "Invalid seconds: ${builder.seconds} > 59")
+
+        return builder.build()
+    }
+}
+
+internal class CookieDateBuilder {
+    var seconds: Int? = null
+    var minutes: Int? = null
+    var hours: Int? = null
+
+    var dayOfMonth: Int? = null
+    var month: Month? = null
+    var year: Int? = null
+
+    fun build(): GMTDate = GMTDate(seconds!!, minutes!!, hours!!, dayOfMonth!!, month!!, year!!)
 }
