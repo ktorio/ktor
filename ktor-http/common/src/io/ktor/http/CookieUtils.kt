bf476b614b8b051893f08e2f73527b4e7ff45ced
Thomas <thomasjamesfowler97@gmail.com>
2019-10-09 12:54:33 +1100

    Added some documentation to StringLexer.

--- ktor-http/common/src/io/ktor/http/CookieUtils.kt
+++ ktor-http/common/src/io/ktor/http/CookieUtils.kt
@@ -6,22 +6,51 @@ package io.ktor.http
 
 import io.ktor.util.date.*
 
+/**
+ * Lexer over a source string, allowing testing, accepting and capture of spans of characters given
+ * some predicates.
+ *
+ * @property source string to iterate over
+ */
 internal class StringLexer(val source: String) {
     var index = 0
 
+    /**
+     * Check if the current character satisfies the predicate
+     *
+     * @param predicate character test
+     */
     fun test(predicate: (Char) -> Boolean): Boolean =
         index < source.length && predicate(source[index])
 
+    /**
+     * Checks if the current character satisfies the predicate, consuming it is so
+     *
+     * @param predicate character test
+     */
     fun accept(predicate: (Char) -> Boolean): Boolean =
         test(predicate).also { if (it) index++ }
 
+    /**
+     * Keep accepting characters while they satisfy the predicate
+     *
+     * @param predicate character test
+     * @see [accept]
+     */
     fun acceptWhile(predicate: (Char) -> Boolean): Boolean {
         if (!test(predicate)) return false
         while (test(predicate)) index++
         return true
     }
 
-    fun capture(block: StringLexer.() -> Unit): String {
+    /**
+     * Run the block on this lexer taking note of the starting and ending index. Returning the span of the
+     * source which was traversed.
+     *
+     * @param block scope of traversal to be captured
+     * @return The traversed span of the source string
+     */
+    inline fun capture(block: StringLexer.() -> Unit): String {
         val start = index
         block()
         return source.substring(start, index)
