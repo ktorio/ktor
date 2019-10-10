2389a85d81670f81e6c41ad7b72780c930fe98bb
Thomas <thomasjamesfowler97@gmail.com>
2019-10-10 11:16:37 +1100

    Added a configurable maximum number of redirects to follow

--- ktor-client/ktor-client-core/common/src/io/ktor/client/features/HttpRedirect.kt
+++ ktor-client/ktor-client-core/common/src/io/ktor/client/features/HttpRedirect.kt
@@ -13,21 +13,29 @@ import io.ktor.util.*
 /**
  * [HttpClient] feature that handles http redirect
  */
-class HttpRedirect {
-    companion object Feature : HttpClientFeature<Unit, HttpRedirect> {
+class HttpRedirect(config: Config) {
+    val maximumRedirects: Int = config.maximumRedirects
+
+    class Config {
+        /* RFC2068 recommends a maximum of five redirection */
+        var maximumRedirects: Int = 5
+    }
+
+    companion object Feature : HttpClientFeature<Config, HttpRedirect> {
         override val key: AttributeKey<HttpRedirect> = AttributeKey("HttpRedirect")
 
-        override fun prepare(block: Unit.() -> Unit): HttpRedirect = HttpRedirect()
+        override fun prepare(block: Config.() -> Unit): HttpRedirect = HttpRedirect(Config().apply(block))
 
         override fun install(feature: HttpRedirect, scope: HttpClient) {
             scope.feature(HttpSend)!!.intercept { origin ->
-                handleCall(origin)
+                handleCall(feature, origin)
             }
         }
 
-        private suspend fun Sender.handleCall(origin: HttpClientCall): HttpClientCall {
+        private suspend fun Sender.handleCall(feature: HttpRedirect, origin: HttpClientCall): HttpClientCall {
             if (!origin.response.status.isRedirect()) return origin
 
+            var redirects = 0
             var call = origin
             while (true) {
                 val location = call.response.headers[HttpHeaders.Location]
@@ -40,8 +48,12 @@ class HttpRedirect {
 
                     location?.let { url.takeFrom(it) }
                 })
+                redirects++
 
                 if (!call.response.status.isRedirect()) return call
+
+                // TODO: Just return last call, return original (closed) call, or throw an exception?
+                if (redirects > feature.maximumRedirects) return call
             }
         }
     }
