8dffe86a1bfebc3148c7b9cd189fb2c22fff9fdd
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2019-10-25 17:15:57 +0300

    Add watchos/tvos native targets

--- ktor-client/ktor-client-tests/ktor-client-tests-dispatcher/build.gradle.kts
+++ ktor-client/ktor-client-tests/ktor-client-tests-dispatcher/build.gradle.kts
@@ -1,13 +1,35 @@
 val ideaActive: Boolean by project.extra
 
-if (ideaActive) {
-    kotlin {
-        sourceSets {
+kotlin {
+    sourceSets {
+        if (ideaActive) {
             val posixIde by creating {
                 kotlin.srcDir("posixIde/src")
             }
 
             get("posixMain").dependsOn(posixIde)
+        } else {
+            configure(listOf(watchosArm32().compilations.get("main").defaultSourceSet)) {
+                kotlin.srcDir("iosArm32/src")
+            }
+
+            configure(
+                listOf(
+                    watchosArm64().compilations.get("main").defaultSourceSet,
+                    tvosArm64().compilations.get("main").defaultSourceSet
+                )
+            ) {
+                kotlin.srcDir("iosArm64/src")
+            }
+
+            configure(
+                listOf(
+                    watchosX86().compilations.get("main").defaultSourceSet,
+                    tvosX64().compilations.get("main").defaultSourceSet
+                )
+            ) {
+                kotlin.srcDir("iosX64")
+            }
         }
     }
 }
