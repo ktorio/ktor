3ee2ae68734ab6d3740ec5ba8e978c2b5502e72d
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2019-12-10 16:18:29 +0300

    Fix withLock usage

--- ktor-utils/common/src/io/ktor/util/collections/ConcurrentCollection.kt
+++ ktor-utils/common/src/io/ktor/util/collections/ConcurrentCollection.kt
@@ -13,45 +13,45 @@ open class ConcurrentCollection<E>(
     private val lock: Lock
 ) : MutableCollection<E> {
 
-    override val size: Int get() = lock.use {
+    override val size: Int get() = lock.withLock {
         delegate.size
     }
 
-    override fun contains(element: E): Boolean = lock.use {
+    override fun contains(element: E): Boolean = lock.withLock {
         delegate.contains(element)
     }
 
-    override fun containsAll(elements: Collection<E>): Boolean = lock.use {
+    override fun containsAll(elements: Collection<E>): Boolean = lock.withLock {
         delegate.containsAll(elements)
     }
 
-    override fun isEmpty(): Boolean = lock.use {
+    override fun isEmpty(): Boolean = lock.withLock {
         delegate.isEmpty()
     }
 
-    override fun add(element: E): Boolean = lock.use {
+    override fun add(element: E): Boolean = lock.withLock {
         delegate.add(element)
     }
 
-    override fun addAll(elements: Collection<E>): Boolean = lock.use {
+    override fun addAll(elements: Collection<E>): Boolean = lock.withLock {
         delegate.addAll(elements)
     }
 
-    override fun clear() = lock.use {
+    override fun clear() = lock.withLock {
         delegate.clear()
     }
 
     override fun iterator(): MutableIterator<E> = delegate.iterator()
 
-    override fun remove(element: E): Boolean = lock.use {
+    override fun remove(element: E): Boolean = lock.withLock {
         delegate.remove(element)
     }
 
-    override fun removeAll(elements: Collection<E>): Boolean = lock.use {
+    override fun removeAll(elements: Collection<E>): Boolean = lock.withLock {
         delegate.removeAll(elements)
     }
 
-    override fun retainAll(elements: Collection<E>): Boolean = lock.use {
+    override fun retainAll(elements: Collection<E>): Boolean = lock.withLock {
         delegate.retainAll(elements)
     }
 }
--- ktor-utils/common/src/io/ktor/util/collections/ConcurrentMap.kt
+++ ktor-utils/common/src/io/ktor/util/collections/ConcurrentMap.kt
@@ -13,21 +13,21 @@ class ConcurrentMap<Key, Value>(
     private val delegate: MutableMap<Key, Value> = mutableMapOf<Key, Value>()
 ) : MutableMap<Key, Value> {
 
-    override val size: Int get() = lock.use { delegate.size }
+    override val size: Int get() = lock.withLock { delegate.size }
 
-    override fun containsKey(key: Key): Boolean = lock.use {
+    override fun containsKey(key: Key): Boolean = lock.withLock {
         delegate.containsKey(key)
     }
 
-    override fun containsValue(value: Value): Boolean = lock.use {
+    override fun containsValue(value: Value): Boolean = lock.withLock {
         delegate.containsValue(value)
     }
 
-    override fun get(key: Key): Value? = lock.use {
+    override fun get(key: Key): Value? = lock.withLock {
         delegate[key]
     }
 
-    override fun isEmpty(): Boolean = lock.use {
+    override fun isEmpty(): Boolean = lock.withLock {
         delegate.isEmpty()
     }
 
@@ -37,19 +37,19 @@ class ConcurrentMap<Key, Value>(
 
     override val values: MutableCollection<Value> = ConcurrentCollection(delegate.values, lock)
 
-    override fun clear() = lock.use {
+    override fun clear() = lock.withLock {
         delegate.clear()
     }
 
-    override fun put(key: Key, value: Value): Value? = lock.use {
+    override fun put(key: Key, value: Value): Value? = lock.withLock {
         delegate.put(key, value)
     }
 
-    override fun putAll(from: Map<out Key, Value>) = lock.use {
+    override fun putAll(from: Map<out Key, Value>) = lock.withLock {
         delegate.putAll(from)
     }
 
-    override fun remove(key: Key): Value? = lock.use {
+    override fun remove(key: Key): Value? = lock.withLock {
         delegate.remove(key)
     }
 
@@ -60,7 +60,7 @@ class ConcurrentMap<Key, Value>(
         "This is accidentally does insert instead of get. Use computeIfAbsent or getOrElse instead.",
         level = DeprecationLevel.ERROR
     )
-    fun getOrDefault(key: Key, block: () -> Value): Value = lock.use {
+    fun getOrDefault(key: Key, block: () -> Value): Value = lock.withLock {
         return computeIfAbsent(key, block)
     }
 
@@ -68,7 +68,7 @@ class ConcurrentMap<Key, Value>(
      * Perform concurrent get and compute [block] if no associated value found in the map and insert the new value.
      * @return an existing value or the result of [block]
      */
-    fun computeIfAbsent(key: Key, block: () -> Value): Value = lock.use {
+    fun computeIfAbsent(key: Key, block: () -> Value): Value = lock.withLock {
         get(key)?.let { return it }
 
         val result = block()
