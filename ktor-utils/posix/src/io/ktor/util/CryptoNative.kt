f2f8dfaa7e2015606c014094defae0811647d9be
Sebastian Ohm <sebastian.ohm@ubimax.com>
2019-12-28 19:27:13 +0100

    Implementing generateNonce() for iOS

--- ktor-utils/posix/src/io/ktor/util/CryptoNative.kt
+++ ktor-utils/posix/src/io/ktor/util/CryptoNative.kt
@@ -4,12 +4,39 @@
 
 package io.ktor.util
 
+import kotlinx.cinterop.*
+import platform.Foundation.*
+import platform.posix.*
+
+private const val NONCE_SIZE_IN_BYTES = 16
 
 /**
- * Generates a nonce string. Could block if the system's entropy source is empty
+ * Generates a nonce string 16 characters long. Could block if the system's entropy source is empty
  */
 @InternalAPI
-actual fun generateNonce(): String = error("[generateNonce] is not supported on iOS")
+actual fun generateNonce(): String {
+    memScoped {
+        return NSData
+            .create(
+                bytes = allocArrayOf(elements = generateRandomByteArray()),
+                length = NONCE_SIZE_IN_BYTES.toULong()
+            ).base64EncodedStringWithOptions(
+                options = 0.toULong()
+            )
+    }
+}
+
+private fun generateRandomByteArray(): ByteArray {
+    val result = ByteArray(NONCE_SIZE_IN_BYTES)
+    for (i in 0..3) {
+        val random = arc4random().toLong()
+        result[(i * 4) + 3] = (random and 0xFFFF).toByte()
+        result[(i * 4) + 2] = ((random ushr 8) and 0xFFFF).toByte()
+        result[(i * 4) + 1] = ((random ushr 16) and 0xFFFF).toByte()
+        result[(i * 4)] = ((random ushr 24) and 0xFFFF).toByte()
+    }
+    return result
+}
 
 /**
  * Create [Digest] from specified hash [name].
