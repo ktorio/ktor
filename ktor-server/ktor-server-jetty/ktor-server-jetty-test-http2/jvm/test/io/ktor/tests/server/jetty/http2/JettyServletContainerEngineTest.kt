309854b7e574c74ecc0078cc5aba16d78f2923de
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2020-01-23 15:25:24 +0300

    Add jacoco coverage

--- ktor-server/ktor-server-jetty/ktor-server-jetty-test-http2/jvm/test/io/ktor/tests/server/jetty/http2/JettyServletContainerEngineTest.kt
+++ ktor-server/ktor-server-jetty/ktor-server-jetty-test-http2/jvm/test/io/ktor/tests/server/jetty/http2/JettyServletContainerEngineTest.kt
@@ -2,7 +2,7 @@
  * Copyright 2014-2019 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
-package io.ktor.tests.server.jetty
+package io.ktor.tests.server.jetty.http2
 
 import io.ktor.server.engine.*
 import io.ktor.server.jetty.*
