9229af9a744a2d5cff10c9b3c296524399c8d7d7
Ibragimov Ruslan <493656+IRus@users.noreply.github.com>
2020-03-02 09:07:57 +0300

    Fixes CPU 100% Apache Ktor Http Client #1018 (#1689)

--- ktor-client/ktor-client-apache/build.gradle.kts
+++ ktor-client/ktor-client-apache/build.gradle.kts
@@ -1,12 +1,19 @@
 description = "Apache backend for ktor http client"
 
 val apache_version: String by project.extra
+val apache_core_version: String by project.extra
 
 kotlin.sourceSets {
     jvmMain {
         dependencies {
             api(project(":ktor-client:ktor-client-core"))
             api("org.apache.httpcomponents:httpasyncclient:$apache_version")
+            implementation("org.apache.httpcomponents:httpcore-nio:$apache_core_version") {
+                because("https://github.com/ktorio/ktor/issues/1018")
+            }
+            implementation("org.apache.httpcomponents:httpcore:$apache_core_version") {
+                because("https://github.com/ktorio/ktor/issues/1018")
+            }
         }
     }
     jvmTest {
