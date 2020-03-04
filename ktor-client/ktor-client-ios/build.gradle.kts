0d1a4cca14e9d2fe1aaff59e85934cfb79b86e1b
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2020-03-04 10:20:53 +0300

    Cleanup iOS proxy configuration

--- ktor-client/ktor-client-ios/build.gradle.kts
+++ ktor-client/ktor-client-ios/build.gradle.kts
@@ -3,19 +3,6 @@ val ideaActive: Boolean by project.extra
 val serialization_version: String by project.extra
 
 kotlin.sourceSets {
-    if (!ideaActive) {
-        listOf("watchosArm32Main", "watchosArm64Main", "watchosX86Main").map { getByName(it) }.forEach {
-            it.kotlin.srcDir("watchos/src")
-        }
-        listOf("tvosArm64Main", "tvosX64Main", "iosArm32Main", "iosArm64Main", "iosX64Main", "macosX64Main").map { getByName(it) }.forEach {
-            it.kotlin.srcDir("nonWatchos/src")
-        }
-    } else {
-        darwinMain {
-            kotlin.srcDir("nonWatchos/src")
-        }
-    }
-
     darwinMain {
         dependencies {
             api(project(":ktor-client:ktor-client-core"))
--- ktor-client/ktor-client-ios/darwin/src/io/ktor/client/engine/ios/ProxySupportCommon.kt
+++ ktor-client/ktor-client-ios/darwin/src/io/ktor/client/engine/ios/ProxySupportCommon.kt
@@ -4,6 +4,29 @@
 
 package io.ktor.client.engine.ios
 
+import io.ktor.http.*
+import platform.CFNetwork.*
+import platform.CoreFoundation.*
 import platform.Foundation.*
 
-internal expect fun NSURLSessionConfiguration.setupProxy(config: IosClientEngineConfig)
+internal fun NSURLSessionConfiguration.setupProxy(config: IosClientEngineConfig) {
+    val proxy = config.proxy ?: return
+    val url = proxy.url
+
+    when (url.protocol) {
+        URLProtocol.HTTP -> setupHttpProxy(url)
+        URLProtocol.HTTPS -> setupHttpProxy(url)
+//        URLProtocol.SOCKS -> setupSocksProxy(url)
+        else -> error("Proxy type ${url.protocol.name} is unsupported by iOS client engine.")
+    }
+}
+
+internal fun NSURLSessionConfiguration.setupHttpProxy(url: Url) {
+    connectionProxyDictionary = mapOf(
+        kCFNetworkProxiesHTTPEnable.toNSString() to 1,
+        kCFNetworkProxiesHTTPProxy.toNSString() to url.host,
+        kCFNetworkProxiesHTTPPort.toNSString() to url.port
+    )
+}
+
+internal fun CFStringRef?.toNSString(): NSString = CFBridgingRelease(this) as NSString
