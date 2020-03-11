d25a538271168c1561abf385cd6fb41e81b7089e
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2020-03-11 10:25:17 +0300

    Remove old iOS test task

--- ktor-client/ktor-client-tests/common/test/io/ktor/client/tests/FullFormTest.kt
+++ ktor-client/ktor-client-tests/common/test/io/ktor/client/tests/FullFormTest.kt
@@ -4,15 +4,10 @@
 
 package io.ktor.client.tests
 
-import io.ktor.client.call.*
 import io.ktor.client.request.*
-import io.ktor.client.response.*
 import io.ktor.client.statement.*
 import io.ktor.client.tests.utils.*
 import io.ktor.http.*
-import io.ktor.util.*
-import kotlinx.coroutines.*
-import io.ktor.utils.io.core.*
 import kotlin.test.*
 
 class FullFormTest : ClientLoader() {
