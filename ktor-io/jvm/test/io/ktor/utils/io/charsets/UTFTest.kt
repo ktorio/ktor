34996a6e960a99cf494b17f23a49f3e321f3aec4
dmitrievanthony <dmitrievanthony@gmail.com>
2020-03-13 15:52:42 +0300

    Fix UTF8 parsing (#1718).

--- ktor-io/jvm/test/io/ktor/utils/io/charsets/UTFTest.kt
+++ ktor-io/jvm/test/io/ktor/utils/io/charsets/UTFTest.kt
@@ -11,12 +11,48 @@ import kotlin.test.*
 class UTFTest {
     @Test
     fun testDecodeUTF8LineOnBufferWithOffset() {
-        val buffer = "HELLO\nWORLD\r\n1\r\n2\n".allocateBufferWithOffset(4)
+        val buffer = "HELLO\nWORLD\r\n1\r\n2\n".allocateBufferWithOffset(offset = 4)
 
-        buffer.assertEqualsDecodedUTF8Line("HELLO", 6)
-        buffer.assertEqualsDecodedUTF8Line("WORLD", 13)
-        buffer.assertEqualsDecodedUTF8Line("1", 16)
-        buffer.assertEqualsDecodedUTF8Line("2", 18)
+        buffer.assertEqualsDecodedUTF8Line("HELLO", expectedPosition = 6)
+        buffer.assertEqualsDecodedUTF8Line("WORLD", expectedPosition = 13)
+        buffer.assertEqualsDecodedUTF8Line("1", expectedPosition = 16)
+        buffer.assertEqualsDecodedUTF8Line("2", expectedPosition = 18)
+        assertEquals(0, buffer.remaining())
+    }
+
+    @Test
+    fun testDecodeUTF8LineOnBufferWithOffsetAndStringWithoutNewLineAtTheEnd() {
+        val buffer = "HELLO WORLD".allocateBufferWithOffset(offset = 4)
+
+        buffer.assertEqualsDecodedUTF8Line("HELLO WORLD", expectedPosition = 11)
+        assertEquals(0, buffer.remaining())
+    }
+
+    @Test
+    fun testDecodeUTF8LineOnBufferWithOffsetAndEmptyString() {
+        val buffer = "".allocateBufferWithOffset(offset = 4)
+
+        buffer.assertEqualsDecodedUTF8Line("", expectedPosition = 0)
+        assertEquals(0, buffer.remaining())
+    }
+
+    @Test
+    fun testDecodeUTF8LineOnBufferWithOffsetAnd2ByteChars() {
+        val buffer = "ПРИВЕТ\nМИР\r\n1\r\n2\n".allocateBufferWithOffset(offset = 4)
+
+        buffer.assertEqualsDecodedUTF8Line("ПРИВЕТ", expectedPosition = 13)
+        buffer.assertEqualsDecodedUTF8Line("МИР", expectedPosition = 21)
+        buffer.assertEqualsDecodedUTF8Line("1", expectedPosition = 24)
+        buffer.assertEqualsDecodedUTF8Line("2", expectedPosition = 26)
+        assertEquals(0, buffer.remaining())
+    }
+
+    @Test
+    fun testDecodeUTF8LineOnBufferWithOffsetAndStringWithoutNewLineAtTheEndAnd2ByteChars() {
+        val buffer = "ПРИВЕТМИР".allocateBufferWithOffset(offset = 4)
+
+        buffer.assertEqualsDecodedUTF8Line("ПРИВЕТМИР", expectedPosition = 18)
+        assertEquals(0, buffer.remaining())
     }
 
     private fun ByteBuffer.assertEqualsDecodedUTF8Line(expectedResult: String, expectedPosition: Int) {
@@ -28,11 +64,12 @@ class UTFTest {
     }
 
     private fun String.allocateBufferWithOffset(offset: Int): ByteBuffer {
-        val buffer: ByteBuffer = ByteBuffer.allocate(offset + length)
+        val array = toByteArray()
+        val buffer: ByteBuffer = ByteBuffer.allocate(offset + array.size)
         repeat(offset) { buffer.put(0) }
 
         val result = buffer.slice()
-        toByteArray().forEach { buffer.put(it) }
+        array.forEach { buffer.put(it) }
 
         return result
     }
