b95a83e1d0b64b6679d29788204cb28c0f1ba2e3
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2020-03-13 17:54:05 +0300

    Fix duplicate source roots

--- ktor-test-dispatcher/build.gradle.kts
+++ ktor-test-dispatcher/build.gradle.kts
@@ -1,5 +1,6 @@
 val ideaActive: Boolean by project.extra
 val isMacosHost: Boolean by project.extra
+
 kotlin {
     sourceSets {
         if (ideaActive) {
@@ -13,28 +14,6 @@ kotlin {
             }
 
             get("posixMain").dependsOn(posixIde)
-        } else {
-            configure(listOf(watchosArm32().compilations.get("main").defaultSourceSet)) {
-                kotlin.srcDir("iosArm32/src")
-            }
-
-            configure(
-                listOf(
-                    watchosArm64().compilations.get("main").defaultSourceSet,
-                    tvosArm64().compilations.get("main").defaultSourceSet
-                )
-            ) {
-                kotlin.srcDir("iosArm64/src")
-            }
-
-            configure(
-                listOf(
-                    watchosX86().compilations.get("main").defaultSourceSet,
-                    tvosX64().compilations.get("main").defaultSourceSet
-                )
-            ) {
-                kotlin.srcDir("iosX64")
-            }
         }
     }
 }
---
+++ ktor-test-dispatcher/tvosX64/src/TestTvosX64.kt
@@ -0,0 +1,28 @@
+@file:Suppress("INVISIBLE_MEMBER", "INVISIBLE_REFERENCE")
+
+import kotlinx.coroutines.*
+import kotlin.coroutines.*
+import platform.Foundation.*
+
+/**
+ * Amount of time any task is processed and can't be rescheduled.
+ */
+private const val TIME_QUANTUM = 0.01
+
+/**
+ * Test runner for native suspend tests.
+ */
+actual fun testSuspend(
+    context: CoroutineContext,
+    block: suspend CoroutineScope.() -> Unit
+): Unit = runBlocking {
+    val loop = coroutineContext[ContinuationInterceptor] as EventLoop
+
+    val task = launch { block() }
+    while (!task.isCompleted) {
+        val date = NSDate().addTimeInterval(TIME_QUANTUM) as NSDate
+        NSRunLoop.mainRunLoop.runUntilDate(date)
+
+        loop.processNextEvent()
+    }
+}
