576cee74ae7ec48e34e87f3e83290c63a9e6cae9
Sergey Igushkin <hotkeytlt@gmail.com>
2020-04-17 10:41:58 +0300

    Workaround for cinterops used in common code: publish them as source set outputs

--- ktor-utils/build.gradle.kts
+++ ktor-utils/build.gradle.kts
@@ -1,6 +1,7 @@
 import org.jetbrains.kotlin.gradle.plugin.mpp.*
 
 val nativeCompilations: List<KotlinNativeCompilation> by project.extra
+val ideaActive: Boolean by project.extra
 
 kotlin {
     configure(nativeCompilations) {
@@ -21,6 +22,17 @@ kotlin {
             dependencies {
                 api(project(":ktor-test-dispatcher"))
             }
+        }
+
+        // Hack: register the Native interop klibs as outputs of Kotlin source sets:
+        if (!ideaActive) {
+            val utilsInterop by creating
+            getByName("posixMain").dependsOn(utilsInterop)
+            apply(from = "$rootDir/gradle/interop-as-source-set-klib.gradle")
+            (project.ext.get("registerInteropAsSourceSetOutput") as groovy.lang.Closure<*>).invoke(
+                linuxX64().compilations["main"].cinterops["utils"],
+                utilsInterop
+            )
         }
     }
 }
