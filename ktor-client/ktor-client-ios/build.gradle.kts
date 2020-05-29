9d621d8b8856a3dd5dffa6443152a4d527594723
Dmitry Savvinov <dsavvinov@gmail.com>
2020-05-29 11:59:05 +0300

    HMPP & 1.4-M2 fixes, ready for review (#1905)
    * Gradle 6.3
    
    * Kotlin 1.4-M2-eap-72
    
    * Update kotlinx libs dev versions
    
    serialization_version=0.20.0-mpp-dev-7
    coroutines_version=1.3.6-mpp-dev-10
    atomicfu_version=0.14.3-mpp-dev-10
    benchmarks_version=0.2.0-mpp-dev-5
    
    * Fix MPP dependencies: use only root modules
    
    * Workaround for invisible expect constructor with default param
    
    * Workaround for cinterops used in common code: publish them as source set outputs
    
    * Workaround for Bintray treating .sha512 files as artifacts
    
    * Workaround for https://youtrack.jetbrains.com/issue/KT-38872
    
    * Workaround for https://youtrack.jetbrains.com/issue/KT-39037
    
    * Enable Gradle module metadata in all modules
    
    * Workaround https://youtrack.jetbrains.com/issue/KT-39063
    
    * Rename root MPP module so that it has no suffix, add -common to metadata module
    
    * Enable linuxX64 cross-cinterop
    
    * Fix names of legacy JS tasks
    
    * Advance versions for 1.4-M2
    
    * Remove obsolete workaround
    
    Co-authored-by: Sergey Igushkin <hotkeytlt@gmail.com>
    Co-authored-by: Sergey Igushkin <sergey.igushkin@jetbrains.com>

--- ktor-client/ktor-client-ios/build.gradle.kts
+++ ktor-client/ktor-client-ios/build.gradle.kts
@@ -12,7 +12,6 @@ kotlin.sourceSets {
         dependencies {
             api(project(":ktor-client:ktor-client-features:ktor-client-logging"))
             api(project(":ktor-client:ktor-client-features:ktor-client-json"))
-            api("org.jetbrains.kotlinx:kotlinx-serialization-runtime-native:$serialization_version")
         }
     }
 }
