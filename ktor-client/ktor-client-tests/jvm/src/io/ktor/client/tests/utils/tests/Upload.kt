a07792fb0a77793e76dc2e134a3db8e876331645
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2020-06-18 15:54:20 +0300

    Fix sending blank ContentType in Apache engine

---
+++ ktor-client/ktor-client-tests/jvm/src/io/ktor/client/tests/utils/tests/Upload.kt
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2014-2020 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package io.ktor.client.tests.utils.tests
+
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+
+fun Application.uploadTest() {
+    routing {
+        route("upload") {
+            post("content") {
+                val message = call.request.headers[HttpHeaders.ContentType] ?: "EMPTY"
+                call.respond(HttpStatusCode.OK, message)
+            }
+        }
+    }
+}
