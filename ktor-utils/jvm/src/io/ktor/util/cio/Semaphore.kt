d3b5498f6abf74e9420896449b869c116f3af97f
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2020-07-15 17:37:30 +0300

    Revert Semaphore breaking change

---
+++ ktor-utils/jvm/src/io/ktor/util/cio/Semaphore.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2014-2020 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package io.ktor.util.cio
+
+import kotlinx.coroutines.sync.Semaphore
+
+@Deprecated(
+    "Ktor Semaphore is deprecated and will be removed in ktor 2.0.0. Consider using kotlinx.coroutines Semaphore instead.",
+    level = DeprecationLevel.WARNING,
+    replaceWith = ReplaceWith("Semaphore", "kotlinx.coroutines.sync.Semaphore")
+)
+class Semaphore(val limit: Int) {
+    private val delegate = Semaphore(limit)
+
+    @Deprecated(
+        "Ktor Semaphore is deprecated and will be removed in ktor 2.0.0. Consider using kotlinx.coroutines Semaphore instead.",
+        level = DeprecationLevel.WARNING,
+        replaceWith = ReplaceWith("acquire()", "kotlinx.coroutines.sync.Semaphore")
+    )
+    suspend fun enter() {
+        delegate.acquire()
+    }
+
+    suspend fun acquire() {
+        delegate.acquire()
+    }
+
+    @Deprecated(
+        "Ktor Semaphore is deprecated and will be removed in ktor 2.0.0. Consider using kotlinx.coroutines Semaphore instead.",
+        level = DeprecationLevel.WARNING,
+        replaceWith = ReplaceWith("release()", "kotlinx.coroutines.sync.Semaphore")
+    )
+    fun leave() {
+        delegate.release()
+    }
+
+    fun release() {
+        delegate.release()
+    }
+}
