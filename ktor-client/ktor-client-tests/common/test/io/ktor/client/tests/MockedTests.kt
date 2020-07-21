a8c91b3f51211f3e763120572705a8d0af68a02a
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2020-07-21 15:13:00 +0300

    Remove obsolete check

--- ktor-client/ktor-client-tests/common/test/io/ktor/client/tests/MockedTests.kt
+++ ktor-client/ktor-client-tests/common/test/io/ktor/client/tests/MockedTests.kt
@@ -41,8 +41,7 @@ class MockedTests {
     }
 
     @Test
-    // TODO: unmute when JS IR serialization is fixed https://github.com/Kotlin/kotlinx.serialization/issues/751
-    fun testWithLongJson() = if (isKotlinJsIr) Unit else testWithEngine(MockEngine) {
+    fun testWithLongJson() = testWithEngine(MockEngine) {
         config {
             install(JsonFeature) {
                 serializer = KotlinxSerializer()
