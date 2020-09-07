47e4d0cfbb825db3b37fd847c9ea50baa42fff16
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2020-09-07 12:08:40 +0300

    Revert "socketTimeout in EndpointConfig was read only (#2030)"
    This reverts commit 614b372d

--- ktor-client/ktor-client-cio/common/src/io/ktor/client/engine/cio/CIOEngineConfig.kt
+++ ktor-client/ktor-client-cio/common/src/io/ktor/client/engine/cio/CIOEngineConfig.kt
@@ -71,7 +71,7 @@ class EndpointConfig {
     /**
      * Socket timeout in millis.
      */
-    var socketTimeout: Long = HttpTimeout.INFINITE_TIMEOUT_MS
+    val socketTimeout: Long = HttpTimeout.INFINITE_TIMEOUT_MS
 
     /**
      * Maximum number of connection attempts.
