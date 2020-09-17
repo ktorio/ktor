97386a8d28dd9bdec85701bc9e589e2607f79cc9
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2020-09-17 18:40:29 +0300

    KTOR-973 Add ConcurrentList equals hashcode toString

--- ktor-utils/common/test/io/ktor/util/ConcurrentListTest.kt
+++ ktor-utils/common/test/io/ktor/util/ConcurrentListTest.kt
@@ -5,6 +5,7 @@
 package io.ktor.util
 
 import io.ktor.util.collections.*
+import kotlin.random.*
 import kotlin.test.*
 
 class ConcurrentListTest {
@@ -21,6 +22,36 @@ class ConcurrentListTest {
 
         assertEquals(0, list.size)
         assertTrue(list.isEmpty())
+    }
+
+    @Test
+    fun testEquals() {
+        assertEquals(emptyList<Unit>(), ConcurrentList())
+
+        val single = ConcurrentList<Int>().apply {
+            add(1)
+        }
+
+        assertEquals(listOf(1), single)
+
+        assertNotEquals(listOf(1), ConcurrentList<Int>().apply {
+            add(2)
+        })
+
+        assertNotEquals(listOf(1), ConcurrentList<Int>().apply {
+            add(1)
+            add(2)
+        })
+
+        assertEquals(listOf(1, 2), ConcurrentList<Int>().apply {
+            add(1)
+            add(2)
+        })
+
+        assertNotEquals(listOf(1, 2, 3), ConcurrentList<Int>().apply {
+            add(1)
+            add(2)
+        })
     }
 
     @Test
