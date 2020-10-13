3750a920226e677d94f17fc11c5c406915baa981
Rustam <rxsinukov@gmail.com>
2020-10-13 12:10:09 +0200

    KTOR-1125 - Rename misleading CIO config property (#2114)
    Subsystem
    Client/Server, related modules
    
    Motivation
    CIO: client throws ConnectTimeoutException when endpoint.connectRetryAttempts = 0
    
    Solution
    Deprecate misleading property. Add new property and delegate the old one to the new one.

--- ktor-client/ktor-client-cio/common/src/io/ktor/client/engine/cio/Endpoint.kt
+++ ktor-client/ktor-client-cio/common/src/io/ktor/client/engine/cio/Endpoint.kt
@@ -152,14 +152,14 @@ internal class Endpoint(
     }
 
     private suspend fun connect(requestData: HttpRequestData): Socket {
-        val retryAttempts = config.endpoint.connectRetryAttempts
+        val connectAttempts = config.endpoint.connectAttempts
         val (connectTimeout, socketTimeout) = retrieveTimeouts(requestData)
         var timeoutFails = 0
 
         connections.incrementAndGet()
 
         try {
-            repeat(retryAttempts) {
+            repeat(connectAttempts) {
                 val connect: suspend CoroutineScope.() -> Socket = {
                     connectionFactory.connect(address) {
                         this.socketTimeout = socketTimeout
@@ -202,15 +202,15 @@ internal class Endpoint(
 
         connections.decrementAndGet()
 
-        throw getTimeoutException(retryAttempts, timeoutFails, requestData)
+        throw getTimeoutException(connectAttempts, timeoutFails, requestData)
     }
 
     /**
-     * Defines exact type of exception based on [retryAttempts] and [timeoutFails].
+     * Defines exact type of exception based on [connectAttempts] and [timeoutFails].
      */
-    private fun getTimeoutException(retryAttempts: Int, timeoutFails: Int, request: HttpRequestData) =
+    private fun getTimeoutException(connectAttempts: Int, timeoutFails: Int, request: HttpRequestData) =
         when (timeoutFails) {
-            retryAttempts -> ConnectTimeoutException(request)
+            connectAttempts -> ConnectTimeoutException(request)
             else -> FailToConnectException()
         }
 
