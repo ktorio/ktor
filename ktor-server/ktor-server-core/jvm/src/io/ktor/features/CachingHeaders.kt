c72119d0defc15fcfc39b54dc9aa8e3265a0b1b7
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2020-10-15 17:17:12 +0300

    KTOR-750 Improve multiple cache-control directives behaviour

--- ktor-server/ktor-server-core/jvm/src/io/ktor/features/CachingHeaders.kt
+++ ktor-server/ktor-server-core/jvm/src/io/ktor/features/CachingHeaders.kt
@@ -1,15 +1,15 @@
 /*
- * Copyright 2014-2019 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2014-2020 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package io.ktor.features
 
 import io.ktor.application.*
-import io.ktor.http.content.*
 import io.ktor.http.*
-import io.ktor.util.pipeline.*
+import io.ktor.http.content.*
 import io.ktor.response.*
 import io.ktor.util.*
+import io.ktor.util.pipeline.*
 
 /**
  * Feature that set [CachingOptions] headers for every response.
@@ -40,10 +40,13 @@ public class CachingHeaders(private val optionsProviders: List<(OutgoingContent)
 
         if (options.isNotEmpty()) {
             val headers = Headers.build {
-                options.forEach {
-                    if (it.cacheControl != null)
-                        append(HttpHeaders.CacheControl, it.cacheControl.toString())
-                    it.expires?.let { expires -> append(HttpHeaders.Expires, expires.toHttpDate()) }
+                options.mapNotNull { it.cacheControl }
+                    .mergeCacheControlDirectives()
+                    .ifEmpty { null }?.let { directives ->
+                        append(HttpHeaders.CacheControl, directives.joinToString(separator = ", "))
+                    }
+                options.firstOrNull { it.expires != null }?.expires?.let { expires ->
+                    append(HttpHeaders.Expires, expires.toHttpDate())
                 }
             }
 
@@ -70,7 +73,12 @@ public class CachingHeaders(private val optionsProviders: List<(OutgoingContent)
             val configuration = Configuration().apply(configure)
             val feature = CachingHeaders(configuration.optionsProviders)
 
-            pipeline.sendPipeline.intercept(ApplicationSendPipeline.After) { message -> feature.interceptor(this, message) }
+            pipeline.sendPipeline.intercept(ApplicationSendPipeline.After) { message ->
+                feature.interceptor(
+                    this,
+                    message
+                )
+            }
 
             return feature
         }
