5cc8889bb4a669708aa4f5ecdd0b1897f7b2b358
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2020-10-27 18:20:36 +0300

    KTOR-1195 Introduce PublicAPICandidate annotation

--- ktor-utils/common/src/io/ktor/util/Annotations.kt
+++ ktor-utils/common/src/io/ktor/util/Annotations.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2019 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2014-2020 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package io.ktor.util
@@ -46,3 +46,29 @@ public annotation class InternalAPI
     AnnotationTarget.CONSTRUCTOR
 )
 public annotation class KtorExperimentalAPI
+
+/**
+ * API marked with this annotation is intended to become public in the future [version].
+ * Usually it means that the API can't be public at the moment of development due to
+ * compatibility guarantees restrictions.
+ *
+ * Marking a public declaration with this annotation makes no sense
+ * except for the case when it is also marked with [InternalAPI].
+ *
+ * Please note that the specified [version] and the fact of making something a candidate is not a guarantee,
+ * so the target version could be changed without any notice or even the promotion could be cancelled at all.
+ *
+ * @property version in which the API is planned to be promoted
+ */
+@InternalAPI
+@Retention(AnnotationRetention.SOURCE)
+@Target(
+    AnnotationTarget.CLASS,
+    AnnotationTarget.ANNOTATION_CLASS,
+    AnnotationTarget.CONSTRUCTOR,
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY,
+    AnnotationTarget.PROPERTY_SETTER,
+    AnnotationTarget.TYPEALIAS
+)
+public annotation class PublicAPICandidate(val version: String)
