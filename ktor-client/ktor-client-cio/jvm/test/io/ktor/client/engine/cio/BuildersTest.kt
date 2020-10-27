71dd365aeb8cb53159b05a5438e525cc3577ef0c
Rustam <rxsinukov@gmail.com>
2020-10-27 16:17:21 +0100

    KTOR-1176 Hide ws function from cio module (#2154)

---
+++ ktor-client/ktor-client-cio/jvm/test/io/ktor/client/engine/cio/BuildersTest.kt
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2014-2020 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package io.ktor.client.engine.cio
+
+import io.ktor.client.*
+import io.ktor.client.features.websocket.*
+import kotlinx.coroutines.*
+import kotlin.test.*
+
+class BuildersTest {
+
+    @Test
+    fun testResolvingWsFunction() = runBlocking {
+        try {
+            HttpClient(CIO).ws("http://localhost") {}
+        } catch (_: Throwable) {
+            // no op
+        }
+    }
+}
