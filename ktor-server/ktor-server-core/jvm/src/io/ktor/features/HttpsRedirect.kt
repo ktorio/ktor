bc0fdf7795051aac2801ad341e4301ab5ac9658d
Rustam <rxsinukov@gmail.com>
2020-10-28 17:00:57 +0100

    KTOR-1215 Add explicit visibility modifier (#2166)

--- ktor-server/ktor-server-core/jvm/src/io/ktor/features/HttpsRedirect.kt
+++ ktor-server/ktor-server-core/jvm/src/io/ktor/features/HttpsRedirect.kt
@@ -65,7 +65,7 @@ public class HttpsRedirect(config: Configuration) {
          * Exclude calls with paths matching the [pathSuffix] from being redirected to https by this feature.
         */
         @KtorExperimentalAPI
-        fun excludeSuffix(pathSuffix: String) {
+        public fun excludeSuffix(pathSuffix: String) {
             exclude { call ->
                 call.request.origin.uri.endsWith(pathSuffix)
             }
