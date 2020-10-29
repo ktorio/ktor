bfeb6c8c267516b1cf79b3c8f1015ea5c30fe595
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2020-10-29 16:18:48 +0300

    KTOR-1216 Stabilize server additional tools

--- ktor-server/ktor-server-servlet/jvm/src/io/ktor/server/servlet/JAASBridge.kt
+++ ktor-server/ktor-server-servlet/jvm/src/io/ktor/server/servlet/JAASBridge.kt
@@ -1,17 +1,15 @@
 /*
- * Copyright 2014-2019 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2014-2020 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package io.ktor.server.servlet
 
 import io.ktor.request.*
-import io.ktor.util.*
 import java.security.*
 
 /**
  * Returns Java's JAAS Principal
  */
-@KtorExperimentalAPI
 public val ApplicationRequest.javaSecurityPrincipal: Principal?
     get() = when (this) {
         is ServletApplicationRequest -> servletRequest.userPrincipal
