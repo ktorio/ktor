c7aea63c366288aa2aadb8669f7ce0a88a56b1b3
Hadi Hariri <mail@hadihariri.com>
2020-11-03 16:28:55 +0100

    KTOR-1241 Add comments to script referencing limitation of TeamCity

--- buildScripts/prepareKeysWindows.ps1
+++ buildScripts/prepareKeysWindows.ps1
@@ -1,3 +1,8 @@
+# The reason these build scripts are in the Ktor project is
+# due to a limitation of TeamCity, in that you cannot reference
+# auxiliary build files from the repository where settings are stored
+# For more information see https://youtrack.jetbrains.com/issue/TW-68479
+
 md $Env:SIGN_KEY_LOCATION -force
 cd $Env:SIGN_KEY_LOCATION
 
