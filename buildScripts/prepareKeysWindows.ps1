e556b1b433b0cc80a40c663e2aea495cf70a2b3e
Hadi Hariri <mail@hadihariri.com>
2020-11-03 16:27:20 +0100

    KTOR-1241 Add alias for GPG

--- buildScripts/prepareKeysWindows.ps1
+++ buildScripts/prepareKeysWindows.ps1
@@ -5,7 +5,7 @@ cd $Env:SIGN_KEY_LOCATION
 # Hard-coding path for GPG since this fails on TeamCity
 # $gpg=(get-command gpg.exe).Path
 $gpg="C:\Program Files (x86)\Gpg4win\..\GnuPG\bin\gpg.exe"
-
+Set-Alias -Name gpg2 -Value $gpg
 
 echo "Exporting public key"
 [System.IO.File]::WriteAllText("$pwd\keyfile", $Env:SIGN_KEY_PUBLIC)
