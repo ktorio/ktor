71449463f0b2aab2353257fc8504c1c5e0826c9e
Thiakil <xander@thiakil.com>
2020-11-07 12:23:14 +0800

    add JWT shortcut functions

--- ktor-features/ktor-auth-jwt/jvm/src/io/ktor/auth/jwt/JWTAuth.kt
+++ ktor-features/ktor-auth-jwt/jvm/src/io/ktor/auth/jwt/JWTAuth.kt
@@ -27,18 +27,115 @@ private val JWTAuthKey: Any = "JWTAuth"
 private val JWTLogger: Logger = LoggerFactory.getLogger("io.ktor.auth.jwt")
 
 /**
+ * Shortcut functions for standard registered [JWT Claims](https://tools.ietf.org/html/rfc7519#section-4.1)
+ */
+public interface JWTPayloadHolder {
+    /**
+     * The JWT payload
+     */
+    public val payload: Payload
+
+    /**
+     * Get the value of the "iss" claim, or null if it's not available.
+     *
+     * The "iss" (issuer) claim identifies the principal that issued the
+     * JWT.  The processing of this claim is generally application specific.
+     * The "iss" value is a case-sensitive string containing a StringOrURI
+     * value.  Use of this claim is OPTIONAL.
+     */
+    public val issuer: String? get() = payload.issuer
+
+    /**
+     * Get the value of the "sub" claim, or null if it's not available.
+     *
+     * The "sub" (subject) claim identifies the principal that is the
+     * subject of the JWT.  The claims in a JWT are normally statements
+     * about the subject.  The subject value MUST either be scoped to be
+     * locally unique in the context of the issuer or be globally unique.
+     * The processing of this claim is generally application specific.  The
+     * "sub" value is a case-sensitive string containing a StringOrURI
+     * value.  Use of this claim is OPTIONAL.
+     */
+    public val subject: String? get() = payload.subject
+
+    /**
+     * Get the value of the "aud" claim, or an empty list if it's not available.
+     *
+     * The "aud" (audience) claim identifies the recipients that the JWT is
+     * intended for.  Each principal intended to process the JWT MUST
+     * identify itself with a value in the audience claim.  If the principal
+     * processing the claim does not identify itself with a value in the
+     * "aud" claim when this claim is present, then the JWT MUST be
+     * rejected.  In the general case, the "aud" value is an array of case-
+     * sensitive strings, each containing a StringOrURI value.  In the
+     * special case when the JWT has one audience, the "aud" value MAY be a
+     * single case-sensitive string containing a StringOrURI value.  The
+     * interpretation of audience values is generally application specific.
+     * Use of this claim is OPTIONAL.
+     */
+    public val audience: List<String> get() = payload.audience ?: emptyList()
+
+    /**
+     * Get the value of the "exp" claim, or null if it's not available.
+     *
+     * The "exp" (expiration time) claim identifies the expiration time on
+     * or after which the JWT MUST NOT be accepted for processing.  The
+     * processing of the "exp" claim requires that the current date/time
+     * MUST be before the expiration date/time listed in the "exp" claim.
+     * Implementers MAY provide for some small leeway, usually no more than
+     * a few minutes, to account for clock skew. Use of this claim is OPTIONAL.
+     */
+    public val expiresAt: Date? get() = payload.expiresAt
+
+    /**
+     * Get the value of the "nbf" claim, or null if it's not available.
+     *
+     * The "nbf" (not before) claim identifies the time before which the JWT
+     * MUST NOT be accepted for processing.  The processing of the "nbf"
+     * claim requires that the current date/time MUST be after or equal to
+     * the not-before date/time listed in the "nbf" claim.  Implementers MAY
+     * provide for some small leeway, usually no more than a few minutes, to
+     * account for clock skew. Use of this claim is OPTIONAL.
+     */
+    public val notBefore: Date? get() = payload.notBefore
+
+    /**
+     * Get the value of the "iat" claim, or null if it's not available.
+     *
+     * The "iat" (issued at) claim identifies the time at which the JWT was
+     * issued.  This claim can be used to determine the age of the JWT.
+     * Use of this claim is OPTIONAL.
+     */
+    public val issuedAt: Date? get() = payload.issuedAt
+
+    /**
+     * Get the value of the "jti" claim, or null if it's not available.
+     *
+     * The "jti" (JWT ID) claim provides a unique identifier for the JWT.
+     * The identifier value MUST be assigned in a manner that ensures that
+     * there is a negligible probability that the same value will be
+     * accidentally assigned to a different data object; if the application
+     * uses multiple issuers, collisions MUST be prevented among values
+     * produced by different issuers as well.  The "jti" claim can be used
+     * to prevent the JWT from being replayed.  The "jti" value is a case-
+     * sensitive string.  Use of this claim is OPTIONAL.
+     */
+    public val jwtId: String? get() = payload.id
+}
+
+/**
  * Represents a JWT credential consist of the specified [payload]
  * @param payload JWT
  * @see Payload
  */
-public class JWTCredential(public val payload: Payload) : Credential
+public class JWTCredential(public override val payload: Payload) : Credential, JWTPayloadHolder
 
 /**
  * Represents a JWT principal consist of the specified [payload]
  * @param payload JWT
  * @see Payload
  */
-public class JWTPrincipal(public val payload: Payload) : Principal
+public class JWTPrincipal(public override val payload: Payload) : Principal, JWTPayloadHolder
 
 /**
  * JWT verifier configuration function. It is applied on the verifier builder.
