d5fdc3301777f872ece90f7eb6c81c8b6a200d9b
thomas <thomasjsvos@gmail.com>
2020-11-11 19:06:38 +0100

    Add test for using HttpClient with Dispatchers.Default

--- ktor-client/ktor-client-tests/posix/test/io/ktor/client/tests/HttpClientFreezingTest.kt
+++ ktor-client/ktor-client-tests/posix/test/io/ktor/client/tests/HttpClientFreezingTest.kt
@@ -8,6 +8,7 @@ import io.ktor.client.*
 import io.ktor.client.request.*
 import io.ktor.client.tests.utils.*
 import io.ktor.utils.io.*
+import kotlinx.coroutines.*
 import kotlin.test.*
 
 class HttpClientFreezingTest : ClientLoader() {
@@ -18,6 +19,18 @@ class HttpClientFreezingTest : ClientLoader() {
             client.makeShared()
             val response = client.get<String>("$TEST_SERVER/content/hello")
             assertEquals("hello", response)
+        }
+    }
+
+    @Test
+    fun testRequestWithDefaultDispatcher() = clientTests {
+        test { client ->
+            withContext(Dispatchers.Default) {
+                val response = client.post<String>("$TEST_SERVER/echo") {
+                    body = "hello"
+                }
+                assertEquals("hello", response)
+            }
         }
     }
 }
