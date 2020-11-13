122c326483ba6aa627a2969610655e9e121e4222
Rustam <rxsinukov@gmail.com>
2020-11-13 16:16:58 +0100

    KTOR-1229 Fix mapping of SocketTimeoutException in Android engine (#2192)
    Subsystem
    Client, Android
    
    Motivation
    Timeout﻿ feature: android engine throws Java's SocketTimeoutException instead of ConnectTimeoutException
    
    Solution
    Not only connect() but also statusCode and other methods on HttpURLConnection can throw an exception that we need to map. This PR wraps all of them in a block.

--- ktor-client/ktor-client-android/jvm/src/io/ktor/client/engine/android/AndroidClientEngine.kt
+++ ktor-client/ktor-client-android/jvm/src/io/ktor/client/engine/android/AndroidClientEngine.kt
@@ -80,22 +80,22 @@ public class AndroidClientEngine(override val config: AndroidEngineConfig) : Htt
             }
         }
 
-        connection.timeoutAwareConnect(data)
+        return connection.timeoutAwareConnection(data) { connection ->
+            val responseCode = connection.responseCode
+            val responseMessage = connection.responseMessage
+            val statusCode = responseMessage?.let { HttpStatusCode(responseCode, it) }
+                ?: HttpStatusCode.fromValue(responseCode)
 
-        val responseCode = connection.responseCode
-        val responseMessage = connection.responseMessage
-        val statusCode = responseMessage?.let { HttpStatusCode(responseCode, it) }
-            ?: HttpStatusCode.fromValue(responseCode)
+            val content: ByteReadChannel = connection.content(callContext, data)
+            val headerFields: Map<String, List<String>> = connection.headerFields
+                .mapKeys { it.key?.toLowerCase() ?: "" }
+                .filter { it.key.isNotBlank() }
 
-        val content: ByteReadChannel = connection.content(callContext, data)
-        val headerFields: Map<String, List<String>> = connection.headerFields
-            .mapKeys { it.key?.toLowerCase() ?: "" }
-            .filter { it.key.isNotBlank() }
+            val version: HttpProtocolVersion = HttpProtocolVersion.HTTP_1_1
+            val responseHeaders = HeadersImpl(headerFields)
 
-        val version: HttpProtocolVersion = HttpProtocolVersion.HTTP_1_1
-        val responseHeaders = HeadersImpl(headerFields)
-
-        return HttpResponseData(statusCode, requestTime, responseHeaders, version, content, callContext)
+            HttpResponseData(statusCode, requestTime, responseHeaders, version, content, callContext)
+        }
     }
 
     private fun getProxyAwareConnection(urlString: String): HttpURLConnection {
