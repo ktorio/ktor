15a6edd687c05a558acef3a80c4a6fa64df324b6
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2020-11-17 12:59:48 +0300

    KTOR-1290 Specialize shared usage to native platform in perf

---
+++ ktor-io/common/src/io/ktor/utils/io/core/BufferSharedState.kt
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2014-2020 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package io.ktor.utils.io.core
+
+internal expect class BufferSharedState(limit: Int) {
+    var readPosition: Int
+    var writePosition: Int
+    var startGap: Int
+    var limit: Int
+    var attachment: Any?
+}
