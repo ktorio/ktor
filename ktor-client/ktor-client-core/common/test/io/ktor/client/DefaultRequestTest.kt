4a4bcdbdfccfd8d2b8c14e898b26686a41f6bcbd
hfhbd <hfhbd@users.noreply.github.com>
2020-11-21 13:41:37 +0100

    KTOR-730 Add failing BaseURL Test

---
+++ ktor-client/ktor-client-core/common/test/io/ktor/client/DefaultRequestTest.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2014-2020 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package io.ktor.client
+
+import io.ktor.client.features.*
+import io.ktor.client.request.*
+import kotlin.test.*
+
+class DefaultRequestTest {
+    private val client = HttpClient {
+        defaultRequest {
+            baseURL("https://jsonplaceholder.typicode.com/todos")
+        }
+    }
+
+    @Test
+    fun testUsingBaseURL() = runBlocking {
+        val todo = client.get<String>("/1")
+        assertTrue(todo.isNotEmpty())
+    }
+
+    @Test
+    fun overrideBaseURL() = runBlocking {
+        val overridden = client.get<String>("https://ktor.io/docs")
+        assertTrue(overridden.isNotEmpty())
+    }
+
+    @Test
+    fun localhost() = runBlocking {
+        val local = client.get<String>("http://localhost/")
+        assertTrue(local.isNotEmpty())
+    }
+}
