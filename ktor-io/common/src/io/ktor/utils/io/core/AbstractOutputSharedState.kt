a01821b55195cddaeb66bb6cc25f3a38df2e7120
Leonid Stashevsky <e5l@users.noreply.github.com>
2020-11-23 12:09:50 +0300

    KTOR-1290 Specialize shared usage to native platform in perf (#2202)

---
+++ ktor-io/common/src/io/ktor/utils/io/core/AbstractOutputSharedState.kt
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2014-2020 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package io.ktor.utils.io.core
+
+import io.ktor.utils.io.bits.*
+import io.ktor.utils.io.core.internal.*
+
+internal expect class AbstractOutputSharedState() {
+    var head: ChunkBuffer?
+    var tail: ChunkBuffer?
+
+    var tailMemory: Memory
+    var tailPosition: Int
+    var tailEndExclusive: Int
+
+    var tailInitialPosition: Int
+
+    var chainedSize: Int
+}
---
+++ ktor-io/posix/src/io/ktor/utils/io/core/AbstractOutputSharedState.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2014-2020 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package io.ktor.utils.io.core
+
+import io.ktor.utils.io.bits.*
+import io.ktor.utils.io.core.internal.*
+import kotlinx.atomicfu.*
+
+internal actual class AbstractOutputSharedState {
+    private val _head: AtomicRef<ChunkBuffer?> = atomic(null)
+    private val _tail: AtomicRef<ChunkBuffer?> = atomic(null)
+    private val _tailMemory: AtomicRef<Memory> = atomic(Memory.Empty)
+    private val _tailPosition: AtomicInt = atomic(0)
+    private val _tailEndExclusive: AtomicInt = atomic(0)
+    private val _tailInitialPosition: AtomicInt = atomic(0)
+    private val _chainedSize: AtomicInt = atomic(0)
+
+    actual var head: ChunkBuffer?
+        get() = _head.value
+        set(value) {
+            _head.value = value
+        }
+
+    actual var tail: ChunkBuffer?
+        get() = _tail.value
+        set(value) {
+            _tail.value = value
+        }
+
+    actual var tailMemory: Memory
+        get() = _tailMemory.value
+        set(value) {
+            _tailMemory.value = value
+        }
+
+    actual var tailPosition: Int
+        get() = _tailPosition.value
+        set(value) {
+            _tailPosition.value = value
+        }
+
+    actual var tailEndExclusive: Int
+        get() = _tailEndExclusive.value
+        set(value) {
+            _tailEndExclusive.value = value
+        }
+
+    actual var tailInitialPosition: Int
+        get() = _tailInitialPosition.value
+        set(value) {
+            _tailInitialPosition.value = value
+        }
+
+    actual var chainedSize: Int
+        get() = _chainedSize.value
+        set(value) {
+            _chainedSize.value = value
+        }
+}
