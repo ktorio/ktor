d8a228b0cc17ef6ec6f7c623966aa05db78bfedd
hfhbd <hfhbd@users.noreply.github.com>
2020-12-20 18:29:29 +0100

    Split multi server engine test into a testsuite with subclasses for each engine
    Split multi server engine test into a testsuite with subclasses for each engine

--- ktor-server/ktor-server-tomcat/jvm/test/io/ktor/tests/server/tomcat/TomcatEngineTest.kt
+++ ktor-server/ktor-server-tomcat/jvm/test/io/ktor/tests/server/tomcat/TomcatEngineTest.kt
@@ -6,13 +6,16 @@ package io.ktor.tests.server.tomcat
 
 import io.ktor.application.*
 import io.ktor.client.statement.*
+import io.ktor.network.tls.certificates.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import io.ktor.server.engine.*
 import io.ktor.server.servlet.*
 import io.ktor.server.testing.suites.*
 import io.ktor.server.tomcat.*
 import org.apache.catalina.core.*
 import org.apache.tomcat.util.descriptor.web.*
+import java.io.*
 import java.util.logging.*
 import javax.servlet.*
 import javax.servlet.Filter
@@ -135,3 +138,21 @@ class TomcatSustainabilityTestSuite :
         super.testBlockingConcurrency()
     }
 }
+
+class TomcatClientCertTest :
+    ClientCertTestSuite<TomcatApplicationEngine, TomcatApplicationEngine.Configuration>(Tomcat, {
+        val serverKeyStorePath = File.createTempFile("serverKeys", "jks")
+
+        EngineSSLConnectorBuilder(
+            keyAlias = "mykey",
+            keyStore = ca.generateCertificate(file = serverKeyStorePath, keyType = KeyType.Server),
+            keyStorePassword = { "changeit".toCharArray() },
+            privateKeyPassword = { "changeit".toCharArray() },
+        ).apply {
+            keyStorePath = serverKeyStorePath
+
+            val trustStorePath = File.createTempFile("trustStore", "jks")
+            trustStore = ca.trustStore(trustStorePath)
+            this.trustStorePath = trustStorePath
+        }
+    })
