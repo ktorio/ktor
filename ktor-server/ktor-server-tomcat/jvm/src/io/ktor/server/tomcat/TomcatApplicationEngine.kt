9d029784aa7623f7791b7dcd29826772bbcc014d
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2020-12-21 15:44:01 +0100

    Remove unnecessary duplicate code

--- ktor-server/ktor-server-tomcat/jvm/src/io/ktor/server/tomcat/TomcatApplicationEngine.kt
+++ ktor-server/ktor-server-tomcat/jvm/src/io/ktor/server/tomcat/TomcatApplicationEngine.kt
@@ -79,27 +79,7 @@ public class TomcatApplicationEngine(environment: ApplicationEngineEnvironment, 
                                     "the property in the EngineSSLConnectorConfig class used"
                             )
                         }
-
-                        if (ktorConnector.trustStore != null && ktorConnector.trustStorePath == null) {
-                            throw IllegalArgumentException("Tomcat requires trustStorePath for client certificate authentication")
-                        }
-                        if (ktorConnector.trustStorePath != null) {
-                            setAttribute("clientAuth", true)
-                            setAttribute("truststoreFile", ktorConnector.trustStorePath!!.absolutePath)
-                        } else {
-                            setAttribute("clientAuth", false)
-                        }
-
-                        if (ktorConnector.trustStore != null && ktorConnector.trustStorePath == null) {
-                            throw IllegalArgumentException("Tomcat requires trustStorePath for client certificate authentication")
-                        }
-                        if (ktorConnector.trustStorePath != null) {
-                            setAttribute("clientAuth", true)
-                            setAttribute("truststoreFile", ktorConnector.trustStorePath!!.absolutePath)
-                        } else {
-                            setAttribute("clientAuth", false)
-                        }
-
+                        
                         if (ktorConnector.trustStore != null && ktorConnector.trustStorePath == null) {
                             throw IllegalArgumentException("Tomcat requires trustStorePath for client certificate authentication")
                         }
