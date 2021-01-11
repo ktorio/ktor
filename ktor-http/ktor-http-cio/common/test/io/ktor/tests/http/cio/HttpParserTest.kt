ffbb45faf2feb6376bca53988ca5c1a587f03277
Valmir Ferreira <valmirf2n@gmail.com>
2021-01-11 05:16:24 -0300

    KTOR-370 Fix status code response accepting any numeric value (#1882) (#2283)

--- ktor-http/ktor-http-cio/common/test/io/ktor/tests/http/cio/HttpParserTest.kt
+++ ktor-http/ktor-http-cio/common/test/io/ktor/tests/http/cio/HttpParserTest.kt
@@ -138,6 +138,41 @@ class HttpParserTest {
         }
     }
 
+    @Test
+    fun parseStatusCodeShouldBeValid(): Unit = test {
+        listOf(
+            """
+            HTTP/1.1 100 OK
+            """.trimIndent(),
+                """
+            HTTP/1.1 999 OK
+            """.trimIndent()
+        ).forEach {
+            val response = parseResponse(ByteReadChannel(it))!!
+            assertEquals("OK", response.statusText.toString())
+        }
+    }
+
+    @Test
+    fun parseStatusCodeShouldFailWhenOutOfRange(): Unit = test {
+        assertFailsWith<ParserException> {
+            parseResponse(ByteReadChannel("HTTP/1.1 0 OK"))!!
+        }
+        assertFailsWith<ParserException> {
+            parseResponse(ByteReadChannel("HTTP/1.1 99 OK"))!!
+        }
+        assertFailsWith<ParserException> {
+            parseResponse(ByteReadChannel("HTTP/1.1 1000 OK"))!!
+        }
+    }
+
+    @Test
+    fun parseStatusCodeShouldFailWhenStatusCodeIsNegative(): Unit = test {
+        assertFailsWith<NumberFormatException> {
+            parseResponse(ByteReadChannel("HTTP/1.1 -100 OK"))!!
+        }
+    }
+
     @OptIn(InternalCoroutinesApi::class)
     private fun test(block: suspend () -> Unit) {
         var completed = false
