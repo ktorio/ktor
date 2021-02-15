9c51358cd6004ae1a6f1506f71cdf7ee038761cb
Rustam <rxsinukov@gmail.com>
2021-02-15 22:31:40 +0100

    Move data conversion to common code to reuse it in client (#2341)
    * KTOR-1780 Add new multiplatform converters
    
    * KTOR-1780 deprecations
    
    * KTOR-1780 api dump
    
    * KTOR-1780 add missing doc
    
    * KTOR-1780 codestyle
    
    * KTOR-1780 Add deprecations
    
    * KTOR-1780 code review fixes
    
    * KTOR-1780 code review fixes 2
    
    * a
    
    * Add support for java.lang.Character

---
+++ ktor-utils/jvm/src/io/ktor/util/reflect/TypeInfoJvm.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2014-2021 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package io.ktor.util.reflect
+
+import java.lang.reflect.*
+import java.lang.reflect.Type
+import kotlin.reflect.*
+
+public actual typealias Type = java.lang.reflect.Type
+
+@PublishedApi
+internal open class TypeBase<T>
+
+@OptIn(ExperimentalStdlibApi::class)
+public actual inline fun <reified T> typeInfo(): TypeInfo {
+    val base = object : TypeBase<T>() {}
+    val superType = base::class.java.genericSuperclass!!
+
+    val reifiedType = (superType as ParameterizedType).actualTypeArguments.first()!!
+    return typeInfoImpl(reifiedType, T::class, typeOf<T>())
+}
+
+public fun typeInfoImpl(reifiedType: Type, kClass: KClass<*>, kType: KType): TypeInfo =
+    TypeInfoImpl(kClass, reifiedType, kType)
+
+/**
+ * Check [this] is instance of [type].
+ */
+public actual fun Any.instanceOf(type: KClass<*>): Boolean = type.java.isInstance(this)
+
+@OptIn(ExperimentalStdlibApi::class)
+internal actual val KType.platformType: Type
+    get() = javaType
