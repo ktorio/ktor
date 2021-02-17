474b3e882e54fb16415344206ed69fa37dcded24
satyam-agarwal <satyam.agarwal@finn.no>
2021-02-17 20:26:50 +0100

    Attempt to add other jdks

--- ktor-network/ktor-network-tls/jvm/src/io/ktor/network/tls/CipherSuitesJvm.kt
+++ ktor-network/ktor-network-tls/jvm/src/io/ktor/network/tls/CipherSuitesJvm.kt
@@ -7,6 +7,8 @@ package io.ktor.network.tls
 import io.ktor.network.tls.platform.*
 
 internal actual fun CipherSuite.isSupported(): Boolean = when (platformVersion.major) {
-    "1.8.0" -> platformVersion.minor >= 161 || keyStrength <= 256
-    else -> keyStrength <= 256
+    "1.8.0" -> platformVersion.minor >= 161 || keyStrength <= 128
+    "1.7.0" -> platformVersion.minor >= 171 || keyStrength <= 128
+    "1.6.0" -> platformVersion.minor >= 181 || keyStrength <= 128
+    else -> true
 }
