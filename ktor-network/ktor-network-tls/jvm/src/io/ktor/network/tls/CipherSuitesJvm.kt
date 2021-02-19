d7d1b65796a1b05f40a0d1d652b3c3f4e26351b2
satyamagarwal <agarwal.satyam3@gmail.com>
2021-02-19 12:29:41 +0100

    KTOR-1914 : Allow 256 bits Cipher Suites to be used (#2319)
    * KTOR-1914 : Allow 256 bits Cipher Suites to be used
    
    Co-authored-by: satyam-agarwal <satyam.agarwal@finn.no>

--- ktor-network/ktor-network-tls/jvm/src/io/ktor/network/tls/CipherSuitesJvm.kt
+++ ktor-network/ktor-network-tls/jvm/src/io/ktor/network/tls/CipherSuitesJvm.kt
@@ -8,5 +8,7 @@ import io.ktor.network.tls.platform.*
 
 internal actual fun CipherSuite.isSupported(): Boolean = when (platformVersion.major) {
     "1.8.0" -> platformVersion.minor >= 161 || keyStrength <= 128
-    else -> keyStrength <= 128
+    "1.7.0" -> platformVersion.minor >= 171 || keyStrength <= 128
+    "1.6.0" -> platformVersion.minor >= 181 || keyStrength <= 128
+    else -> true
 }
