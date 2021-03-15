2f2d85592a752218dd74d479bf0dc50cb10f52ed
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2021-03-15 19:37:08 +0300

    KTOR-2307 Remove bintray usages

--- ktor-http/common/test/io/ktor/tests/http/UrlTest.kt
+++ ktor-http/common/test/io/ktor/tests/http/UrlTest.kt
@@ -124,7 +124,7 @@ class UrlTest {
     @Test
     fun testEqualsInQueryValue() {
         val urlString =
-            "https://akamai.bintray.com/22/225b067044aa56f36590ef56d41e256cd1d0887b176bfdeec123ecccc6057790" +
+            "https://akamai.removed.com/22/225b067044aa56f36590ef56d41e256cd1d0887b176bfdeec123ecccc6057790" +
                 "?__gda__=exp=1604350711~hmac=417cbd5a97b4c499e2cf7e9eae5dfb9ad95b42cb3ff76c5fb0fae70e2a42db9c&..."
 
         val url = URLBuilder().apply {
