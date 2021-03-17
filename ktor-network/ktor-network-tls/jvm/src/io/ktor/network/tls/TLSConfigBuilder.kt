05ce3c1153ef0c4cda39411e9d16e126d13ea2c8
hfhbd <hfhbd@users.noreply.github.com>
2021-03-17 09:32:17 +0100

    KTOR-940 Add deprecation

--- ktor-network/ktor-network-tls/jvm/src/io/ktor/network/tls/TLSConfigBuilder.kt
+++ ktor-network/ktor-network-tls/jvm/src/io/ktor/network/tls/TLSConfigBuilder.kt
@@ -93,6 +93,7 @@ public fun TLSConfigBuilder.addKeyStore(store: KeyStore, password: CharArray) {
  * Add client certificates from [store] by using the certificate with specific [alias]
  * or all certificates, if [alias] is null.
  */
+@Deprecated("Please use the nullable overload", ReplaceWith("addKeyStore(store, password as CharArray?, alias)"), level = DeprecationLevel.WARNING)
 public fun TLSConfigBuilder.addKeyStore(store: KeyStore, password: CharArray, alias: String? = null) {
     addKeyStore(store, password as CharArray?, alias)
 }
