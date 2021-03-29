e25d04042329da725e05874a812bf04f436ed6c6
Xander <thiakil@users.noreply.github.com>
2021-03-29 15:05:27 +0800

    Add direct access to payload in JWTCredential & JWTPrincipal (#1998)
    * add JWT shortcut functions
    
    * add shortcuts for non-registered JWT claims
    
    * JWT: update api file
    
    * convert interface to abstract class

--- ktor-features/ktor-auth-jwt/jvm/src/io/ktor/auth/jwt/JWTAuth.kt
+++ ktor-features/ktor-auth-jwt/jvm/src/io/ktor/auth/jwt/JWTAuth.kt
@@ -21,24 +21,161 @@ import org.slf4j.*
 import java.security.interfaces.*
 import java.util.*
 import java.util.concurrent.*
+import kotlin.reflect.*
 
 private val JWTAuthKey: Any = "JWTAuth"
 
 private val JWTLogger: Logger = LoggerFactory.getLogger("io.ktor.auth.jwt")
 
 /**
+ * Shortcut functions for standard registered [JWT Claims](https://tools.ietf.org/html/rfc7519#section-4.1)
+ */
+public abstract class JWTPayloadHolder(
+    /**
+     * The JWT payload
+     */
+    public val payload: Payload
+) {
+
+    /**
+     * Get the value of the "iss" claim, or null if it's not available.
+     *
+     * The "iss" (issuer) claim identifies the principal that issued the
+     * JWT.  The processing of this claim is generally application specific.
+     * The "iss" value is a case-sensitive string containing a StringOrURI
+     * value.  Use of this claim is OPTIONAL.
+     */
+    public val issuer: String? get() = payload.issuer
+
+    /**
+     * Get the value of the "sub" claim, or null if it's not available.
+     *
+     * The "sub" (subject) claim identifies the principal that is the
+     * subject of the JWT.  The claims in a JWT are normally statements
+     * about the subject.  The subject value MUST either be scoped to be
+     * locally unique in the context of the issuer or be globally unique.
+     * The processing of this claim is generally application specific.  The
+     * "sub" value is a case-sensitive string containing a StringOrURI
+     * value.  Use of this claim is OPTIONAL.
+     */
+    public val subject: String? get() = payload.subject
+
+    /**
+     * Get the value of the "aud" claim, or an empty list if it's not available.
+     *
+     * The "aud" (audience) claim identifies the recipients that the JWT is
+     * intended for.  Each principal intended to process the JWT MUST
+     * identify itself with a value in the audience claim.  If the principal
+     * processing the claim does not identify itself with a value in the
+     * "aud" claim when this claim is present, then the JWT MUST be
+     * rejected.  In the general case, the "aud" value is an array of case-
+     * sensitive strings, each containing a StringOrURI value.  In the
+     * special case when the JWT has one audience, the "aud" value MAY be a
+     * single case-sensitive string containing a StringOrURI value.  The
+     * interpretation of audience values is generally application specific.
+     * Use of this claim is OPTIONAL.
+     */
+    public val audience: List<String> get() = payload.audience ?: emptyList()
+
+    /**
+     * Get the value of the "exp" claim, or null if it's not available.
+     *
+     * The "exp" (expiration time) claim identifies the expiration time on
+     * or after which the JWT MUST NOT be accepted for processing.  The
+     * processing of the "exp" claim requires that the current date/time
+     * MUST be before the expiration date/time listed in the "exp" claim.
+     * Implementers MAY provide for some small leeway, usually no more than
+     * a few minutes, to account for clock skew. Use of this claim is OPTIONAL.
+     */
+    public val expiresAt: Date? get() = payload.expiresAt
+
+    /**
+     * Get the value of the "nbf" claim, or null if it's not available.
+     *
+     * The "nbf" (not before) claim identifies the time before which the JWT
+     * MUST NOT be accepted for processing.  The processing of the "nbf"
+     * claim requires that the current date/time MUST be after or equal to
+     * the not-before date/time listed in the "nbf" claim.  Implementers MAY
+     * provide for some small leeway, usually no more than a few minutes, to
+     * account for clock skew. Use of this claim is OPTIONAL.
+     */
+    public val notBefore: Date? get() = payload.notBefore
+
+    /**
+     * Get the value of the "iat" claim, or null if it's not available.
+     *
+     * The "iat" (issued at) claim identifies the time at which the JWT was
+     * issued.  This claim can be used to determine the age of the JWT.
+     * Use of this claim is OPTIONAL.
+     */
+    public val issuedAt: Date? get() = payload.issuedAt
+
+    /**
+     * Get the value of the "jti" claim, or null if it's not available.
+     *
+     * The "jti" (JWT ID) claim provides a unique identifier for the JWT.
+     * The identifier value MUST be assigned in a manner that ensures that
+     * there is a negligible probability that the same value will be
+     * accidentally assigned to a different data object; if the application
+     * uses multiple issuers, collisions MUST be prevented among values
+     * produced by different issuers as well.  The "jti" claim can be used
+     * to prevent the JWT from being replayed.  The "jti" value is a case-
+     * sensitive string.  Use of this claim is OPTIONAL.
+     */
+    public val jwtId: String? get() = payload.id
+
+    /**
+     * Retrieve a non-RFC JWT string Claim by its name
+     *
+     * @param name The claim's key as it appears in the JSON object
+     * @return the Claim's value or null if not available or not a string
+     */
+    public operator fun get(name: String): String? {
+        return payload.getClaim(name).asString()
+    }
+
+    /**
+     * Retrieve a non-RFC JWT Claim by its name and attempt to decode as the supplied type
+     *
+     * @param name The claim's key as it appears in the JSON object
+     * @return the Claim's value or null if not available or unable to deserialise
+     */
+    public fun <T : Any> getClaim(name: String, clazz: KClass<T>): T? {
+        return try {
+            payload.getClaim(name).`as`(clazz.javaObjectType)
+        } catch (ex: JWTDecodeException) {
+            null
+        }
+    }
+
+    /**
+     * Retrieve a non-RFC JWT Claim by its name and attempt to decode as a list of the supplied type
+     *
+     * @param name The claim's key as it appears in the JSON object
+     * @return the Claim's value or an empty list if not available or unable to deserialise
+     */
+    public fun <T : Any> getListClaim(name: String, clazz: KClass<T>): List<T> {
+        return try {
+            payload.getClaim(name).asList(clazz.javaObjectType)
+        } catch (ex: JWTDecodeException) {
+            emptyList()
+        }
+    }
+}
+
+/**
  * Represents a JWT credential consist of the specified [payload]
  * @param payload JWT
  * @see Payload
  */
-public class JWTCredential(public val payload: Payload) : Credential
+public class JWTCredential(payload: Payload) : Credential, JWTPayloadHolder(payload)
 
 /**
  * Represents a JWT principal consist of the specified [payload]
  * @param payload JWT
  * @see Payload
  */
-public class JWTPrincipal(public val payload: Payload) : Principal
+public class JWTPrincipal(payload: Payload) : Principal, JWTPayloadHolder(payload)
 
 /**
  * JWT verifier configuration function. It is applied on the verifier builder.
