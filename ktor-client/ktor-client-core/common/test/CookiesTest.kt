6763a7344059c1f9dfdb2f0816e98559862ef604
Hadi Hariri <mail@hadihariri.com>
2021-04-29 08:29:08 +0200

    KTOR-2604 Update copyright year (#2439)

--- ktor-client/ktor-client-core/common/test/CookiesTest.kt
+++ ktor-client/ktor-client-core/common/test/CookiesTest.kt
@@ -5,8 +5,8 @@ import io.ktor.test.dispatcher.*
 import kotlin.test.*
 
 /*
- * Copyright 2014-2020 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
- */
+* Copyright 2014-2021 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+*/
 
 class CookiesTest {
 
--- ktor-client/ktor-client-features/ktor-client-logging/build.gradle.kts
+++ ktor-client/ktor-client-features/ktor-client-logging/build.gradle.kts
@@ -1,6 +1,6 @@
 /*
- * Copyright 2014-2020 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
- */
+* Copyright 2014-2021 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+*/
 
 val slf4j_version: String by project.extra
 
