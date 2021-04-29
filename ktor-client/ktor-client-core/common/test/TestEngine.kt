21c90af1dffc697eccfd865fdab6493efc33dda9
Hadi Hariri <mail@hadihariri.com>
2021-04-29 08:23:05 +0200

    KTOR-2604 Update copyright year

--- ktor-client/ktor-client-core/common/test/TestEngine.kt
+++ ktor-client/ktor-client-core/common/test/TestEngine.kt
@@ -5,8 +5,8 @@ import kotlinx.coroutines.*
 import kotlin.coroutines.*
 
 /*
- * Copyright 2014-2020 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
- */
+* Copyright 2014-2021 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+*/
 
 internal object TestEngine : HttpClientEngine {
     override val dispatcher: CoroutineDispatcher
