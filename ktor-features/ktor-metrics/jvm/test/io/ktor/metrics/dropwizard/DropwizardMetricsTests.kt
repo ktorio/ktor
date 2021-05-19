4acafb3892c072b8bc841ce740d65469a19a9c3e
t3 <plindgr@gmail.com>
2021-05-19 22:09:26 +0200

    KTOR-2527 using baseName prefix for all metrics

--- ktor-features/ktor-metrics/jvm/test/io/ktor/metrics/dropwizard/DropwizardMetricsTests.kt
+++ ktor-features/ktor-metrics/jvm/test/io/ktor/metrics/dropwizard/DropwizardMetricsTests.kt
@@ -5,11 +5,11 @@
 package io.ktor.metrics.dropwizard
 
 import com.codahale.metrics.*
-import com.codahale.metrics.jvm.*
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.testing.*
+import org.hamcrest.CoreMatchers.*
 import org.junit.*
 import org.junit.Assert.*
 
@@ -33,29 +33,7 @@ class DropwizardMetricsTests {
             uri = "/uri"
         }
 
-        assertEquals(1, testRegistry.meter("/uri/(method:GET).200").count)
-    }
-
-    @Test
-    fun testUsePreconfiguredRegistry(): Unit = withTestApplication {
-        val testRegistry = MetricRegistry()
-        testRegistry.register("jvm.memory", MemoryUsageGaugeSet())
-
-        application.install(DropwizardMetrics) {
-            registry = testRegistry
-        }
-
-        application.routing {
-            get("/uri") {
-                call.respond("hello")
-            }
-        }
-
-        handleRequest {
-            uri = "/uri"
-        }
-
-        assertEquals(1, testRegistry.meter("/uri/(method:GET).200").count)
+        assertEquals(1, testRegistry.meter("ktor.calls./uri/(method:GET).200").count)
     }
 
     @Test
@@ -68,5 +46,25 @@ class DropwizardMetricsTests {
         }
 
         assertEquals(setOf("ktor.calls.active", "ktor.calls.duration", "ktor.calls.exceptions"), testRegistry.names)
+    }
+
+    @Test
+    fun `should prefix all metrics with baseName`(): Unit = withTestApplication {
+        val prefix = "foo.bar"
+        val registry =
+            application.install(DropwizardMetrics) {
+                baseName = prefix
+                registerJvmMetricSets = false
+            }.registry
+
+        application.routing {
+            get("/uri") {
+                call.respond("hello")
+            }
+        }
+
+        handleRequest { uri = "/uri" }
+
+        assertThat(registry.names, everyItem(startsWith(prefix)))
     }
 }
