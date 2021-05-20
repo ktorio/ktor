47a2272a78956ab679eb24570938cec8ea7329b9
Cliffred van Velzen <cliffred@cliffred.nl>
2021-05-20 22:48:17 +0200

    KTOR-2709 Deprecate TypeBase instead of deleting it

--- ktor-utils/jvm/src/io/ktor/util/reflect/TypeInfoJvm.kt
+++ ktor-utils/jvm/src/io/ktor/util/reflect/TypeInfoJvm.kt
@@ -8,6 +8,10 @@ import kotlin.reflect.*
 
 public actual typealias Type = java.lang.reflect.Type
 
+@PublishedApi
+@Deprecated("Will be removed")
+internal open class TypeBase<T>
+
 @OptIn(ExperimentalStdlibApi::class)
 public actual inline fun <reified T> typeInfo(): TypeInfo {
     val kType = typeOf<T>()
