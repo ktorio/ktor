663f170885eefa1510b81001592cdf8bb91917b9
Leonid Stashevsky <e5l@users.noreply.github.com>
2021-06-08 11:06:07 +0300

    KTOR-2472 Implement Client Events (#2491)
    * KTOR-2472 Extract events to the shared module
    
    * KTOR-2472 Add client events
    
    * KTOR-2472 Update public API
    
    * KTOR-2472 Add client events tests
    
    * KTOR-2472 Fix code style issues
    
    * KTOR-2472 Fix review problems
    
    * KTOR-2472 Fix native tests
    
    * KTOR-2472 Fix style and language

--- ktor-server/ktor-server-core/jvm/src/io/ktor/application/ApplicationEvents.kt
+++ ktor-server/ktor-server-core/jvm/src/io/ktor/application/ApplicationEvents.kt
@@ -4,67 +4,27 @@
 
 package io.ktor.application
 
-import io.ktor.util.*
-import io.ktor.util.internal.*
-import kotlinx.coroutines.*
+import io.ktor.events.*
 
 /**
- * Provides events for [Application] lifecycle
+ * Provides events for [Application] lifecycle.
  */
-public class ApplicationEvents {
-    private val handlers = CopyOnWriteHashMap<EventDefinition<*>, LockFreeLinkedListHead>()
-
-    /**
-     * Subscribe [handler] to an event specified by [definition]
-     */
-    public fun <T> subscribe(definition: EventDefinition<T>, handler: EventHandler<T>): DisposableHandle {
-        val registration = HandlerRegistration(handler)
-        handlers.computeIfAbsent(definition) { LockFreeLinkedListHead() }.addLast(registration)
-        return registration
-    }
-
-    /**
-     * Unsubscribe [handler] from an event specified by [definition]
-     */
-    public fun <T> unsubscribe(definition: EventDefinition<T>, handler: EventHandler<T>) {
-        handlers[definition]?.forEach<HandlerRegistration> {
-            if (it.handler == handler) it.remove()
-        }
-    }
-
-    /**
-     * Rise an event specified by [definition] with the specified [value] and call all handlers
-     *
-     * Handlers are called in order of subscriptions.
-     * If some handler throws an exception, all handlers will still run and then exception will be rethrown.
-     */
-    public fun <T> raise(definition: EventDefinition<T>, value: T) {
-        var exception: Throwable? = null
-        handlers[definition]?.forEach<HandlerRegistration> { registration ->
-            try {
-                @Suppress("UNCHECKED_CAST")
-                (registration.handler as EventHandler<T>)(value)
-            } catch (e: Throwable) {
-                exception?.addSuppressed(e) ?: run { exception = e }
-            }
-        }
-        exception?.let { throw it }
-    }
-
-    private class HandlerRegistration(val handler: EventHandler<*>) : LockFreeLinkedListNode(), DisposableHandle {
-        override fun dispose() {
-            remove()
-        }
-    }
-}
+@Deprecated(
+    "ApplicationEvents has been renamed to Events.",
+    level = DeprecationLevel.WARNING,
+    replaceWith = ReplaceWith("Events", "io.ktor.events.Events")
+)
+public typealias ApplicationEvents = io.ktor.events.Events
 
 /**
- * Specifies signature for the event handler
+ * Specifies signature for the event handler.
  */
-public typealias EventHandler<T> = (T) -> Unit
-
-// TODO: make two things: definition that is kept private to subsystem, and declaration which is public.
-// Invoke only by definition, subscribe by declaration
+@Deprecated(
+    "EventHandler has been moved to package io.ktor.events",
+    level = DeprecationLevel.WARNING,
+    replaceWith = ReplaceWith("EventHandler<T>", "io.ktor.events.EventHandler")
+)
+public typealias EventHandler<T> = io.ktor.events.EventHandler<T>
 
 /**
  * Definition of an event.
@@ -74,4 +34,9 @@ public typealias EventHandler<T> = (T) -> Unit
  *
  * @param T specifies what is a type of a value passed to the event
  */
-public open class EventDefinition<T>
+@Deprecated(
+    "EventDefinition<T> has been moved to io.ktor.events",
+    level = DeprecationLevel.WARNING,
+    replaceWith = ReplaceWith("Events", "io.ktor.events.EventDefinition")
+)
+public typealias EventDefinition<T> = io.ktor.events.EventDefinition<T>
