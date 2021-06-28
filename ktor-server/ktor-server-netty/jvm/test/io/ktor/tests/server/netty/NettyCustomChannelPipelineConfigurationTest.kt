b9c76f4248a33b750f69cf2ea6ecf601a73559f8
Thiyagu06 <thiyagu103@gmail.com>
2021-06-28 09:34:02 +0200

    KTOR-356 Fix to provide a way to configure netty pipeline with user-provided custom channel handler (#2472)
    * KTOR-356 fix to provide a way to configure netty pipeline with user-provided custom channel handler

---
+++ ktor-server/ktor-server-netty/jvm/test/io/ktor/tests/server/netty/NettyCustomChannelPipelineConfigurationTest.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2014-2021 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package io.ktor.tests.server.netty
+
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.testing.*
+import io.netty.channel.*
+import kotlin.test.*
+
+abstract class NettyCustomChannelTest<TEngine : ApplicationEngine, TConfiguration : ApplicationEngine.Configuration>(
+    hostFactory: ApplicationEngineFactory<TEngine, TConfiguration>
+) : EngineTestBase<TEngine, TConfiguration>(hostFactory) {
+
+    var counter = 0
+
+    @Test
+    fun testCustomChannelHandlerInvoked() {
+        createAndStartServer {
+            handle {
+                call.respondText("Hello")
+            }
+        }
+
+        withUrl("/") {
+            assertEquals(HttpStatusCode.OK.value, status.value)
+            assertNotEquals(0, counter)
+        }
+    }
+}
+
+class NettyCustomChannelPipelineConfigurationTest :
+    NettyCustomChannelTest<NettyApplicationEngine, NettyApplicationEngine.Configuration>(Netty) {
+
+    override fun configure(configuration: NettyApplicationEngine.Configuration) {
+        configuration.shareWorkGroup = true
+        configuration.channelPipelineConfig = {
+            addLast(
+                "customHandler",
+                object : ChannelInboundHandlerAdapter() {
+                    override fun channelRead(ctx: ChannelHandlerContext, msg: Any) {
+                        counter = counter.plus(1)
+                        super.channelRead(ctx, msg)
+                    }
+                }
+            )
+        }
+    }
+}
