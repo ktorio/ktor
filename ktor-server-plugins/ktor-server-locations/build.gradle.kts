7a2458d6d1f3667098767ac40bfadbb9665b0ec4
rsinukov <rxsinukov@gmail.com>
2021-07-05 17:27:17 +0200

    KTOR-2865 Move more modules * `ktor-server:ktor-server` to `ktor-server` * `ktor-plugins` to `ktor-server:ktor-plugins`

--- ktor-server-plugins/ktor-server-locations/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-kotlin.sourceSets {
-    val jvmMain by getting {
-        dependencies {
-            api(project(":ktor-server-plugins:ktor-server-auth"))
-            api(project(":ktor-server-plugins:ktor-server-data-conversion"))
-        }
-    }
-}
