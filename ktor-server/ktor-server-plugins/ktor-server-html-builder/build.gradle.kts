8ba6faf992d234ef0bf708b17380c643ee0a7b60
Rustam <rxsinukov@gmail.com>
2021-07-06 19:09:22 +0200

    KTOR-2865 Move more modules (#2548)

---
+++ ktor-server/ktor-server-plugins/ktor-server-html-builder/build.gradle.kts
@@ -0,0 +1,14 @@
+val kotlinx_html_version: String by extra
+
+kotlin.sourceSets {
+    val jvmMain by getting {
+        dependencies {
+            api("org.jetbrains.kotlinx:kotlinx-html-jvm:$kotlinx_html_version")
+        }
+    }
+    val jvmTest by getting {
+        dependencies {
+            api(project(":ktor-server:ktor-server-plugins:ktor-server-status-pages"))
+        }
+    }
+}
