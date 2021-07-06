be8d90b0925653caf40b3cb5336ffff9b58fc552
Rustam <rxsinukov@gmail.com>
2021-07-06 19:09:22 +0200

    KTOR-2865 Move more modules (#2548)

---
+++ ktor-server/ktor-server-plugins/ktor-server-freemarker/build.gradle.kts
@@ -0,0 +1,14 @@
+kotlin.sourceSets {
+    val jvmMain by getting {
+        dependencies {
+            api("org.freemarker:freemarker:[2.3.20, 2.4)")
+        }
+    }
+    val jvmTest by getting {
+        dependencies {
+            api(project(":ktor-server:ktor-server-plugins:ktor-server-status-pages"))
+            api(project(":ktor-server:ktor-server-plugins:ktor-server-compression"))
+            api(project(":ktor-server:ktor-server-plugins:ktor-server-conditional-headers"))
+        }
+    }
+}
