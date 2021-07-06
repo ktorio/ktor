00d31c97014271734e4936e3eac572a73cbd45fb
Rustam <rxsinukov@gmail.com>
2021-07-06 19:09:22 +0200

    KTOR-2865 Move more modules (#2548)

--- ktor-server/ktor-server-test-suites/build.gradle.kts
+++ ktor-server/ktor-server-test-suites/build.gradle.kts
@@ -6,19 +6,19 @@ kotlin.sourceSets {
     val jvmMain by getting {
         dependencies {
             implementation(project(":ktor-server:ktor-server-test-host"))
-            implementation(project(":ktor-server-plugins:ktor-server-compression"))
-            implementation(project(":ktor-server-plugins:ktor-server-partial-content"))
-            implementation(project(":ktor-server-plugins:ktor-server-status-pages"))
-            implementation(project(":ktor-server-plugins:ktor-server-conditional-headers"))
-            implementation(project(":ktor-server-plugins:ktor-server-forwarded-header"))
-            implementation(project(":ktor-server-plugins:ktor-server-auto-head-response"))
+            implementation(project(":ktor-server:ktor-server-plugins:ktor-server-compression"))
+            implementation(project(":ktor-server:ktor-server-plugins:ktor-server-partial-content"))
+            implementation(project(":ktor-server:ktor-server-plugins:ktor-server-status-pages"))
+            implementation(project(":ktor-server:ktor-server-plugins:ktor-server-conditional-headers"))
+            implementation(project(":ktor-server:ktor-server-plugins:ktor-server-forwarded-header"))
+            implementation(project(":ktor-server:ktor-server-plugins:ktor-server-auto-head-response"))
 
             implementation("org.jetbrains.kotlinx:kotlinx-coroutines-debug:$coroutines_version")
         }
     }
     val jvmTest by getting {
         dependencies {
-            api(project(":ktor-server:ktor-server", configuration = "testOutput"))
+            api(project(":ktor-server", configuration = "testOutput"))
         }
     }
 }
