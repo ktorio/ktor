621f5b1387bad73c9ce0c91a1e67b180ae7b8e8f
Alexandra Tudor <atudor@adobe.com>
2021-08-03 20:11:50 +0300

    KTOR-2992 Add filtering support in Ktor client response interceptor

--- ktor-client/ktor-client-tests/common/test/io/ktor/client/tests/ResponseObserverTest.kt
+++ ktor-client/ktor-client-tests/common/test/io/ktor/client/tests/ResponseObserverTest.kt
@@ -7,9 +7,11 @@ package io.ktor.client.tests
 import io.ktor.client.plugins.observer.*
 import io.ktor.client.request.*
 import io.ktor.client.tests.utils.*
+import io.ktor.utils.io.concurrent.*
 import kotlin.test.*
 
 class ResponseObserverTest : ClientLoader() {
+    private var observerCalls by shared(0)
 
     @Test
     fun testEmptyResponseObserverIsNotFreezing() = clientTests {
@@ -37,6 +39,56 @@ class ResponseObserverTest : ClientLoader() {
             client.get("$TEST_SERVER/download") {
                 parameter("size", (1024 * 10).toString())
             }
+        }
+    }
+
+    @Test
+    fun testResponseObserverCalledWhenNoFilterPresent() = clientTests {
+        config {
+            ResponseObserver {
+                observerCalls++
+            }
+        }
+
+        test { client ->
+            client.get<HttpResponse>("$TEST_SERVER/download") {
+                parameter("size", (1024 * 10).toString())
+            }
+            assertEquals(1, observerCalls)
+        }
+    }
+
+    @Test
+    fun testResponseObserverCalledWhenFilterMatched() = clientTests {
+        config {
+            install(ResponseObserver) {
+                onResponse { observerCalls++ }
+                filter { true }
+            }
+        }
+
+        test { client ->
+            client.get<HttpResponse>("$TEST_SERVER/download") {
+                parameter("size", (1024 * 10).toString())
+            }
+            assertEquals(1, observerCalls)
+        }
+    }
+
+    @Test
+    fun testResponseObserverNotCalledWhenFilterNotMatched() = clientTests {
+        config {
+            install(ResponseObserver) {
+                onResponse { observerCalls++ }
+                filter { false }
+            }
+        }
+
+        test { client ->
+            client.get<HttpResponse>("$TEST_SERVER/download") {
+                parameter("size", (1024 * 10).toString())
+            }
+            assertEquals(0, observerCalls)
         }
     }
 }
