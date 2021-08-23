794127b226ae4c8ff5e8b99bb8c96a1418b2568f
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2021-08-23 14:29:01 +0300

    KTOR-2981 Drop npm publication

--- buildSrc/src/main/kotlin/JsConfiguration.kt
+++ buildSrc/src/main/kotlin/JsConfiguration.kt
@@ -1,12 +1,8 @@
 /*
  * Copyright 2014-2021 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
-import com.moowork.gradle.node.npm.*
 import org.gradle.api.*
-import org.gradle.api.tasks.*
-import org.gradle.internal.file.impl.DefaultFileMetadata.*
 import org.gradle.kotlin.dsl.*
-import org.jetbrains.kotlin.gradle.plugin.*
 import java.io.*
 
 val skipModules = listOf(
@@ -37,7 +33,6 @@ fun Project.configureJsModules() {
     }
 
     configureTestTask()
-    configurePublishing()
 }
 
 private fun Project.configureJsTasks() {
@@ -89,39 +84,4 @@ private fun Project.configureTestTask() {
 
     val jsIrBrowserTest by tasks.getting
     jsIrBrowserTest.onlyIf { shouldRunJsBrowserTest }
-}
-
-private val Project.NPM_TEMP_DIR: File get() = file("$projectDir/npm")
-private val Project.NPM_DEPLOY_DIR: File get() = file("$buildDir/npm")
-private val NPM_AUTH_TOKEN: String? get() = System.getenv("NPM_AUTH_TOKEN")
-
-private val Project.jsTarget: KotlinTarget
-    get() = kotlin.targets.findByName("jsLegacy") ?: kotlin.targets.findByName("js") ?: error("Fail to find js target")
-
-private fun Project.configurePublishing() {
-    NPM_AUTH_TOKEN ?: return
-
-    val kotlin_version: String by extra
-
-    apply(plugin = "com.moowork.node")
-
-    val prepareTask = tasks.create<Copy>("preparePublishTask") {
-        from(NPM_TEMP_DIR) {
-            afterEvaluate {
-                this@from.expand(properties + mapOf("kotlinDependency" to "\"kotlin\": \"$kotlin_version\""))
-            }
-        }
-
-        val main by jsTarget.compilations.getting
-
-        from(main.output.allOutputs)
-        into(NPM_DEPLOY_DIR)
-    }
-
-    tasks.create<NpmTask>("publishNpm") {
-        dependsOn(prepareTask)
-
-        setWorkingDir(NPM_DEPLOY_DIR)
-        setArgs(listOf("publish", "--//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN", "--tag=latest"))
-    }
 }
