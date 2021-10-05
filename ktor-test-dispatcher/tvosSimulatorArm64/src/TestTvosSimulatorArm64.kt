8f0e9975fb31e8406404298d7fe8497363dd2832
Leonid Stashevsky <e5l@users.noreply.github.com>
2021-10-05 07:51:51 +0300

    Add Apple Silicon targets

---
+++ ktor-test-dispatcher/tvosSimulatorArm64/src/TestTvosSimulatorArm64.kt
@@ -0,0 +1,30 @@
+@file:Suppress("INVISIBLE_MEMBER", "INVISIBLE_REFERENCE")
+
+package io.ktor.test.dispatcher
+
+import kotlinx.coroutines.*
+import platform.Foundation.*
+import kotlin.coroutines.*
+
+/**
+ * Amount of time any task is processed and can't be rescheduled.
+ */
+private const val TIME_QUANTUM = 0.01
+
+/**
+ * Test runner for native suspend tests.
+ */
+public actual fun testSuspend(
+    context: CoroutineContext,
+    block: suspend CoroutineScope.() -> Unit
+): Unit = runBlocking {
+    val loop = ThreadLocalEventLoop.currentOrNull()!!
+
+    val task = launch { block() }
+    while (!task.isCompleted) {
+        val date = NSDate().addTimeInterval(TIME_QUANTUM) as NSDate
+        NSRunLoop.mainRunLoop.runUntilDate(date)
+
+        loop.processNextEvent()
+    }
+}
