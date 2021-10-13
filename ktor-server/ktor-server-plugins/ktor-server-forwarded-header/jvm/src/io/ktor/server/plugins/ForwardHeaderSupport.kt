b101e784ef4ee1a27562f77b88499456f5a194b7
Daria Usova <daria.usova@jetbrains.com>
2021-10-13 09:36:38 +0300

    return old comment

--- ktor-server/ktor-server-plugins/ktor-server-forwarded-header/jvm/src/io/ktor/server/plugins/ForwardHeaderSupport.kt
+++ ktor-server/ktor-server-plugins/ktor-server-forwarded-header/jvm/src/io/ktor/server/plugins/ForwardHeaderSupport.kt
@@ -12,6 +12,7 @@ import kotlin.reflect.*
 
 /**
  * `X-Forwarded-*` headers support
+ * See http://ktor.io/servers/features/forward-headers.html for details
  */
 public object XForwardedHeaderSupport :
     ApplicationPlugin<ApplicationCallPipeline, XForwardedHeaderSupport.Config, XForwardedHeaderSupport.Config> {
