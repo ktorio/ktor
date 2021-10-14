001afe790338bc53853bd6cfddbca6196a2dd1e3
leonid.stashevsky <leonid.stashevsky@gmail.com>
2021-10-14 14:07:09 +0200

    KTOR-3243 Ignore flaky testTimeoutPriority

--- ktor-client/ktor-client-cio/jvm/test/io/ktor/client/engine/cio/CIORequestTest.kt
+++ ktor-client/ktor-client-cio/jvm/test/io/ktor/client/engine/cio/CIORequestTest.kt
@@ -19,6 +19,7 @@ import io.ktor.util.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.debug.junit4.*
 import org.junit.*
+import org.junit.Ignore
 import java.nio.channels.*
 import kotlin.test.*
 import kotlin.test.Test
@@ -69,6 +70,7 @@ class CIORequestTest : TestWithKtor() {
     }
 
     @Test
+    @Ignore
     fun testTimeoutPriority() {
         testWithEngine(CIO) {
             config {
