ba302ad8ff6a2b35bed16cd391ba0d8675db3f7b
Thomas <thomasjamesfowler97@gmail.com>
2021-10-28 15:36:56 +1100

    Replaced custom `mapFirstNotNull` with stdlib `firstNotNullOfOrNull`

--- ktor-server/ktor-server-plugins/ktor-server-content-negotiation/jvm/src/io/ktor/server/plugins/ContentNegotiation.kt
+++ ktor-server/ktor-server-plugins/ktor-server-content-negotiation/jvm/src/io/ktor/server/plugins/ContentNegotiation.kt
@@ -177,7 +177,7 @@ public class ContentNegotiation internal constructor(
                 }
 
                 // Pick the first one that can convert the subject successfully
-                val converted = suitableConverters.mapFirstNotNull {
+                val converted = suitableConverters.firstNotNullOfOrNull {
                     it.converter.serialize(
                         contentType = it.contentType,
                         charset = call.request.headers.suitableCharset(),
@@ -264,15 +264,4 @@ public fun List<ContentTypeWithQuality>.sortedByQuality(): List<ContentTypeWithQ
             asterisks
         }.thenByDescending { it.contentType.parameters.size }
     )
-}
-
-private inline fun <F, T> Iterable<F>.mapFirstNotNull(block: (F) -> T?): T? {
-    @Suppress("LoopToCallChain")
-    for (element in this) {
-        val mapped = block(element)
-        if (mapped != null) {
-            return mapped
-        }
-    }
-    return null
 }
