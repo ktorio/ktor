9724a73bb88974414ba810e8aa0175996b491031
Thomas <thomasjamesfowler97@gmail.com>
2021-11-07 14:51:23 +1100

    Removed shared package prefix

--- ktor-client/ktor-client-plugins/ktor-client-content-negotiation/common/test/io/ktor/client/plugins/TestContentConverter.kt
+++ ktor-client/ktor-client-plugins/ktor-client-content-negotiation/common/test/io/ktor/client/plugins/TestContentConverter.kt
@@ -6,7 +6,7 @@ package io.ktor.client.plugins
 
 import io.ktor.http.*
 import io.ktor.http.content.*
-import io.ktor.shared.serialization.*
+import io.ktor.serialization.*
 import io.ktor.util.reflect.*
 import io.ktor.utils.io.*
 import io.ktor.utils.io.charsets.*
