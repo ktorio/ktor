206894f0c522057b289dc172df901c14c27d5aa6
Rustam <rxsinukov@gmail.com>
2021-12-02 13:55:56 +0100

    KTOR-3466 Change log level from `INFO` to `ERROR` for tests (#2713)

--- ktor-server/ktor-server-plugins/build.gradle.kts
+++ ktor-server/ktor-server-plugins/build.gradle.kts
@@ -11,6 +11,11 @@ subprojects {
                     api(project(":ktor-server:ktor-server-test-host"))
                 }
             }
+            val jvmTest by getting {
+                dependencies {
+                    api(project(":ktor-server:ktor-server-core", configuration = "testOutput"))
+                }
+            }
         }
     }
 }
