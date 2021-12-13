77567db8ddbfdb24185b6c021c09ab99d66f2435
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2021-12-13 09:58:29 +0100

    KTOR-3561 Bump nodeJS to 16.13.1 (#2733)

--- build.gradle.kts
+++ build.gradle.kts
@@ -209,6 +209,6 @@ if (docs != null) {
 
 // https://youtrack.jetbrains.com/issue/KT-49109
 rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin> {
-    val nodeM1Version = "16.0.0"
+    val nodeM1Version = "16.13.1"
     rootProject.the<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension>().nodeVersion = nodeM1Version
 }
