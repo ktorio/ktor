b49ccccafab7cdff8ba38b1c88b8ac50c759d000
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2022-01-19 09:21:32 +0100

    KTOR-3712 Remove explicit JVM 8 target

--- ktor-server/ktor-server-plugins/ktor-server-metrics-micrometer/build.gradle.kts
+++ ktor-server/ktor-server-plugins/ktor-server-metrics-micrometer/build.gradle.kts
@@ -1,10 +1,3 @@
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().all {
-    kotlinOptions {
-        // micrometer uses jdk 1.8 features, so we need to use that version here
-        jvmTarget = "1.8"
-    }
-}
-
 kotlin {
     sourceSets {
         val jvmMain by getting {
@@ -16,4 +9,3 @@ kotlin {
         }
     }
 }
-
