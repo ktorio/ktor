c04402b204aa3b2ead530c37064efa6d05a0b4ad
Brilyantov Vadim <vadim.briliantov@jetbrains.com>
2022-01-25 17:37:11 +0300

    KTOR-3740 Support routing scoped plugins in hooks (#2793)
    * KTOR-3740 Support routing scoped plugins in hooks
    
    * KTOR-3740 Add test on routing scoped hooks
    
    * KTOR-3740 Fix API dump

--- ktor-server/ktor-server-core/jvmAndNix/test/io/ktor/server/application/HooksTest.kt
+++ ktor-server/ktor-server-core/jvmAndNix/test/io/ktor/server/application/HooksTest.kt
@@ -5,7 +5,9 @@
 package io.ktor.server.application
 
 import io.ktor.client.request.*
+import io.ktor.http.*
 import io.ktor.server.response.*
+import io.ktor.server.routing.*
 import io.ktor.server.testing.*
 import kotlin.test.*
 
@@ -21,7 +23,7 @@ class HooksTest {
         val currentHandler = HookHandler()
 
         val myHook = object : Hook<HookHandler.() -> Unit> {
-            override fun install(application: Application, handler: HookHandler.() -> Unit) {
+            override fun install(application: ApplicationCallPipeline, handler: HookHandler.() -> Unit) {
                 currentHandler.apply(handler)
 
                 application.intercept(ApplicationCallPipeline.Call) {
@@ -117,6 +119,69 @@ class HooksTest {
 
             assertNotNull(state.fail)
             assertEquals("Failure", state.fail?.message)
+        }
+    }
+
+    @Test
+    fun testHookWithRoutingPlugin() {
+        val OnCallHook = object : Hook<() -> Unit> {
+            override fun install(application: ApplicationCallPipeline, handler: () -> Unit) {
+                application.intercept(ApplicationCallPipeline.Call) {
+                    handler()
+                }
+            }
+        }
+
+        val myOutput = mutableListOf<Int>()
+        fun myPrintln(value: Int) {
+            myOutput += value
+        }
+
+        class MyConfig {
+            var myValue: Int = 0
+        }
+
+        val MyRoutePlugin = createRouteScopedPlugin("MyRoutePlugin", ::MyConfig) {
+            on(OnCallHook) {
+                myPrintln(pluginConfig.myValue)
+            }
+        }
+
+        testApplication {
+            routing {
+                route("/1") {
+                    install(MyRoutePlugin) {
+                        myValue = 1
+                    }
+                    get {
+                        call.respond("Hello-1")
+                    }
+                }
+                route("/2") {
+                    install(MyRoutePlugin) {
+                        myValue = 2
+                    }
+                    get {
+                        call.respond("Hello-2")
+                    }
+                }
+                get("/3") {
+                    call.respond("Hello-3")
+                }
+            }
+
+            suspend fun ApplicationTestBuilder.assertOutput(requestPath: String, expectedOutput: List<Int>) {
+                myOutput.clear()
+                runCatching {
+                    client.get(requestPath)
+                }
+
+                assertContentEquals(expectedOutput.toTypedArray(), myOutput.toTypedArray())
+            }
+
+            assertOutput("/1", listOf(1))
+            assertOutput("/2", listOf(2))
+            assertOutput("/3", emptyList())
         }
     }
 }
