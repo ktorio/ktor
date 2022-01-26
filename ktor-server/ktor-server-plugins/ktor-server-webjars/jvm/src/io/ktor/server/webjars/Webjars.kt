d2885791c5ed6513007a6e48928182eb1a3dee79
Vadim Briliantov <vadim.briliantov@jetbrains.com>
2022-01-26 13:52:23 +0300

    KTOR-3667 Rewrite WebJars plugin to new API

--- ktor-server/ktor-server-plugins/ktor-server-webjars/jvm/src/io/ktor/server/webjars/Webjars.kt
+++ ktor-server/ktor-server-plugins/ktor-server-webjars/jvm/src/io/ktor/server/webjars/Webjars.kt
@@ -1,7 +1,3 @@
-/*
- * Copyright 2014-2021 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
- */
-
 package io.ktor.server.webjars
 
 import io.ktor.http.*
@@ -9,10 +5,8 @@ import io.ktor.http.content.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
-import io.ktor.util.*
 import io.ktor.util.cio.*
 import io.ktor.util.date.*
-import io.ktor.util.pipeline.*
 import io.ktor.utils.io.*
 import io.ktor.utils.io.jvm.javaio.*
 import org.webjars.*
@@ -20,107 +14,82 @@ import java.io.*
 import java.time.*
 
 /**
+ * Webjars plugin configuration.
+ */
+public class WebjarsConfig {
+    /**
+     * Path prefix at which the installed plugin responds.
+     */
+    public var path: String = "/webjars/"
+        set(value) {
+            field = buildString(value.length + 2) {
+                if (!value.startsWith('/')) {
+                    append('/')
+                }
+                append(value)
+                if (!endsWith('/')) {
+                    append('/')
+                }
+            }
+        }
+
+    /**
+     * Makes no effect. Will be dropped in future releases.
+     */
+    @Suppress("unused")
+    @Deprecated("This is no longer used and will be dropped in future releases.", level = DeprecationLevel.ERROR)
+    public var zone: ZoneId = ZoneId.systemDefault()
+}
+
+private fun extractWebJar(path: String, knownWebJars: Set<String>, locator: WebJarAssetLocator): String {
+    val firstDelimiter = if (path.startsWith("/")) 1 else 0
+    val nextDelimiter = path.indexOf("/", 1)
+    val webjar = if (nextDelimiter > -1) path.substring(firstDelimiter, nextDelimiter) else ""
+    val partialPath = path.substring(nextDelimiter + 1)
+    if (webjar !in knownWebJars) {
+        throw IllegalArgumentException("jar $webjar not found")
+    }
+    return locator.getFullPath(webjar, partialPath)
+}
+
+/**
  * This plugin listens to requests starting with the specified path prefix and responding with static content
  * packaged into webjars. A [WebJarAssetLocator] is used to look for static files.
  */
-public class Webjars internal constructor(private val webjarsPrefix: String) {
-    init {
+public val Webjars: ApplicationPlugin<Application, WebjarsConfig, PluginInstance> =
+    createApplicationPlugin("Webjars", ::WebjarsConfig) {
+        val webjarsPrefix = pluginConfig.path
         require(webjarsPrefix.startsWith("/"))
         require(webjarsPrefix.endsWith("/"))
-    }
 
-    @Deprecated("Use install(Webjars), there is no need to instantiate it directly.", level = DeprecationLevel.ERROR)
-    public constructor(configuration: Configuration) : this(configuration.path)
+        val locator = WebJarAssetLocator()
+        val knownWebJars = locator.webJars?.keys?.toSet() ?: emptySet()
+        val lastModified = GMTDate()
 
-    private val locator = WebJarAssetLocator()
-    private val knownWebJars = locator.webJars?.keys?.toSet() ?: emptySet()
-    private val lastModified = GMTDate()
-
-    private fun extractWebJar(path: String): String {
-        val firstDelimiter = if (path.startsWith("/")) 1 else 0
-        val nextDelimiter = path.indexOf("/", 1)
-        val webjar = if (nextDelimiter > -1) path.substring(firstDelimiter, nextDelimiter) else ""
-        val partialPath = path.substring(nextDelimiter + 1)
-        if (webjar !in knownWebJars) {
-            throw IllegalArgumentException("jar $webjar not found")
-        }
-        return locator.getFullPath(webjar, partialPath)
-    }
-
-    /**
-     * Plugin configuration.
-     */
-    public class Configuration {
-        /**
-         * Path prefix at which the installed plugin responds.
-         */
-        public var path: String = "/webjars/"
-            set(value) {
-                field = buildString(value.length + 2) {
-                    if (!value.startsWith('/')) {
-                        append('/')
-                    }
-                    append(value)
-                    if (!endsWith('/')) {
-                        append('/')
-                    }
+        onCall { call ->
+            val fullPath = call.request.path()
+            if (fullPath.startsWith(webjarsPrefix) &&
+                call.request.httpMethod == HttpMethod.Get &&
+                fullPath.last() != '/'
+            ) {
+                val resourcePath = fullPath.removePrefix(webjarsPrefix)
+                try {
+                    val location = extractWebJar(resourcePath, knownWebJars, locator)
+                    val stream = WebjarsConfig::class.java.classLoader.getResourceAsStream(location) ?: return@onCall
+                    call.respond(
+                        InputStreamContent(
+                            stream,
+                            ContentType.defaultForFilePath(fullPath),
+                            lastModified
+                        )
+                    )
+                } catch (multipleFiles: MultipleMatchesException) {
+                    call.respond(HttpStatusCode.InternalServerError)
+                } catch (_: IllegalArgumentException) {
                 }
             }
-
-        /**
-         * Makes no effect. Will be dropped in future releases.
-         */
-        @Suppress("unused")
-        @Deprecated("This is no longer used and will be dropped in future releases.", level = DeprecationLevel.ERROR)
-        public var zone: ZoneId = ZoneId.systemDefault()
-    }
-
-    private suspend fun intercept(context: PipelineContext<Unit, ApplicationCall>) {
-        val fullPath = context.call.request.path()
-        if (fullPath.startsWith(webjarsPrefix) &&
-            context.call.request.httpMethod == HttpMethod.Get &&
-            fullPath.last() != '/'
-        ) {
-            val resourcePath = fullPath.removePrefix(webjarsPrefix)
-            try {
-                val location = extractWebJar(resourcePath)
-                val stream = Webjars::class.java.classLoader.getResourceAsStream(location) ?: return
-                context.call.respond(
-                    InputStreamContent(
-                        stream,
-                        ContentType.defaultForFilePath(fullPath),
-                        lastModified
-                    )
-                )
-            } catch (multipleFiles: MultipleMatchesException) {
-                context.call.respond(HttpStatusCode.InternalServerError)
-            } catch (notFound: IllegalArgumentException) {
-            }
         }
     }
-
-    /**
-     * Plugin installation companion.
-     */
-    public companion object Plugin : ApplicationPlugin<ApplicationCallPipeline, Configuration, Webjars> {
-
-        override val key: AttributeKey<Webjars> = AttributeKey("Webjars")
-
-        override fun install(
-            pipeline: ApplicationCallPipeline,
-            configure: Configuration.() -> Unit
-        ): Webjars {
-            val configuration = Configuration().apply(configure)
-
-            val plugin = Webjars(configuration.path)
-
-            pipeline.intercept(ApplicationCallPipeline.Plugins) {
-                plugin.intercept(this)
-            }
-            return plugin
-        }
-    }
-}
 
 private class InputStreamContent(
     val input: InputStream,
