bff240b374af60a77c5da6fe7c31d97abe205f31
Usova Daria <usovadasha@myttk.ru>
2022-01-28 17:13:52 +0700

    Rewrite HSTS Plugin to New Plugins API (#2807)

--- ktor-server/ktor-server-plugins/ktor-server-hsts/jvmAndNix/src/io/ktor/server/plugins/hsts/KotlinTimeJvm.kt
+++ ktor-server/ktor-server-plugins/ktor-server-hsts/jvmAndNix/src/io/ktor/server/plugins/hsts/KotlinTimeJvm.kt
@@ -11,7 +11,7 @@ import kotlin.time.Duration.Companion.seconds
 /**
  * Duration to tell the client to keep the host in a list of known HSTS hosts.
  */
-public var HSTS.Configuration.maxAgeDuration: Duration
+public var HSTSConfig.maxAgeDuration: Duration
     get() = maxAgeInSeconds.seconds
     set(newMaxAge) {
         require(!newMaxAge.isNegative()) { "Only non-negative durations can be specified" }
