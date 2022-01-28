25e6a1c22a40026b738b6c2dba0beb69ecda8148
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2022-01-28 09:14:24 +0100

    Fix ktor-server-tests publication

--- ktor-server/ktor-server-tests/jvmAndNix/src/Stub.kt
+++ ktor-server/ktor-server-tests/jvmAndNix/src/Stub.kt
@@ -1,7 +1,7 @@
 /*
  * Copyright 2014-2022 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
-package io.ktor.server
+package io.ktor.server.tests
 
 internal fun stub() {
 }
