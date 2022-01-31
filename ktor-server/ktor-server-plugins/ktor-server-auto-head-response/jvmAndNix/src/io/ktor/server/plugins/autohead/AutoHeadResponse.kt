13c799d74e12374c9af7b769ce7fc198e4379c70
Leonid Stashevsky <e5l@users.noreply.github.com>
2022-01-31 09:53:50 +0100

    KTOR-3670 Rewrite Auto Head to New Plugins API (#2784)
    * KTOR-3670 Rewrite AutoHead to new plugins API

--- ktor-server/ktor-server-plugins/ktor-server-auto-head-response/jvmAndNix/src/io/ktor/server/plugins/autohead/AutoHeadResponse.kt
+++ ktor-server/ktor-server-plugins/ktor-server-auto-head-response/jvmAndNix/src/io/ktor/server/plugins/autohead/AutoHeadResponse.kt
@@ -10,41 +10,33 @@ import io.ktor.server.application.*
 import io.ktor.server.plugins.*
 import io.ktor.server.response.*
 import io.ktor.util.*
-import io.ktor.util.pipeline.*
 
 /**
- * A plugin that automatically respond to HEAD requests
+ * A plugin that automatically respond to HEAD requests.
  */
-public object AutoHeadResponse : ApplicationPlugin<ApplicationCallPipeline, Unit, Unit> {
-    private val HeadPhase = PipelinePhase("HEAD")
+public val AutoHeadResponse: ApplicationPlugin<Application, Unit, PluginInstance> =
+    createApplicationPlugin("AutoHeadResponse") {
 
-    override val key: AttributeKey<Unit> = AttributeKey<Unit>("Automatic Head Response")
+        onCall { call ->
+            if (call.request.local.method != HttpMethod.Head) return@onCall
+            call.mutableOriginConnectionPoint.method = HttpMethod.Get
+        }
 
-    override fun install(pipeline: ApplicationCallPipeline, configure: Unit.() -> Unit) {
-        Unit.configure()
+        onCallRespond.afterTransform { call, _ ->
+            if (call.request.local.method != HttpMethod.Head) return@afterTransform
 
-        pipeline.intercept(ApplicationCallPipeline.Plugins) {
-            if (call.request.local.method == HttpMethod.Head) {
-                call.response.pipeline.insertPhaseBefore(ApplicationSendPipeline.TransferEncoding, HeadPhase)
-                call.response.pipeline.intercept(HeadPhase) { message ->
-                    if (message is OutgoingContent && message !is OutgoingContent.NoContent) {
-                        proceedWith(HeadResponse(message))
-                    }
-                }
-
-                // Pretend the request was with GET method so that all normal routes and interceptors work
-                // but in the end we will drop the content
-                call.mutableOriginConnectionPoint.method = HttpMethod.Get
+            transformBody { body ->
+                if (body is OutgoingContent.NoContent) return@transformBody body
+                HeadResponse(body)
             }
         }
     }
 
-    private class HeadResponse(val original: OutgoingContent) : OutgoingContent.NoContent() {
-        override val status: HttpStatusCode? get() = original.status
-        override val contentType: ContentType? get() = original.contentType
-        override val contentLength: Long? get() = original.contentLength
-        override fun <T : Any> getProperty(key: AttributeKey<T>) = original.getProperty(key)
-        override fun <T : Any> setProperty(key: AttributeKey<T>, value: T?) = original.setProperty(key, value)
-        override val headers get() = original.headers
-    }
+private class HeadResponse(val original: OutgoingContent) : OutgoingContent.NoContent() {
+    override val status: HttpStatusCode? get() = original.status
+    override val contentType: ContentType? get() = original.contentType
+    override val contentLength: Long? get() = original.contentLength
+    override fun <T : Any> getProperty(key: AttributeKey<T>) = original.getProperty(key)
+    override fun <T : Any> setProperty(key: AttributeKey<T>, value: T?) = original.setProperty(key, value)
+    override val headers get() = original.headers
 }
