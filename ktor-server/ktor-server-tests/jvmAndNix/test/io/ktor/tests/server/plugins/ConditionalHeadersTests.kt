934118f3c72f63543fcb682d9fdebcf36bc8e77c
Usova Daria <usovadasha@myttk.ru>
2022-02-02 10:08:21 +0700

    Rewrite ConditionalHeaders to New Plugins API (#2817)

--- ktor-server/ktor-server-tests/jvmAndNix/test/io/ktor/tests/server/plugins/ConditionalHeadersTests.kt
+++ ktor-server/ktor-server-tests/jvmAndNix/test/io/ktor/tests/server/plugins/ConditionalHeadersTests.kt
@@ -5,6 +5,8 @@
 
 package io.ktor.tests.server.plugins
 
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.http.content.*
 import io.ktor.server.application.*
@@ -12,92 +14,95 @@ import io.ktor.server.plugins.conditionalheaders.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.testing.*
+import kotlinx.coroutines.*
 import kotlin.test.*
 
-@Suppress("DEPRECATION")
 class ETagsTest {
-    private fun withConditionalApplication(body: TestApplicationEngine.() -> Unit) = withTestApplication {
-        application.install(ConditionalHeaders) {
-            version { listOf(EntityTagVersion("tag1")) }
-        }
-        application.routing {
-            handle {
-                call.respondText("response")
+    private fun withConditionalApplication(body: suspend ApplicationTestBuilder.() -> Unit) = testApplication {
+        application {
+            install(ConditionalHeaders) {
+                version { listOf(EntityTagVersion("tag1")) }
+            }
+            routing {
+                handle {
+                    call.respondText("response")
+                }
             }
         }
-
-        body()
+        runBlocking {
+            body()
+        }
     }
 
     @Test
     fun testNoConditions(): Unit = withConditionalApplication {
-        val result = handleRequest {}
-        assertEquals(HttpStatusCode.OK, result.response.status())
-        assertEquals("response", result.response.content)
-        assertEquals("\"tag1\"", result.response.headers[HttpHeaders.ETag])
+        val result = client.get {}
+        assertEquals(HttpStatusCode.OK, result.status)
+        assertEquals("response", result.bodyAsText())
+        assertEquals("\"tag1\"", result.headers[HttpHeaders.ETag])
     }
 
     @Test
     fun testIfMatchConditionAccepted(): Unit = withConditionalApplication {
-        val result = handleRequest {
-            addHeader(HttpHeaders.IfMatch, "tag1")
+        val result = client.get {
+            header(HttpHeaders.IfMatch, "tag1")
         }
-        assertEquals(HttpStatusCode.OK, result.response.status())
-        assertEquals("response", result.response.content)
-        assertEquals("\"tag1\"", result.response.headers[HttpHeaders.ETag])
+        assertEquals(HttpStatusCode.OK, result.status)
+        assertEquals("response", result.bodyAsText())
+        assertEquals("\"tag1\"", result.headers[HttpHeaders.ETag])
     }
 
     @Test
     fun testIfMatchConditionFailed(): Unit = withConditionalApplication {
-        val result = handleRequest {
-            addHeader(HttpHeaders.IfMatch, "tag2")
+        val result = client.get {
+            header(HttpHeaders.IfMatch, "tag2")
         }
-        assertEquals(HttpStatusCode.PreconditionFailed, result.response.status())
+        assertEquals(HttpStatusCode.PreconditionFailed, result.status)
     }
 
     @Test
     fun testIfNoneMatchConditionAccepted(): Unit = withConditionalApplication {
-        val result = handleRequest {
-            addHeader(HttpHeaders.IfNoneMatch, "tag1")
+        val result = client.get {
+            header(HttpHeaders.IfNoneMatch, "tag1")
         }
-        assertEquals(HttpStatusCode.NotModified, result.response.status())
-        assertEquals("\"tag1\"", result.response.headers[HttpHeaders.ETag])
+        assertEquals(HttpStatusCode.NotModified, result.status)
+        assertEquals("\"tag1\"", result.headers[HttpHeaders.ETag])
     }
 
     @Test
     fun testIfNoneMatchWeakConditionAccepted(): Unit = withConditionalApplication {
-        val result = handleRequest {
-            addHeader(HttpHeaders.IfNoneMatch, "W/tag1")
+        val result = client.get {
+            header(HttpHeaders.IfNoneMatch, "W/tag1")
         }
-        assertEquals(HttpStatusCode.NotModified, result.response.status())
+        assertEquals(HttpStatusCode.NotModified, result.status)
     }
 
     @Test
     fun testIfNoneMatchConditionFailed(): Unit = withConditionalApplication {
-        val result = handleRequest {
-            addHeader(HttpHeaders.IfNoneMatch, "tag2")
+        val result = client.get {
+            header(HttpHeaders.IfNoneMatch, "tag2")
         }
-        assertEquals(HttpStatusCode.OK, result.response.status())
-        assertEquals("response", result.response.content)
-        assertEquals("\"tag1\"", result.response.headers[HttpHeaders.ETag])
+        assertEquals(HttpStatusCode.OK, result.status)
+        assertEquals("response", result.bodyAsText())
+        assertEquals("\"tag1\"", result.headers[HttpHeaders.ETag])
     }
 
     @Test
     fun testIfMatchStar(): Unit = withConditionalApplication {
-        val result = handleRequest {
-            addHeader(HttpHeaders.IfMatch, "*")
+        val result = client.get {
+            header(HttpHeaders.IfMatch, "*")
         }
-        assertEquals(HttpStatusCode.OK, result.response.status())
-        assertEquals("response", result.response.content)
-        assertEquals("\"tag1\"", result.response.headers[HttpHeaders.ETag])
+        assertEquals(HttpStatusCode.OK, result.status)
+        assertEquals("response", result.bodyAsText())
+        assertEquals("\"tag1\"", result.headers[HttpHeaders.ETag])
     }
 
     @Test
     fun testIfNoneMatchStar(): Unit = withConditionalApplication {
-        val result = handleRequest {
-            addHeader(HttpHeaders.IfNoneMatch, "*")
+        val result = client.get {
+            header(HttpHeaders.IfNoneMatch, "*")
         }
-        assertEquals(HttpStatusCode.OK, result.response.status())
+        assertEquals(HttpStatusCode.OK, result.status)
         // note: star for if-none-match is a special case
         // that should be handled separately
         // so we always pass it
@@ -105,37 +110,39 @@ class ETagsTest {
 
     @Test
     fun testIfNoneMatchListConditionFailed(): Unit = withConditionalApplication {
-        val result = handleRequest {
-            addHeader(HttpHeaders.IfNoneMatch, "tag0,tag1,tag3")
+        val result = client.get {
+            header(HttpHeaders.IfNoneMatch, "tag0,tag1,tag3")
         }
-        assertEquals(HttpStatusCode.NotModified, result.response.status())
+        assertEquals(HttpStatusCode.NotModified, result.status)
     }
 
     @Test
     fun testIfNoneMatchListConditionSuccess(): Unit = withConditionalApplication {
-        val result = handleRequest {
-            addHeader(HttpHeaders.IfNoneMatch, "tag2")
+        val result = client.get {
+            header(HttpHeaders.IfNoneMatch, "tag2")
         }
-        assertEquals(HttpStatusCode.OK, result.response.status())
-        assertEquals("response", result.response.content)
-        assertEquals("\"tag1\"", result.response.headers[HttpHeaders.ETag])
+        assertEquals(HttpStatusCode.OK, result.status)
+        assertEquals("response", result.bodyAsText())
+        assertEquals("\"tag1\"", result.headers[HttpHeaders.ETag])
     }
 
     @Test
     fun testIfMatchListConditionAccepted(): Unit = withConditionalApplication {
-        val result = handleRequest {
-            addHeader(HttpHeaders.IfMatch, "tag0,tag1,tag3")
+        runBlocking {
+            val result = client.get {
+                header(HttpHeaders.IfMatch, "tag0,tag1,tag3")
+            }
+            assertEquals(HttpStatusCode.OK, result.status)
+            assertEquals("response", result.bodyAsText())
+            assertEquals("\"tag1\"", result.headers[HttpHeaders.ETag])
         }
-        assertEquals(HttpStatusCode.OK, result.response.status())
-        assertEquals("response", result.response.content)
-        assertEquals("\"tag1\"", result.response.headers[HttpHeaders.ETag])
     }
 
     @Test
     fun testIfMatchListConditionFailed(): Unit = withConditionalApplication {
-        val result = handleRequest {
-            addHeader(HttpHeaders.IfMatch, "tag0,tag2,tag3")
+        val result = client.get {
+            header(HttpHeaders.IfMatch, "tag0,tag2,tag3")
         }
-        assertEquals(HttpStatusCode.PreconditionFailed, result.response.status())
+        assertEquals(HttpStatusCode.PreconditionFailed, result.status)
     }
 }
