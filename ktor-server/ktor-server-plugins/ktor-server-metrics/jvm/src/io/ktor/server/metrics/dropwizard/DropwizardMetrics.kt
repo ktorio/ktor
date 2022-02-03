3291c77bbd05842f7a74749d9317f97366921a1f
Brilyantov Vadim <vadim.briliantov@jetbrains.com>
2022-02-03 17:38:55 +0300

    KTOR-3666 Rewrite metrics to the new plugins API (#2828)
    * KTOR-3666 Rewrite Metrics to new plugins API
    
    * Fix review comments, and codestyle
    
    * Fix api dump
    
    * Fix api dump
    
    * Fix api dump
    
    * Update after code review

--- ktor-server/ktor-server-plugins/ktor-server-metrics/jvm/src/io/ktor/server/metrics/dropwizard/DropwizardMetrics.kt
+++ ktor-server/ktor-server-plugins/ktor-server-metrics/jvm/src/io/ktor/server/metrics/dropwizard/DropwizardMetrics.kt
@@ -8,135 +8,90 @@ import com.codahale.metrics.*
 import com.codahale.metrics.MetricRegistry.*
 import com.codahale.metrics.jvm.*
 import io.ktor.server.application.*
-import io.ktor.server.routing.*
-import io.ktor.util.*
-import io.ktor.util.pipeline.*
 import java.util.concurrent.*
 
 /**
- * Dropwizard metrics support plugin. See https://ktor.io/servers/features/metrics.html for details.
- * @property registry dropwizard metrics registry
- * @property baseName metrics base name (prefix)
+ * Metrics plugin configuration object that is used during plugin installation.
  */
-public class DropwizardMetrics private constructor(
-    public val registry: MetricRegistry,
-    public val baseName: String = name("ktor.calls")
-) {
-    private val duration = registry.timer(name(baseName, "duration"))
-    private val active = registry.counter(name(baseName, "active"))
-    private val exceptions = registry.meter(name(baseName, "exceptions"))
-    private val httpStatus = ConcurrentHashMap<Int, Meter>()
+public class DropwizardMetricsConfig {
+    /**
+     * Dropwizard metrics base name (prefix)
+     */
+    public var baseName: String = name("ktor.calls")
 
     /**
-     * Metrics plugin configuration object that is used during plugin installation.
+     * Dropwizard metric registry.
      */
-    public class Configuration {
-        /**
-         * Dropwizard metrics base name (prefix)
-         */
-        public var baseName: String = name("ktor.calls")
-
-        /**
-         * Dropwizard metric registry.
-         */
-        public var registry: MetricRegistry = MetricRegistry()
-
-        /**
-         * By default, this plugin will register `MetricSet`s from
-         * [metrics-jvm](https://metrics.dropwizard.io/4.1.2/manual/jvm.html) in the configured [MetricRegistry].
-         * Set this to false to not register them.
-         */
-        public var registerJvmMetricSets: Boolean = true
-    }
+    public var registry: MetricRegistry = MetricRegistry()
 
     /**
-     * Metrics plugin companion
+     * By default, this plugin will register `MetricSet`s from
+     * [metrics-jvm](https://metrics.dropwizard.io/4.1.2/manual/jvm.html) in the configured [MetricRegistry].
+     * Set this to false to not register them.
      */
-    public companion object Plugin : ApplicationPlugin<Application, Configuration, DropwizardMetrics> {
-        override val key: AttributeKey<DropwizardMetrics> = AttributeKey("metrics")
-
-        private class RoutingMetrics(val name: String, val context: Timer.Context)
-
-        private val routingMetricsKey = AttributeKey<RoutingMetrics>("metrics")
-
-        override fun install(pipeline: Application, configure: Configuration.() -> Unit): DropwizardMetrics {
-            val configuration = Configuration().apply(configure)
-            val plugin = DropwizardMetrics(configuration.registry, configuration.baseName)
-
-            if (configuration.registerJvmMetricSets) {
-                listOf<Pair<String, () -> Metric>>(
-                    "jvm.memory" to ::MemoryUsageGaugeSet,
-                    "jvm.garbage" to ::GarbageCollectorMetricSet,
-                    "jvm.threads" to ::ThreadStatesGaugeSet,
-                    "jvm.files" to ::FileDescriptorRatioGauge,
-                    "jvm.attributes" to ::JvmAttributeGaugeSet
-                )
-                    .filter { (name, _) ->
-                        !configuration.registry.names.any { existingName -> existingName.startsWith(name) }
-                    }
-                    .forEach { (name, metric) -> configuration.registry.register(name, metric()) }
-            }
-
-            val phase = PipelinePhase("DropwizardMetrics")
-            pipeline.insertPhaseBefore(ApplicationCallPipeline.Monitoring, phase)
-            pipeline.intercept(phase) {
-                plugin.before(call)
-                try {
-                    proceed()
-                } catch (e: Exception) {
-                    plugin.exception(call, e)
-                    throw e
-                } finally {
-                    plugin.after(call)
-                }
-            }
-
-            pipeline.environment.monitor.subscribe(Routing.RoutingCallStarted) { call ->
-                val name = call.route.toString()
-                val meter = plugin.registry.meter(name(plugin.baseName, name, "meter"))
-                val timer = plugin.registry.timer(name(plugin.baseName, name, "timer"))
-                meter.mark()
-                val context = timer.time()
-                call.attributes.put(
-                    routingMetricsKey,
-                    RoutingMetrics(name, context)
-                )
-            }
-
-            pipeline.environment.monitor.subscribe(Routing.RoutingCallFinished) { call ->
-                val routingMetrics = call.attributes.take(routingMetricsKey)
-                val status = call.response.status()?.value ?: 0
-                val statusMeter = plugin.registry.meter(name(plugin.baseName, routingMetrics.name, status.toString()))
-                statusMeter.mark()
-                routingMetrics.context.stop()
-            }
-
-            return plugin
-        }
-    }
-
-    private data class CallMeasure(val timer: Timer.Context)
-
-    private val measureKey = AttributeKey<CallMeasure>("metrics")
-
-    private fun before(call: ApplicationCall) {
-        active.inc()
-        call.attributes.put(measureKey, CallMeasure(duration.time()))
-    }
-
-    private fun after(call: ApplicationCall) {
-        active.dec()
-        val meter = httpStatus.computeIfAbsent(call.response.status()?.value ?: 0) {
-            registry.meter(name(baseName, "status", it.toString()))
-        }
-        meter.mark()
-        call.attributes.getOrNull(measureKey)?.apply {
-            timer.stop()
-        }
-    }
-
-    @Suppress("UNUSED_PARAMETER")
-    private fun exception(call: ApplicationCall, e: Throwable) {
-        exceptions.mark()
-    }
+    public var registerJvmMetricSets: Boolean = true
 }
+
+/**
+ * Dropwizard metrics support plugin. See https://ktor.io/servers/features/metrics.html for details.
+ */
+public val DropwizardMetrics: ApplicationPlugin<Application, DropwizardMetricsConfig, PluginInstance> =
+    createApplicationPlugin("DropwizardMetrics", ::DropwizardMetricsConfig) {
+        val duration = pluginConfig.registry.timer(name(pluginConfig.baseName, "duration"))
+        val active = pluginConfig.registry.counter(name(pluginConfig.baseName, "active"))
+        val exceptions = pluginConfig.registry.meter(name(pluginConfig.baseName, "exceptions"))
+        val httpStatus = ConcurrentHashMap<Int, Meter>()
+
+        if (pluginConfig.registerJvmMetricSets) {
+            listOf<Pair<String, () -> Metric>>(
+                "jvm.memory" to ::MemoryUsageGaugeSet,
+                "jvm.garbage" to ::GarbageCollectorMetricSet,
+                "jvm.threads" to ::ThreadStatesGaugeSet,
+                "jvm.files" to ::FileDescriptorRatioGauge,
+                "jvm.attributes" to ::JvmAttributeGaugeSet
+            ).filter { (name, _) ->
+                !pluginConfig.registry.names.any { existingName -> existingName.startsWith(name) }
+            }.forEach { (name, metric) -> pluginConfig.registry.register(name, metric()) }
+        }
+
+        on(CallFailed) { _, _ ->
+            exceptions.mark()
+        }
+
+        on(CallRouted) { call ->
+            val name = call.route.toString()
+            val meter = pluginConfig.registry.meter(name(pluginConfig.baseName, name, "meter"))
+            val timer = pluginConfig.registry.timer(name(pluginConfig.baseName, name, "timer"))
+            meter.mark()
+            val context = timer.time()
+            call.attributes.put(
+                routingMetricsKey,
+                RoutingMetrics(name, context)
+            )
+        }
+
+        on(RoutedCallProcessed) { call ->
+            val routingMetrics = call.attributes.take(routingMetricsKey)
+            val status = call.response.status()?.value ?: 0
+            val statusMeter =
+                pluginConfig.registry.meter(name(pluginConfig.baseName, routingMetrics.name, status.toString()))
+            statusMeter.mark()
+            routingMetrics.context.stop()
+        }
+
+        on(BeforeCall) { call ->
+            active.inc()
+            call.attributes.put(measureKey, CallMeasure(duration.time()))
+        }
+
+        on(AfterCall) { call ->
+            active.dec()
+            val meter = httpStatus.computeIfAbsent(call.response.status()?.value ?: 0) {
+                pluginConfig.registry.meter(name(pluginConfig.baseName, "status", it.toString()))
+            }
+            meter.mark()
+            call.attributes.getOrNull(measureKey)?.apply {
+                timer.stop()
+            }
+        }
+    }
