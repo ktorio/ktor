8455448b868e01f2adfacb37d0b245609053e70d
Zach Kozar <zachkozar@vt.edu>
2022-02-07 19:38:23 -0500

    Rewrite Jte to New Plugins API

--- ktor-server/ktor-server-plugins/ktor-server-jte/jvm/src/io/ktor/server/jte/Jte.kt
+++ ktor-server/ktor-server-plugins/ktor-server-jte/jvm/src/io/ktor/server/jte/Jte.kt
@@ -10,8 +10,6 @@ import io.ktor.http.*
 import io.ktor.http.ContentType
 import io.ktor.http.content.*
 import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.util.*
 
 /**
  * Represents content handled by the [Jte] plugin.
@@ -29,41 +27,24 @@ public class JteContent(
 )
 
 /**
+ * A configuration for the [Jte] plugin, where the Jte [TemplateEngine] can be customized.
+ */
+public class JteConfig {
+    public lateinit var templateEngine: TemplateEngine
+}
+
+/**
  * A plugin that allows you to use jte templates as views within your application.
  * Provides the ability to respond with [JteContent]
  */
-public class Jte private constructor(config: Config) {
+public val Jte: ApplicationPlugin<Application, JteConfig, PluginInstance> = createApplicationPlugin(
+    "jte",
+    ::JteConfig
+) {
 
-    /**
-     * The jte [TemplateEngine] used by this plugin
-     */
-    public val templateEngine: TemplateEngine = config.templateEngine
+    val templateEngine = pluginConfig.templateEngine
 
-    public class Config {
-        public lateinit var templateEngine: TemplateEngine
-    }
-
-    /**
-     * A plugin installing companion object
-     */
-    public companion object Plugin : ApplicationPlugin<ApplicationCallPipeline, Config, Jte> {
-
-        override val key: AttributeKey<Jte> = AttributeKey("jte")
-
-        override fun install(
-            pipeline: ApplicationCallPipeline,
-            configure: Config.() -> Unit
-        ): Jte {
-            val plugin = Jte(Config().apply(configure))
-            pipeline.sendPipeline.intercept(ApplicationSendPipeline.Transform) { value ->
-                if (value is JteContent)
-                    proceedWith(plugin.process(value))
-            }
-            return plugin
-        }
-    }
-
-    private fun process(content: JteContent): OutgoingContent {
+    fun process(content: JteContent): OutgoingContent {
         val writer = StringOutput()
         templateEngine.render(content.template, content.params, writer)
 
@@ -71,5 +52,12 @@ public class Jte private constructor(config: Config) {
         if (content.etag != null)
             result.versions += EntityTagVersion(content.etag)
         return result
+    }
+
+    onCallRespond { _, message ->
+        if (message is JteContent)
+            transformBody {
+                process(message)
+            }
     }
 }
