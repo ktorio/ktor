1462b4693a36aebf68f925754195390bd1c3b9be
andreyaksenov <andrey.aksenov@jetbrains.com>
2022-03-02 14:32:34 +0300

    KTOR-3966 suppress max-line-length

--- ktor-client/ktor-client-plugins/ktor-client-json/common/src/io/ktor/client/plugins/json/JsonSerializer.kt
+++ ktor-client/ktor-client-plugins/ktor-client-json/common/src/io/ktor/client/plugins/json/JsonSerializer.kt
@@ -12,7 +12,7 @@ import io.ktor.utils.io.core.*
 /**
  * Client json serializer.
  */
-@Deprecated("Please use ContentNegotiation plugin and its converters: https://ktor.io/docs/migrating-2.html#serialization-client")
+@Deprecated("Please use ContentNegotiation plugin and its converters: https://ktor.io/docs/migrating-2.html#serialization-client") // ktlint-disable max-line-length
 public interface JsonSerializer {
     /**
      * Convert data object to [OutgoingContent].
