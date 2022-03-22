303ba19df2307e6f92826d43b0e2ea4a4bb6f1ec
Andrey.Aksenov <Andrey.Aksenov@jetbrains.com>
2022-03-22 16:50:09 +0300

    KTOR-4051 update API docs for CallLogging

--- ktor-server/ktor-server-plugins/ktor-server-call-logging/jvm/src/io/ktor/server/plugins/callloging/CallLogging.kt
+++ ktor-server/ktor-server-plugins/ktor-server-call-logging/jvm/src/io/ktor/server/plugins/callloging/CallLogging.kt
@@ -10,7 +10,11 @@ import io.ktor.server.application.hooks.*
 import org.slf4j.event.*
 
 /**
- * Logs application lifecycle and call events.
+ * A plugin that allows you to log incoming client requests.
+ * You can configure [CallLogging] in multiple ways: specify a logging level,
+ * filter requests based on a specified condition, customize log messages, and so on.
+ *
+ * You can learn more from [Call logging](https://ktor.io/docs/call-logging.html).
  */
 public val CallLogging: ApplicationPlugin<CallLoggingConfig> = createApplicationPlugin(
     "CallLogging",
