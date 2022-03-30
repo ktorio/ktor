b8266d7b395504a3eeb1507718caa1717266dbdf
olme04 <olme04@zohomail.eu>
2022-03-30 12:34:44 +0300

    remove printlns and add comment

--- ktor-network/ktor-network-tls/jvm/src/io/ktor/network/tls/SSLEngineWrapper.kt
+++ ktor-network/ktor-network-tls/jvm/src/io/ktor/network/tls/SSLEngineWrapper.kt
@@ -12,8 +12,7 @@ import javax.net.ssl.*
 internal class SSLEngineWrapper(
     private val engine: SSLEngine,
     private val bufferAllocator: SSLEngineBufferAllocator,
-    private val output: ByteWriteChannel,
-    private val debugString: String
+    private val output: ByteWriteChannel
 ) {
     private val wrapLock = Mutex()
     private var wrapDestination = bufferAllocator.allocatePacket(0)
@@ -27,28 +26,19 @@ internal class SSLEngineWrapper(
         var result: SSLEngineResult
         wrapDestination.clear()
         while (true) {
-            // println("[$debugString] WRAP_BEFORE: $wrapDestination")
             result = engine.wrap(wrapSource, wrapDestination)
-            // println("[$debugString] WRAP_RESULT: $result")
-            // println("[$debugString] WRAP_AFTER: $wrapDestination")
             if (result.status != SSLEngineResult.Status.BUFFER_OVERFLOW) break
-            // println("[$debugString] WRAP_OVERFLOW: $wrapDestination")
             wrapDestination = bufferAllocator.reallocatePacket(wrapDestination, flip = true)
-            // println("[$debugString] WRAP_OVERFLOW_REALLOCATE: $wrapDestination")
         }
-        // println("[$debugString] WRAP_WRITE_BEFORE: $wrapDestination")
         if (result.bytesProduced() > 0) {
             wrapDestination.flip()
-            // println("[$debugString] WRAP_WRITE: $wrapDestination")
             output.writeFully(wrapDestination)
             output.flush()
         }
-        // println("[$debugString] WRAP_WRITE_AFTER: $wrapDestination")
         return result
     }
 
     suspend fun close(cause: Throwable?): SSLEngineResult = wrapLock.withLock {
-        // println("[$debugString] CLOSE: $cause")
         val temp = bufferAllocator.allocateApplication(0)
         var result: SSLEngineResult
         do {
