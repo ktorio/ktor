5d1b9af59db66f036b0d822a87ae3a1f5803e7bf
Andrey.Aksenov <Andrey.Aksenov@jetbrains.com>
2022-04-21 16:42:51 +0300

    KTOR-2271 api docs for app engine

--- ktor-server/ktor-server-host-common/jvmAndNix/src/io/ktor/server/engine/ApplicationEngine.kt
+++ ktor-server/ktor-server-host-common/jvmAndNix/src/io/ktor/server/engine/ApplicationEngine.kt
@@ -9,22 +9,22 @@ import io.ktor.server.engine.internal.*
 import io.ktor.util.network.*
 
 /**
- * Engine which runs an application
+ * An engine which runs an application.
  */
 public interface ApplicationEngine {
 
     /**
-     * Configuration for the [ApplicationEngine]
+     * A configuration for the [ApplicationEngine].
      */
     @Suppress("MemberVisibilityCanBePrivate")
     public open class Configuration {
         /**
-         * Provides currently available parallelism, e.g. number of available processors
+         * Returns the current parallelism level (e.g. the number of available processors).
          */
         public val parallelism: Int = availableProcessorsBridge()
 
         /**
-         * Specifies size of the event group for accepting connections
+         * Specifies how many threads are used to accept new connections and start call processing.
          */
         public var connectionGroupSize: Int = parallelism / 2 + 1
 
@@ -34,7 +34,7 @@ public interface ApplicationEngine {
         public var workerGroupSize: Int = parallelism / 2 + 1
 
         /**
-         * Specifies size of the event group for running application code
+         * Specifies the minimum size of a thread pool used to process application calls.
          */
         public var callGroupSize: Int = parallelism
     }
@@ -43,33 +43,35 @@ public interface ApplicationEngine {
      * Local addresses for application connectors.
      * If [environment]'s [connector]s was configured to use port=0, you can use this function to get an actual port
      * for these connectors.
-     * Available after server was started.
+     * Available after a server is started.
      */
     public suspend fun resolvedConnectors(): List<EngineConnectorConfig>
 
     /**
-     * Environment with which this engine is running
+     * An environment used to run this engine.
      */
     public val environment: ApplicationEngineEnvironment
 
     /**
-     * Currently running application instance
+     * A currently running application instance.
      */
     public val application: Application get() = environment.application
 
     /**
-     * Starts this [ApplicationEngine]
+     * Starts this [ApplicationEngine].
      *
-     * @param wait if true, this function does not exit until application engine stops and exits
+     * @param wait if true, then the `start` call blocks a current thread until it finishes its execution.
+     * If you run `start` from the main thread with `wait = false` and nothing else blocking this thread,
+     * then your application will be terminated without handling any requests.
      * @return returns this instance
      */
     public fun start(wait: Boolean = false): ApplicationEngine
 
     /**
-     * Stops this [ApplicationEngine]
+     * Stops this [ApplicationEngine].
      *
      * @param gracePeriodMillis the maximum amount of time for activity to cool down
-     * @param timeoutMillis the maximum amount of time to wait until server stops gracefully
+     * @param timeoutMillis the maximum amount of time to wait until a server stops gracefully
      */
     public fun stop(gracePeriodMillis: Long = 500, timeoutMillis: Long = 500)
 }
