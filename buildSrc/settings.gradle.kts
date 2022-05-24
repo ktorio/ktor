6216a26e6b3aab44e7251fb92470821d2bdb6540
Alexander Likhachev <Alexander.Likhachev@jetbrains.com>
2022-05-24 23:16:03 +0300

    Add a way to override Kotlin version from version catalog by kotlin_version property

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,3 +1,6 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 /*
 * Copyright 2014-2021 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
 */
@@ -17,6 +20,24 @@ dependencyResolutionManagement {
     versionCatalogs {
         val libs by creating {
             from(files("../gradle/libs.versions.toml"))
+
+            fun kotlinVersionFromProjectRootProperties(): String? {
+                val properties = Properties()
+                FileInputStream(file("../gradle.properties")).use {
+                    properties.load(it)
+                }
+                return properties["kotlin_version"]?.toString()
+            }
+
+            val kotlinVersion = if (extra.has("kotlin_version")) {
+                extra.get("kotlin_version").toString()
+            } else {
+                kotlinVersionFromProjectRootProperties()
+            }
+
+            if (kotlinVersion != null) {
+                version("kotlin-version", kotlinVersion)
+            }
         }
     }
 }
