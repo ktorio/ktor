b2df22e0b7b2e9dea4c9f67ffffae3e27d6c9650
Alexander Likhachev <Alexander.Likhachev@jetbrains.com>
2022-05-24 22:27:07 +0300

    Set Gradle Plugin API version attribute to select proper KGP variant
    Workaround for https://github.com/gradle/gradle/issues/20847

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -27,6 +27,15 @@ repositories {
 sourceSets.main {
 }
 
+configurations.configureEach {
+    if (isCanBeResolved) {
+        attributes {
+            @Suppress("UnstableApiUsage")
+            attribute(GradlePluginApiVersion.GRADLE_PLUGIN_API_VERSION_ATTRIBUTE, project.objects.named(GradleVersion.current().version))
+        }
+    }
+}
+
 dependencies {
     implementation(kotlin("gradle-plugin", "1.8.10"))
     implementation(kotlin("serialization", "1.8.10"))
