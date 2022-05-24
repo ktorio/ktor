c258dcc9b5fb75ec4e69edb26a698c37bd968d0c
Alexander Likhachev <alexander.likhachev@jetbrains.com>
2022-05-24 21:53:19 +0200

    Use Gradle version catalog as single source of truth

--- buildSrc/src/main/kotlin/TargetsConfig.kt
+++ buildSrc/src/main/kotlin/TargetsConfig.kt
@@ -26,6 +26,7 @@ val Project.hasNative: Boolean get() =
     hasCommon || hasNix || hasPosix || hasLinux || hasDarwin || hasDesktop || hasWindows
 
 fun Project.configureTargets() {
+    val coroutinesVersion = rootProject.versionCatalog.findVersion("coroutines-version").get().requiredVersion
     configureCommon()
     if (hasJvm) configureJvm()
 
@@ -73,6 +74,18 @@ fun Project.configureTargets() {
                 }
                 wasmJsTest {
                     dependsOn(jsAndWasmSharedTest)
+                }
+            }
+
+            val commonMain by getting {
+                dependencies {
+                    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+                }
+            }
+
+            val commonTest by getting {
+                dependencies {
+                    implementation(kotlin("test"))
                 }
             }
 
