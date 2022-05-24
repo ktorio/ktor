4115212533beca76c9361dc9f2e2f655ad428e20
Alexander Likhachev <Alexander.Likhachev@jetbrains.com>
2022-05-24 23:16:03 +0300

    Add a way to override Kotlin version from version catalog by kotlin_version property

--- build.gradle.kts
+++ build.gradle.kts
@@ -105,6 +105,8 @@ doctor {
     enableTestCaching = false
 }
 
+val kotlinVersion = libs.versions.kotlin.version.get()
+
 allprojects {
     group = "io.ktor"
     version = configuredVersion
@@ -129,6 +131,16 @@ allprojects {
 
     configurations {
         maybeCreate("testOutput")
+    }
+
+    configurations.configureEach {
+        if (isCanBeResolved) {
+            resolutionStrategy.eachDependency {
+                if (requested.group == "org.jetbrains.kotlin") {
+                    useVersion(kotlinVersion)
+                }
+            }
+        }
     }
 
     kotlin {
