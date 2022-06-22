8237baf3e622848e8d2abcf18bac3e69599bb1b6
mvicsokolova <maria.sokolova@jetbrains.com>
2022-06-22 14:08:11 +0300

    WA for broken atomicfu compiler plugin dependency in kotlin 1.7.0
    (See: https://github.com/Kotlin/kotlinx.atomicfu/issues/232)

--- build.gradle.kts
+++ build.gradle.kts
@@ -139,6 +139,35 @@ allprojects {
         }
     }
 
+    dependencies {
+        components {
+            withModule("org.jetbrains.kotlin:atomicfu") {
+                withVariant("runtimeElements") {
+                    val runtimeElementsVariant = this
+                    this@withModule.addVariant("runtimeJsIrElements") {
+                        attributes.attribute(
+                            org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType.attribute,
+                            org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType.js
+                        )
+                        attributes.attribute(
+                            org.jetbrains.kotlin.gradle.targets.js.KotlinJsCompilerAttribute.jsCompilerAttribute,
+                            org.jetbrains.kotlin.gradle.targets.js.KotlinJsCompilerAttribute.ir
+                        )
+                        attributes.attribute(Usage.USAGE_ATTRIBUTE, project.objects.named(Usage.JAVA_RUNTIME))
+                        runtimeElementsVariant.withFiles {
+                            val runtimeElementsFiles = this as org.gradle.api.internal.artifacts.repositories.resolver.DefaultMutableVariantFilesMetadata
+                            this@addVariant.withFiles {
+                                runtimeElementsFiles.files.forEach {
+                                    addFile(it.name)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     kotlin {
         targets.all {
 
