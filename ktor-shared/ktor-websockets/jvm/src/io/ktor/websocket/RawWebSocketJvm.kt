3013273c7324ca7c83b25c38cd641bc1f3427b5b
Sebastian Mayr <smayr@atlassian.com>
2022-10-14 15:58:40 -0400

    KTOR-5006: Close WS when reading fragmented control frames
    Fragmented control frames are explicitly disallowed by the websocket
    spec.

--- ktor-shared/ktor-websockets/jvm/src/io/ktor/websocket/RawWebSocketJvm.kt
+++ ktor-shared/ktor-websockets/jvm/src/io/ktor/websocket/RawWebSocketJvm.kt
@@ -70,6 +70,9 @@ internal class RawWebSocketJvm(
             } catch (cause: FrameTooBigException) {
                 outgoing.send(Frame.Close(CloseReason(CloseReason.Codes.TOO_BIG, cause.message)))
                 filtered.close(cause)
+            } catch (cause: ProtocolViolationException) {
+                outgoing.send(Frame.Close(CloseReason(CloseReason.Codes.PROTOCOL_ERROR, cause.message)))
+                filtered.close(cause)
             } catch (cause: CancellationException) {
                 reader.incoming.cancel(cause)
             } catch (cause: Throwable) {
