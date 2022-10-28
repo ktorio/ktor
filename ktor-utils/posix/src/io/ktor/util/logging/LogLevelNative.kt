9bb9e3037e1e7aca264f3485a4f4a960e0d844bb
Leonid Stashevsky <e5l@users.noreply.github.com>
2022-10-28 12:44:37 +0300

    KTOR-4998 Add Log Level Flag for Native (#3215)

---
+++ ktor-utils/posix/src/io/ktor/util/logging/LogLevelNative.kt
@@ -0,0 +1,14 @@
+// ktlint-disable filename
+/*
+ * Copyright 2014-2022 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package io.ktor.util.logging
+
+public enum class LogLevel {
+    TRACE,
+    DEBUG,
+    INFO,
+    WARN,
+    ERROR,
+}
