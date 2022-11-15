f30c23ecf598be65a48ccb01e6eeaf64ffc3fc11
Rustam <rxsinukov@gmail.com>
2022-11-15 14:23:23 +0100

    KTOR-5161 New client plugins API (#3244)

---
+++ ktor-client/ktor-client-core/common/src/io/ktor/client/plugins/api/ClientPluginInstance.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2014-2021 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package io.ktor.client.plugins.api
+
+import io.ktor.client.*
+import io.ktor.util.*
+import io.ktor.utils.io.core.*
+
+/**
+ * An instance of [ClientPlugin] that can be installed into [HttpClient].
+ */
+public class ClientPluginInstance<PluginConfig : Any> internal constructor(
+    internal val config: PluginConfig,
+    internal val name: String,
+    internal val body: ClientPluginBuilder<PluginConfig>.() -> Unit
+) : Closeable {
+
+    private var onClose: () -> Unit = {}
+
+    @InternalAPI
+    public fun install(scope: HttpClient) {
+        val pluginBuilder = ClientPluginBuilder(AttributeKey(name), scope, config).apply(body)
+        this.onClose = pluginBuilder.onClose
+        pluginBuilder.hooks.forEach { it.install(scope) }
+    }
+
+    override fun close() {
+        onClose.invoke()
+    }
+}
---
+++ ktor-client/ktor-client-core/common/src/io/ktor/client/plugins/api/CreatePluginUtils.kt
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2014-2021 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package io.ktor.client.plugins.api
+
+import io.ktor.client.*
+import io.ktor.client.plugins.*
+import io.ktor.util.*
+
+/**
+ * Client plugins factory.
+ */
+public interface ClientPlugin<PluginConfig : Any> : HttpClientPlugin<PluginConfig, ClientPluginInstance<PluginConfig>>
+
+/**
+ * Creates an [ClientPlugin] that can be installed into an [HttpClient].
+ *
+ * The example below creates a plugin that prints adds header to every request:
+ * ```
+ * class CustomHeaderPluginConfig {
+ *    var headerName: String = "X-Custom-Header"
+ *    var headerValue: String = "Custom-Header-Value"
+ * }
+ * val CustomHeaderPlugin = createClientPlugin("CustomHeaderPlugin", ::CustomHeaderPluginConfig) {
+ *     val headerName = pluginConfig.headerName
+ *     val headerValue = pluginConfig.headerValue
+ *
+ *     onRequest { request, _ ->
+ *        request.headers.append(headerName, headerValue)
+ *     }
+ * }
+ *
+ * client.install(CustomHeaderPlugin) {
+ *   headerName = "X-Another-Header"
+ *   headerValue = "Another-Header-Value"
+ * }
+ * ```
+ *
+ * @param name A name of a plugin that is used to get its instance.
+ * @param createConfiguration Defines how the initial [PluginConfigT] of your new plugin can be created.
+ * Note that it may be modified later when a user of your plugin calls [HttpClientConfig.install].
+ * @param body Allows you to define handlers ([onRequest], [onResponse], and so on) that
+ * can modify the behaviour of an [HttpClient] where your plugin is installed.
+ **/
+public fun <PluginConfigT : Any> createClientPlugin(
+    name: String,
+    createConfiguration: () -> PluginConfigT,
+    body: ClientPluginBuilder<PluginConfigT>.() -> Unit
+): ClientPlugin<PluginConfigT> =
+    object : ClientPlugin<PluginConfigT> {
+        override val key: AttributeKey<ClientPluginInstance<PluginConfigT>> = AttributeKey(name)
+
+        override fun prepare(block: PluginConfigT.() -> Unit): ClientPluginInstance<PluginConfigT> {
+            val config = createConfiguration().apply(block)
+            return ClientPluginInstance(config, name, body)
+        }
+
+        @OptIn(InternalAPI::class)
+        override fun install(plugin: ClientPluginInstance<PluginConfigT>, scope: HttpClient) {
+            plugin.install(scope)
+        }
+    }
+
+/**
+ * Creates an [ClientPlugin] with empty config that can be installed into an [HttpClient].
+ *
+ * The example below creates a plugin that p
+ * val CustomHeaderPlugin = createClientPlugin("CustomHeaderPlugin") { *
+ *     onRequest { request, _ ->
+ *        request.headers.append("X-Custom-Header", "Custom-Header-Value")
+ *     }
+ * }
+ *
+ * client.install(CustomHeaderPlugin)
+ * ```
+ *
+ * @param name A name of a plugin that is used to get its instance.
+ * @param body Allows you to define handlers ([onRequest], [onResponse], and so on) that
+ * can modify the behaviour of an [HttpClient] where your plugin is installed.
+ **/
+public fun createClientPlugin(
+    name: String,
+    body: ClientPluginBuilder<Unit>.() -> Unit
+): ClientPlugin<Unit> = createClientPlugin(name, {}, body)
