cfd44d1efa2e88915c323dd4abeaed0da560c7c9
Maria Skripchenko <maria.skripchenko@jetbrains.com>
2022-11-22 13:31:58 +0100

    add api doc

--- ktor-server/ktor-server-core/jvmAndNix/src/io/ktor/server/config/MergedApplicationConfig.kt
+++ ktor-server/ktor-server-core/jvmAndNix/src/io/ktor/server/config/MergedApplicationConfig.kt
@@ -4,20 +4,28 @@
 
 package io.ktor.server.config
 
-/**
- * Merge configuration combining all their keys.
- * If key is not found in one of the configs, search will continue in the next config in the list.
- */
 @Deprecated("Use mergeWith/mergeWithFallback instead.")
 public fun List<ApplicationConfig>.merge(): ApplicationConfig {
     require(isNotEmpty()) { "List of configs can not be empty" }
     return foldRight(last()) { config, acc -> config.mergeWithFallback(acc) }
 }
 
+/**
+ * Merge configuration combining all their keys.
+ * If the key is in both configs - the value from Second config will be used.
+ *
+ * @see [mergeWithFallback]
+ */
 public fun ApplicationConfig.mergeWith(other: ApplicationConfig): ApplicationConfig {
     return MergedApplicationConfig(other, this)
 }
 
+/**
+ * Merge configuration combining all their keys.
+ * If the key is in both configs - the value from First config will be used.
+ *
+ * @see [mergeWith]
+ */
 public fun ApplicationConfig.mergeWithFallback(other: ApplicationConfig): ApplicationConfig {
     return MergedApplicationConfig(this, other)
 }
