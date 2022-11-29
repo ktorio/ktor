68b674c66a937cd61f16af1c7e085d1feb9b9b7b
leonid.stashevsky <leonid.stashevsky@gmail.com>
2022-11-29 09:02:31 +0100

    Update Gradle and internal Kotlin version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,43 +3,46 @@
 */
 
 plugins {
-    `kotlin-dsl`
-    kotlin("plugin.serialization") version "1.6.21"
+    id("org.gradle.kotlin.kotlin-dsl") version "3.2.1"
+    kotlin("plugin.serialization") version "1.7.21"
 }
 
 val buildSnapshotTrain = properties["build_snapshot_train"]?.toString()?.toBoolean() == true
 
 repositories {
-    maven("https://plugins.gradle.org/m2")
-    maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
-
     if (buildSnapshotTrain) {
         mavenLocal()
     }
+
+    mavenCentral()
+    maven("https://plugins.gradle.org/m2")
+    maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
 }
 
 sourceSets.main {
 }
 
 dependencies {
-    implementation(kotlin("gradle-plugin", "1.6.21"))
-    implementation(kotlin("serialization", "1.6.21"))
+    implementation(kotlin("gradle-plugin", "1.7.21"))
+    implementation(kotlin("serialization", "1.7.21"))
 
     val ktlint_version = libs.versions.ktlint.version.get()
+    val ktor_test_version = libs.versions.ktor.test.version.get()
+
     implementation("org.jmailen.gradle:kotlinter-gradle:$ktlint_version")
 
-    implementation("io.ktor:ktor-server-default-headers:2.0.2")
-    implementation("io.ktor:ktor-server-netty:2.0.2")
-    implementation("io.ktor:ktor-server-cio:2.0.2")
-    implementation("io.ktor:ktor-server-jetty:2.0.2")
-    implementation("io.ktor:ktor-server-websockets:2.0.2")
-    implementation("io.ktor:ktor-server-auth:2.0.2")
-    implementation("io.ktor:ktor-server-caching-headers:2.0.2")
-    implementation("io.ktor:ktor-server-conditional-headers:2.0.2")
-    implementation("io.ktor:ktor-server-compression:2.0.2")
-    implementation("io.ktor:ktor-server-content-negotiation:2.0.2")
-    implementation("io.ktor:ktor-serialization-kotlinx:2.0.2")
-    implementation("io.ktor:ktor-network-tls-certificates:2.0.2")
+    implementation("io.ktor:ktor-server-default-headers:$ktor_test_version")
+    implementation("io.ktor:ktor-server-netty:$ktor_test_version")
+    implementation("io.ktor:ktor-server-cio:$ktor_test_version")
+    implementation("io.ktor:ktor-server-jetty:$ktor_test_version")
+    implementation("io.ktor:ktor-server-websockets:$ktor_test_version")
+    implementation("io.ktor:ktor-server-auth:$ktor_test_version")
+    implementation("io.ktor:ktor-server-caching-headers:$ktor_test_version")
+    implementation("io.ktor:ktor-server-conditional-headers:$ktor_test_version")
+    implementation("io.ktor:ktor-server-compression:$ktor_test_version")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktor_test_version")
+    implementation("io.ktor:ktor-serialization-kotlinx:$ktor_test_version")
+    implementation("io.ktor:ktor-network-tls-certificates:$ktor_test_version")
 
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.logback.classic)
--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -4,11 +4,12 @@
 pluginManagement {
     val build_snapshot_train: String? by settings
     repositories {
-        maven("https://plugins.gradle.org/m2")
         if (build_snapshot_train?.toBoolean() == true) {
             mavenLocal()
         }
 
+        maven("https://plugins.gradle.org/m2")
+        mavenCentral()
         maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
     }
 }
