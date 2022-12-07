85736c5fc91ac7fd9e231cae8719a99423d2e9ac
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2022-12-07 09:28:01 +0100

    Update webjars-jquery to 3.6.1

--- ktor-server/ktor-server-plugins/ktor-server-webjars/jvm/test/io/ktor/server/webjars/WebjarsTest.kt
+++ ktor-server/ktor-server-plugins/ktor-server-webjars/jvm/test/io/ktor/server/webjars/WebjarsTest.kt
@@ -120,7 +120,7 @@ class WebjarsTest {
         withTestApplication {
             application.install(Webjars)
 
-            handleRequest(HttpMethod.Get, "/webjars/jquery/3.3.1/jquery.js").let { call ->
+            handleRequest(HttpMethod.Get, "/webjars/jquery/3.6.1/jquery.js").let { call ->
                 assertEquals(HttpStatusCode.OK, call.response.status())
                 assertEquals("application/javascript", call.response.headers["Content-Type"])
             }
@@ -132,7 +132,7 @@ class WebjarsTest {
         withTestApplication {
             application.install(Webjars)
             application.install(ConditionalHeaders)
-            handleRequest(HttpMethod.Get, "/webjars/jquery/3.3.1/jquery.js").let { call ->
+            handleRequest(HttpMethod.Get, "/webjars/jquery/3.6.1/jquery.js").let { call ->
                 assertEquals(HttpStatusCode.OK, call.response.status())
                 assertEquals("application/javascript", call.response.headers["Content-Type"])
                 assertNotNull(call.response.headers["Last-Modified"])
