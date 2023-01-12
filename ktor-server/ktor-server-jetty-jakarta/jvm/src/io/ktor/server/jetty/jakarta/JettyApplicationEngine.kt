81cdf402ef97dfcf6278091031267e8e3b5410f9
Mariia Skripchenko <61115099+marychatte@users.noreply.github.com>
2023-01-12 11:05:44 +0100

    KTOR-5266 & KTOR-5267 Update Jetty to version 11 and Tomcat to version 10 (#3339)

---
+++ ktor-server/ktor-server-jetty-jakarta/jvm/src/io/ktor/server/jetty/jakarta/JettyApplicationEngine.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2014-2023 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package io.ktor.server.jetty.jakarta
+
+import io.ktor.server.engine.*
+import kotlinx.coroutines.*
+
+/**
+ * [ApplicationEngine] implementation for running in a standalone Jetty
+ */
+public class JettyApplicationEngine(
+    environment: ApplicationEngineEnvironment,
+    configure: Configuration.() -> Unit
+) : JettyApplicationEngineBase(environment, configure) {
+
+    private val dispatcher = server.threadPool.asCoroutineDispatcher()
+
+    override fun start(wait: Boolean): JettyApplicationEngine {
+        server.handler = JettyKtorHandler(environment, this::pipeline, dispatcher, configuration)
+        super.start(wait)
+        return this
+    }
+}
--- ktor-server/ktor-server-test-host/build.gradle.kts
+++ ktor-server/ktor-server-test-host/build.gradle.kts
@@ -17,17 +17,13 @@ kotlin.sourceSets {
         dependencies {
             api(project(":ktor-network:ktor-network-tls"))
 
-            api(project(":ktor-client:ktor-client-jetty"))
+            api(project(":ktor-client:ktor-client-apache"))
             api(project(":ktor-network:ktor-network-tls:ktor-network-tls-certificates"))
             api(project(":ktor-server:ktor-server-plugins:ktor-server-call-logging"))
 
             // Not ideal, but prevents an additional artifact, and this is usually just included for testing,
             // so shouldn"t increase the size of the final artifact.
             api(project(":ktor-server:ktor-server-plugins:ktor-server-websockets"))
-
-            api(libs.jetty.http2.client)
-            api(libs.jetty.client)
-            api(libs.jetty.http2.client.transport)
 
             if (jetty_alpn_boot_version != null) {
                 api(libs.jetty.alpn.boot)
