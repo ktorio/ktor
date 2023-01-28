1252462361a177f3d67a811d46668d6848692798
Sergey Prytkov <sergey@razz.team>
2023-01-28 11:14:28 +0300

    KTOR-5493 formbuilder append collection

--- ktor-client/ktor-client-tests/common/test/io/ktor/client/tests/ContentTest.kt
+++ ktor-client/ktor-client-tests/common/test/io/ktor/client/tests/ContentTest.kt
@@ -185,6 +185,8 @@ class ContentTest : ClientLoader(5 * 60) {
                 }
                 append("hello", 5)
                 append("world", true)
+                append("world", true)
+                append("engines[]", listOf("Jvm", "Js", "Native"))
             }
         }
 
