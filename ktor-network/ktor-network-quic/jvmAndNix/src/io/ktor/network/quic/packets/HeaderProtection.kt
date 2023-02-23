57566b7e0daedb9755ecf0c8c91d04a1ad3a514a
Alexander Sysoev <58986158+Mr3zee@users.noreply.github.com>
2023-02-23 09:57:22 +0100

    [QUIC] QUIC packetezation (#3414)
    * [QUIC] prototype packet header reader
    
    * [QUIC] completed packet reader (without TLS impl)
    
    * [QUIC] minor improvements
    
    * [QUIC] added explicit types to vars
    
    * [QUIC] added PacketWriter
    
    * [QUIC] bug fixing
    
    * [QUIC] added tests for packet number utils
    
    * [QUIC] fix typo
    
    * [QUIC] formatKotlin
    
    * [QUIC] fix native compilation
    
    * [QUIC] fix unused parameters warning
    
    * [QUIC] fix macos and linux compilation
    
    * [QUIC] fix review comments
    
    ---------
    
    Co-authored-by: Alexander Sysoev <Alexander.Sysoev@jetbrains.com>

---
+++ ktor-network/ktor-network-quic/jvmAndNix/src/io/ktor/network/quic/packets/HeaderProtection.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2014-2023 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+@file:Suppress("UNUSED_PARAMETER")
+
+package io.ktor.network.quic.packets
+
+import io.ktor.network.quic.bytes.*
+
+@Suppress("NOTHING_TO_INLINE")
+internal object HeaderProtection {
+    const val HP_FLAGS_LONG_MASK: UInt8 = 0x0Fu
+    const val HP_FLAGS_SHORT_MASK: UInt8 = 0x1Fu
+
+    /**
+     * Returns a part of the header protection mask, that applies to first byte of packet's header
+     *
+     * @param headerMask - 0x1Fu for Short headers, 0x0Fu for Long headers
+     */
+    inline fun flagsHPMask(hp: Long, headerMask: UInt8): UInt8 = (hp ushr 4).toUByte() and headerMask
+
+    /**
+     * Returns a part of the header protection mask, that applies to encrypted packet number with the length of 1 byte
+     */
+    inline fun pnHPMask1(hp: Long): UInt8 = (hp ushr 3).toUByte()
+
+    /**
+     * Returns a part of the header protection mask, that applies to encrypted packet number with the length of 2 bytes
+     */
+    inline fun pnHPMask2(hp: Long): UInt16 = (hp ushr 2).toUShort()
+
+    /**
+     * Returns a part of the header protection mask, that applies to encrypted packet number with the length of 3 bytes
+     */
+    inline fun pnHPMask3(hp: Long): UInt32 = (hp ushr 1).toUInt() and 0x00FFFFFFu
+
+    /**
+     * Returns a part of the header protection mask, that applies to encrypted packet number with the length of 4 bytes
+     */
+    inline fun pnHPMask4(hp: Long): UInt32 = hp.toUInt()
+
+    fun headerProtection(
+        headerProtectionKey: String,
+        sample: ByteArray,
+    ): Long {
+        TODO("crypto")
+    }
+}
