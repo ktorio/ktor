a8cd54bfc704e49219d84917d0372e0c48e4274c
Maria Skripchenko <maria.skripchenko@jetbrains.com>
2023-03-08 16:55:56 +0100

    KTOR-5595 Fix ClassCastException when DropwizardMetrics with MicrometerMetrics

--- ktor-server/ktor-server-plugins/ktor-server-metrics-micrometer/jvm/test/io/ktor/server/metrics/micrometer/MicrometerMetricsTests.kt
+++ ktor-server/ktor-server-plugins/ktor-server-metrics-micrometer/jvm/test/io/ktor/server/metrics/micrometer/MicrometerMetricsTests.kt
@@ -4,8 +4,11 @@
 
 package io.ktor.server.metrics.micrometer
 
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.metrics.dropwizard.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.testing.*
@@ -377,6 +380,25 @@ class MicrometerMetricsTests {
             assertEquals(gauge, configurableGauge)
         }
     }
+
+    @Test
+    fun `with DropwizardMetrics plugin`(): Unit = testApplication {
+        application {
+            install(MicrometerMetrics)
+            install(DropwizardMetrics)
+
+            routing {
+                get("/") {
+                    call.respondText { "OK" }
+                }
+            }
+        }
+
+        val response = client.get("/")
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals("OK", response.bodyAsText())
+    }
+
 
     private fun TestApplicationEngine.metersAreRegistered(
         meterBinder: KClass<out MeterBinder>,
