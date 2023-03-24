3847c263f6827ee14ae7fd302be60ae9fcff1cca
Anastasiia Spaseeva <anastasiia.spaseeva@jetbrains.com>
2023-03-24 11:07:55 +0100

    Commmunity project build: adopt build scripts for K2 build infrastructure
    add dependency on gradle parameter kotlin_repo_url everywhere next to maven kotlin/dev (required for downloading kotlin artifacts on CI/CD directly from the build
    
    parametrize by gradle parameters kotlin language/api versions
    
    Use kotlin 1.8.20-RC2 by default

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,12 +47,22 @@ buildscript {
     // This flag is also used in settings.gradle to exclude native-only projects
     extra["native_targets_enabled"] = rootProject.properties["disable_native_targets"] == null
 
+    extra["kotlin_repo_url"] = rootProject.properties["kotlin_repo_url"]
+    val kotlin_repo_url: String? by extra
+
     repositories {
         mavenLocal()
         mavenCentral()
         google()
         gradlePluginPortal()
         maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
+        if (kotlin_repo_url != null) {
+            maven(kotlin_repo_url!!)
+        }
+    }
+
+    dependencies {
+        classpath("org.jetbrains.kotlinx:atomicfu-gradle-plugin:${rootProject.properties["atomicfu_version"]}")
     }
 }
 
@@ -116,11 +126,17 @@ allprojects {
 
     setupTrainForSubproject()
 
+    extra["kotlin_repo_url"] = rootProject.properties["kotlin_repo_url"]
+    val kotlin_repo_url: String? by extra
+
     repositories {
         mavenLocal()
         mavenCentral()
         maven(url = "https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
         maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
+        if (kotlin_repo_url != null) {
+            maven(kotlin_repo_url!!)
+        }
     }
 
     val nonDefaultProjectStructure: List<String> by rootProject.extra
@@ -141,6 +157,24 @@ allprojects {
                 if (requested.group == "org.jetbrains.kotlin") {
                     useVersion(kotlinVersion)
                 }
+            }
+        }
+    }
+
+    configurations.all {
+        if (name == "metadataCompileClasspath") return@all
+        resolutionStrategy.dependencySubstitution.all {
+            if (name == "metadataCompileClasspath") return@all
+            val requestedComponent = requested
+            if (requestedComponent is ModuleComponentSelector &&
+                requestedComponent.group == "org.jetbrains.kotlin" &&
+                requestedComponent.module == "atomicfu"
+            ) {
+                if (name == "metadataCompileClasspath") return@all
+                useTarget(
+                    "${requestedComponent.group}:kotlinx-atomicfu-runtime:$kotlinVersion",
+                    "Using proper kotlinx-atomicfu-runtime instead of Gradle plugin"
+                )
             }
         }
     }
@@ -222,6 +256,14 @@ fun KotlinMultiplatformExtension.setCompilationOptions() {
 }
 
 fun KotlinMultiplatformExtension.configureSourceSets() {
+
+    extra["kotlin_language_version"] = rootProject.properties["kotlin_language_version"]
+    val kotlin_language_version: String? by extra
+
+    extra["kotlin_api_version"] = rootProject.properties["kotlin_api_version"]
+    val kotlin_api_version: String? by extra
+
+
     sourceSets
         .matching { it.name !in listOf("main", "test") }
         .all {
@@ -234,6 +276,9 @@ fun KotlinMultiplatformExtension.configureSourceSets() {
 
             languageSettings.apply {
                 progressiveMode = true
+                languageVersion = kotlin_language_version
+                apiVersion = kotlin_api_version
+                optIn("kotlinx.cinterop.ExperimentalForeignApi")
             }
         }
 }
