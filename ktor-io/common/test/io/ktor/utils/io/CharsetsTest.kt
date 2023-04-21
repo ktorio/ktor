9dd309af2d9b7a296f13dabb23b6ee13bc659048
Maria Dumanskaya <maria.dumanskaya@jetbrains.com>
2023-04-21 11:56:02 +0200

    fix test because of KT-57661

--- ktor-io/common/test/io/ktor/utils/io/CharsetsTest.kt
+++ ktor-io/common/test/io/ktor/utils/io/CharsetsTest.kt
@@ -6,30 +6,30 @@ import kotlin.test.*
 class CharsetsTest {
     @Test
     fun testUtf8() {
-        Charset.forName("UTF-8").newEncoder().encode("test").release()
+        charsetForName("UTF-8").newEncoder().encode("test").release()
     }
 
     @Test
     fun testISO() {
-        Charset.forName("UTF-8").newEncoder().encode("test").release()
+        charsetForName("UTF-8").newEncoder().encode("test").release()
     }
 
     @Test
     fun testLatin1() {
-        Charset.forName("Latin1").newEncoder().encode("test").release()
+        charsetForName("Latin1").newEncoder().encode("test").release()
     }
 
     @Test
     fun testNonExisting() {
         assertFailsWith<IllegalArgumentException> {
-            Charset.forName("abracadabra-encoding")
+            charsetForName("abracadabra-encoding")
         }
     }
 
     @Test
     fun testIllegal() {
         assertFailsWith<IllegalArgumentException> {
-            Charset.forName("%s")
+            charsetForName("%s")
         }
     }
 }
