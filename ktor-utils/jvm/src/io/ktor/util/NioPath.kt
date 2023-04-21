00c25d9910085fed28a67676ae09ea59c2e224b1
Rustam <rxsinukov@gmail.com>
2023-04-21 12:11:39 +0200

    KTOR-4275 API to use java.nio.Path as resources (#3558)

--- ktor-utils/jvm/src/io/ktor/util/NioPath.kt
+++ ktor-utils/jvm/src/io/ktor/util/NioPath.kt
@@ -8,24 +8,17 @@ import java.io.*
 import java.nio.file.*
 
 /**
- * Finds an extension of the given Path
- *
- * Extension is a substring of a [Path.getFileName] after last dot
- */
-public val Path.extension: String get() = fileName.toString().substringAfterLast(".")
-
-/**
  * Append a [relativePath] safely that means that adding any extra `..` path elements will not let
  * access anything out of the reference directory (unless you have symbolic or hard links or multiple mount points)
  */
-public fun Path.combineSafe(relativePath: Path): File {
+public fun Path.combineSafe(relativePath: Path): Path {
     val normalized = relativePath.normalizeAndRelativize()
     if (normalized.startsWith("..")) {
         throw InvalidPathException(relativePath.toString(), "Relative path $relativePath beginning with .. is invalid")
     }
     check(!normalized.isAbsolute) { "Bad relative path $relativePath" }
 
-    return resolve(normalized).toFile()
+    return resolve(normalized)
 }
 
 /**
