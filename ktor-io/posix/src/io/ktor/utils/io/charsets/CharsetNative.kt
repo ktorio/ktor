4538471613ece4fd37a84488ce8b058c860312d8
leonid.stashevsky <leonid.stashevsky@gmail.com>
2023-04-27 11:57:16 +0200

    KTOR-5824 Drop extension on the missing companion

--- ktor-io/posix/src/io/ktor/utils/io/charsets/CharsetNative.kt
+++ ktor-io/posix/src/io/ktor/utils/io/charsets/CharsetNative.kt
@@ -7,12 +7,22 @@ import kotlinx.cinterop.*
 import platform.iconv.*
 import platform.posix.*
 
+/**
+ * Find a charset by name.
+ */
+public actual fun Charsets.forName(name: String): Charset = Charset.forName(name)
+
+/**
+ * Check if a charset is supported by the current platform.
+ */
+public actual fun Charsets.isSupported(name: String): Boolean = Charset.isSupported(name)
+
 public actual abstract class Charset(internal val _name: String) {
     public actual abstract fun newEncoder(): CharsetEncoder
     public actual abstract fun newDecoder(): CharsetDecoder
 
-    public actual companion object {
-        public actual fun forName(name: String): Charset {
+    public companion object {
+        public fun forName(name: String): Charset {
             if (name == "UTF-8" || name == "utf-8" || name == "UTF8" || name == "utf8") return Charsets.UTF_8
             if (name == "ISO-8859-1" || name == "iso-8859-1" || name == "ISO_8859_1") return Charsets.ISO_8859_1
             if (name == "UTF-16" || name == "utf-16" || name == "UTF16" || name == "utf16") return Charsets.UTF_16
@@ -20,7 +30,7 @@ public actual abstract class Charset(internal val _name: String) {
             return CharsetImpl(name)
         }
 
-        public actual fun isSupported(charset: String): Boolean = when (charset) {
+        public fun isSupported(charset: String): Boolean = when (charset) {
             "UTF-8", "utf-8", "UTF8", "utf8" -> true
             "ISO-8859-1", "iso-8859-1" -> true
             "UTF-16", "utf-16", "UTF16", "utf16" -> true
@@ -31,10 +41,7 @@ public actual abstract class Charset(internal val _name: String) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other !is Charset) return false
-
-        if (_name != other._name) return false
-
-        return true
+        return _name == other._name
     }
 
     override fun hashCode(): Int {
