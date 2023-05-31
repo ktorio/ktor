7cbeaea129007e8ac7645426bd52f1c5e9d9da86
Alexander Shabalin <projedi666@gmail.com>
2023-05-31 09:57:25 +0200

    Remove deprecated Kotlin/Native targets. (#3634)
    iosArm32 and watchosX86 were deprecated in Kotlin 1.8.20 and will
    be removed in Kotlin 1.9.20.
    See https://kotl.in/native-targets-tiers for details.

--- buildSrc/src/main/kotlin/NativeUtils.kt
+++ buildSrc/src/main/kotlin/NativeUtils.kt
@@ -37,7 +37,6 @@ fun Project.iosTargets(): List<String> = fastOr {
         listOf(
             iosX64(),
             iosArm64(),
-            iosArm32(),
             iosSimulatorArm64(),
         ).map { it.name }
     }
@@ -46,7 +45,6 @@ fun Project.iosTargets(): List<String> = fastOr {
 fun Project.watchosTargets(): List<String> = fastOr {
     with(kotlin) {
         listOf(
-            watchosX86(),
             watchosX64(),
             watchosArm32(),
             watchosArm64(),
