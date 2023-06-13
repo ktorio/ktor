07ef64d7928b8c7e3edd073a2c11a936e035463d
Maria Dumanskaya <maria.dumanskaya@jetbrains.com>
2023-06-13 18:58:11 +0200

    add more workaround for KT-57661

--- ktor-io/posix/src/io/ktor/utils/io/charsets/CharsetNative.kt
+++ ktor-io/posix/src/io/ktor/utils/io/charsets/CharsetNative.kt
@@ -11,23 +11,6 @@ public actual abstract class Charset(internal val _name: String) {
     public actual abstract fun newEncoder(): CharsetEncoder
     public actual abstract fun newDecoder(): CharsetDecoder
 
-    public actual companion object {
-        public actual fun forName(name: String): Charset {
-            if (name == "UTF-8" || name == "utf-8" || name == "UTF8" || name == "utf8") return Charsets.UTF_8
-            if (name == "ISO-8859-1" || name == "iso-8859-1" || name == "ISO_8859_1") return Charsets.ISO_8859_1
-            if (name == "UTF-16" || name == "utf-16" || name == "UTF16" || name == "utf16") return Charsets.UTF_16
-
-            return CharsetImpl(name)
-        }
-
-        public actual fun isSupported(charset: String): Boolean = when (charset) {
-            "UTF-8", "utf-8", "UTF8", "utf8" -> true
-            "ISO-8859-1", "iso-8859-1" -> true
-            "UTF-16", "utf-16", "UTF16", "utf16" -> true
-            else -> false
-        }
-    }
-
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other !is Charset) return false
@@ -44,6 +27,21 @@ public actual abstract class Charset(internal val _name: String) {
     override fun toString(): String {
         return _name
     }
+}
+
+public actual fun charsetForName(name: String): Charset {
+    if (name == "UTF-8" || name == "utf-8" || name == "UTF8" || name == "utf8") return Charsets.UTF_8
+    if (name == "ISO-8859-1" || name == "iso-8859-1" || name == "ISO_8859_1") return Charsets.ISO_8859_1
+    if (name == "UTF-16" || name == "utf-16" || name == "UTF16" || name == "utf16") return Charsets.UTF_16
+
+    return CharsetImpl(name)
+}
+
+public actual fun isCharsetSupported(charset: String): Boolean = when (charset) {
+    "UTF-8", "utf-8", "UTF8", "utf8" -> true
+    "ISO-8859-1", "iso-8859-1" -> true
+    "UTF-16", "utf-16", "UTF16", "utf16" -> true
+    else -> false
 }
 
 private class CharsetImpl(name: String) : Charset(name) {
