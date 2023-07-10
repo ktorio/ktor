5bdf7f63edaff57c5c3917ecb4f2268489c1bade
Rustam <rxsinukov@gmail.com>
2023-07-10 15:09:41 +0200

    KTOR-6073 Webjars plugin should include caching headers and ETag by default (#3681)

--- ktor-server/ktor-server-plugins/ktor-server-webjars/jvm/src/io/ktor/server/webjars/WebjarsUtils.kt
+++ ktor-server/ktor-server-plugins/ktor-server-webjars/jvm/src/io/ktor/server/webjars/WebjarsUtils.kt
@@ -10,18 +10,18 @@ package io.ktor.server.webjars
 
 import io.ktor.http.*
 import io.ktor.http.content.*
-import io.ktor.server.application.*
-import io.ktor.server.request.*
-import io.ktor.server.response.*
 import io.ktor.util.cio.*
 import io.ktor.util.date.*
 import io.ktor.utils.io.*
 import io.ktor.utils.io.jvm.javaio.*
 import org.webjars.*
 import java.io.*
-import java.time.*
 
-internal fun extractWebJar(path: String, knownWebJars: Set<String>, locator: WebJarAssetLocator): String {
+internal fun extractWebJar(
+    path: String,
+    knownWebJars: Set<String>,
+    locator: WebJarAssetLocator
+): Pair<String, WebJarAssetLocator.WebJarInfo> {
     val firstDelimiter = if (path.startsWith("/")) 1 else 0
     val nextDelimiter = path.indexOf("/", 1)
     val webjar = if (nextDelimiter > -1) path.substring(firstDelimiter, nextDelimiter) else ""
@@ -29,17 +29,14 @@ internal fun extractWebJar(path: String, knownWebJars: Set<String>, locator: Web
     if (webjar !in knownWebJars) {
         throw IllegalArgumentException("jar $webjar not found")
     }
-    return locator.getFullPath(webjar, partialPath)
+    val info = locator.allWebJars[webjar] ?: throw IllegalArgumentException("jar $webjar not found")
+    return locator.getFullPath(webjar, partialPath) to info
 }
 
 internal class InputStreamContent(
-    val input: InputStream,
-    override val contentType: ContentType,
-    lastModified: GMTDate
+    private val input: InputStream,
+    override val contentType: ContentType
 ) : OutgoingContent.ReadChannelContent() {
-    init {
-        versions += LastModifiedVersion(lastModified)
-    }
 
     override fun readFrom(): ByteReadChannel = input.toByteReadChannel(pool = KtorDefaultPool)
 }
