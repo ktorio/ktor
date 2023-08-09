c1fd64c0289fa587207e95ebd220ea4d39c14d72
Leonid Stashevsky <e5l@users.noreply.github.com>
2023-08-09 17:05:38 +0300

    KTOR-6122 Add type to AttributeKey equals (#3718)
    * KTOR-6122 Add type to AttributeKey equals

--- ktor-utils/common/src/io/ktor/util/Attributes.kt
+++ ktor-utils/common/src/io/ktor/util/Attributes.kt
@@ -4,12 +4,26 @@
 
 package io.ktor.util
 
+import kotlin.reflect.*
+
 /**
  * Specifies a key for an attribute in [Attributes]
  * @param T is a type of the value stored in the attribute
  * @param name is a name of the attribute for diagnostic purposes. Can't be blank
  */
-public class AttributeKey<T : Any>(public val name: String) {
+public inline fun <reified T : Any> AttributeKey(name: String): AttributeKey<T> =
+    AttributeKey(name, T::class.toString())
+
+/**
+ * Specifies a key for an attribute in [Attributes]
+ * @param T is a type of the value stored in the attribute
+ * @param name is a name of the attribute for diagnostic purposes. Can't be blank
+ */
+
+public class AttributeKey<T : Any> @PublishedApi internal constructor(
+    public val name: String,
+    private val type: String
+) {
     init {
         if (name.isEmpty()) {
             throw IllegalStateException("Name can't be blank")
@@ -20,13 +34,9 @@ public class AttributeKey<T : Any>(public val name: String) {
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
-        if (other == null || this::class != other::class) return false
+        if (other == null || other !is AttributeKey<*>) return false
 
-        other as AttributeKey<*>
-
-        if (name != other.name) return false
-
-        return true
+        return name == other.name && type == other.type
     }
 
     override fun hashCode(): Int {
