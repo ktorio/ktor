06359f9de0aecaddf6967549b17714164d210515
Pavel Kargashinsky <pavel.kargashinsky@jetbrains.com>
2023-08-16 21:31:55 +0200

    1.9.20 migration: Supress NO_ACTUAL_FOR_EXPECT, KT-61039

--- ktor-utils/js/src/io/ktor/util/ContentEncodersJs.kt
+++ ktor-utils/js/src/io/ktor/util/ContentEncodersJs.kt
@@ -9,6 +9,7 @@ package io.ktor.util
 /**
  * Implementation of [ContentEncoder] using gzip algorithm
  */
+@Suppress("NO_ACTUAL_FOR_EXPECT")
 public actual object GZipEncoder : ContentEncoder, Encoder by Identity {
     actual override val name: String = "gzip"
 }
@@ -16,6 +17,7 @@ public actual object GZipEncoder : ContentEncoder, Encoder by Identity {
 /**
  * Implementation of [ContentEncoder] using deflate algorithm
  */
+@Suppress("NO_ACTUAL_FOR_EXPECT")
 public actual object DeflateEncoder : ContentEncoder, Encoder by Identity {
     actual override val name: String = "deflate"
 }
