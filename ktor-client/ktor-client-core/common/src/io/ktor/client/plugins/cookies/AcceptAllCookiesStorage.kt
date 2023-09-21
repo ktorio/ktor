9c5f5713de057a780843da83e00ed4f1422b7875
rsinukov <rxsinukov@gmail.com>
2023-09-21 20:42:46 +0200

    KTOR-2023 AcceptAllCookiesStorage ignores cookie's max-age

--- ktor-client/ktor-client-core/common/src/io/ktor/client/plugins/cookies/AcceptAllCookiesStorage.kt
+++ ktor-client/ktor-client-core/common/src/io/ktor/client/plugins/cookies/AcceptAllCookiesStorage.kt
@@ -5,7 +5,6 @@
 package io.ktor.client.plugins.cookies
 
 import io.ktor.http.*
-import io.ktor.util.collections.*
 import io.ktor.util.date.*
 import kotlinx.atomicfu.*
 import kotlinx.coroutines.sync.*
@@ -14,29 +13,38 @@ import kotlin.math.*
 /**
  * [CookiesStorage] that stores all the cookies in an in-memory map.
  */
-@Suppress("DEPRECATION")
-public class AcceptAllCookiesStorage : CookiesStorage {
-    private val container: MutableList<Cookie> = mutableListOf()
+public class AcceptAllCookiesStorage(private val clock: () -> Long = { getTimeMillis() }) : CookiesStorage {
+
+    private data class CookieWithTimestamp(val cookie: Cookie, val createdAt: Long)
+
+    private val container: MutableList<CookieWithTimestamp> = mutableListOf()
     private val oldestCookie: AtomicLong = atomic(0L)
     private val mutex = Mutex()
 
     override suspend fun get(requestUrl: Url): List<Cookie> = mutex.withLock {
-        val now = getTimeMillis()
+        val now = clock()
         if (now >= oldestCookie.value) cleanup(now)
 
-        return@withLock container.filter { it.matches(requestUrl) }
+        val cookies = container.filter { it.cookie.matches(requestUrl) }.map { it.cookie }
+        return@withLock cookies
     }
 
-    override suspend fun addCookie(requestUrl: Url, cookie: Cookie): Unit = mutex.withLock {
+    override suspend fun addCookie(requestUrl: Url, cookie: Cookie) {
         with(cookie) {
-            if (name.isBlank()) return@withLock
+            if (name.isBlank()) return
         }
 
-        container.removeAll { it.name == cookie.name && it.matches(requestUrl) }
-        container.add(cookie.fillDefaults(requestUrl))
-        cookie.expires?.timestamp?.let { expires ->
-            if (oldestCookie.value > expires) {
-                oldestCookie.value = expires
+        mutex.withLock {
+            container.removeAll { (existingCookie, _) ->
+                existingCookie.name == cookie.name && existingCookie.matches(requestUrl)
+            }
+            val createdAt = clock()
+            container.add(CookieWithTimestamp(cookie.fillDefaults(requestUrl), createdAt))
+
+            cookie.maxAgeOrExpires(createdAt)?.let {
+                if (oldestCookie.value > it) {
+                    oldestCookie.value = it
+                }
             }
         }
     }
@@ -45,15 +53,18 @@ public class AcceptAllCookiesStorage : CookiesStorage {
     }
 
     private fun cleanup(timestamp: Long) {
-        container.removeAll { cookie ->
-            val expires = cookie.expires?.timestamp ?: return@removeAll false
+        container.removeAll { (cookie, createdAt) ->
+            val expires = cookie.maxAgeOrExpires(createdAt) ?: return@removeAll false
             expires < timestamp
         }
 
-        val newOldest = container.fold(Long.MAX_VALUE) { acc, cookie ->
-            cookie.expires?.timestamp?.let { min(acc, it) } ?: acc
+        val newOldest = container.fold(Long.MAX_VALUE) { acc, (cookie, createdAt) ->
+            cookie.maxAgeOrExpires(createdAt)?.let { min(acc, it) } ?: acc
         }
 
         oldestCookie.value = newOldest
     }
+
+    private fun Cookie.maxAgeOrExpires(createdAt: Long): Long? =
+        maxAge?.let { createdAt + it * 1000 } ?: expires?.timestamp
 }
