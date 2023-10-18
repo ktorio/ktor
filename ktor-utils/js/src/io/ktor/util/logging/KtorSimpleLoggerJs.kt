127a0ca21d89537e410e97ce60e287c958542ca8
Rustam <rxsinukov@gmail.com>
2023-10-18 14:59:29 +0200

    KTOR-6275 Ktor JS client unconfigurable logging in node (#3783)

--- ktor-utils/js/src/io/ktor/util/logging/KtorSimpleLoggerJs.kt
+++ ktor-utils/js/src/io/ktor/util/logging/KtorSimpleLoggerJs.kt
@@ -4,45 +4,67 @@
 
 package io.ktor.util.logging
 
+import io.ktor.util.*
+
 @Suppress("FunctionName")
 public actual fun KtorSimpleLogger(name: String): Logger = object : Logger {
+
+    override val level: LogLevel = when (PlatformUtils.IS_NODE) {
+        true -> runCatching { js("process.env.KTOR_LOG_LEVEL") as String? }
+            .getOrNull()
+            ?.let { rawLevel: String -> LogLevel.entries.firstOrNull { it.name == rawLevel } }
+            ?: LogLevel.INFO
+
+        false -> LogLevel.TRACE
+    }
+
     override fun error(message: String) {
+        if (level > LogLevel.ERROR) return
         console.error(message)
     }
 
     override fun error(message: String, cause: Throwable) {
+        if (level > LogLevel.ERROR) return
         console.error("$message, cause: $cause")
     }
 
     override fun warn(message: String) {
+        if (level > LogLevel.WARN) return
         console.warn(message)
     }
 
     override fun warn(message: String, cause: Throwable) {
+        if (level > LogLevel.WARN) return
         console.warn("$message, cause: $cause")
     }
 
     override fun info(message: String) {
+        if (level > LogLevel.INFO) return
         console.info(message)
     }
 
     override fun info(message: String, cause: Throwable) {
+        if (level > LogLevel.INFO) return
         console.info("$message, cause: $cause")
     }
 
     override fun debug(message: String) {
+        if (level > LogLevel.DEBUG) return
         console.debug("DEBUG: $message")
     }
 
     override fun debug(message: String, cause: Throwable) {
+        if (level > LogLevel.DEBUG) return
         console.debug("DEBUG: $message, cause: $cause")
     }
 
     override fun trace(message: String) {
+        if (level > LogLevel.TRACE) return
         console.debug("TRACE: $message")
     }
 
     override fun trace(message: String, cause: Throwable) {
+        if (level > LogLevel.TRACE) return
         console.debug("TRACE: $message, cause: $cause")
     }
 }
