4913e3db9f7844195d92578faaa07eaf9822d05f
Rustam <rxsinukov@gmail.com>
2023-11-08 12:28:38 +0100

    KTOR-6318 Calculate session encryption mac over encrypted value (#3814)

--- ktor-server/ktor-server-tests/jvm/test/io/ktor/server/sessions/SessionTestJvm.kt
+++ ktor-server/ktor-server-tests/jvm/test/io/ktor/server/sessions/SessionTestJvm.kt
@@ -59,7 +59,7 @@ class SessionTestJvm {
                 assertEquals(
                     "00112233445566778899aabbccddeeff/" +
                         "c3850fc1ddc62f71ec5eaad6d393b91fa809fe32a1cf0cb4730788c5a489daef:" +
-                        "51a5e9fcd1c91418f9a623bafa5022a524348e44244265dc0cab2cebacc28a5d",
+                        "65f0bd15bfa7e96b8bd9e134e23a24860b324e361c606e6800af050f5d104b68",
                     sessionCookie!!.value
                 )
             }
@@ -72,6 +72,115 @@ class SessionTestJvm {
 
             handleRequest(HttpMethod.Get, "/4") {
                 addHeader(HttpHeaders.Cookie, "$cookieName=abc/abc:abc")
+            }.let { call ->
+                assertEquals("ok:null", call.response.content)
+            }
+
+            commonSignedChecks()
+        }
+    }
+
+    @Test
+    fun testSessionEncryptedBackwardCompatible() {
+        val encryptKey = hex("00112233445566778899aabbccddeeff")
+        val signKey = hex("02030405060708090a0b0c")
+        val forcedIvForTesting = hex("00112233445566778899aabbccddeeff")
+
+        withTestApplication {
+            application.install(Sessions) {
+                cookie<TestUserSession>(cookieName) {
+                    transform(
+                        SessionTransportTransformerEncrypt(
+                            encryptKey,
+                            signKey,
+                            { forcedIvForTesting },
+                            backwardCompatibleRead = true
+                        )
+                    )
+                }
+            }
+
+            application.routing {
+                get("/3") {
+                    call.sessions.set(TestUserSession("id2", emptyList()))
+                    call.respondText("ok")
+                }
+                get("/4") {
+                    call.respondText("ok:" + call.sessions.get<TestUserSession>()?.userId)
+                }
+            }
+
+            handleRequest(HttpMethod.Get, "/3").let { call ->
+                val sessionCookie = call.response.cookies[cookieName]
+                assertEquals(
+                    "00112233445566778899aabbccddeeff/" +
+                        "c3850fc1ddc62f71ec5eaad6d393b91fa809fe32a1cf0cb4730788c5a489daef:" +
+                        "65f0bd15bfa7e96b8bd9e134e23a24860b324e361c606e6800af050f5d104b68",
+                    sessionCookie!!.value
+                )
+            }
+
+            handleRequest(HttpMethod.Get, "/4") {
+                addHeader(
+                    HttpHeaders.Cookie,
+                    "$cookieName=" + "00112233445566778899aabbccddeeff/" +
+                        "c3850fc1ddc62f71ec5eaad6d393b91fa809fe32a1cf0cb4730788c5a489daef:" +
+                        "51a5e9fcd1c91418f9a623bafa5022a524348e44244265dc0cab2cebacc28a5d"
+                )
+            }.let { call ->
+                assertEquals("ok:id2", call.response.content)
+            }
+
+            commonSignedChecks()
+        }
+    }
+
+    @Test
+    fun testSessionEncryptedNotBackwardCompatible() {
+        val encryptKey = hex("00112233445566778899aabbccddeeff")
+        val signKey = hex("02030405060708090a0b0c")
+        val forcedIvForTesting = hex("00112233445566778899aabbccddeeff")
+
+        withTestApplication {
+            application.install(Sessions) {
+                cookie<TestUserSession>(cookieName) {
+                    transform(
+                        SessionTransportTransformerEncrypt(
+                            encryptKey,
+                            signKey,
+                            { forcedIvForTesting },
+                        )
+                    )
+                }
+            }
+
+            application.routing {
+                get("/3") {
+                    call.sessions.set(TestUserSession("id2", emptyList()))
+                    call.respondText("ok")
+                }
+                get("/4") {
+                    call.respondText("ok:" + call.sessions.get<TestUserSession>()?.userId)
+                }
+            }
+
+            handleRequest(HttpMethod.Get, "/3").let { call ->
+                val sessionCookie = call.response.cookies[cookieName]
+                assertEquals(
+                    "00112233445566778899aabbccddeeff/" +
+                        "c3850fc1ddc62f71ec5eaad6d393b91fa809fe32a1cf0cb4730788c5a489daef:" +
+                        "65f0bd15bfa7e96b8bd9e134e23a24860b324e361c606e6800af050f5d104b68",
+                    sessionCookie!!.value
+                )
+            }
+
+            handleRequest(HttpMethod.Get, "/4") {
+                addHeader(
+                    HttpHeaders.Cookie,
+                    "$cookieName=" + "00112233445566778899aabbccddeeff/" +
+                        "c3850fc1ddc62f71ec5eaad6d393b91fa809fe32a1cf0cb4730788c5a489daef:" +
+                        "51a5e9fcd1c91418f9a623bafa5022a524348e44244265dc0cab2cebacc28a5d"
+                )
             }.let { call ->
                 assertEquals("ok:null", call.response.content)
             }
