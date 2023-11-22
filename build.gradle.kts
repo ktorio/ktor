c98012b9228d4317bef5ce20213c372fe53c725d
Margarita Bobova <margarita.bobova@jetbrains.com>
2023-11-22 17:12:53 +0100

    2.0.0 migration: Remove deprecated irTarget?.compilations?.all

--- build.gradle.kts
+++ build.gradle.kts
@@ -244,11 +244,6 @@ fun Project.setupJvmToolchain() {
 
 fun KotlinMultiplatformExtension.setCompilationOptions() {
     targets.all {
-        if (this is KotlinJsTarget) {
-            irTarget?.compilations?.all {
-                configureCompilation()
-            }
-        }
         compilations.all {
             configureCompilation()
         }
