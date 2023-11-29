690ba51e1b4f67720bc1113d5179a1955245c2a0
Al Sutton <al.sutton@alsutton.com>
2023-11-29 08:57:37 +0000

    Remove assumption "JKS" is always available.
    On some platforms (e.g. Android) Java KeyStore (JKS) support is not
    available. Using the default platform keystore ensures that keys can
    still be stored on those platforms.

--- ktor-server/ktor-server-netty/jvm/src/io/ktor/server/netty/NettyChannelInitializer.kt
+++ ktor-server/ktor-server-netty/jvm/src/io/ktor/server/netty/NettyChannelInitializer.kt
@@ -161,7 +161,7 @@ public class NettyChannelInitializer(
     private fun EngineSSLConnectorConfig.trustManagerFactory(): TrustManagerFactory? {
         val trustStore = trustStore ?: trustStorePath?.let { file ->
             FileInputStream(file).use { fis ->
-                KeyStore.getInstance("JKS").also { it.load(fis, null) }
+                KeyStore.getInstance(KeyStore.getDefaultType()).also { it.load(fis, null) }
             }
         }
         return trustStore?.let { store ->
