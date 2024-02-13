bennyhuo/ktor-for-ohos

Fix tests with proper test return types

ktor-client/ktor-client-tests/build.gradle.kts
@@ -19,6 +19,7 @@ kotlin.sourceSets {
         dependencies {
             api(project(":ktor-client:ktor-client-mock"))
             api(project(":ktor-test-dispatcher"))
+            implementation(libs.kotlinx.coroutines.test)
         }
     }
     commonTest {

ktorio/ktor

ktor-client/ktor-client-tests/build.gradle.kts
@@ -20,6 +20,7 @@ kotlin.sourceSets {
             api(project(":ktor-client:ktor-client-mock"))
             api(project(":ktor-test-dispatcher"))
             api(libs.kotlin.test)
+            api(libs.kotlinx.coroutines.test)
         }
     }
     commonTest {