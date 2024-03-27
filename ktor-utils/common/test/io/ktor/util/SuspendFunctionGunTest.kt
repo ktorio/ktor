5beb308a328a1c1b5af7ffad137c0e050bb6fb25
Aleksei Tirman <aleksei.tirman@jetbrains.com>
2024-03-27 13:28:40 +0200

    KTOR-6878 Fix stack overflow when SuspendFunctionGun trying to access continuation context of the same continuation object recursively

---
+++ ktor-utils/common/test/io/ktor/util/SuspendFunctionGunTest.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2014-2024 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package io.ktor.util
+
+import io.ktor.test.dispatcher.*
+import io.ktor.util.pipeline.*
+import kotlin.coroutines.*
+import kotlin.test.*
+
+class SuspendFunctionGunTest {
+    @Test
+    fun throwsErrorWhenNoDistinctContinuation() = testSuspend {
+        val gun = SuspendFunctionGun(Unit, Unit, listOf({_, _, _ ->}))
+        gun.addContinuation(gun.continuation)
+
+        val cause = assertFailsWith<IllegalStateException> { gun.continuation.context }
+        assertEquals("Not started", cause.message)
+    }
+
+    @Test
+    fun returnsLastDistinctContinuation() = testSuspend {
+        val gun = SuspendFunctionGun(Unit, Unit, listOf({_, _, _ ->}, {_, _, _ ->}))
+        val continuation = Continuation<Unit>(EmptyCoroutineContext) {}
+        gun.addContinuation(continuation)
+        gun.addContinuation(gun.continuation)
+
+        assertEquals(gun.continuation.context, continuation.context)
+    }
+
+    @Test
+    fun returnsFirstDistinctContinuation() = testSuspend {
+        val gun = SuspendFunctionGun(Unit, Unit, listOf({_, _, _ ->}))
+        val continuation = Continuation<Unit>(EmptyCoroutineContext) {}
+        gun.addContinuation(continuation)
+
+        assertEquals(gun.continuation.context, continuation.context)
+    }
+}
