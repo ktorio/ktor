d5b4053da623b18305f72f7594180ae5faa788cd
Aleksei Tirman <aleksei.tirman@jetbrains.com>
2024-04-03 14:30:06 +0300

    KTOR-5383 Fix parsing server host and port when using NettyConnection… (#4014)
    * KTOR-5383 Fix parsing server host and port when using NettyConnectionPoint and CIOConnectionPoint

--- ktor-server/ktor-server-test-suites/jvm/src/io/ktor/server/testing/suites/ConnectionTestSuite.kt
+++ ktor-server/ktor-server-test-suites/jvm/src/io/ktor/server/testing/suites/ConnectionTestSuite.kt
@@ -7,13 +7,13 @@ package io.ktor.server.testing.suites
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.coroutines.*
-import org.junit.jupiter.api.*
 import java.net.*
 import kotlin.test.*
 import kotlin.test.Test
@@ -76,6 +76,52 @@ abstract class ConnectionTestSuite(val engine: ApplicationEngineFactory<*, *>) {
             serverStarted.join()
             val response = HttpClient(CIO).get("http://127.0.0.1:$serverPort/")
             assertEquals(HttpStatusCode.OK, response.status)
+        }
+
+        server.stop(50, 100)
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    @Test
+    fun testIpv6ServerHostAndPort() = runBlocking {
+        val serverStarted = CompletableDeferred<Unit>()
+        val serverPort = withContext(Dispatchers.IO) { ServerSocket(0).use { it.localPort } }
+        val server = embeddedServer(
+            engine,
+            applicationProperties(applicationEnvironment()) {
+                module {
+                    routing {
+                        get("/") {
+                            val local = call.request.local
+                            call.respondText { "${local.serverHost}:${local.serverPort}" }
+                        }
+                    }
+                }
+            }
+        ) {
+            connector { port = serverPort }
+        }
+
+        server.monitor.subscribe(ServerReady) {
+            serverStarted.complete(Unit)
+        }
+
+        GlobalScope.launch {
+            server.start(true)
+        }
+
+        withTimeout(5000) {
+            serverStarted.join()
+            val client = HttpClient(CIO)
+            client.get("http://[::1]:$serverPort/").let { response ->
+                assertEquals(HttpStatusCode.OK, response.status)
+                assertEquals("[::1]:$serverPort", response.bodyAsText())
+            }
+            client.get("http://[0:0:0:0:0:0:0:1]:$serverPort/").let { response ->
+                assertEquals(HttpStatusCode.OK, response.status)
+                assertEquals("[0:0:0:0:0:0:0:1]:$serverPort", response.bodyAsText())
+            }
+            client.close()
         }
 
         server.stop(50, 100)
