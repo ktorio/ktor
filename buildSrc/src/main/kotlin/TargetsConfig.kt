272f1c4b3d8bc7832d7c5bc7ab3b732676aa8072
Anton Lakotka <anton.lakotka@jetbrains.com>
2024-05-30 11:20:07 +0200

    Fix ExperimentalWasmDsl dsl usage
    Since Kotlin 2.0.20-Beta2 they have moved this OptIn annotation to the other package

--- buildSrc/src/main/kotlin/TargetsConfig.kt
+++ buildSrc/src/main/kotlin/TargetsConfig.kt
@@ -6,8 +6,6 @@
 import org.gradle.api.*
 import org.gradle.kotlin.dsl.*
 import org.jetbrains.kotlin.gradle.*
-import org.jetbrains.kotlin.gradle.plugin.*
-import org.jetbrains.kotlin.gradle.targets.js.dsl.*
 import java.io.*
 
 val Project.files: Array<File> get() = project.projectDir.listFiles() ?: emptyArray()
