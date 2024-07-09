bc34b51884d37267ea7bd4380e65388808104023
leonid.stashevsky <leonid.stashevsky@gmail.com>
2024-07-09 10:58:38 +0200

    Update configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -221,16 +221,3 @@ fun KotlinMultiplatformExtension.configureSourceSets() {
             }
         }
 }
-
-// Drop this configuration when the Node.JS version in KGP will support wasm gc milestone 4
-// check it here:
-// https://github.com/JetBrains/kotlin/blob/master/libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/nodejs/NodeJsRootExtension.kt
-with(org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin.apply(rootProject)) {
-    // canary nodejs that supports recent Wasm GC changes
-    nodeVersion = "21.0.0-v8-canary202309167e82ab1fa2"
-    nodeDownloadBaseUrl = "https://nodejs.org/download/v8-canary"
-}
-// Drop this when node js version become stable
-tasks.withType(org.jetbrains.kotlin.gradle.targets.js.npm.tasks.KotlinNpmInstallTask::class).configureEach {
-    args.add("--ignore-engines")
-}
