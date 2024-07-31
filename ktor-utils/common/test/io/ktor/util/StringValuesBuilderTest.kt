ce5e2a504be16aadbf87b1e96e04eab294c362af
brandon <2858207+igloo12@users.noreply.github.com>
2024-07-30 22:06:48 -0700

    KTOR-6946 headers in the default request are overriding custom headers

--- ktor-utils/common/test/io/ktor/util/StringValuesBuilderTest.kt
+++ ktor-utils/common/test/io/ktor/util/StringValuesBuilderTest.kt
@@ -71,23 +71,4 @@ class StringValuesBuilderTest {
         builder.appendIfNameAndValueAbsent("key", "value3")
         assertEquals(listOf("value1", "value3"), builder.getAll("key"))
     }
-
-    @Test
-    fun replaceAll() {
-        val original = StringValuesBuilderImpl(caseInsensitiveName = true).also {
-            it.append("Key1", "value1")
-            it.append("Key2", "value2")
-            it.append("Key3", "value3")
-        }
-
-        val replace = StringValuesBuilderImpl(caseInsensitiveName = true).also {
-            it.append("Key1", "replace1")
-            it.append("Key2", "replace2")
-        }
-
-        original.replaceAll(replace)
-        assertEquals(original["Key1"], "replace1")
-        assertEquals(original["Key2"], "replace2")
-        assertEquals(original["Key3"], "value3")
-    }
 }
