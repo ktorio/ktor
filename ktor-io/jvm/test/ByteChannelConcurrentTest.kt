3e5d1e0e2b69212e95a13a48dcd04865e7273a36
Bruce Hamilton <bruce.hamilton@jetbrains.com>
2024-08-21 09:37:36 +0200

    KTOR-7324 Add smarter resume and tests for concurrent bytechannel access problem

--- ktor-io/jvm/test/ByteChannelConcurrentTest.kt
+++ ktor-io/jvm/test/ByteChannelConcurrentTest.kt
@@ -4,6 +4,8 @@
 
 import io.ktor.utils.io.*
 import kotlinx.coroutines.*
+import kotlinx.coroutines.sync.*
+import kotlinx.coroutines.test.*
 import kotlin.test.*
 
 /*
@@ -11,10 +13,49 @@ import kotlin.test.*
  */
 
 class ByteChannelConcurrentTest {
+
+    @OptIn(DelicateCoroutinesApi::class)
+    @Test
+    fun cannotSubscribeTwiceForContent() = runTest {
+        val channel = ByteChannel()
+        val read1 = GlobalScope.async { channel.readByte() }
+        val read2 = GlobalScope.async { channel.readByte() }
+
+        assertFailsWith<IllegalStateException> {
+            awaitAll(read1, read2)
+        }.also {
+            assertEquals("Concurrent read attempts", it.message)
+        }
+    }
+
+    @OptIn(DelicateCoroutinesApi::class, InternalAPI::class)
+    @Test
+    fun cannotSubscribeTwiceForFlush() = runTest {
+        val channel = ByteChannel()
+        val exceptionHandler = CoroutineExceptionHandler { _, cause ->
+            assertTrue(cause is IllegalStateException)
+            assertEquals("Concurrent write attempts", cause.message)
+        }
+        val write1 = GlobalScope.launch(exceptionHandler) {
+            channel.writeByteArray(ByteArray(CHANNEL_MAX_SIZE))
+        }
+        val write2 = GlobalScope.launch(exceptionHandler) {
+            // concurrent writes can also cause NPEs
+            delay(100)
+            channel.writeByteArray(ByteArray(CHANNEL_MAX_SIZE))
+        }
+        val read = GlobalScope.async {
+            delay(100)
+            channel.readBuffer(CHANNEL_MAX_SIZE * 2)
+        }
+
+        joinAll(write1, write2, read)
+    }
+
     @OptIn(DelicateCoroutinesApi::class)
     @Test
     fun testReadAndWriteConcurrentWithCopyTo() = runBlocking {
-        repeat(100000) {
+        repeat(10000) {
             val result = ByteChannel()
 
             val writer = GlobalScope.reader {
