59d713434092424530ffa8dce253e81327157a3b
Lutosław <68021034+jacekgajek@users.noreply.github.com>
2024-09-12 11:14:05 +0200

    KTOR-7202 Add respondPath functions (#4114)

--- ktor-server/ktor-server-tests/jvm/test/io/ktor/server/http/RespondFunctionsJvmTest.kt
+++ ktor-server/ktor-server-tests/jvm/test/io/ktor/server/http/RespondFunctionsJvmTest.kt
@@ -10,6 +10,8 @@ import io.ktor.http.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.testing.*
+import java.io.*
+import java.nio.file.*
 import kotlin.test.*
 
 class RespondFunctionsJvmTest {
@@ -32,5 +34,39 @@ class RespondFunctionsJvmTest {
             assertEquals("1, 2", response.bodyAsBytes().joinToString())
             assertEquals("2", response.headers[HttpHeaders.ContentLength])
         }
+    }
+
+    @Test
+    fun testRespondFile() = testApplication {
+        val baseDirPath = "jvm/test-resources/"
+        val filePath = "test-resource.txt"
+        routing {
+            get("/respondFile-File-String") {
+                call.respondFile(File(baseDirPath), filePath)
+            }
+            get("/respondFile-File") {
+                call.respondFile(File(baseDirPath + filePath))
+            }
+            get("/respondFile-Path-Path") {
+                call.respondPath(Paths.get(baseDirPath), Paths.get(filePath))
+            }
+            get("/respondFile-Path") {
+                call.respondPath(Paths.get(baseDirPath + filePath))
+            }
+        }
+
+        val expected = "plain"
+        val firstBody = client.get("/respondFile-File-String").trimmedBody()
+        val secondBody = client.get("/respondFile-File").trimmedBody()
+        val thirdBody = client.get("/respondFile-Path-Path").trimmedBody()
+        val fourthBody = client.get("/respondFile-Path").trimmedBody()
+        assertEquals(expected, firstBody)
+        assertEquals(expected, secondBody)
+        assertEquals(expected, thirdBody)
+        assertEquals(expected, fourthBody)
+    }
+
+    private suspend fun HttpResponse.trimmedBody(): String {
+        return bodyAsText().trim()
     }
 }
