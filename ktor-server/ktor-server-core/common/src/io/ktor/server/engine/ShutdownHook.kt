eb7e82e5bfb9fcf2ce72601bbeac7e5c7e42743b
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2024-09-20 14:21:27 +0200

    Move the common shutdown hook logic to the common source set

--- ktor-server/ktor-server-core/common/src/io/ktor/server/engine/ShutdownHook.kt
+++ ktor-server/ktor-server-core/common/src/io/ktor/server/engine/ShutdownHook.kt
@@ -4,16 +4,9 @@
 
 package io.ktor.server.engine
 
-import io.ktor.events.*
+import io.ktor.server.application.ApplicationStarting
 
-/**
- * Adds automatic application shutdown hooks management. Should be used **before** starting the engine.
- * Once application termination noticed, [stop] block will be executed.
- * Please note that a shutdown hook only registered when the application is running. If the application
- * is already stopped then there will be no hook and no [stop] function invocation possible.
- * So [stop] block will be called once or never.
- */
-public expect fun ApplicationEngine.addShutdownHook(monitor: Events, stop: () -> Unit)
+internal expect val SHUTDOWN_HOOK_ENABLED: Boolean
 
 /**
  * Adds automatic application shutdown hooks management. Should be used **before** starting the server.
@@ -22,4 +15,17 @@ public expect fun ApplicationEngine.addShutdownHook(monitor: Events, stop: () ->
  * is already stopped then there will be no hook and no [stop] function invocation possible.
  * So [stop] block will be called once or never.
  */
-public expect fun EmbeddedServer<*, *>.addShutdownHook(stop: () -> Unit)
+public fun EmbeddedServer<*, *>.addShutdownHook(stop: () -> Unit) {
+    if (SHUTDOWN_HOOK_ENABLED) {
+        monitor.subscribe(ApplicationStarting) { platformAddShutdownHook(stop) }
+    }
+}
+
+/**
+ * Adds automatic application shutdown hooks management. Should be used **before** starting the server.
+ * Once application termination noticed, [stop] block will be executed.
+ * Please note that a shutdown hook only registered when the application is running. If the application
+ * is already stopped then there will be no hook and no [stop] function invocation possible.
+ * So [stop] block will be called once or never.
+ */
+internal expect fun EmbeddedServer<*, *>.platformAddShutdownHook(stop: () -> Unit)
