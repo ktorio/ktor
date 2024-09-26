73665d560fc82f068fd84675b56e454f21c1cad7
Mariia Skripchenko <61115099+marychatte@users.noreply.github.com>
2024-09-26 10:30:32 +0200

    Add test

--- ktor-client/ktor-client-tests/common/test/io/ktor/client/tests/WebSocketTest.kt
+++ ktor-client/ktor-client-tests/common/test/io/ktor/client/tests/WebSocketTest.kt
@@ -6,6 +6,8 @@ package io.ktor.client.tests
 
 import io.ktor.client.*
 import io.ktor.client.plugins.*
+import io.ktor.client.plugins.auth.*
+import io.ktor.client.plugins.auth.providers.*
 import io.ktor.client.plugins.websocket.*
 import io.ktor.client.request.*
 import io.ktor.client.tests.utils.*
@@ -348,6 +350,30 @@ class WebSocketTest : ClientLoader() {
                     defs.awaitAll()
                 }
             }
+        }
+    }
+
+    @Test
+    fun testWithAuthPlugin() = clientTests(
+        ENGINES_WITHOUT_WS
+    ) {
+        config {
+            install(WebSockets)
+
+            install(Auth) {
+                bearer {
+                    loadTokens { BearerTokens("invalid", "invalid") }
+                    refreshTokens { BearerTokens("valid", "valid") }
+                }
+            }
+        }
+
+        test { client ->
+            client.webSocket("$TEST_WEBSOCKET_SERVER/auth/websocket") {
+                val frame = incoming.receive() as Frame.Text
+                assertEquals("Hello from server", frame.readText())
+            }
+            client.close()
         }
     }
 }
