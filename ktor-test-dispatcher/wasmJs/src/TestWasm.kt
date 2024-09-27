7f2157e85e39266ba152f753adcf23f306e9c878
leonid.stashevsky <leonid.stashevsky@gmail.com>
2024-09-27 09:02:12 +0200

    fixup! Update kotlinx-coroutines monorepo to v1.9.0

--- ktor-test-dispatcher/wasmJs/src/TestWasm.kt
+++ ktor-test-dispatcher/wasmJs/src/TestWasm.kt
@@ -7,6 +7,11 @@ package io.ktor.test.dispatcher
 import kotlinx.coroutines.*
 import kotlinx.coroutines.test.*
 import kotlin.coroutines.*
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.DurationUnit
+import kotlin.time.toDuration
 
 /**
  * Test runner for js suspend tests.
@@ -16,7 +21,7 @@ public actual fun testSuspend(
     context: CoroutineContext,
     timeoutMillis: Long,
     block: suspend CoroutineScope.() -> Unit
-): TestResult = GlobalScope.promise(block = {
-    withTimeout(timeoutMillis, block)
-    null
-}, context = context)
+): TestResult = runTest(context=context, timeout=timeoutMillis.toDuration(DurationUnit.MILLISECONDS)) {
+
+    block()
+}
