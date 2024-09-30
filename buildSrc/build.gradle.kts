15b3c486bf2639cf4fa542b7f44dfd6b476fd34f
Margarita Bobova <margarita.bobova@jetbrains.com>
2024-09-30 16:49:19 +0200

    Fixate kotlin version 2.0.20 for buildSrc, until Gradle don't support LV 2.1

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -35,8 +35,8 @@ val ktor_version = "3.0.0-rc-2-eap-1091"
 
 dependencies {
     val kotlinVersion = libs.versions.kotlin.version.get()
-    implementation(kotlin("gradle-plugin", kotlinVersion))
-    implementation(kotlin("compiler-embeddable", kotlinVersion))
+    implementation(kotlin("gradle-plugin", "2.1.0-dev-8424"))
+    implementation(kotlin("compiler-embeddable", "2.1.0-dev-8424")) //gradle don't support LV 2.1
     implementation(kotlin("serialization", kotlinVersion))
 
     val ktlint_version = libs.versions.ktlint.version.get()
