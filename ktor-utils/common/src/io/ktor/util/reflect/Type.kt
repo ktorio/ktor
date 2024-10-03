bc1f584accda117d44545d69aece9a72b3414adc
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2024-10-03 18:39:32 +0200

    KTOR-7479 Use TypeInfo and typeOfOrNull in AttributeKey

--- ktor-utils/common/src/io/ktor/util/reflect/Type.kt
+++ ktor-utils/common/src/io/ktor/util/reflect/Type.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2021 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2014-2024 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package io.ktor.util.reflect
@@ -15,15 +15,31 @@ public expect val KType.platformType: Type
 
 /**
  * Ktor type information.
- * @property type: source KClass<*>
- * @property reifiedType: type with substituted generics
- * @property kotlinType: kotlin reified type with all generic type parameters.
+ * @property type Source KClass<*>
+ * @property reifiedType Type with substituted generics
+ * @property kotlinType Kotlin reified type with all generic type parameters.
  */
 public data class TypeInfo(
     public val type: KClass<*>,
     public val reifiedType: Type,
     public val kotlinType: KType? = null
-)
+) {
+
+    override fun hashCode(): Int {
+        return kotlinType?.hashCode() ?: type.hashCode()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is TypeInfo) return false
+
+        return if (kotlinType != null || other.kotlinType != null) {
+            kotlinType == other.kotlinType
+        } else {
+            type == other.type
+        }
+    }
+}
 
 /**
  * Returns [TypeInfo] for the specified type [T]
@@ -36,9 +52,11 @@ public expect inline fun <reified T> typeInfo(): TypeInfo
 public expect fun Any.instanceOf(type: KClass<*>): Boolean
 
 @PublishedApi
-internal inline fun <reified T> tryGetType(): KType? = try {
-    // We need to wrap getting type in try catch because of https://youtrack.jetbrains.com/issue/KT-42913
+internal inline fun <reified T> typeOfOrNull(): KType? = try {
+    // We need to wrap getting a type in try catch because of:
+    // - KT-42913
+    // - KTOR-7479 (R8 in full mode strips class signatures)
     typeOf<T>()
-} catch (cause: Throwable) {
+} catch (_: Throwable) {
     null
 }
