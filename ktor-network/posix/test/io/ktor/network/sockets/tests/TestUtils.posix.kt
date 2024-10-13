c4ee4366312f08e355fc33b393c273cd7f42c2d6
Oleg Yukhnevich <whyoleg@gmail.com>
2024-10-13 19:22:36 +0300

    Move `supportsUnixDomainSockets` to posix and use Platform instead of expect/actual

---
+++ ktor-network/posix/test/io/ktor/network/sockets/tests/TestUtils.posix.kt
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2014-2024 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package io.ktor.network.sockets.tests
+
+import kotlin.experimental.*
+
+@OptIn(ExperimentalNativeApi::class)
+internal actual fun Any.supportsUnixDomainSockets(): Boolean = when (Platform.osFamily) {
+    OsFamily.MACOSX, OsFamily.LINUX -> true
+    else                            -> false
+}
