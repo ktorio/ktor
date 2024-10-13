aac5a603468e248c45e37f772a5ee1ba8f377dea
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2024-10-13 14:59:09 +0200

    Update deprecated API

--- buildSrc/src/main/kotlin/test/server/TestTcpServer.kt
+++ buildSrc/src/main/kotlin/test/server/TestTcpServer.kt
@@ -16,31 +16,36 @@ internal class TestTcpServer(val port: Int, handler: suspend (Socket) -> Unit) :
     override val coroutineContext: CoroutineContext
 
     init {
-        val server = aSocket(selector).tcp().bind(port = port)
+        var server: ServerSocket? = null
 
         coroutineContext = GlobalScope.launch {
+            server = aSocket(selector).tcp().bind(port = port)
             while (isActive) {
                 val socket = try {
-                    server.accept()
+                    server?.accept()
                 } catch (cause: Throwable) {
                     println("Test server is fail to accept: $cause")
                     cause.printStackTrace()
                     continue
                 }
 
-                try {
-                    socket.use { handler(it) }
-                } catch (cause: Throwable) {
-                    println("Exception in tcp server: $cause")
-                    cause.printStackTrace()
+                launch {
+                    try {
+                        socket?.use { handler(it) }
+                    } catch (cause: Throwable) {
+                        println("Exception in tcp server: $cause")
+                        cause.printStackTrace()
+                    }
                 }
             }
         }.apply {
             invokeOnCompletion {
-                server.close()
+                server?.close()
             }
         }
     }
 
-    override fun close() { coroutineContext.cancel() }
+    override fun close() {
+        coroutineContext.cancel()
+    }
 }
