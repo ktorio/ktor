7f872f76ebabc66cdb40e41b50cb050c91778ce9
Andreas Scheja <a.scheja@gmail.com>
2024-10-16 08:48:37 +0200

    KTOR-7571 byte read channel cpu lock (#4397)
    * KTOR-7571 Add tests exposing the issue
    
    * KTOR-7571 Extract common code
    
    * KTOR-7571 Await number of required bytes
    
    * KTOR-7571 Make sure to yield to other coroutines in case awaitContent does not

--- ktor-io/jvm/test/ByteReadChannelOperationsJvmTest.kt
+++ ktor-io/jvm/test/ByteReadChannelOperationsJvmTest.kt
@@ -33,4 +33,46 @@ class ByteReadChannelOperationsJvmTest {
         channel.close()
         assertEquals(-1, channel.readAvailable { buffer -> buffer.remaining() })
     }
+
+    @Test
+    fun testReadShortIsCooperative() = runBlocking {
+        val theAnswerAsBytes: ByteArray = byteArrayOf(0, 42)
+        val channel = ByteChannel()
+        launch {
+            for (byte in theAnswerAsBytes) {
+                delay(10)
+                channel.writeByte(byte)
+                channel.flush()
+            }
+        }
+        assertEquals(42, channel.readShort())
+    }
+
+    @Test
+    fun testReadIntIsCooperative() = runBlocking {
+        val theAnswerAsBytes: ByteArray = byteArrayOf(0, 0, 0, 42)
+        val channel = ByteChannel()
+        launch {
+            for (byte in theAnswerAsBytes) {
+                delay(10)
+                channel.writeByte(byte)
+                channel.flush()
+            }
+        }
+        assertEquals(42, channel.readInt())
+    }
+
+    @Test
+    fun testReadLongIsCooperative() = runBlocking {
+        val theAnswerAsBytes: ByteArray = byteArrayOf(0, 0, 0, 0, 0, 0, 0, 42)
+        val channel = ByteChannel()
+        launch {
+            for (byte in theAnswerAsBytes) {
+                delay(10)
+                channel.writeByte(byte)
+                channel.flush()
+            }
+        }
+        assertEquals(42, channel.readLong())
+    }
 }
