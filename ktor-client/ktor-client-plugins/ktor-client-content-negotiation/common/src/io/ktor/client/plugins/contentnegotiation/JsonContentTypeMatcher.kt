e34185255c3b4072b26c6bc8151decb8a9c7d126
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2024-10-25 17:01:26 +0200

    Add operator contains to ContentType objects

--- ktor-client/ktor-client-plugins/ktor-client-content-negotiation/common/src/io/ktor/client/plugins/contentnegotiation/JsonContentTypeMatcher.kt
+++ ktor-client/ktor-client-plugins/ktor-client-content-negotiation/common/src/io/ktor/client/plugins/contentnegotiation/JsonContentTypeMatcher.kt
@@ -18,6 +18,6 @@ public object JsonContentTypeMatcher : ContentTypeMatcher {
         }
 
         val value = contentType.withoutParameters().toString()
-        return value.startsWith("application/", ignoreCase = true) && value.endsWith("+json", ignoreCase = true)
+        return value in ContentType.Application && value.endsWith("+json", ignoreCase = true)
     }
 }
