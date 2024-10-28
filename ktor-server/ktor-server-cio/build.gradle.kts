90dc63e73e376e0d818628ca1c6c10ef656dd552
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2024-10-28 14:38:24 +0100

    KTOR-7606 Make Closable implement AutoCloseable (#4414)
    * Use stdlib `use` extension-function
    * Update type checks

--- ktor-server/ktor-server-cio/build.gradle.kts
+++ ktor-server/ktor-server-cio/build.gradle.kts
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2014-2024 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
 description = ""
 
 kotlin.sourceSets {
@@ -14,11 +18,11 @@ kotlin.sourceSets {
             api(project(":ktor-server:ktor-server-core"))
             api(project(":ktor-client:ktor-client-cio"))
             api(project(":ktor-server:ktor-server-test-suites"))
+            api(project(":ktor-server:ktor-server-test-base"))
         }
     }
     jvmTest {
         dependencies {
-            api(project(":ktor-server:ktor-server-test-base"))
             api(project(":ktor-server:ktor-server-core", configuration = "testOutput"))
             api(libs.logback.classic)
         }
