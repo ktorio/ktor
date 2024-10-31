b02882dec870fc33a9ffcc542a2e479317a4c68e
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2024-10-31 14:17:39 +0100

    Enable configuration cache

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,8 @@
  * Copyright 2014-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
+enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
+
 pluginManagement {
     includeBuild("build-settings-logic")
 }
