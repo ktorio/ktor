7773f79dea5db6380eaf9c018df245b9ec6acb47
Oleg Yukhnevich <whyoleg@gmail.com>
2024-10-31 15:24:47 +0200

    KTOR-6004 Support TCP and Unix socket for wasm-js and js via NodeJS (#4411)
    * Drop `jvmAndNix` shared source set
    * Commonize `ktor-network` and `ktor-network-tls`
    * Support TCP and Unix sockets for wasm-js and js on Node
    * Move `supportsUnixDomainSockets` to posix and use Platform instead of expect/actual

--- ktor-network/jvm/test/io/ktor/network/sockets/tests/TestUtilsJvm.kt
+++ ktor-network/jvm/test/io/ktor/network/sockets/tests/TestUtilsJvm.kt
@@ -1,11 +1,8 @@
 /*
- * Copyright 2014-2021 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2014-2024 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package io.ktor.network.sockets.tests
-
-import java.nio.file.*
-import kotlin.io.path.*
 
 private const val UNIX_DOMAIN_SOCKET_ADDRESS_CLASS = "java.net.UnixDomainSocketAddress"
 
@@ -16,14 +13,4 @@ internal actual fun Any.supportsUnixDomainSockets(): Boolean {
     } catch (e: ClassNotFoundException) {
         false
     }
-}
-
-internal actual fun createTempFilePath(basename: String): String {
-    val tempFile = Files.createTempFile(basename, "")
-    tempFile.deleteIfExists()
-    return tempFile.toString()
-}
-
-internal actual fun removeFile(path: String) {
-    Files.deleteIfExists(Path(path))
 }
