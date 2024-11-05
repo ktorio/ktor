1261aa4c05f56f50f83cd74f4d5d615332a826a5
Anton Erofeev <antonerofeev11@gmail.com>
2024-11-05 11:25:23 +0300

    Fix names of test packages according to location (#4450)

--- ktor-server/ktor-server-core/common/test/io/ktor/server/routing/RouteTest.kt
+++ ktor-server/ktor-server-core/common/test/io/ktor/server/routing/RouteTest.kt
@@ -2,18 +2,14 @@
  * Copyright 2014-2021 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
-package io.ktor.tests.routing
+package io.ktor.server.routing
 
-import io.ktor.client.plugins.api.*
 import io.ktor.client.request.*
 import io.ktor.http.*
-import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
-import io.ktor.server.routing.*
 import io.ktor.server.routing.RoutingRoot.Plugin.RoutingCallStarted
 import io.ktor.server.testing.*
-import io.ktor.util.pipeline.*
 import kotlin.test.*
 
 class RouteTest {
