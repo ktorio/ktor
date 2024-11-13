0c15320ec356c90a05f943edbbb1bffabbbe99af
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2024-11-13 10:50:25 +0100

    KTOR-7738 Add aggregating publish tasks

--- buildSrc/src/main/kotlin/NamedDomainObjectCollection.kt
+++ buildSrc/src/main/kotlin/NamedDomainObjectCollection.kt
@@ -4,4 +4,8 @@
 
 import org.gradle.api.*
 
+internal fun <T> NamedDomainObjectCollection<T>.maybeNamed(name: String): NamedDomainObjectProvider<T>? {
+    return if (name in names) named(name) else null
+}
+
 internal inline fun <reified T> NamedDomainObjectCollection<*>.findByName(name: String): T? = findByName(name) as? T
