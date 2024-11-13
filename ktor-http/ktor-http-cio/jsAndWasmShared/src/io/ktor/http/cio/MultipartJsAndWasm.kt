c27cd7519933c78312e1346a29118c51f2ff3887
Leonid Stashevsky <e5l@users.noreply.github.com>
2024-11-13 13:50:36 +0100

    KTOR-6632 Support receiving multipart data with Ktor client (#4458)

---
+++ ktor-http/ktor-http-cio/jsAndWasmShared/src/io/ktor/http/cio/MultipartJsAndWasm.kt
@@ -0,0 +1,11 @@
+package io.ktor.http.cio
+
+import io.ktor.utils.io.*
+
+/*
+ * Copyright 2014-2024 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+internal actual fun ByteReadChannel.discardBlocking() {
+    cancel()
+}
