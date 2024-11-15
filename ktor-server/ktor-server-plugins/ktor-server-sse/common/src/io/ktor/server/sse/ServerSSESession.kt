7c5fc075475a82dd96edab46f962be5af5150335
Mariia Skripchenko <61115099+marychatte@users.noreply.github.com>
2024-11-15 13:28:42 +0100

    KTOR-7435 Add serialization for SSE (#4363)

--- ktor-server/ktor-server-plugins/ktor-server-sse/common/src/io/ktor/server/sse/ServerSSESession.kt
+++ ktor-server/ktor-server-plugins/ktor-server-sse/common/src/io/ktor/server/sse/ServerSSESession.kt
@@ -6,17 +6,34 @@ package io.ktor.server.sse
 
 import io.ktor.server.application.*
 import io.ktor.sse.*
+import io.ktor.util.reflect.*
+import io.ktor.websocket.*
 import kotlinx.coroutines.*
 
 /**
- * Represents a server-side server-sent events session.
+ * Represents a server-side Server-Sent Events (SSE) session.
  * An [ServerSSESession] allows the server to send [ServerSentEvent] to the client over a single HTTP connection.
  *
- * @see [SSE]
+ * Example of usage:
+ * ```kotlin
+ * install(SSE)
+ * routing {
+ *     sse("/default") {
+ *         repeat(100) {
+ *             send(ServerSentEvent("event $it"))
+ *         }
+ *     }
+ * }
+ * ```
+ *
+ * To learn more, see [the SSE](https://en.wikipedia.org/wiki/Server-sent_events)
+ * and [the SSE specification](https://html.spec.whatwg.org/multipage/server-sent-events.html).
+ *
+ * @see SSE
  */
 public interface ServerSSESession : CoroutineScope {
     /**
-     * Associated received [call] that originating this session.
+     * The received [call] that originated this session.
      */
     public val call: ApplicationCall
 
@@ -54,4 +71,62 @@ public interface ServerSSESession : CoroutineScope {
      * before closing the session, you can use the [send] function for it.
      */
     public suspend fun close()
+}
+
+/**
+ * Represents a server-side Server-Sent Events (SSE) session with serialization support.
+ * An [ServerSSESessionWithSerialization] allows the server to send [ServerSentEvent] to the client over a single HTTP connection.
+ *
+ * Example of usage:
+ * ```kotlin
+ * install(SSE)
+ * routing {
+ *     sse("/serialization", serialize = { typeInfo, it ->
+ *         val serializer = Json.serializersModule.serializer(typeInfo.kotlinType!!)
+ *         Json.encodeToString(serializer, it)
+ *     }) {
+ *         send(Customer(0, "Jet", "Brains"))
+ *         send(Product(0, listOf(100, 200)))
+ *     }
+ * }
+ * ```
+ *
+ * To learn more, see [the SSE](https://en.wikipedia.org/wiki/Server-sent_events)
+ * and [the SSE specification](https://html.spec.whatwg.org/multipage/server-sent-events.html).
+ *
+ * @see SSE
+ */
+public interface ServerSSESessionWithSerialization : ServerSSESession {
+    /**
+     * Serializer for transforming data object into field `data` of `ServerSentEvent`.
+     */
+    public val serializer: (TypeInfo, Any) -> String
+}
+
+public suspend inline fun <reified T : Any> ServerSSESessionWithSerialization.send(event: TypedServerSentEvent<T>) {
+    send(
+        ServerSentEvent(
+            event.data?.let {
+                serializer(typeInfo<T>(), it)
+            },
+            event.event,
+            event.id,
+            event.retry,
+            event.comments
+        )
+    )
+}
+
+public suspend inline fun <reified T : Any> ServerSSESessionWithSerialization.send(
+    data: T? = null,
+    event: String? = null,
+    id: String? = null,
+    retry: Long? = null,
+    comments: String? = null
+) {
+    send(TypedServerSentEvent(data, event, id, retry, comments))
+}
+
+public suspend inline fun <reified T : Any> ServerSSESessionWithSerialization.send(data: T) {
+    send(ServerSentEvent(serializer(typeInfo<T>(), data)))
 }
