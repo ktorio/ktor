29fd734b2167e8d0c0f8d270aded57f4c845b0c1
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2024-11-15 16:51:33 +0100

    Disable Gradle Doctor task monitoring by default

---
+++ buildSrc/src/main/kotlin/conventions.gradleDoctor.gradle.kts
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2014-2024 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+import org.gradle.api.services.internal.RegisteredBuildServiceProvider
+
+plugins {
+    id("com.osacky.doctor")
+}
+
+doctor {
+    enableTestCaching = false
+}
+
+// Always monitor tasks on CI, but disable it locally by default with providing an option to opt-in.
+// See 'doctor.enableTaskMonitoring' in gradle.properties for details.
+val enableTasksMonitoring = CI ||
+    properties.getOrDefault("doctor.enableTaskMonitoring", "false").toString().toBoolean()
+
+if (!enableTasksMonitoring) {
+    logger.info("Gradle Doctor task monitoring is disabled.")
+    gradle.sharedServices.unregister("listener-service")
+}
+
+fun BuildServiceRegistry.unregister(name: String) {
+    val registration = registrations.getByName(name)
+    registrations.remove(registration)
+    (registration.service as RegisteredBuildServiceProvider<*, *>).maybeStop()
+}
