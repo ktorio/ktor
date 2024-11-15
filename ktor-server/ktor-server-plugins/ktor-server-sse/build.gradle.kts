4ebdc6da8f77941033fb85acbeb2cb60de5962f4
Mariia Skripchenko <61115099+marychatte@users.noreply.github.com>
2024-11-15 13:28:42 +0100

    KTOR-7435 Add serialization for SSE (#4363)

--- ktor-server/ktor-server-plugins/ktor-server-sse/build.gradle.kts
+++ ktor-server/ktor-server-plugins/ktor-server-sse/build.gradle.kts
@@ -1,9 +1,19 @@
 description = "Server-sent events (SSE) support"
 
+plugins {
+    id("kotlinx-serialization")
+}
+
 kotlin.sourceSets {
     commonMain {
         dependencies {
             api(project(":ktor-shared:ktor-sse"))
+        }
+    }
+    commonTest {
+        dependencies {
+            api(project(":ktor-shared:ktor-serialization:ktor-serialization-kotlinx"))
+            api(project(":ktor-shared:ktor-serialization:ktor-serialization-kotlinx:ktor-serialization-kotlinx-json"))
         }
     }
 }
