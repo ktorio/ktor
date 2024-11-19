5de8b73bf93023391a6fabd915e21c9d3b75baab
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2024-11-19 10:28:47 +0100

    fixup! Increase timeouts for linuxX64 tests to check timeouted tests

--- ktor-server/ktor-server-test-suites/common/src/io/ktor/server/testing/suites/HttpServerCommonTestSuite.kt
+++ ktor-server/ktor-server-test-suites/common/src/io/ktor/server/testing/suites/HttpServerCommonTestSuite.kt
@@ -31,8 +31,6 @@ import kotlinx.coroutines.*
 import kotlinx.io.*
 import kotlin.coroutines.*
 import kotlin.test.*
-import kotlin.time.Duration
-import kotlin.time.Duration.Companion.seconds
 
 abstract class HttpServerCommonTestSuite<TEngine : ApplicationEngine, TConfiguration : ApplicationEngine.Configuration>(
     hostFactory: ApplicationEngineFactory<TEngine, TConfiguration>
