6746d7a58b81288b390646ff3e8e43e799b9f2bf
Oleg Yukhnevich <whyoleg@gmail.com>
2024-11-19 07:44:57 +0200

    Use static libcurl build with conan (#4445)
    * Test libcurl build with conan
    
    * Take both curl and openssl from the same build
    
    * test no linux cache
    
    * Move K/N linux cache disabling to just `ktor-client-curl` module

--- ktor-client/ktor-client-curl/build.gradle.kts
+++ ktor-client/ktor-client-curl/build.gradle.kts
@@ -1,23 +1,8 @@
 /*
- * Copyright 2014-2024 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2014-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
-val paths = listOf(
-    "/opt/homebrew/opt/curl/include/",
-    "/opt/local/include/",
-    "/usr/local/include/",
-    "/usr/include/",
-    "/usr/local/opt/curl/include/",
-    "/usr/include/x86_64-linux-gnu/",
-    "/usr/local/Cellar/curl/7.62.0/include/",
-    "/usr/local/Cellar/curl/7.63.0/include/",
-    "/usr/local/Cellar/curl/7.65.3/include/",
-    "/usr/local/Cellar/curl/7.66.0/include/",
-    "/usr/local/Cellar/curl/7.80.0/include/",
-    "/usr/local/Cellar/curl/7.80.0_1/include/",
-    "/usr/local/Cellar/curl/7.81.0/include/",
-    "desktop/interop/mingwX64/include/",
-)
+import org.jetbrains.kotlin.gradle.plugin.mpp.*
 
 plugins {
     id("kotlinx-serialization")
@@ -25,19 +10,14 @@ plugins {
 }
 
 kotlin {
-    createCInterop("libcurl", listOf("macosX64", "linuxX64", "mingwX64")) {
-        definitionFile = File(projectDir, "desktop/interop/libcurl.def")
-        includeDirs.headerFilterOnly(paths)
-    }
-
-    createCInterop("libcurl", listOf("macosArm64")) {
-        definitionFile = File(projectDir, "desktop/interop/libcurl_arm64.def")
-        includeDirs.headerFilterOnly(paths)
-    }
-
-    createCInterop("libcurl", listOf("linuxArm64")) {
-        definitionFile = File(projectDir, "desktop/interop/libcurl_linux_arm64.def")
-        includeDirs.headerFilterOnly(listOf("desktop/interop/linuxArm64/include/"))
+    targets.withType<KotlinNativeTarget>().configureEach {
+        compilations.named("main") {
+            cinterops.create("libcurl") {
+                definitionFile = file("desktop/interop/libcurl.def")
+                includeDirs(file("desktop/interop/include"))
+                extraOpts("-libraryPath", file("desktop/interop/lib/${target.name}"))
+            }
+        }
     }
 
     sourceSets {
