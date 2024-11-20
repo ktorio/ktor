601efacd4b15b4ff231bf265dedec0580d2ef19e
Mariia Skripchenko <61115099+marychatte@users.noreply.github.com>
2024-11-20 15:49:09 +0100

    move Errors.kt from jvm to common

--- ktor-server/ktor-server-core/jvm/src/io/ktor/server/engine/DefaultTransformJvm.kt
+++ ktor-server/ktor-server-core/jvm/src/io/ktor/server/engine/DefaultTransformJvm.kt
@@ -6,6 +6,7 @@ package io.ktor.server.engine
 
 import io.ktor.http.*
 import io.ktor.http.cio.*
+import io.ktor.http.cio.internals.*
 import io.ktor.http.content.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.UnsupportedMediaTypeException
