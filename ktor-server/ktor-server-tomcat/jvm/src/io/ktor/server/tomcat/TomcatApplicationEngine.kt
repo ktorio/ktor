b6ca7efa3d0c20f7984ee372494d0b87909f83e9
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2024-12-05 17:51:22 +0100

    Fix docs deferred -> completable job

--- ktor-server/ktor-server-tomcat/jvm/src/io/ktor/server/tomcat/TomcatApplicationEngine.kt
+++ ktor-server/ktor-server-tomcat/jvm/src/io/ktor/server/tomcat/TomcatApplicationEngine.kt
@@ -9,19 +9,20 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.servlet.*
-import kotlinx.atomicfu.*
-import kotlinx.coroutines.*
-import org.apache.catalina.connector.*
+import kotlinx.atomicfu.atomic
+import kotlinx.coroutines.CompletableJob
+import org.apache.catalina.connector.Connector
 import org.apache.catalina.startup.Tomcat
-import org.apache.coyote.http2.*
-import org.apache.tomcat.jni.*
-import org.apache.tomcat.util.net.*
-import org.apache.tomcat.util.net.jsse.*
-import org.apache.tomcat.util.net.openssl.*
-import org.slf4j.*
-import java.nio.file.*
-import javax.servlet.*
-import kotlin.coroutines.*
+import org.apache.coyote.http2.Http2Protocol
+import org.apache.tomcat.jni.Library
+import org.apache.tomcat.jni.SSL
+import org.apache.tomcat.util.net.SSLImplementation
+import org.apache.tomcat.util.net.jsse.JSSEImplementation
+import org.apache.tomcat.util.net.openssl.OpenSSLImplementation
+import org.slf4j.Logger
+import java.nio.file.Files
+import javax.servlet.MultipartConfigElement
+import kotlin.coroutines.CoroutineContext
 
 /**
  * Tomcat application engine that runs it in embedded mode
@@ -46,7 +47,7 @@ public class TomcatApplicationEngine(
 
     private val tempDirectory by lazy { Files.createTempDirectory("ktor-server-tomcat-") }
 
-    private var cancellationDeferred: CompletableJob? = null
+    private var cancellationJob: CompletableJob? = null
 
     private val ktorServlet = object : KtorServlet() {
         override val managedByEngineHeaders: Set<String>
@@ -149,7 +150,7 @@ public class TomcatApplicationEngine(
         resolvedConnectorsDeferred.complete(connectors)
         monitor.raiseCatching(ServerReady, environment, environment.log)
 
-        cancellationDeferred = stopServerOnCancellation(
+        cancellationJob = stopServerOnCancellation(
             applicationProvider(),
             configuration.shutdownGracePeriod,
             configuration.shutdownTimeout
@@ -164,7 +165,7 @@ public class TomcatApplicationEngine(
     override fun stop(gracePeriodMillis: Long, timeoutMillis: Long) {
         if (!stopped.compareAndSet(expect = false, update = true)) return
 
-        cancellationDeferred?.complete()
+        cancellationJob?.complete()
         monitor.raise(ApplicationStopPreparing, environment)
         server.stop()
         server.destroy()
