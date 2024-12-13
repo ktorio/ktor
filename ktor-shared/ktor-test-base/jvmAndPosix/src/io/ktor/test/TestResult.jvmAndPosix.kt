e8e0f47c49c2f2d54eb2c5af9050c0db75492e09
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2024-12-13 23:45:01 +0100

    Add multiplatform implementation of runTestWithData

---
+++ ktor-shared/ktor-test-base/jvmAndPosix/src/io/ktor/test/TestResult.jvmAndPosix.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2014-2024 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package io.ktor.test
+
+import kotlinx.coroutines.test.TestResult
+
+@Suppress("CAST_NEVER_SUCCEEDS")
+internal actual val DummyTestResult = Unit as TestResult
+
+actual inline fun TestResult.andThen(block: () -> Any): TestResult = also { block() }
+
+internal actual inline fun testWithRecover(recover: (Throwable) -> Unit, test: () -> TestResult): TestResult {
+    try {
+        test()
+    } catch (cause: Throwable) {
+        recover(cause)
+    }
+    return DummyTestResult
+}
+
+internal actual inline fun <T> runTestForEach(items: Iterable<T>, test: (T) -> TestResult): TestResult {
+    for (item in items) test(item)
+    return DummyTestResult
+}
+
+internal actual inline fun retryTest(retries: Int, test: (Int) -> TestResult): TestResult {
+    lateinit var lastCause: Throwable
+    repeat(retries + 1) { attempt ->
+        try {
+            return test(attempt)
+        } catch (cause: Throwable) {
+            lastCause = cause
+        }
+    }
+    throw lastCause
+}
