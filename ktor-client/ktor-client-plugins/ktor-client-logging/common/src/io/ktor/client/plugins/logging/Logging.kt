4722f0113d2121bc0aa9dc543cd139d8c42b619a
Aleksei.Tirman <aleksei.tirman@jetbrains.com>
2024-12-17 14:42:17 +0200

    KTOR-7806 Add logging with standard Okhttp-like format

--- ktor-client/ktor-client-plugins/ktor-client-logging/common/src/io/ktor/client/plugins/logging/Logging.kt
+++ ktor-client/ktor-client-plugins/ktor-client-logging/common/src/io/ktor/client/plugins/logging/Logging.kt
@@ -7,16 +7,25 @@ package io.ktor.client.plugins.logging
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.plugins.api.*
+import io.ktor.client.plugins.isSaved
 import io.ktor.client.plugins.observer.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
+import io.ktor.client.utils.*
 import io.ktor.http.*
 import io.ktor.http.content.*
 import io.ktor.util.*
 import io.ktor.util.pipeline.*
 import io.ktor.utils.io.*
 import io.ktor.utils.io.charsets.*
-import kotlinx.coroutines.*
+import io.ktor.utils.io.core.readText
+import io.ktor.utils.io.core.writeFully
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.io.Buffer
 
 private val ClientCallLogger = AttributeKey<HttpClientCallLogger>("CallLogger")
 private val DisableLogging = AttributeKey<Unit>("DisableLogging")
@@ -30,6 +39,12 @@ public class LoggingConfig {
     internal val sanitizedHeaders = mutableListOf<SanitizedHeader>()
 
     private var _logger: Logger? = null
+
+    /**
+     * If true, turns on the [OkHttp logging format](https://github.com/square/okhttp/blob/master/okhttp-logging-interceptor/src/main/kotlin/okhttp3/logging/HttpLoggingInterceptor.kt#L56).
+     * Writes only application-level logs because the low-level HTTP communication is hidden within the engine implementations.
+     */
+    public var standardFormat: Boolean = false
 
     /**
      * Specifies a [Logger] instance.
@@ -69,14 +84,311 @@ public class LoggingConfig {
  *
  * You can learn more from [Logging](https://ktor.io/docs/client-logging.html).
  */
-@OptIn(InternalAPI::class)
+@OptIn(InternalAPI::class, DelicateCoroutinesApi::class)
 public val Logging: ClientPlugin<LoggingConfig> = createClientPlugin("Logging", ::LoggingConfig) {
     val logger: Logger = pluginConfig.logger
     val level: LogLevel = pluginConfig.level
     val filters: List<(HttpRequestBuilder) -> Boolean> = pluginConfig.filters
     val sanitizedHeaders: List<SanitizedHeader> = pluginConfig.sanitizedHeaders
+    val stdFormat = pluginConfig.standardFormat
 
     fun shouldBeLogged(request: HttpRequestBuilder): Boolean = filters.isEmpty() || filters.any { it(request) }
+
+    fun isNone(): Boolean = level == LogLevel.NONE
+    fun isInfo(): Boolean = level == LogLevel.INFO
+    fun isHeaders(): Boolean = level == LogLevel.HEADERS
+    fun isBody(): Boolean = level == LogLevel.BODY || level == LogLevel.ALL
+
+    /**
+     * Detects if the body is a binary data
+     * @return
+     * Boolean: true if the body is a binary data.
+     * Long?: body size if calculated.
+     * ByteReadChannel: body channel with the original data.
+     */
+    suspend fun detectIfBinary(body: ByteReadChannel, contentLength: Long?, contentType: ContentType?, headers: Headers): Triple<Boolean, Long?, ByteReadChannel> {
+        if (headers.contains(HttpHeaders.ContentEncoding)) {
+            return Triple(true, contentLength, body)
+        }
+
+        val charset = if (contentType != null) {
+            contentType.charset() ?: Charsets.UTF_8
+        } else {
+            Charsets.UTF_8
+        }
+
+        var isBinary = false
+        val firstChunk = ByteArray(1024)
+        val firstReadSize = body.readAvailable(firstChunk)
+
+        if (firstReadSize < 1) {
+            return Triple(false, 0L, body)
+        }
+
+        val buffer = Buffer().apply { writeFully(firstChunk, 0, firstReadSize) }
+        val firstChunkText = charset.newDecoder().decode(buffer, firstReadSize)
+
+        var lastCharIndex = -1
+        for (ch in firstChunkText) {
+            lastCharIndex += 1
+        }
+
+        for ((i, ch) in firstChunkText.withIndex()) {
+            if (ch == '\ufffd' && i != lastCharIndex) {
+                isBinary = true
+                break
+            }
+        }
+
+        if (!isBinary) {
+            val channel = ByteChannel()
+
+            val copied = client.async {
+                channel.writeFully(firstChunk, 0, firstReadSize)
+                val copied = body.copyTo(channel)
+                channel.flushAndClose()
+                copied
+            }.await()
+
+            return Triple(isBinary, copied + firstReadSize, channel)
+        }
+
+        return Triple(isBinary, contentLength, body)
+    }
+
+    suspend fun logRequestBody(content: OutgoingContent, contentLength: Long?, headers: Headers, method: HttpMethod, body: ByteReadChannel) {
+        val (isBinary, size, newBody) = detectIfBinary(body, contentLength, content.contentType, headers)
+
+        if (!isBinary) {
+            val contentType = content.contentType
+            val charset = if (contentType != null) {
+                contentType.charset() ?: Charsets.UTF_8
+            } else {
+                Charsets.UTF_8
+            }
+
+            logger.log(newBody.readRemaining().readText(charset = charset))
+            logger.log("--> END ${method.value} ($size-byte body)")
+        } else {
+            var type = "binary"
+            if (headers.contains(HttpHeaders.ContentEncoding)) {
+                type = "encoded"
+            }
+
+            if (size != null) {
+                logger.log("--> END ${method.value} ($type $size-byte body omitted)")
+            } else {
+                logger.log("--> END ${method.value} ($type body omitted)")
+            }
+        }
+    }
+
+    suspend fun logOutgoingContent(content: OutgoingContent, method: HttpMethod, headers: Headers, process: (ByteReadChannel) -> ByteReadChannel = { it }): OutgoingContent? {
+        return when(content) {
+            is OutgoingContent.ByteArrayContent -> {
+                val bytes = content.bytes()
+                logRequestBody(content, bytes.size.toLong(), headers, method, ByteReadChannel(bytes))
+                null
+            }
+            is OutgoingContent.ContentWrapper -> {
+                logOutgoingContent(content.delegate(), method, headers, process)
+            }
+            is OutgoingContent.NoContent -> {
+                logger.log("--> END ${method.value}")
+                null
+            }
+            is OutgoingContent.ProtocolUpgrade -> {
+                logger.log("--> END ${method.value}")
+                null
+            }
+            is OutgoingContent.ReadChannelContent -> {
+                val (origChannel, newChannel) = content.readFrom().split(client)
+                logRequestBody(content, content.contentLength, headers, method, newChannel)
+                LoggedContent(content, origChannel)
+            }
+            is OutgoingContent.WriteChannelContent -> {
+                val channel = ByteChannel()
+
+                client.launch {
+                    content.writeTo(channel)
+                    channel.close()
+                }
+
+                val (origChannel, newChannel) = channel.split(client)
+                logRequestBody(content, content.contentLength, headers, method, newChannel)
+                LoggedContent(content, origChannel)
+            }
+        }
+    }
+
+    suspend fun logRequestStdFormat(request: HttpRequestBuilder): OutgoingContent? {
+        if (isNone()) return null
+
+        val uri = URLBuilder().takeFrom(request.url).build().pathQuery()
+        val body = request.body
+        val headers = HeadersBuilder().apply {
+            if (body is OutgoingContent && request.method != HttpMethod.Get && request.method != HttpMethod.Head && body !is EmptyContent) {
+                body.contentType?.let {
+                    appendIfNameAbsent(HttpHeaders.ContentType, it.toString())
+                }
+                body.contentLength?.let {
+                    appendIfNameAbsent(HttpHeaders.ContentLength, it.toString())
+                }
+            }
+            appendAll(request.headers)
+        }.build()
+
+        val contentLength = headers[HttpHeaders.ContentLength]?.toLongOrNull()
+        val startLine = when {
+            (request.method == HttpMethod.Get) || (request.method == HttpMethod.Head)
+                || ((isHeaders() || isBody()) && contentLength != null)
+                || (isHeaders() && contentLength == null)
+                || headers.contains(HttpHeaders.ContentEncoding) -> "--> ${request.method.value} $uri"
+
+            isInfo() && contentLength != null -> "--> ${request.method.value} $uri ($contentLength-byte body)"
+
+            body is OutgoingContent.WriteChannelContent
+                || body is OutgoingContent.ReadChannelContent -> "--> ${request.method.value} $uri (unknown-byte body)"
+
+            else -> {
+                val size = calcRequestBodySize(request.body, headers)
+                "--> ${request.method.value} $uri ($size-byte body)"
+            }
+        }
+
+        logger.log(startLine)
+
+        if (!isHeaders() && !isBody()) {
+            return null
+        }
+
+        for ((name, values) in headers.entries()) {
+            if (sanitizedHeaders.find { sh -> sh.predicate(name) } == null) {
+                logger.log("$name: ${values.joinToString(separator = ", ")}")
+            } else {
+                logger.log("$name: ██")
+            }
+        }
+
+        if (!isBody() || request.method == HttpMethod.Get || request.method == HttpMethod.Head) {
+            logger.log("--> END ${request.method.value}")
+            return null
+        }
+
+        logger.log("")
+
+        if (body !is OutgoingContent) {
+            logger.log("--> END ${request.method.value}")
+            return null
+        }
+
+        val newContent = if (request.headers[HttpHeaders.ContentEncoding] == "gzip") {
+             logOutgoingContent(body, request.method, headers) { channel ->
+                GZipEncoder.decode(channel)
+            }
+        } else {
+            logOutgoingContent(body, request.method, headers)
+        }
+
+        return newContent
+    }
+
+    suspend fun logResponseBody(response: HttpResponse, body: ByteReadChannel) {
+        logger.log("")
+
+        val (isBinary, size, newBody) = detectIfBinary(body, response.contentLength(), response.contentType(), response.headers)
+        val duration = response.responseTime.timestamp - response.requestTime.timestamp
+
+        if (size == 0L) {
+            logger.log("<-- END HTTP (${duration}ms, $size-byte body)")
+            return
+        }
+
+        if (!isBinary) {
+            val contentType = response.contentType()
+            val charset = if (contentType != null) {
+                contentType.charset() ?: Charsets.UTF_8
+            } else {
+                Charsets.UTF_8
+            }
+
+            logger.log(newBody.readRemaining().readText(charset = charset))
+            logger.log("<-- END HTTP (${duration}ms, $size-byte body)")
+        } else {
+            var type = "binary"
+            if (response.headers.contains(HttpHeaders.ContentEncoding)) {
+                type = "encoded"
+            }
+
+            if (size != null) {
+                logger.log("<-- END HTTP (${duration}ms, $type $size-byte body omitted)")
+            } else {
+                logger.log("<-- END HTTP (${duration}ms, $type body omitted)")
+            }
+        }
+    }
+
+    suspend fun logResponseStdFormat(response: HttpResponse): HttpResponse {
+        if (isNone()) return response
+
+        val contentLength = response.headers[HttpHeaders.ContentLength]?.toLongOrNull()
+        val request = response.request
+        val duration = response.responseTime.timestamp - response.requestTime.timestamp
+
+        val startLine = when {
+            response.headers[HttpHeaders.TransferEncoding] == "chunked"
+                && (isInfo() || isHeaders()) ->  "<-- ${response.status} ${request.url.pathQuery()} (${duration}ms, unknown-byte body)"
+
+            isInfo() && contentLength != null -> "<-- ${response.status} ${request.url.pathQuery()} (${duration}ms, $contentLength-byte body)"
+
+            isBody() || (isInfo() && contentLength == null) || (isHeaders() && contentLength != null)
+                || (response.headers[HttpHeaders.ContentEncoding] == "gzip") -> "<-- ${response.status} ${request.url.pathQuery()} (${duration}ms)"
+
+            else -> "<-- ${response.status} ${request.url.pathQuery()} (${duration}ms, unknown-byte body)"
+        }
+
+        logger.log(startLine)
+
+        if (!isHeaders() && !isBody()) {
+            return response
+        }
+
+        for ((name, values) in response.headers.entries()) {
+            if (sanitizedHeaders.find { sh -> sh.predicate(name) } == null) {
+                logger.log("$name: ${values.joinToString(separator = ", ")}")
+            } else {
+                logger.log("$name: ██")
+            }
+        }
+
+        if (!isBody()) {
+            logger.log("<-- END HTTP")
+            return response
+        }
+
+        if (contentLength != null && contentLength == 0L) {
+            logger.log("<-- END HTTP (${duration}ms, $contentLength-byte body)")
+            return response
+        }
+
+        if (response.contentType() == ContentType.Text.EventStream) {
+            logger.log("<-- END HTTP (streaming)")
+            return response
+        }
+
+        if (response.isSaved) {
+            logResponseBody(response, response.rawContent)
+            return response
+        }
+
+        val (origChannel, newChannel) = response.rawContent.split(response)
+
+        logResponseBody(response, newChannel)
+
+        val call = response.call.wrapWithContent(origChannel)
+        return call.response
+    }
+
 
     @OptIn(DelicateCoroutinesApi::class)
     suspend fun logRequestBody(
@@ -165,6 +477,23 @@ public val Logging: ClientPlugin<LoggingConfig> = createClientPlugin("Logging", 
             return@on
         }
 
+        if (stdFormat) {
+            val content = logRequestStdFormat(request)
+
+            try {
+                if (content != null) {
+                    proceedWith(content)
+                } else {
+                    proceed()
+                }
+            } catch (cause: Throwable) {
+                logger.log("<-- HTTP FAILED: $cause")
+                throw cause
+            }
+
+            return@on
+        }
+
         val loggedRequest = try {
             logRequest(request)
         } catch (_: Throwable) {
@@ -180,7 +509,18 @@ public val Logging: ClientPlugin<LoggingConfig> = createClientPlugin("Logging", 
         }
     }
 
+    on(ResponseAfterEncodingHook) { response ->
+        if (stdFormat) {
+            val newResponse = logResponseStdFormat(response)
+            if (newResponse != response) {
+                proceedWith(newResponse)
+            }
+        }
+    }
+
     on(ResponseHook) { response ->
+        if (stdFormat) return@on
+
         if (level == LogLevel.NONE || response.call.attributes.contains(DisableLogging)) return@on
 
         val callLogger = response.call.attributes[ClientCallLogger]
@@ -201,6 +541,8 @@ public val Logging: ClientPlugin<LoggingConfig> = createClientPlugin("Logging", 
     }
 
     on(ReceiveHook) { call ->
+        if (stdFormat) return@on
+
         if (level == LogLevel.NONE || call.attributes.contains(DisableLogging)) {
             return@on
         }
@@ -216,6 +558,8 @@ public val Logging: ClientPlugin<LoggingConfig> = createClientPlugin("Logging", 
             throw cause
         }
     }
+
+    if (stdFormat) return@createClientPlugin
 
     if (!level.body) return@createClientPlugin
 
@@ -237,6 +581,34 @@ public val Logging: ClientPlugin<LoggingConfig> = createClientPlugin("Logging", 
     }
 
     ResponseObserver.install(ResponseObserver.prepare { onResponse(observer) }, client)
+}
+
+private fun Url.pathQuery(): String {
+    return buildString {
+        if (encodedPath.isEmpty()) {
+            append("/")
+        } else {
+            append(encodedPath)
+        }
+
+        if (encodedQuery.isEmpty()) return@buildString
+
+        append("?")
+        append(encodedQuery)
+    }
+}
+
+@OptIn(DelicateCoroutinesApi::class)
+private fun calcRequestBodySize(content: Any, headers: Headers): Long {
+    check(content is OutgoingContent)
+
+    return when(content) {
+        is OutgoingContent.ByteArrayContent -> content.bytes().size.toLong()
+        is OutgoingContent.ContentWrapper -> calcRequestBodySize(content.delegate(), content.headers)
+        is OutgoingContent.NoContent -> 0
+        is OutgoingContent.ProtocolUpgrade -> 0
+        else -> error("Unable to calculate the size for type ${content::class.simpleName}")
+    }
 }
 
 /**
@@ -268,10 +640,29 @@ private object ResponseHook : ClientHook<suspend ResponseHook.Context.(response:
     }
 }
 
+private object ResponseAfterEncodingHook : ClientHook<suspend ResponseAfterEncodingHook.Context.(response: HttpResponse) -> Unit> {
+
+    class Context(private val context: PipelineContext<HttpResponse, Unit>) {
+        suspend fun proceedWith(response: HttpResponse) = context.proceedWith(response)
+    }
+
+    override fun install(
+        client: HttpClient,
+        handler: suspend Context.(response: HttpResponse) -> Unit
+    ) {
+        val afterState = PipelinePhase("AfterState")
+        client.receivePipeline.insertPhaseAfter(HttpReceivePipeline.State, afterState)
+        client.receivePipeline.intercept(afterState) {
+            handler(Context(this), subject)
+        }
+    }
+}
+
 private object SendHook : ClientHook<suspend SendHook.Context.(response: HttpRequestBuilder) -> Unit> {
 
     class Context(private val context: PipelineContext<Any, HttpRequestBuilder>) {
         suspend fun proceedWith(content: Any) = context.proceedWith(content)
+        suspend fun proceed() = context.proceed()
     }
 
     override fun install(
