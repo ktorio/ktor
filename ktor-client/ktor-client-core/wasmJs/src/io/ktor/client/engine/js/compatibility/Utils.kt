5ff1a3cabe58bcf664fcfdde0800680b397aa3b6
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2025-01-28 09:46:03 +0100

    KTOR-7734 Do not abort fetch after its completion (#4629)

--- ktor-client/ktor-client-core/wasmJs/src/io/ktor/client/engine/js/compatibility/Utils.kt
+++ ktor-client/ktor-client-core/wasmJs/src/io/ktor/client/engine/js/compatibility/Utils.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2023 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2014-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package io.ktor.client.engine.js.compatibility
@@ -10,14 +10,26 @@ import io.ktor.client.fetch.*
 import io.ktor.client.utils.*
 import io.ktor.util.*
 import io.ktor.utils.io.*
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlin.js.Promise
 
+@OptIn(InternalCoroutinesApi::class)
 internal suspend fun commonFetch(
     input: String,
     init: RequestInit,
     config: JsClientEngineConfig,
+    callJob: Job,
 ): org.w3c.fetch.Response = suspendCancellableCoroutine { continuation ->
+    val controller = AbortController()
+    init.signal = controller.signal
+
+    val abortOnCallCompletion = callJob.invokeOnCompletion(onCancelling = true) {
+        controller.abort()
+    }
+
     val promise: Promise<org.w3c.fetch.Response> = when {
         PlatformUtils.IS_BROWSER -> fetch(input, init)
         else -> {
@@ -40,16 +52,15 @@ internal suspend fun commonFetch(
             continuation.resumeWith(Result.failure(Error("Fail to fetch", JsError(it))))
             null
         }
-    )
+    ).finally { abortOnCallCompletion.dispose() }
 }
 
-private fun abortControllerCtorBrowser(): AbortController =
-    js("AbortController")
-
-internal fun AbortController(): AbortController {
+private fun AbortController(): AbortController {
     val ctor = abortControllerCtorBrowser()
     return makeJsNew(ctor)
 }
+
+private fun abortControllerCtorBrowser(): AbortController = js("AbortController")
 
 internal fun CoroutineScope.readBody(
     response: org.w3c.fetch.Response
