ff38089d85fc000a9a45b7b1fa3724cc99e12691
thomas <thomasjsvos@gmail.com>
2025-01-31 03:13:36 +0100

    fixup! KTOR-8135 Socket accept not throwing error on socket close on native

--- ktor-network/jsAndWasmShared/src/io/ktor/network/sockets/ServerSocketContext.kt
+++ ktor-network/jsAndWasmShared/src/io/ktor/network/sockets/ServerSocketContext.kt
@@ -55,7 +55,7 @@ internal class ServerSocketContext(
 private class ServerSocketImpl(
     override val localAddress: SocketAddress,
     override val socketContext: Job,
-    private val incoming: ReceiveChannel<Socket>,
+    private val incoming: Channel<Socket>,
     private val server: Server
 ) : ServerSocket {
     override suspend fun accept(): Socket = incoming.receive()
@@ -68,6 +68,7 @@ private class ServerSocketImpl(
     }
 
     override fun close() {
+        incoming.close(IOException("Server socket closed"))
         socketContext.cancel("Server socket closed")
     }
 }
