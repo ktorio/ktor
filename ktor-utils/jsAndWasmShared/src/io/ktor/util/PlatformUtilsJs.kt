563432516803635c879b0b22bf9145a55e494bce
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2025-02-06 11:46:20 +0100

    Remove internal deprecated isNewMemoryModel

--- ktor-utils/jsAndWasmShared/src/io/ktor/util/PlatformUtilsJs.kt
+++ ktor-utils/jsAndWasmShared/src/io/ktor/util/PlatformUtilsJs.kt
@@ -1,19 +1,11 @@
 /*
- * Copyright 2014-2024 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2014-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package io.ktor.util
 
 internal actual val PlatformUtils.isDevelopmentMode: Boolean
     get() = false
-
-@Deprecated(
-    "New memory model is now enabled by default. The property will be removed in the future.",
-    level = DeprecationLevel.WARNING,
-    replaceWith = ReplaceWith("true")
-)
-internal actual val PlatformUtils.isNewMemoryModel: Boolean
-    get() = true
 
 internal fun hasNodeApi(): Boolean = js(
 """
--- ktor-utils/jvm/src/io/ktor/util/PlatformUtilsJvm.kt
+++ ktor-utils/jvm/src/io/ktor/util/PlatformUtilsJvm.kt
@@ -1,6 +1,6 @@
 /*
-* Copyright 2014-2021 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
-*/
+ * Copyright 2014-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
 
 package io.ktor.util
 
@@ -11,11 +11,3 @@ public actual val PlatformUtils.platform: Platform
 
 internal actual val PlatformUtils.isDevelopmentMode: Boolean
     get() = System.getProperty(DEVELOPMENT_MODE_KEY)?.toBoolean() == true
-
-@Deprecated(
-    "New memory model is now enabled by default. The property will be removed in the future.",
-    level = DeprecationLevel.WARNING,
-    replaceWith = ReplaceWith("true")
-)
-internal actual val PlatformUtils.isNewMemoryModel: Boolean
-    get() = true
