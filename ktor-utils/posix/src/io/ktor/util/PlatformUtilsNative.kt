47326f726e310d83e4ef16e5bd5e31bbcf217588
Anton Erofeev <antonerofeev11@gmail.com>
2025-02-06 18:30:01 +0300

    Remove use of Platform.isExperimentalMM (#4646)

--- ktor-utils/posix/src/io/ktor/util/PlatformUtilsNative.kt
+++ ktor-utils/posix/src/io/ktor/util/PlatformUtilsNative.kt
@@ -1,15 +1,11 @@
 /*
- * Copyright 2014-2024 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2014-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package io.ktor.util
 
 internal actual val PlatformUtils.isDevelopmentMode: Boolean
     get() = false
-
-@OptIn(ExperimentalStdlibApi::class)
-internal actual val PlatformUtils.isNewMemoryModel: Boolean
-    get() = isExperimentalMM()
 
 public actual val PlatformUtils.platform: Platform
     get() = Platform.Native
