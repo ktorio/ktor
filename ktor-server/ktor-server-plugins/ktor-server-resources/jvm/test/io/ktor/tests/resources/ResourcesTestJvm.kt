7f0979ac4398e8c009a222c15bdde9f807e0e587
Leonid Stashevsky <e5l@users.noreply.github.com>
2025-02-12 10:58:27 +0100

    KTOR-6671 Fix Resources: a / route isn't resolved when there is a sibling (#4667)

--- ktor-server/ktor-server-plugins/ktor-server-resources/jvm/test/io/ktor/tests/resources/ResourcesTestJvm.kt
+++ ktor-server/ktor-server-plugins/ktor-server-resources/jvm/test/io/ktor/tests/resources/ResourcesTestJvm.kt
@@ -4,15 +4,25 @@
 
 package io.ktor.tests.resources
 
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
 import io.ktor.resources.*
+import io.ktor.server.http.content.*
 import io.ktor.server.resources.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import kotlinx.serialization.*
-import kotlinx.serialization.descriptors.*
-import kotlinx.serialization.encoding.*
-import java.math.*
-import kotlin.test.*
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import java.math.BigDecimal
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class ResourcesTestJvm {
 
@@ -56,5 +66,26 @@ class ResourcesTestJvm {
             LocationWithBigNumbers(bd, bi),
             "/?bd=123456789012345678901234567890&bi=123456789012345678901234567890"
         )
+    }
+
+    @Resource("/")
+    class Home
+
+    @Test
+    fun testHomeResourceWithStaticResource() = testResourcesApplication {
+        var executed = false
+        routing {
+            get<Home> {
+                executed = true
+                call.respondText("OK")
+            }
+            staticResources("/", "static")
+        }
+
+        client.get("/").let { response ->
+            assertTrue(executed)
+            assertEquals(HttpStatusCode.OK, response.status)
+            assertEquals("OK", response.bodyAsText())
+        }
     }
 }
