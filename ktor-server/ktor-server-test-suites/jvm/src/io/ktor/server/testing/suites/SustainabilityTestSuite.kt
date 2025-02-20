f4c7ab0cd8a40f3690280fb3e2f304b057f04f67
Leonid Stashevsky <e5l@users.noreply.github.com>
2025-02-20 07:22:17 +0100

    KTOR-7139 Fix exception thrown in onCallRespond makes the client wait… (#4610)
    * KTOR-7139 Fux exception thrown in onCallRespond makes the client wait for a response indefinitely

--- ktor-server/ktor-server-test-suites/jvm/src/io/ktor/server/testing/suites/SustainabilityTestSuite.kt
+++ ktor-server/ktor-server-test-suites/jvm/src/io/ktor/server/testing/suites/SustainabilityTestSuite.kt
@@ -28,15 +28,21 @@ import io.ktor.utils.io.core.*
 import io.ktor.utils.io.jvm.javaio.*
 import io.ktor.utils.io.streams.*
 import kotlinx.coroutines.*
-import kotlinx.coroutines.debug.*
-import org.slf4j.*
-import java.io.*
-import java.net.*
+import kotlinx.coroutines.debug.DebugProbes
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.slf4j.Marker
+import org.slf4j.event.Level
+import org.slf4j.helpers.AbstractLogger
+import java.io.File
+import java.io.IOException
+import java.net.HttpURLConnection
+import java.net.Proxy
+import java.net.URL
 import java.util.*
 import java.util.concurrent.*
-import java.util.concurrent.atomic.*
+import java.util.concurrent.atomic.AtomicInteger
 import kotlin.test.*
-import kotlin.use
 
 abstract class SustainabilityTestSuite<TEngine : ApplicationEngine, TConfiguration : ApplicationEngine.Configuration>(
     hostFactory: ApplicationEngineFactory<TEngine, TConfiguration>
@@ -790,11 +796,10 @@ abstract class SustainabilityTestSuite<TEngine : ApplicationEngine, TConfigurati
         val loggerDelegate = LoggerFactory.getLogger("ktor.test")
         val logger = object : Logger by loggerDelegate {
             override fun error(message: String?, cause: Throwable?) {
-                println(cause.toString())
                 exceptions.add(cause!!)
             }
         }
-        val phase = EnginePipeline.Before
+
         val server = createServer(log = logger) {
             routing {
                 get("/req") {
@@ -802,7 +807,10 @@ abstract class SustainabilityTestSuite<TEngine : ApplicationEngine, TConfigurati
                 }
             }
         }
-        (server.engine as BaseApplicationEngine).pipeline.intercept(phase) {
+
+        val phase = EnginePipeline.Before
+        val pipeline = (server.engine as BaseApplicationEngine).pipeline
+        pipeline.intercept(phase) {
             throw IllegalStateException("Failed in engine pipeline")
         }
         startServer(server)
@@ -813,7 +821,7 @@ abstract class SustainabilityTestSuite<TEngine : ApplicationEngine, TConfigurati
             }
         }) {
             assertEquals(HttpStatusCode.InternalServerError, status, "Failed in engine pipeline")
-            assertEquals(exceptions.size, 1, "Failed in phase $phase")
+            assertEquals(1, exceptions.size, "Failed in phase $phase")
             assertEquals("Failed in engine pipeline", exceptions[0].message)
             exceptions.clear()
         }
@@ -918,6 +926,57 @@ abstract class SustainabilityTestSuite<TEngine : ApplicationEngine, TConfigurati
 
         assertTrue(failCause != null)
         assertIs<IOException>(failCause)
+    }
+
+    @Test
+    fun testOnCallRespondException() = runTest {
+        var loggedException: Throwable? = null
+        val log = object : AbstractLogger() {
+            override fun isTraceEnabled(): Boolean = false
+            override fun isTraceEnabled(marker: Marker?): Boolean = false
+            override fun isDebugEnabled(): Boolean = false
+            override fun isDebugEnabled(marker: Marker?): Boolean = false
+            override fun isInfoEnabled(): Boolean = false
+            override fun isInfoEnabled(marker: Marker?): Boolean = false
+            override fun isWarnEnabled(): Boolean = false
+            override fun isWarnEnabled(marker: Marker?): Boolean = false
+
+            override fun isErrorEnabled(): Boolean = true
+
+            override fun isErrorEnabled(marker: Marker?): Boolean = true
+
+            override fun getFullyQualifiedCallerName(): String = "TEST"
+
+            override fun handleNormalizedLoggingCall(
+                level: Level?,
+                marker: Marker?,
+                messagePattern: String?,
+                arguments: Array<out Any>?,
+                throwable: Throwable?
+            ) {
+                loggedException = throwable
+            }
+        }
+
+        createAndStartServer(log = log) {
+            application.install(
+                createApplicationPlugin("MyPlugin") {
+                    onCallRespond { _ ->
+                        error("oh nooooo")
+                    }
+                }
+            )
+
+            get {
+                call.respondText("hello world")
+            }
+        }
+
+        withUrl("") {
+            assertEquals(HttpStatusCode.InternalServerError, status)
+            assertNotNull(loggedException)
+            loggedException = null
+        }
     }
 }
 
