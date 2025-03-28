c6fa0f0c2846bf3274cd49e92ae6fb1a28b4cbc4
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2025-03-28 12:06:29 +0100

    Add a test showing the issue

--- ktor-client/ktor-client-tests/common/test/io/ktor/client/tests/ResponseObserverTest.kt
+++ ktor-client/ktor-client-tests/common/test/io/ktor/client/tests/ResponseObserverTest.kt
@@ -4,12 +4,13 @@
 
 package io.ktor.client.tests
 
+import io.ktor.client.engine.mock.*
+import io.ktor.client.plugins.*
 import io.ktor.client.plugins.observer.*
 import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.client.test.base.*
-import kotlin.test.Test
-import kotlin.test.assertFalse
-import kotlin.test.assertTrue
+import kotlin.test.*
 
 class ResponseObserverTest : ClientLoader() {
     private var observerCalled = false
@@ -90,6 +91,46 @@ class ResponseObserverTest : ClientLoader() {
                 parameter("size", (1024 * 10).toString())
             }
             assertFalse { observerCalled }
+        }
+    }
+
+    @Test
+    fun testSavedResponseCanBeReadMultipleTimes() = testWithEngine(MockEngine) {
+        val bodyContent = "Hello"
+        var wasSaved: Boolean? = null
+
+        suspend fun assertBodyCanBeReadMultipleTimes(response: HttpResponse) {
+            assertEquals(bodyContent, response.bodyAsText(), "First read failed")
+            assertEquals(bodyContent, response.bodyAsText(), "It should be possible to read body multiple times")
+        }
+
+        config {
+            install(ResponseObserver) {
+                onResponse { response ->
+                    wasSaved = response.isSaved
+                    // In the response observer itself
+                    assertBodyCanBeReadMultipleTimes(response)
+                }
+            }
+
+            engine {
+                addHandler { respondOk(bodyContent) }
+            }
+        }
+
+        test { client ->
+            // In the pipeline after ResponseObserver
+            client.receivePipeline.intercept(HttpReceivePipeline.After) { response ->
+                assertBodyCanBeReadMultipleTimes(response)
+                proceedWith(response)
+            }
+
+            val response = client.get("/")
+            assertNotNull(wasSaved, "Response observer should be called")
+            assertTrue(wasSaved!!, "Response should be saved before reaching the response observer")
+
+            // After all pipelines
+            assertBodyCanBeReadMultipleTimes(response)
         }
     }
 }
