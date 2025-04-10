0293116c6088ee84cbad76447446399b01bb0463
Chris Lemaire <chris.lemaire@jetbrains.com>
2025-04-10 09:12:04 +0200

    fixup! Add ',' to parameter quoting terminals in HttpHeaderValueParser
    rename function

--- ktor-http/common/src/io/ktor/http/HttpHeaderValueParser.kt
+++ ktor-http/common/src/io/ktor/http/HttpHeaderValueParser.kt
@@ -213,7 +213,7 @@ private fun parseHeaderValueParameterValueQuoted(value: String, start: Int): Pai
         val currentChar = value[position]
 
         when {
-            currentChar == '"' && value.nextIsSemicolonOrEnd(position) -> {
+            currentChar == '"' && value.nextIsDelimiterOrEnd(position) -> {
                 return position + 1 to builder.toString()
             }
 
@@ -232,7 +232,7 @@ private fun parseHeaderValueParameterValueQuoted(value: String, start: Int): Pai
     return position to '"' + builder.toString()
 }
 
-private fun String.nextIsSemicolonOrEnd(start: Int): Boolean {
+private fun String.nextIsDelimiterOrEnd(start: Int): Boolean {
     var position = start + 1
     loop@ while (position < length && get(position) == ' ') {
         position += 1
