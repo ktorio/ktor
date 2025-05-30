39b03686dda60a11ef0b3c433238686f84b6cce9
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2025-05-30 16:12:54 +0200

    KTOR-7892 swagger: support OAuth2 authentication flow
    Expose route serving oauth2-redirect.html to enable OAuth2 authorization redirect handling in Swagger UI

--- ktor-server/ktor-server-plugins/ktor-server-swagger/jvm/test/io/ktor/server/plugins/swagger/SwaggerTest.kt
+++ ktor-server/ktor-server-plugins/ktor-server-swagger/jvm/test/io/ktor/server/plugins/swagger/SwaggerTest.kt
@@ -37,6 +37,7 @@ class SwaggerTest {
                     url: '/swagger/documentation.yaml',
                     dom_id: '#swagger-ui',
                     deepLinking: false,
+                    oauth2RedirectUrl: window.location.origin + '/swagger/oauth2-redirect.html',
                     presets: [
                         SwaggerUIBundle.presets.apis,
                         SwaggerUIStandalonePreset
@@ -79,6 +80,7 @@ class SwaggerTest {
                     url: '/swagger/documentation.yaml',
                     dom_id: '#swagger-ui',
                     deepLinking: true,
+                    oauth2RedirectUrl: window.location.origin + '/swagger/oauth2-redirect.html',
                     presets: [
                         SwaggerUIBundle.presets.apis,
                         SwaggerUIStandalonePreset
@@ -121,6 +123,7 @@ class SwaggerTest {
                     url: '/swagger/documentation.yaml',
                     dom_id: '#swagger-ui',
                     deepLinking: false,
+                    oauth2RedirectUrl: window.location.origin + '/swagger/oauth2-redirect.html',
                     presets: [
                         SwaggerUIBundle.presets.apis,
                         SwaggerUIStandalonePreset
@@ -178,6 +181,7 @@ class SwaggerTest {
                     url: '/swagger/documentation.yaml',
                     dom_id: '#swagger-ui',
                     deepLinking: false,
+                    oauth2RedirectUrl: window.location.origin + '/swagger/oauth2-redirect.html',
                     presets: [
                         SwaggerUIBundle.presets.apis,
                         SwaggerUIStandalonePreset
@@ -189,6 +193,99 @@ class SwaggerTest {
               </body>
             </html>
             
+            """.trimIndent(),
+            response
+        )
+    }
+
+    @Test
+    fun testSwaggerFromResources2() = testApplication {
+        routing {
+            swaggerUI("swagger")
+        }
+
+        val response = client.get("/swagger/oauth2-redirect.html").bodyAsText()
+        assertEquals(
+            """<!DOCTYPE html>
+<html>
+  <head>
+    <title>Swagger UI: OAuth2 Redirect</title>
+  </head>
+  <body>
+    <script>'use strict';
+(function () {
+    function run () {
+        var oauth2 = window.opener.swaggerUIRedirectOauth2;
+        var sentState = oauth2.state;
+        var redirectUrl = oauth2.redirectUrl;
+        var isValid, qp, arr;
+
+        if (/code|token|error/.test(window.location.hash)) {
+            qp = window.location.hash.substring(1).replace('?', '&');
+        } else {
+            qp = location.search.substring(1);
+        }
+
+        arr = qp.split("&")
+        arr.forEach(function (v,i,_arr) { _arr[i] = '"' + v.replace('=', '":"') + '"';})
+        qp = qp ? JSON.parse('{' + arr.join() + '}',
+                function (key, value) {
+                    return key === "" ? value : decodeURIComponent(value)
+                }
+        ) : {}
+
+        isValid = qp.state === sentState
+
+        if ((
+          oauth2.auth.schema.get("flow") === "accessCode" ||
+          oauth2.auth.schema.get("flow") === "authorizationCode" ||
+          oauth2.auth.schema.get("flow") === "authorization_code"
+        ) && !oauth2.auth.code) {
+            if (!isValid) {
+                oauth2.errCb({
+                    authId: oauth2.auth.name,
+                    source: "auth",
+                    level: "warning",
+                    message: "Authorization may be unsafe, passed state was changed in server Passed state wasn't returned from auth server"
+                });
+            }
+
+            if (qp.code) {
+                delete oauth2.state;
+                oauth2.auth.code = qp.code;
+                oauth2.callback({auth: oauth2.auth, redirectUrl: redirectUrl});
+            } else {
+                let oauthErrorMsg
+                if (qp.error) {
+                    oauthErrorMsg = "["+qp.error+"]: " +
+                        (qp.error_description ? qp.error_description+ ". " : "no accessCode received from the server. ") +
+                        (qp.error_uri ? "More info: "+qp.error_uri : "");
+                }
+
+                oauth2.errCb({
+                    authId: oauth2.auth.name,
+                    source: "auth",
+                    level: "error",
+                    message: oauthErrorMsg || "[Authorization failed]: no accessCode received from the server"
+                });
+            }
+        } else {
+            oauth2.callback({auth: oauth2.auth, token: qp, isValid: isValid, redirectUrl: redirectUrl});
+        }
+        window.close();
+    }
+
+    if( document.readyState !== 'loading' ) {
+      run();
+    } else {
+      document.addEventListener('DOMContentLoaded', function () {
+        run();
+      });
+    }
+})();</script>
+  </body>
+</html>
+
             """.trimIndent(),
             response
         )
