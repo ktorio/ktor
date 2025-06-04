1e484c24fa38ac2f538fe76b8f36a5c8fb370167
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2025-06-04 10:29:20 +0200

    KTOR-8558: Fix JS tasks running with configure-on-demand enabled (#4906)
    * Update yarn.lock

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@
  * Copyright 2014-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
+import ktorbuild.dependsOnSubprojects
+
 plugins {
     id("ktorbuild.doctor")
     id("ktorbuild.publish.verifier")
@@ -9,3 +11,8 @@ plugins {
 
 println("Build version: ${project.version}")
 println("Kotlin version: ${libs.versions.kotlin.get()}")
+
+// Workaround to make the ':packageJsonUmbrella' task compatible with configuration on demand
+// Issue: https://youtrack.jetbrains.com/issue/KT-55701
+tasks.named { it == "packageJsonUmbrella" }
+    .configureEach { dependsOnSubprojects() }
