0336cd7b16de662c128bb1d1f4a3ffd72ff2e802
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2025-06-20 16:50:13 +0200

    Add Gradle tasks to update libcurl

--- ktor-client/ktor-client-curl/build.gradle.kts
+++ ktor-client/ktor-client-curl/build.gradle.kts
@@ -3,6 +3,8 @@
  */
 
 import ktorbuild.createCInterop
+import ktorbuild.targets.KtorTargets
+import ktorbuild.vcpkg.*
 
 plugins {
     id("ktorbuild.project.library")
@@ -10,10 +12,14 @@ plugins {
     id("test-server")
 }
 
+val interopDir = file("desktop/interop")
+val includeDir = interopDir.resolve("include")
+fun libraryPath(target: String) = interopDir.resolve("lib/$target")
+
 kotlin {
     createCInterop("libcurl", sourceSet = "desktop") { target ->
-        includeDirs(file("desktop/interop/include"))
-        extraOpts("-libraryPath", file("desktop/interop/lib/$target"))
+        includeDirs(includeDir)
+        extraOpts("-libraryPath", libraryPath(target))
     }
 
     sourceSets {
@@ -29,3 +35,28 @@ kotlin {
         }
     }
 }
+
+//region Libcurl tasks
+// Register `libcurlUpdate[Target]` tasks updating libcurl binaries for the corresponding desktop target.
+// These tasks should be run only in the suitable environment to avoid cross-compilation.
+// To update libcurl binaries, run `libcurlUpdate*` on each platform.
+for (target in KtorTargets.resolveTargets("desktop")) {
+    val capitalizedTarget = target.replaceFirstChar { it.uppercase() }
+    val libcurlInstall = tasks.register<VcpkgInstall>("libcurlInstall$capitalizedTarget") {
+        install(manifestPath = "libcurl", target = target)
+        // Use overlays to link against system zlib (except for Windows)
+        useOverlays = target != "mingwX64"
+    }
+
+    // Headers are equal for all targets, so update them only once
+    val libcurlUpdateHeaders = if (target == "macosArm64") {
+        registerSyncHeadersTask("libcurlUpdateHeaders", from = libcurlInstall, into = includeDir, library = "curl")
+    } else {
+        null
+    }
+
+    registerSyncBinariesTask("libcurlUpdate$capitalizedTarget", from = libcurlInstall, into = libraryPath(target)) {
+        if (libcurlUpdateHeaders != null) dependsOn(libcurlUpdateHeaders)
+    }
+}
+//endregion
