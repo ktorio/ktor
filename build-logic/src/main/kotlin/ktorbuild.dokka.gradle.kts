d53397f6dfb746406dccd1e692d11c444eb932bb
Alexander Sysoev <Alexander.Sysoev@jetbrains.com>
2025-06-24 16:27:43 +0200

    Fix footer for non index.html pages

--- build-logic/src/main/kotlin/ktorbuild.dokka.gradle.kts
+++ build-logic/src/main/kotlin/ktorbuild.dokka.gradle.kts
@@ -2,6 +2,9 @@
  * Copyright 2014-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
+import org.jetbrains.dokka.gradle.engine.plugins.DokkaHtmlPluginParameters
+import java.time.Year
+
 plugins {
     id("org.jetbrains.dokka")
 }
@@ -11,6 +14,12 @@ dokka {
         sourceLink {
             localDirectory = rootDir
             remoteUrl("https://github.com/ktorio/ktor/blob/$version")
+        }
+    }
+
+    pluginsConfiguration.apply {
+        (this as ExtensionAware).extensions.configure<DokkaHtmlPluginParameters> {
+            footerMessage.set("© ${Year.now()} JetBrains s.r.o and contributors. Apache License 2.0")
         }
     }
 }
