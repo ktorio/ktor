8dd50d5ba9937342ce6fc2f5e3ef79006db8b09a
Aleksei Tirman <aleksei.tirman@jetbrains.com>
2025-07-04 10:35:22 +0300

    KTOR-5922 Discover HTTP proxy settings for Android

---
+++ ktor-client/ktor-client-cio/jvm/src/io/ktor/client/engine/cio/Proxy.jvm.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2014-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package io.ktor.client.engine.cio
+
+import io.ktor.client.engine.ProxyConfig
+import java.net.InetSocketAddress
+import java.net.Proxy
+
+internal actual fun discoverHttpProxy(): ProxyConfig? {
+    var proxy: Proxy? = null
+
+    try {
+        // For Android
+        val host = System.getProperty("http.proxyHost")
+        val port = System.getProperty("http.proxyPort").toIntOrNull() ?: 80
+
+        if (host != null && host.isNotEmpty()) {
+            proxy = Proxy(Proxy.Type.HTTP, InetSocketAddress(host, port))
+        }
+    } catch (_: SecurityException) {}
+
+    return proxy
+}
---
+++ ktor-client/ktor-client-cio/wasmJs/src/io/ktor/client/engine/cio/Proxy.wasmJs.kt
@@ -0,0 +1,9 @@
+/*
+ * Copyright 2014-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package io.ktor.client.engine.cio
+
+import io.ktor.client.engine.ProxyConfig
+
+//internal actual fun discoverHttpProxy(): ProxyConfig? = null
