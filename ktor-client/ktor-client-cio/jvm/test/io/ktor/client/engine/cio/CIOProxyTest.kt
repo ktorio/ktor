39a0272b73e30b41ac181233a4483c9df05ce12d
Aleksei Tirman <aleksei.tirman@jetbrains.com>
2025-07-04 11:50:53 +0300

    Fix

---
+++ ktor-client/ktor-client-cio/jvm/test/io/ktor/client/engine/cio/CIOProxyTest.kt
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2014-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package io.ktor.client.engine.cio
+
+import kotlin.test.Test
+
+class CIOProxyTest {
+    @Test
+    fun test() {
+        
+    }
+}
