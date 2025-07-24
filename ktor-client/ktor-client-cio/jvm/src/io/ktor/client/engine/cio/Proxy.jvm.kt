c9365876775c06914d26712beb785d8b8671eb6d
Aleksei Tirman <aleksei.tirman@jetbrains.com>
2025-07-24 15:10:51 +0300

    Check if the single proxy is direct

--- ktor-client/ktor-client-cio/jvm/src/io/ktor/client/engine/cio/Proxy.jvm.kt
+++ ktor-client/ktor-client-cio/jvm/src/io/ktor/client/engine/cio/Proxy.jvm.kt
@@ -7,13 +7,20 @@ package io.ktor.client.engine.cio
 import io.ktor.client.engine.ProxyConfig
 import io.ktor.http.Url
 import io.ktor.http.toURI
+import java.net.Proxy
 import java.net.ProxySelector
 
 internal actual fun lookupGlobalProxy(url: Url): ProxyConfig? {
     val proxies = ProxySelector.getDefault().select(url.toURI())
 
     return if (proxies.isNotEmpty()) {
-        proxies.first()
+        val proxy = proxies.first()
+
+        if (proxies.size == 1 && proxy.type() == Proxy.Type.DIRECT) {
+            null
+        } else {
+            proxies.first()
+        }
     } else {
         null
     }
