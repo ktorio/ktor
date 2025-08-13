0dddd77e5160150bd68b5a3f6e342283f19a6285
Margarita Bobova <margarita.bobova@jetbrains.com>
2025-08-13 20:29:05 +0200

    Kotlin 2.3 migration: KT-78304, Fix UPPER_BOUND_VIOLATED

--- build-logic/src/main/kotlin/ktorbuild/dsl/Collections.kt
+++ build-logic/src/main/kotlin/ktorbuild/dsl/Collections.kt
@@ -9,10 +9,7 @@ import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.NamedDomainObjectProvider
 import org.gradle.api.provider.Provider
 
-fun <T : Any> NamedDomainObjectContainer<T>.maybeRegister(
-    name: String,
-    configure: T.() -> Unit
-): NamedDomainObjectProvider<T> {
+fun <T : Any> NamedDomainObjectContainer<T>.maybeRegister(name: String, configure: T.() -> Unit): NamedDomainObjectProvider<T> {
     return if (name in names) named(name, configure) else register(name, configure)
 }
 
