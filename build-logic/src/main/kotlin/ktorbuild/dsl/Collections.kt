590fbbd1c405abe7500bb41492b2fdec20d10a4d
Margarita Bobova <margarita.bobova@jetbrains.com>
2025-08-13 20:29:05 +0200

    Kotlin 2.3 migration: KT-78304, Fix UPPER_BOUND_VIOLATED

--- build-logic/src/main/kotlin/ktorbuild/dsl/Collections.kt
+++ build-logic/src/main/kotlin/ktorbuild/dsl/Collections.kt
@@ -9,15 +9,12 @@ import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.NamedDomainObjectProvider
 import org.gradle.api.provider.Provider
 
-fun <T : Any> NamedDomainObjectContainer<T>.maybeRegister(
-    name: String,
-    configure: T.() -> Unit
-): NamedDomainObjectProvider<T> {
+fun <T : Any> NamedDomainObjectContainer<T>.maybeRegister(name: String, configure: T.() -> Unit): NamedDomainObjectProvider<T> {
     return if (name in names) named(name, configure) else register(name, configure)
 }
 
-inline fun <T, R> Provider<out Iterable<T>>.mapValue(crossinline transform: (T) -> R): Provider<List<R>> =
+inline fun <T : Any, R : Any> Provider<out Iterable<T>>.mapValue(crossinline transform: (T) -> R): Provider<List<R>> =
     map { it.map(transform) }
 
-inline fun <T, R> Provider<out Iterable<T>>.flatMapValue(crossinline transform: (T) -> Iterable<R>): Provider<List<R>> =
+inline fun <T : Any, R : Any> Provider<out Iterable<T>>.flatMapValue(crossinline transform: (T) -> Iterable<R>): Provider<List<R>> =
     map { it.flatMap(transform) }
