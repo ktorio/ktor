3e8654299dcc65ab81ff60ba5618d744e3997371
zibet27 <oleh.pantus@jetbrains.com>
2025-08-21 15:31:24 +0200

    WebRTC.rs Client. Remove android jvm/native targets.

--- ktor-client/ktor-client-webrtc/ktor-client-webrtc-rs/build.gradle.kts
+++ ktor-client/ktor-client-webrtc/ktor-client-webrtc-rs/build.gradle.kts
@@ -4,15 +4,13 @@
 import gobley.gradle.GobleyHost
 import gobley.gradle.cargo.dsl.*
 import gobley.gradle.Variant
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 description = "Ktor WebRTC Engine based on the WebRTC.rs and Gobley"
 
 plugins {
-    id("dev.gobley.rust") version "0.3.2"
-    id("dev.gobley.cargo") version "0.3.2"
-    id("dev.gobley.uniffi") version "0.3.2"
-    id("com.android.library")
+    id("dev.gobley.rust") version "0.3.3"
+    id("dev.gobley.cargo") version "0.3.3"
+    id("dev.gobley.uniffi") version "0.3.3"
     id("ktorbuild.project.library")
     kotlin("plugin.atomicfu") version libs.versions.kotlin.get()
 }
@@ -20,14 +18,12 @@ plugins {
 uniffi {
     generateFromLibrary {
         namespace = "ktor_client_webrtc"
-        // variant = gobley.gradle.Variant.Release is not supported yet for macOS on arm
+        variant = Variant.Release
     }
 }
 
 cargo {
-    // Configure build variants
-    jvmVariant = Variant.Debug
-    jvmPublishingVariant = Variant.Release
+    jvmVariant = Variant.Release
     nativeVariant = Variant.Release
 
     builds.jvm {
@@ -37,13 +33,6 @@ cargo {
 }
 
 kotlin {
-    androidTarget {
-        publishLibraryVariants("release")
-        compilerOptions {
-            jvmTarget = JvmTarget.JVM_17
-        }
-    }
-
     jvmToolchain(17)
 
     sourceSets {
@@ -60,37 +49,15 @@ kotlin {
 }
 
 // disable cross-compilation
+tasks.named { it == "cargoBuildLinuxX64Release" }.configureEach {
+    onlyIf { GobleyHost.Platform.Linux.isCurrent }
+}
+tasks.named { it == "cargoBuildLinuxArm64Release" }.configureEach {
+    onlyIf { GobleyHost.Platform.Linux.isCurrent }
+}
 tasks.named { it == "cinteropRustLinuxArm64" }.configureEach {
     onlyIf { GobleyHost.Platform.Linux.isCurrent }
 }
 tasks.named { it == "cinteropRustLinuxX64" }.configureEach {
     onlyIf { GobleyHost.Platform.Linux.isCurrent }
-}
-
-// Gobley's Android Native target depends on Android Gradle Plugin (not Android Gradle KMP Plugin) to use ndk toolchain
-android {
-    namespace = "io.ktor.client.webrtc.rs"
-    compileSdk = 36
-
-    defaultConfig {
-        consumerProguardFiles("proguard-rules.pro")
-        ndk.abiFilters.add("arm64-v8a")
-    }
-
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
-
-    publishing {
-        singleVariant("release") {
-            withSourcesJar()
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
 }
