91d68921fd0b9d9c7c748a9fe189d5c89958dd24
Bruce Hamilton <bruce.hamilton@jetbrains.com>
2025-09-18 16:22:08 +0200

    KTOR-8740 Fill in missing attributes for HTMX dsl

--- ktor-shared/ktor-htmx/ktor-htmx-html/common/src/io/ktor/htmx/html/HxAttributes.kt
+++ ktor-shared/ktor-htmx/ktor-htmx-html/common/src/io/ktor/htmx/html/HxAttributes.kt
@@ -33,6 +33,29 @@ public class HxAttributes(override val map: DelegatingMap) : StringMapDelegate {
     public var target: String? by HxAttributeKeys.Target
     public var trigger: String? by HxAttributeKeys.Trigger
     public var vals: String? by HxAttributeKeys.Vals
+    public var boost: String? by HxAttributeKeys.Boost
+    public var confirm: String? by HxAttributeKeys.Confirm
+    public var delete: String? by HxAttributeKeys.Delete
+    public var disable: String? by HxAttributeKeys.Disable
+    public var disabledElt: String? by HxAttributeKeys.DisabledElt
+    public var disinherit: String? by HxAttributeKeys.Disinherit
+    public var encoding: String? by HxAttributeKeys.Encoding
+    public var ext: String? by HxAttributeKeys.Ext
+    public var history: String? by HxAttributeKeys.History
+    public var historyElt: String? by HxAttributeKeys.HistoryElt
+    public var include: String? by HxAttributeKeys.Include
+    public var indicator: String? by HxAttributeKeys.Indicator
+    public var inherit: String? by HxAttributeKeys.Inherit
+    public var params: String? by HxAttributeKeys.Params
+    public var patch: String? by HxAttributeKeys.Patch
+    public var preserve: String? by HxAttributeKeys.Preserve
+    public var prompt: String? by HxAttributeKeys.Prompt
+    public var put: String? by HxAttributeKeys.Put
+    public var replaceUrl: String? by HxAttributeKeys.ReplaceUrl
+    public var request: String? by HxAttributeKeys.Request
+    public var sync: String? by HxAttributeKeys.Sync
+    public var validate: String? by HxAttributeKeys.Validate
+    public var vars: String? by HxAttributeKeys.Vars
 
     public val on: On
         get() = On(map)
