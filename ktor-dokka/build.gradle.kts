928ad76384c4e37d8cdad56c0c239b058a019d33
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2025-10-17 14:47:14 +0200

    KTOR-8893 Remove patch version from API docs versioning (#5129)
    * Strip patch version from API docs
    * Put older versions in the root of API docs

--- ktor-dokka/build.gradle.kts
+++ ktor-dokka/build.gradle.kts
@@ -9,7 +9,10 @@ plugins {
     id("ktorbuild.dokka")
 }
 
-val projectVersion = project.version.toString()
+// Strip patch version (e.g., 3.0.3 -> 3.0.x, 3.3.0-rc.1 -> 3.3.x)
+val apiVersion = project.version.toString()
+    .replace(Regex("""^(\d+\.\d+)\..*$"""), "$1.x")
+
 val dokkaVersionsDirectory = resolveVersionsDirectory()
 
 dokka {
@@ -17,13 +20,14 @@ dokka {
 
     pluginsConfiguration {
         versioning {
-            version = projectVersion
+            version = apiVersion
             if (dokkaVersionsDirectory != null) olderVersionsDir = dokkaVersionsDirectory
+            olderVersionsDirName = "" // Put versions in the docs root directory
         }
     }
 
     dokkaPublications.html {
-        if (dokkaVersionsDirectory != null) outputDirectory = dokkaVersionsDirectory.resolve(projectVersion)
+        if (dokkaVersionsDirectory != null) outputDirectory = dokkaVersionsDirectory.resolve(apiVersion)
     }
 }
 
