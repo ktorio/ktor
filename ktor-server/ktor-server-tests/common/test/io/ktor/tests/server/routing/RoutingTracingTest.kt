c2bcbd67e93be4871a1cd36c88de6e207e501a3c
Mariia Skripchenko <61115099+marychatte@users.noreply.github.com>
2025-10-23 23:03:20 +0200

    KTOR-7639 Separate path and other selectors in traces

--- ktor-server/ktor-server-tests/common/test/io/ktor/tests/server/routing/RoutingTracingTest.kt
+++ ktor-server/ktor-server-tests/common/test/io/ktor/tests/server/routing/RoutingTracingTest.kt
@@ -23,16 +23,16 @@ class RoutingTracingTest {
     Trace for [bar]
     /, segment:0 -> SUCCESS @ /
       /bar, segment:1 -> SUCCESS @ /bar
-        /bar/(method:GET), segment:1 -> SUCCESS @ /bar/(method:GET)
+        /bar [(method:GET)], segment:1 -> SUCCESS @ /bar [(method:GET)]
       /baz, segment:0 -> FAILURE "Selector didn't match" @ /baz
       /{param}, segment:0 -> FAILURE "Better match was already found" @ /{param}
       /*, segment:0 -> FAILURE "Better match was already found" @ /*
-      /(header:a = x), segment:0 -> FAILURE "Selector didn't match" @ /(header:a = x)
-      /(header:b = x), segment:0 -> FAILURE "Selector didn't match" @ /(header:b = x)
+      / [(header:a = x)], segment:0 -> FAILURE "Selector didn't match" @ / [(header:a = x)]
+      / [(header:b = x)], segment:0 -> FAILURE "Selector didn't match" @ / [(header:b = x)]
     Matched routes:
       "" -> "bar" -> "(method:GET)"
     Routing resolve result:
-      SUCCESS @ /bar/(method:GET)
+      SUCCESS @ /bar [(method:GET)]
             """.trimIndent(),
             trace()
         )
@@ -47,20 +47,20 @@ class RoutingTracingTest {
     Trace for [bar, x]
     /, segment:0 -> SUCCESS @ /
       /bar, segment:1 -> SUCCESS @ /bar
-        /bar/(method:GET), segment:1 -> FAILURE "Not all segments matched" @ /bar/(method:GET)
+        /bar [(method:GET)], segment:1 -> FAILURE "Not all segments matched" @ /bar [(method:GET)]
       /baz, segment:0 -> FAILURE "Selector didn't match" @ /baz
       /{param}, segment:1 -> SUCCESS; Parameters [param=[bar]] @ /{param}
-        /{param}/(method:GET), segment:1 -> FAILURE "Not all segments matched" @ /{param}/(method:GET)
+        /{param} [(method:GET)], segment:1 -> FAILURE "Not all segments matched" @ /{param} [(method:GET)]
         /{param}/x, segment:2 -> SUCCESS @ /{param}/x
-          /{param}/x/(method:GET), segment:2 -> SUCCESS @ /{param}/x/(method:GET)
+          /{param}/x [(method:GET)], segment:2 -> SUCCESS @ /{param}/x [(method:GET)]
           /{param}/x/z, segment:2 -> FAILURE "Selector didn't match" @ /{param}/x/z
       /*, segment:0 -> FAILURE "Better match was already found" @ /*
-      /(header:a = x), segment:0 -> FAILURE "Selector didn't match" @ /(header:a = x)
-      /(header:b = x), segment:0 -> FAILURE "Selector didn't match" @ /(header:b = x)
+      / [(header:a = x)], segment:0 -> FAILURE "Selector didn't match" @ / [(header:a = x)]
+      / [(header:b = x)], segment:0 -> FAILURE "Selector didn't match" @ / [(header:b = x)]
     Matched routes:
       "" -> "{param}" -> "x" -> "(method:GET)"
     Routing resolve result:
-      SUCCESS; Parameters [param=[bar]] @ /{param}/x/(method:GET)
+      SUCCESS; Parameters [param=[bar]] @ /{param}/x [(method:GET)]
             """.trimIndent(),
             trace()
         )
@@ -76,19 +76,19 @@ class RoutingTracingTest {
     /, segment:0 -> SUCCESS @ /
       /bar, segment:0 -> FAILURE "Selector didn't match" @ /bar
       /baz, segment:1 -> SUCCESS @ /baz
-        /baz/(method:GET), segment:1 -> FAILURE "Not all segments matched" @ /baz/(method:GET)
+        /baz [(method:GET)], segment:1 -> FAILURE "Not all segments matched" @ /baz [(method:GET)]
         /baz/x, segment:2 -> SUCCESS @ /baz/x
-          /baz/x/(method:GET), segment:2 -> SUCCESS @ /baz/x/(method:GET)
+          /baz/x [(method:GET)], segment:2 -> SUCCESS @ /baz/x [(method:GET)]
           /baz/x/{optional?}, segment:2 -> FAILURE "Better match was already found" @ /baz/x/{optional?}
         /baz/{y}, segment:1 -> FAILURE "Better match was already found" @ /baz/{y}
       /{param}, segment:0 -> FAILURE "Better match was already found" @ /{param}
       /*, segment:0 -> FAILURE "Better match was already found" @ /*
-      /(header:a = x), segment:0 -> FAILURE "Selector didn't match" @ /(header:a = x)
-      /(header:b = x), segment:0 -> FAILURE "Selector didn't match" @ /(header:b = x)
+      / [(header:a = x)], segment:0 -> FAILURE "Selector didn't match" @ / [(header:a = x)]
+      / [(header:b = x)], segment:0 -> FAILURE "Selector didn't match" @ / [(header:b = x)]
     Matched routes:
       "" -> "baz" -> "x" -> "(method:GET)"
     Routing resolve result:
-      SUCCESS @ /baz/x/(method:GET)
+      SUCCESS @ /baz/x [(method:GET)]
             """.trimIndent(),
             trace()
         )
@@ -104,19 +104,19 @@ class RoutingTracingTest {
     /, segment:0 -> SUCCESS @ /
       /bar, segment:0 -> FAILURE "Selector didn't match" @ /bar
       /baz, segment:1 -> SUCCESS @ /baz
-        /baz/(method:GET), segment:1 -> FAILURE "Not all segments matched" @ /baz/(method:GET)
+        /baz [(method:GET)], segment:1 -> FAILURE "Not all segments matched" @ /baz [(method:GET)]
         /baz/x, segment:1 -> FAILURE "Selector didn't match" @ /baz/x
         /baz/{y}, segment:2 -> SUCCESS; Parameters [y=[doo]] @ /baz/{y}
-          /baz/{y}/(method:GET), segment:2 -> SUCCESS @ /baz/{y}/(method:GET)
+          /baz/{y} [(method:GET)], segment:2 -> SUCCESS @ /baz/{y} [(method:GET)]
           /baz/{y}/value, segment:2 -> FAILURE "Selector didn't match" @ /baz/{y}/value
       /{param}, segment:0 -> FAILURE "Better match was already found" @ /{param}
       /*, segment:0 -> FAILURE "Better match was already found" @ /*
-      /(header:a = x), segment:0 -> FAILURE "Selector didn't match" @ /(header:a = x)
-      /(header:b = x), segment:0 -> FAILURE "Selector didn't match" @ /(header:b = x)
+      / [(header:a = x)], segment:0 -> FAILURE "Selector didn't match" @ / [(header:a = x)]
+      / [(header:b = x)], segment:0 -> FAILURE "Selector didn't match" @ / [(header:b = x)]
     Matched routes:
       "" -> "baz" -> "{y}" -> "(method:GET)"
     Routing resolve result:
-      SUCCESS; Parameters [y=[doo]] @ /baz/{y}/(method:GET)
+      SUCCESS; Parameters [y=[doo]] @ /baz/{y} [(method:GET)]
             """.trimIndent(),
             trace()
         )
@@ -132,20 +132,20 @@ class RoutingTracingTest {
     /, segment:0 -> SUCCESS @ /
       /bar, segment:0 -> FAILURE "Selector didn't match" @ /bar
       /baz, segment:1 -> SUCCESS @ /baz
-        /baz/(method:GET), segment:1 -> FAILURE "Not all segments matched" @ /baz/(method:GET)
+        /baz [(method:GET)], segment:1 -> FAILURE "Not all segments matched" @ /baz [(method:GET)]
         /baz/x, segment:2 -> SUCCESS @ /baz/x
-          /baz/x/(method:GET), segment:2 -> FAILURE "Not all segments matched" @ /baz/x/(method:GET)
+          /baz/x [(method:GET)], segment:2 -> FAILURE "Not all segments matched" @ /baz/x [(method:GET)]
           /baz/x/{optional?}, segment:3 -> SUCCESS; Parameters [optional=[z]] @ /baz/x/{optional?}
-            /baz/x/{optional?}/(method:GET), segment:3 -> SUCCESS @ /baz/x/{optional?}/(method:GET)
+            /baz/x/{optional?} [(method:GET)], segment:3 -> SUCCESS @ /baz/x/{optional?} [(method:GET)]
         /baz/{y}, segment:1 -> FAILURE "Better match was already found" @ /baz/{y}
       /{param}, segment:0 -> FAILURE "Better match was already found" @ /{param}
       /*, segment:0 -> FAILURE "Better match was already found" @ /*
-      /(header:a = x), segment:0 -> FAILURE "Selector didn't match" @ /(header:a = x)
-      /(header:b = x), segment:0 -> FAILURE "Selector didn't match" @ /(header:b = x)
+      / [(header:a = x)], segment:0 -> FAILURE "Selector didn't match" @ / [(header:a = x)]
+      / [(header:b = x)], segment:0 -> FAILURE "Selector didn't match" @ / [(header:b = x)]
     Matched routes:
       "" -> "baz" -> "x" -> "{optional?}" -> "(method:GET)"
     Routing resolve result:
-      SUCCESS; Parameters [optional=[z]] @ /baz/x/{optional?}/(method:GET)
+      SUCCESS; Parameters [optional=[z]] @ /baz/x/{optional?} [(method:GET)]
             """.trimIndent(),
             trace()
         )
@@ -161,20 +161,20 @@ class RoutingTracingTest {
     /, segment:0 -> SUCCESS @ /
       /bar, segment:0 -> FAILURE "Selector didn't match" @ /bar
       /baz, segment:1 -> SUCCESS @ /baz
-        /baz/(method:GET), segment:1 -> FAILURE "Not all segments matched" @ /baz/(method:GET)
+        /baz [(method:GET)], segment:1 -> FAILURE "Not all segments matched" @ /baz [(method:GET)]
         /baz/x, segment:2 -> SUCCESS @ /baz/x
-          /baz/x/(method:GET), segment:2 -> FAILURE "Not all segments matched" @ /baz/x/(method:GET)
+          /baz/x [(method:GET)], segment:2 -> FAILURE "Not all segments matched" @ /baz/x [(method:GET)]
           /baz/x/{optional?}, segment:3 -> SUCCESS; Parameters [optional=[value]] @ /baz/x/{optional?}
-            /baz/x/{optional?}/(method:GET), segment:3 -> SUCCESS @ /baz/x/{optional?}/(method:GET)
+            /baz/x/{optional?} [(method:GET)], segment:3 -> SUCCESS @ /baz/x/{optional?} [(method:GET)]
         /baz/{y}, segment:1 -> FAILURE "Better match was already found" @ /baz/{y}
       /{param}, segment:0 -> FAILURE "Better match was already found" @ /{param}
       /*, segment:0 -> FAILURE "Better match was already found" @ /*
-      /(header:a = x), segment:0 -> FAILURE "Selector didn't match" @ /(header:a = x)
-      /(header:b = x), segment:0 -> FAILURE "Selector didn't match" @ /(header:b = x)
+      / [(header:a = x)], segment:0 -> FAILURE "Selector didn't match" @ / [(header:a = x)]
+      / [(header:b = x)], segment:0 -> FAILURE "Selector didn't match" @ / [(header:b = x)]
     Matched routes:
       "" -> "baz" -> "x" -> "{optional?}" -> "(method:GET)"
     Routing resolve result:
-      SUCCESS; Parameters [optional=[value]] @ /baz/x/{optional?}/(method:GET)
+      SUCCESS; Parameters [optional=[value]] @ /baz/x/{optional?} [(method:GET)]
             """.trimIndent(),
             trace()
         )
@@ -191,15 +191,15 @@ class RoutingTracingTest {
       /bar, segment:0 -> FAILURE "Selector didn't match" @ /bar
       /baz, segment:0 -> FAILURE "Selector didn't match" @ /baz
       /{param}, segment:1 -> SUCCESS; Parameters [param=[p]] @ /{param}
-        /{param}/(method:GET), segment:1 -> SUCCESS @ /{param}/(method:GET)
+        /{param} [(method:GET)], segment:1 -> SUCCESS @ /{param} [(method:GET)]
         /{param}/x, segment:1 -> FAILURE "Selector didn't match" @ /{param}/x
       /*, segment:0 -> FAILURE "Better match was already found" @ /*
-      /(header:a = x), segment:0 -> FAILURE "Selector didn't match" @ /(header:a = x)
-      /(header:b = x), segment:0 -> FAILURE "Selector didn't match" @ /(header:b = x)
+      / [(header:a = x)], segment:0 -> FAILURE "Selector didn't match" @ / [(header:a = x)]
+      / [(header:b = x)], segment:0 -> FAILURE "Selector didn't match" @ / [(header:b = x)]
     Matched routes:
       "" -> "{param}" -> "(method:GET)"
     Routing resolve result:
-      SUCCESS; Parameters [param=[p]] @ /{param}/(method:GET)
+      SUCCESS; Parameters [param=[p]] @ /{param} [(method:GET)]
             """.trimIndent(),
             trace()
         )
@@ -216,17 +216,17 @@ class RoutingTracingTest {
       /bar, segment:0 -> FAILURE "Selector didn't match" @ /bar
       /baz, segment:0 -> FAILURE "Selector didn't match" @ /baz
       /{param}, segment:1 -> SUCCESS; Parameters [param=[p]] @ /{param}
-        /{param}/(method:GET), segment:1 -> FAILURE "Not all segments matched" @ /{param}/(method:GET)
+        /{param} [(method:GET)], segment:1 -> FAILURE "Not all segments matched" @ /{param} [(method:GET)]
         /{param}/x, segment:2 -> SUCCESS @ /{param}/x
-          /{param}/x/(method:GET), segment:2 -> SUCCESS @ /{param}/x/(method:GET)
+          /{param}/x [(method:GET)], segment:2 -> SUCCESS @ /{param}/x [(method:GET)]
           /{param}/x/z, segment:2 -> FAILURE "Selector didn't match" @ /{param}/x/z
       /*, segment:0 -> FAILURE "Better match was already found" @ /*
-      /(header:a = x), segment:0 -> FAILURE "Selector didn't match" @ /(header:a = x)
-      /(header:b = x), segment:0 -> FAILURE "Selector didn't match" @ /(header:b = x)
+      / [(header:a = x)], segment:0 -> FAILURE "Selector didn't match" @ / [(header:a = x)]
+      / [(header:b = x)], segment:0 -> FAILURE "Selector didn't match" @ / [(header:b = x)]
     Matched routes:
       "" -> "{param}" -> "x" -> "(method:GET)"
     Routing resolve result:
-      SUCCESS; Parameters [param=[p]] @ /{param}/x/(method:GET)
+      SUCCESS; Parameters [param=[p]] @ /{param}/x [(method:GET)]
             """.trimIndent(),
             trace()
         )
@@ -250,15 +250,15 @@ class RoutingTracingTest {
       /baz, segment:0 -> FAILURE "Selector didn't match" @ /baz
       /{param}, segment:0 -> FAILURE "Selector didn't match" @ /{param}
       /*, segment:0 -> FAILURE "Selector didn't match" @ /*
-      /(header:a = x), segment:0 -> SUCCESS @ /(header:a = x)
-        /(header:a = x)/(method:GET), segment:0 -> SUCCESS @ /(header:a = x)/(method:GET)
-      /(header:b = x), segment:0 -> SUCCESS @ /(header:b = x)
-        /(header:b = x)/(method:GET), segment:0 -> SUCCESS @ /(header:b = x)/(method:GET)
+      / [(header:a = x)], segment:0 -> SUCCESS @ / [(header:a = x)]
+        / [(header:a = x), (method:GET)], segment:0 -> SUCCESS @ / [(header:a = x), (method:GET)]
+      / [(header:b = x)], segment:0 -> SUCCESS @ / [(header:b = x)]
+        / [(header:b = x), (method:GET)], segment:0 -> SUCCESS @ / [(header:b = x), (method:GET)]
     Matched routes:
       "" -> "(header:a = x)" -> "(method:GET)"
       "" -> "(header:b = x)" -> "(method:GET)"
     Routing resolve result:
-      SUCCESS @ /(header:a = x)/(method:GET)
+      SUCCESS @ / [(header:a = x), (method:GET)]
             """.trimIndent(),
             trace()
         )
