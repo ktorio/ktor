6fecb160e9bb6eb4ec90a79878d3e05d59f22744
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2025-11-20 17:01:04 +0100

    Vcpkg: Use Konan toolchain to build libcurl

--- build-logic/src/main/kotlin/ktorbuild/vcpkg/VcpkgInstall.kt
+++ build-logic/src/main/kotlin/ktorbuild/vcpkg/VcpkgInstall.kt
@@ -12,25 +12,29 @@ import org.gradle.api.file.ProjectLayout
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.Property
 import org.gradle.api.provider.Provider
-import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.InputDirectory
-import org.gradle.api.tasks.OutputDirectory
-import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.*
 import org.gradle.process.ExecOperations
+import org.gradle.process.ExecSpec
 import org.gradle.work.DisableCachingByDefault
 import org.jetbrains.kotlin.konan.target.HostManager
+import org.jetbrains.kotlin.konan.util.DependencyDirectories
 import java.io.ByteArrayOutputStream
+import java.io.File
 import javax.inject.Inject
 
 @DisableCachingByDefault(because = "We rely on vcpkg's caches")
 abstract class VcpkgInstall @Inject constructor(
-    private val layout: ProjectLayout,
     private val execOps: ExecOperations,
+    layout: ProjectLayout,
     objects: ObjectFactory,
 ) : DefaultTask() {
 
     @get:InputDirectory
     internal abstract val manifestDir: DirectoryProperty
+
+    @get:Input
+    @get:Optional
+    protected abstract val toolchainDirectory: Property<File>
 
     @get:Input
     internal abstract val triplet: Property<String>
@@ -59,6 +63,11 @@ abstract class VcpkgInstall @Inject constructor(
         this.manifestDir.set(project.file(manifestPath))
         this.triplet.set(triplet)
         outputDirProperty.set(installDir.map { it.dir(triplet) })
+
+        val toolchainDirectoryName = getToolchainDirName(target)
+        if (toolchainDirectoryName != null) {
+            toolchainDirectory.set(DependencyDirectories.defaultDependenciesRoot.resolve(toolchainDirectoryName))
+        }
     }
 
     private fun targetToTriplet(target: String): String = when (target) {
@@ -81,6 +90,7 @@ abstract class VcpkgInstall @Inject constructor(
         val installDir = installDir.get().asFile
         val manifestDir = manifestDir.get().asFile
         val overlayPortsDir = manifestDir.resolve("overlays")
+        val overlayTripletsDir = manifestDir.resolve("triplets")
         val currentTriplet = triplet.get()
 
         val commandLine = listOfNotNull(
@@ -89,11 +99,34 @@ abstract class VcpkgInstall @Inject constructor(
             "--triplet=$currentTriplet",
             "--x-install-root=$installDir",
             "--x-manifest-root=$manifestDir",
-            if (useOverlays.get()) "--overlay-ports=$overlayPortsDir" else null
+            if (overlayPortsDir.isDirectory && useOverlays.get()) "--overlay-ports=$overlayPortsDir" else null,
+            if (overlayTripletsDir.isDirectory) "--overlay-triplets=$overlayTripletsDir" else null
         )
 
-        execOps.exec { commandLine(commandLine) }
+        execOps.exec {
+            commandLine(commandLine)
+            if (toolchainDirectory.isPresent) setToolchainDir(toolchainDirectory.get())
+        }
     }
+
+    private fun ExecSpec.setToolchainDir(toolchainDirectory: File) {
+        check(toolchainDirectory.exists()) {
+            """
+            Toolchain directory does not exist: $toolchainDirectory
+            Run `./gradlew downloadKotlinNativeDistribution` to download toolchain.
+            """.trimIndent()
+        }
+        environment("TOOLCHAIN_DIR", toolchainDirectory.absolutePath)
+        environment("TOOLCHAIN_TARGET", toolchainDirectory.name.substringBefore("-gcc"))
+    }
+}
+
+// See: https://github.com/JetBrains/kotlin/blob/v2.2.21/kotlin-native/konan/konan.properties#L68-L71
+private fun getToolchainDirName(target: String): String? = when (target) {
+    "linuxX64" -> "x86_64-unknown-linux-gnu-gcc-8.3.0-glibc-2.19-kernel-4.9-2"
+    "linuxArm64" -> "aarch64-unknown-linux-gnu-gcc-8.3.0-glibc-2.25-kernel-4.9-2"
+    "mingwX64" -> "llvm-19-x86_64-windows-essentials-134"
+    else -> null
 }
 
 private fun ExecOperations.which(command: String): String? {
