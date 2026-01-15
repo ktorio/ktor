9c532c1ed2a36d4534e1db5be55638138f5def41
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2026-01-15 12:16:37 +0100

    KTOR-9243 Update libcurl to 8.18.0 (#5299)
    * Remove curl port overlay
    * Update libcurl to 8.18.0

--- ktor-client/ktor-client-curl/desktop/interop/include/curl/curlver.h
+++ ktor-client/ktor-client-curl/desktop/interop/include/curl/curlver.h
@@ -32,12 +32,12 @@
 
 /* This is the version number of the libcurl package from which this header
    file origins: */
-#define LIBCURL_VERSION "8.17.0-DEV"
+#define LIBCURL_VERSION "8.18.0"
 
 /* The numeric version number is also available "in parts" by using these
    defines: */
 #define LIBCURL_VERSION_MAJOR 8
-#define LIBCURL_VERSION_MINOR 17
+#define LIBCURL_VERSION_MINOR 18
 #define LIBCURL_VERSION_PATCH 0
 /* This is the numeric version of the libcurl version number, meant for easier
    parsing and comparisons by programs. The LIBCURL_VERSION_NUM define will
@@ -58,7 +58,7 @@
    CURL_VERSION_BITS() macro since curl's own configure script greps for it
    and needs it to contain the full number.
 */
-#define LIBCURL_VERSION_NUM 0x081100
+#define LIBCURL_VERSION_NUM 0x081200
 
 /*
  * This is the date and time when the full source package was created. The
@@ -69,10 +69,10 @@
  *
  * "2007-11-23"
  */
-#define LIBCURL_TIMESTAMP "[unreleased]"
+#define LIBCURL_TIMESTAMP "[vcpkg]"
 
-#define CURL_VERSION_BITS(x,y,z) ((x)<<16|(y)<<8|(z))
-#define CURL_AT_LEAST_VERSION(x,y,z) \
+#define CURL_VERSION_BITS(x, y, z) ((x) << 16 | (y) << 8 | (z))
+#define CURL_AT_LEAST_VERSION(x, y, z) \
   (LIBCURL_VERSION_NUM >= CURL_VERSION_BITS(x, y, z))
 
 #endif /* CURLINC_CURLVER_H */
