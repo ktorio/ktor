57e34e0cfe9e066773154a9cdf801ce2bafed0c9
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2026-02-03 13:54:19 +0100

    KTOR-9274 Curl: Undefined symbol errors when linking Linux ARM64 since 3.4.0 (#5337)
    * Use toolchain settings from konan.properties for vcpkg
    * Configure CA_PATH and CA_BUNDLE for Linux cross-compiling
    * Update libcurl binaries
    * Change File.loadProperties return type
    * Configure line endings for git
    ---------
    
    Co-authored-by: TeamCity <teamcity@jetbrains.com>

--- build-logic/src/main/kotlin/ktorbuild/vcpkg/VcpkgTasks.kt
+++ build-logic/src/main/kotlin/ktorbuild/vcpkg/VcpkgTasks.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2014-2026 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package ktorbuild.vcpkg
@@ -7,16 +7,24 @@ package ktorbuild.vcpkg
 import org.gradle.api.Project
 import org.gradle.api.tasks.Sync
 import org.gradle.api.tasks.TaskProvider
+import org.gradle.kotlin.dsl.named
 import org.gradle.kotlin.dsl.register
+import org.jetbrains.kotlin.gradle.targets.native.internal.KotlinNativeDownloadTask
+import org.jetbrains.kotlin.konan.target.KonanTarget
 import java.io.File
 
 fun Project.registerVcpkgInstallTask(
     library: String,
-    target: String,
+    target: KonanTarget,
     configure: VcpkgInstall.() -> Unit = {}
 ): TaskProvider<VcpkgInstall> {
+    val downloadKotlinNative = tasks.named<KotlinNativeDownloadTask>("downloadKotlinNativeDistribution")
     return tasks.register<VcpkgInstall>("${library}Install") {
         install(library, target)
+        // Do not configure Toolchain on macOS as Konan uses native tools there
+        if (!target.family.isAppleFamily) {
+            nativeDirectoryLocation.set(downloadKotlinNative.flatMap { it.nativeDirectoryLocation })
+        }
         configure()
     }
 }
