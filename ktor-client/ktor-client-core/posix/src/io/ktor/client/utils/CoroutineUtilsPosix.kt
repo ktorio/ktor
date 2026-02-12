61339fe780908632ed2edfffdd70d73abfd02aca
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2026-02-12 17:46:54 +0100

    Remove our MultiWorkerDispatcher implementation

--- ktor-client/ktor-client-core/posix/src/io/ktor/client/utils/CoroutineUtilsPosix.kt
+++ ktor-client/ktor-client-core/posix/src/io/ktor/client/utils/CoroutineUtilsPosix.kt
@@ -1,20 +1,23 @@
 /*
- * Copyright 2014-2019 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2014-2026 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package io.ktor.client.utils
 
-import io.ktor.util.*
 import io.ktor.utils.io.*
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.newFixedThreadPoolContext
 
 /**
  * Creates [CoroutineDispatcher] for client with fixed [threadCount] and specified [dispatcherName].
  *
  * [Report a problem](https://ktor.io/feedback/?fqname=io.ktor.client.utils.clientDispatcher)
  */
+@OptIn(DelicateCoroutinesApi::class)
 @InternalAPI
 public actual fun Dispatchers.clientDispatcher(
     threadCount: Int,
     dispatcherName: String
-): CoroutineDispatcher = Dispatchers.createFixedThreadDispatcher(dispatcherName, threadCount)
+): CoroutineDispatcher = newFixedThreadPoolContext(threadCount, dispatcherName)
