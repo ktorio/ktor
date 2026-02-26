05837723cac9bc0cc1bd695a9f892035006e990b
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2026-02-26 20:44:26 +0100

    KTOR-7659 Make ktor-network compatible with ES modules

--- karma/chrome_bin.js
+++ karma/chrome_bin.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2019 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2014-2026 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
 config.set({
@@ -25,6 +25,15 @@ config.set({
         "mocha": {
             // Disable timeout as we use individual timeouts for tests
             timeout: 0
+        }
+    },
+    "webpack": {
+        // Workaround for Node.js built-in modules in browser tests.
+        // ktor-client-tests depends on ktor-network, which uses @JsModule("node:net") for Node.js socket support.
+        // Webpack cannot resolve the "node:" protocol in browser builds, so we stub it with an empty object.
+        // The Node.js socket code paths are not executed in browser tests anyway.
+        externals: {
+            'node:net': '{}'
         }
     }
 });
