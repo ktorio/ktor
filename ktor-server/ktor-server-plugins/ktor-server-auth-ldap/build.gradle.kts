1d7055185a4bb761f70b553eb63cae30406170cb
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2026-02-27 14:21:11 +0100

    Add constraints for vulnerable dependencies

--- ktor-server/ktor-server-plugins/ktor-server-auth-ldap/build.gradle.kts
+++ ktor-server/ktor-server-plugins/ktor-server-auth-ldap/build.gradle.kts
@@ -1,6 +1,8 @@
 /*
- * Copyright 2014-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2014-2026 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
+
+import ktorbuild.commonsLang3
 
 plugins {
     id("ktorbuild.project.server-plugin")
@@ -16,4 +18,8 @@ kotlin {
             implementation(libs.apacheds.core)
         }
     }
+}
+
+dependencies.constraints {
+    commonsLang3()
 }
