2d8cd8fae1df69eb27a071c483dee5f019b282a3
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2026-03-03 22:55:21 +0100

    Use cache redirector on CI

--- build-settings-logic/src/main/kotlin/ktorsettings.dependency-resolution-management.settings.gradle.kts
+++ build-settings-logic/src/main/kotlin/ktorsettings.dependency-resolution-management.settings.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2014-2026 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
 @file:Suppress("UnstableApiUsage")
@@ -44,7 +44,7 @@ private fun RepositoryHandler.configureRepositories() {
 
     exclusiveContent {
         forRepository {
-            maven("https://packages.jetbrains.team/maven/p/ktor/eap") { name = "KtorEAP" }
+            maven("https://redirector.kotlinlang.org/maven/ktor-eap") { name = "KtorEAP" }
         }
         filter { includeVersionByRegex("io.ktor", ".+", ".+-eap-\\d+") }
     }
