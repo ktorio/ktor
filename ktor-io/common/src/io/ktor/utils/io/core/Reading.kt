106944af78e8f4646b963572005ebcc4584723bc
Valentin Rocher <vrocher@deezer.com>
2026-04-18 15:26:20 +0200

    Apply last changes from JVM reading file

--- ktor-io/common/src/io/ktor/utils/io/core/Reading.kt
+++ ktor-io/common/src/io/ktor/utils/io/core/Reading.kt
@@ -44,7 +44,10 @@ internal class RawSourceChannel(
         get() = buffer
 
     override suspend fun awaitContent(min: Int): Boolean {
-        if (closedToken != null) return true
+        if (closedToken != null) {
+            closedCause?.let { throw it }
+            return buffer.remaining >= min
+        }
 
         withContext(coroutineContext) {
             var result = 0L
