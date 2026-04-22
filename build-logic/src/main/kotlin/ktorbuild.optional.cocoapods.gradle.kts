6b94394e9059ff970a5a05042e57150d69123f36
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2026-04-22 17:46:36 +0200

    Gradle: Light sync mode (#5530)
    Other changes:
    * Allow publishing any target locally
    * Restrict MaxMetaspaceSize as we don't use BCV anymore
    * Fix VcpkgInstall task when native targets are disabled
    * Configure root project in build-logic

--- build-logic/src/main/kotlin/ktorbuild.optional.cocoapods.gradle.kts
+++ build-logic/src/main/kotlin/ktorbuild.optional.cocoapods.gradle.kts
@@ -9,27 +9,35 @@ import org.gradle.kotlin.dsl.support.serviceOf
 import org.jetbrains.kotlin.konan.target.HostManager
 import java.io.OutputStream
 
-pluginManager.withPlugin("org.jetbrains.kotlin.multiplatform") {
-    val cocoaPodsAvailable = localProperty(COCOAPODS_BIN_PROPERTY).map { true }
-        .orElse(providers.gradleProperty(COCOAPODS_BIN_PROPERTY).map { true })
-        .orElse(providers.of(CocoaPodsAvailableValueSource::class) {})
+plugins {
+    id("ktorbuild.base")
+}
 
-    if (!HostManager.hostIsMac || cocoaPodsAvailable.get()) {
-        apply(plugin = COCOAPODS_PLUGIN_ID)
-    } else @Suppress("UnstableApiUsage") {
-        // Disable Darwin targets if CocoaPods is not available
-        ktorBuild.targets["darwin"] = false
+// Darwin targets might be filtered out if Light Sync mode is active
+if (ktorBuild.targets.hasDarwin) {
+    pluginManager.withPlugin("org.jetbrains.kotlin.multiplatform") {
+        val cocoaPodsAvailable = localProperty(COCOAPODS_BIN_PROPERTY).map { true }
+            .orElse(providers.gradleProperty(COCOAPODS_BIN_PROPERTY).map { true })
+            .orElse(providers.of(CocoaPodsAvailableValueSource::class) {})
 
-        val problemReporter = project.serviceOf<Problems>().reporter
-        problemReporter.reportVisible(
-            KtorBuildProblems.missingCocoaPods,
-            details = "CocoaPods not found.",
-            contextualLabel = "Apple targets won't be added in the project.",
-        ) {
-            solution("Install CocoaPods")
-            solution("Ensure 'pod' command is available in PATH")
-            buildFileLocation()
-            documentedAt("https://github.com/ktorio/ktor/blob/main/CONTRIBUTING.md#building-the-project")
+        if (!HostManager.hostIsMac || cocoaPodsAvailable.get()) {
+            pluginManager.apply(COCOAPODS_PLUGIN_ID)
+        } else @Suppress("UnstableApiUsage") {
+            // Disable Darwin targets if CocoaPods is not available
+            val darwinTargets = KtorTargets.resolveTargets("darwin")
+            ktorBuild.targets.filterTargets { it !in darwinTargets }
+
+            val problemReporter = project.serviceOf<Problems>().reporter
+            problemReporter.reportVisible(
+                KtorBuildProblems.missingCocoaPods,
+                details = "CocoaPods not found.",
+                contextualLabel = "Apple targets won't be added in the project.",
+            ) {
+                solution("Install CocoaPods")
+                solution("Ensure 'pod' command is available in PATH")
+                buildFileLocation()
+                documentedAt("https://github.com/ktorio/ktor/blob/main/CONTRIBUTING.md#building-the-project")
+            }
         }
     }
 }
