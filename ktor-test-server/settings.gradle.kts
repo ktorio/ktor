e4c54a8d2fb62b734d1237984b89aa48f076594c
Bruce Hamilton <bruce.hamilton@jetbrains.com>
2026-05-06 11:50:39 +0200

    Downgrade test server for now

--- ktor-test-server/settings.gradle.kts
+++ ktor-test-server/settings.gradle.kts
@@ -22,7 +22,7 @@ plugins {
 dependencyResolutionManagement {
     versionCatalogs {
         create("ktorLibs") {
-            from("io.ktor:ktor-version-catalog:3.5.0-eap-1620")
+            from("io.ktor:ktor-version-catalog:3.3.3")
         }
     }
 }
