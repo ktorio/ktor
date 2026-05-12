b115e630ff0cabaae0280ac49e3474b2556f6282
Bruce Hamilton <bruce.hamilton@jetbrains.com>
2026-05-12 22:14:31 +0200

    Use cache redirector in build-settings-logic

--- build-settings-logic/settings.gradle.kts
+++ build-settings-logic/settings.gradle.kts
@@ -2,6 +2,12 @@
  * Copyright 2014-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
+pluginManagement {
+    repositories {
+        maven("https://cache-redirector.jetbrains.com/plugins.gradle.org/m2")
+    }
+}
+
 plugins {
     // Keep it in sync with libs.versions.toml
     id("com.gradle.develocity") version "4.3.2"
