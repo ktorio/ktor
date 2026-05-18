67b22f1886fdbf5875a2f703ab682c4ec01a23be
zibet27 <oleh.pantus@jetbrains.com>
2026-05-18 17:19:13 +0200

    Add OpenID Connect discovery metadata

---
+++ ktor-server/ktor-server-plugins/ktor-server-auth-openid/build.gradle.kts
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2014-2026 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+plugins {
+    id("ktorbuild.project.server-plugin")
+    id("kotlinx-serialization")
+}
+
+kotlin {
+    sourceSets {
+        jvmMain.dependencies {
+            api(projects.ktorClientCore)
+            api(libs.kotlinx.serialization.json)
+        }
+        jvmTest.dependencies {
+            implementation(projects.ktorServerTestHost)
+        }
+    }
+}
