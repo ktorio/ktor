d2923e4f4e66abae9d11b9dcedf3226fab62a54d
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2026-05-26 14:00:48 +0200

    Fix SustainabilityTestSuite.testBigFile performance (#5656)

--- ktor-server/ktor-server-test-suites/jvm/src/io/ktor/server/testing/suites/SustainabilityTestSuite.kt
+++ ktor-server/ktor-server-test-suites/jvm/src/io/ktor/server/testing/suites/SustainabilityTestSuite.kt
@@ -34,16 +34,11 @@ import org.slf4j.event.Level
 import org.slf4j.helpers.AbstractLogger
 import java.io.File
 import java.io.IOException
-import java.io.InputStream
 import java.net.HttpURLConnection
 import java.net.Proxy
 import java.net.URL
 import java.util.*
-import java.util.concurrent.CopyOnWriteArrayList
-import java.util.concurrent.CountDownLatch
-import java.util.concurrent.Executors
-import java.util.concurrent.LinkedBlockingQueue
-import java.util.concurrent.TimeUnit
+import java.util.concurrent.*
 import java.util.concurrent.atomic.AtomicInteger
 import kotlin.test.*
 import kotlin.time.Duration.Companion.minutes
@@ -384,7 +379,7 @@ abstract class SustainabilityTestSuite<TEngine : ApplicationEngine, TConfigurati
 
     @OptIn(InternalAPI::class)
     @Test
-    fun testBigFile() = runTest(timeout = 3.minutes) {
+    fun testBigFile() = runTest(timeout = 1.minutes) {
         val file = File("build/large-file.dat")
         val rnd = Random()
 
@@ -408,7 +403,7 @@ abstract class SustainabilityTestSuite<TEngine : ApplicationEngine, TConfigurati
         }
 
         withUrl("/file") {
-            val (actualChecksum, actualCount) = body<InputStream>().crcWithSize()
+            val (actualChecksum, actualCount) = rawContent.crcWithSize()
             assertEquals(fileCount, actualCount, "Response size differs from file")
             assertEquals(fileChecksum, actualChecksum, "Response checksum differs from file")
         }
@@ -797,7 +792,7 @@ abstract class SustainabilityTestSuite<TEngine : ApplicationEngine, TConfigurati
         withUrl("/blocking/large") {
             assertEquals(HttpStatusCode.OK, status)
             assertEquals(ContentType.Text.Plain, contentType()?.withoutParameters())
-            val result = rawContent.toInputStream().crcWithSize()
+            val result = rawContent.crcWithSize()
             assertEquals(10000 * 13L, result.second)
         }
     }
