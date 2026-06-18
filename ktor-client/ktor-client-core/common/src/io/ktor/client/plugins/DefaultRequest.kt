e4546f318a9c65235c361b1de890ea908f8a0f69
Aleksei.Tirman <aleksei.tirman@jetbrains.com>
2026-06-18 15:08:50 +0300

    KTOR-9632 Fix merge engine capabilities in the DefaultRequest handler

--- ktor-client/ktor-client-core/common/src/io/ktor/client/plugins/DefaultRequest.kt
+++ ktor-client/ktor-client-core/common/src/io/ktor/client/plugins/DefaultRequest.kt
@@ -94,24 +94,23 @@ public class DefaultRequest private constructor(private val block: DefaultReques
                 }
                 val defaultUrl = defaultRequest.url.build()
                 mergeUrls(defaultUrl, context.url)
-                defaultRequest.attributes.allKeys.forEach {
-                    if (it == ENGINE_CAPABILITIES_KEY) {
-                        val capabilities = context.attributes.computeIfAbsent(ENGINE_CAPABILITIES_KEY) {
-                            mutableMapOf()
-                        }
+                defaultRequest.attributes.allKeys.forEach { key ->
+                    when (key) {
+                        ENGINE_CAPABILITIES_KEY -> {
+                            val capabilities = context.attributes.computeIfAbsent(ENGINE_CAPABILITIES_KEY) {
+                                mutableMapOf()
+                            }
 
-                        for ((key, value) in defaultRequest.attributes[ENGINE_CAPABILITIES_KEY]) {
-                            if (!capabilities.contains(key)) {
-                                capabilities[key] = value
+                            for ((key, value) in defaultRequest.attributes[ENGINE_CAPABILITIES_KEY]) {
+                                if (!capabilities.contains(key)) {
+                                    capabilities[key] = value
+                                }
                             }
                         }
-
-                        return@forEach
-                    }
-
-                    if (!context.attributes.contains(it)) {
-                        @Suppress("UNCHECKED_CAST")
-                        context.attributes.put(it as AttributeKey<Any>, defaultRequest.attributes[it])
+                        !in context.attributes -> {
+                            @Suppress("UNCHECKED_CAST")
+                            context.attributes.put(it as AttributeKey<Any>, defaultRequest.attributes[it])
+                        }
                     }
                 }
 
