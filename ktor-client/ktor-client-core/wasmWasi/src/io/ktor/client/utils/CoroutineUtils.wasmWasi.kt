120b985da9b4baea1a0406016066b197aa06dcc3
Oleg Yukhnevich <whyoleg@gmail.com>
2026-06-25 22:00:07 +0300

    wasi: ktor-client-core

---
+++ ktor-client/ktor-client-core/wasmWasi/src/io/ktor/client/utils/CoroutineUtils.wasmWasi.kt
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2014-2026 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package io.ktor.client.utils
+
+import io.ktor.utils.io.*
+import kotlinx.coroutines.*
+
+/**
+ * Creates [CoroutineDispatcher] for client with fixed [threadCount] and specified [dispatcherName].
+ *
+ * [Report a problem](https://ktor.io/feedback/?fqname=io.ktor.client.utils.clientDispatcher)
+ */
+@InternalAPI
+public actual fun Dispatchers.clientDispatcher(
+    threadCount: Int,
+    dispatcherName: String
+): CoroutineDispatcher = Dispatchers.Default
