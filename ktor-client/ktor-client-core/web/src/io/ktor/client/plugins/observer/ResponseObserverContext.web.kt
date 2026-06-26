d0d6a7e8328b2369c481db0c69d31cc0bb92fb42
Oleg Yukhnevich <whyoleg@gmail.com>
2026-06-26 13:48:18 +0300

    Move some declarations to `nonJvm` source-set as a preparation for wasm-wasi integration (#5720)
    * ktor-websockets
    * ktor-encoding-zstd
    * ktor-server-core
    * ktor-server-content-negotiation
    * ktor-server-double-receive
    * ktor-server-status-pages
    * ktor-server-auth
    * ktor-http-cio
    * ktor-client-content-negotiation
    * ktor-client-logging
    * ktor-client-core
    * ktor-server-test-host
    * ktor-server-partial-content

--- ktor-client/ktor-client-core/web/src/io/ktor/client/plugins/observer/ResponseObserverContext.web.kt
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright 2014-2022 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
- */
-
-package io.ktor.client.plugins.observer
-
-import kotlin.coroutines.*
-
-internal actual suspend fun getResponseObserverContext(): CoroutineContext = EmptyCoroutineContext
