060e9f90d01873dba6eac545c30e67b7065be531
Bruce Hamilton <bruce.hamilton@jetbrains.com>
2026-07-10 11:03:51 +0200

    fixup! KTOR-9703 Optimization for String functions

--- ktor-io/common/src/io/ktor/utils/io/core/Strings.kt
+++ ktor-io/common/src/io/ktor/utils/io/core/Strings.kt
@@ -2,11 +2,16 @@
  * Copyright 2014-2024 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
+// For ABI compatibility
+@file:JvmMultifileClass
+@file:JvmName("StringsKt")
+
 package io.ktor.utils.io.core
 
 import io.ktor.utils.io.charsets.*
 import kotlinx.io.*
-import kotlin.math.*
+import kotlin.jvm.JvmMultifileClass
+import kotlin.jvm.JvmName
 
 public fun String.toByteArray(charset: Charset = Charsets.UTF_8): ByteArray {
     if (charset == Charsets.UTF_8) return encodeToByteArray(throwOnInvalidSequence = true)
--- ktor-io/jvm/src/io/ktor/utils/io/core/Strings.jvm.kt
+++ ktor-io/jvm/src/io/ktor/utils/io/core/Strings.jvm.kt
@@ -2,6 +2,9 @@
  * Copyright 2014-2026 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
+@file:JvmMultifileClass
+@file:JvmName("StringsKt")
+
 package io.ktor.utils.io.core
 
 import io.ktor.utils.io.charsets.Charset
@@ -11,4 +14,7 @@ import kotlinx.io.readString
 
 @OptIn(InternalIoApi::class)
 public actual fun Source.readText(charset: Charset, max: Int): String =
-    readString(max.toLong(), charset)
+    when (max) {
+        Int.MAX_VALUE -> readString(charset)
+        else -> readString(minOf(buffer.size, max.toLong()), charset)
+    }
