72dab670be8c0042d2b1390180b73f41cce4ba31
Bruce Hamilton <bruce.hamilton@jetbrains.com>
2026-09-09 12:45:04 +0200

    fixup! Merge branch 'main' into release/3.x

--- ktor-http/common/src/io/ktor/http/HttpHeaderValueParser.kt
+++ ktor-http/common/src/io/ktor/http/HttpHeaderValueParser.kt
@@ -217,7 +217,7 @@ private fun parseHeaderValueParameterValueQuoted(value: String, start: Int): Pai
                 return position + 1 to builder.toString()
             }
 
-            '\\' if && position < value.lastIndex -> {
+            '\\' if position < value.lastIndex - 2 -> {
                 builder.append(value[position + 1])
                 position += 2
                 continue@loop
